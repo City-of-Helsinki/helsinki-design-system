7d781ec608ee57ed3274da0a4b6f43b918a095c2
Anssi Lehtonen <lehtovaaralainen@gmail.com>
2019-10-14 12:06:33 +0300

    Using css vars from hds-core, cleaned test string from the button skeleton

--- packages/react/src/components/button/Button.js
+++ packages/react/src/components/button/Button.js
@@ -3,7 +3,7 @@ import React from "react";
 import styles from "./Button.module.css";
 
 const Button = ({ children }) => {
-  return <button className={styles.button}>{children} yedh</button>;
+  return <button className={styles.button}>{children}</button>;
 };
 
 export default Button;
