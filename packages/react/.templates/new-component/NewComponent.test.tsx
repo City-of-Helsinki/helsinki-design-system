7951701a0f58d688d0e2871f1290641ea0b383dc
Anssi Lehtonen <lehtovaaralainen@gmail.com>
2019-10-21 16:57:27 +0300

    Added script and templates for new component scaffold helper

---
+++ packages/react/.templates/new-component/NewComponent.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import [-replace-name-capital-] from './[-replace-name-capital-]';
+
+describe('<[-replace-name-capital-] /> spec', () => {
+  it('renders the component', () => {
+    render(<[-replace-name-capital-] />);
+  });
+});
