53c946f200d91b9091f2d543e41eda3a6e04a31c
Bernt Andreas Drange <bernt.andreas.drange@digia.com>
2019-10-25 15:58:53 +0300

    Fix lint for button.tsx

--- packages/react/src/components/button/Button.tsx
+++ packages/react/src/components/button/Button.tsx
@@ -5,5 +5,9 @@ import styles from './Button.module.css';
 type Props = React.PropsWithChildren<{}>;
 
 export default ({ children }: Props) => {
-  return <div className={styles.button}>{children}</div>;
+  return (
+    <button type="button" className={styles.button}>
+      {children}
+    </button>
+  );
 };
