322948cdcb3fc2f09466f535fcb1c3fc67376217
Bernt Andreas Drange <bernt.andreas.drange@digia.com>
2019-10-25 15:58:36 +0300

    Remove deprecated button.js

--- packages/react/src/components/button/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import styles from './Button.module.css';
-
-const Button = ({ children }) => {
-  return <button className={styles.button}>{children}</button>;
-};
-
-export default Button;
