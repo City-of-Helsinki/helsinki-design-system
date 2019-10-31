2cc54f746dfedefe47daaa010d66926a6e176ed3
lehtoinen <lehtovaaralainen@gmail.com>
2019-10-31 14:47:21 +0200

    Updated changed props to TextInput tests

--- packages/react/src/components/textinput/TextInput.test.tsx
+++ packages/react/src/components/textinput/TextInput.test.tsx
@@ -3,8 +3,14 @@ import { render } from '@testing-library/react';
 
 import TextInput from './TextInput';
 
+const textInputProps = {
+  id: 'hdsInput',
+  labelText: 'HDS input field',
+  placeholder: 'A placeholder text',
+};
+
 describe('<TextInput /> spec', () => {
   it('renders the component', () => {
-    render(<TextInput id="myInput" label="My Input" />);
+    render(<TextInput {...textInputProps} />);
   });
 });
