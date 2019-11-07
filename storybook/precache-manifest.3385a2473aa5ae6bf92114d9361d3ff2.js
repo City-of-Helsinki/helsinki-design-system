cedd6a1be59d09cc9bbb35abe37f12ef06fd1aba
jussiarpalahti <jussiarpalahti@users.noreply.github.com>
2019-11-07 09:47:57 +0000

    Automated deployment: Thu Nov  7 09:47:57 UTC 2019 7e578ff23f5c29f3947e21854379b712e877af76

---
+++ storybook/precache-manifest.3385a2473aa5ae6bf92114d9361d3ff2.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ec4b6f418093a06b0f524690db43902",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.26e4eda6c3b6b7593237.bundle.js"
+  },
+  {
+    "revision": "da6abced8393a39fcee5",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.26e4eda6c3b6b7593237.bundle.js"
+  },
+  {
+    "url": "vendors~main.26e4eda6c3b6b7593237.bundle.js"
+  }
+]);
