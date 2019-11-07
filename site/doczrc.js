c650a575ee12ee010739ce84c67bf59506ba8001
lehtoinen <lehtovaaralainen@gmail.com>
2019-11-07 10:56:07 +0200

    Restructured docz config file, added font definitions

--- site/doczrc.js
+++ site/doczrc.js
@@ -1,17 +1,26 @@
-module.exports = {
+const themeConfig = {
+  showDarkModeSwitch: false,
+  fonts: {
+    body: '"HelsinkiGrotesk", Helvetica, Arial, MS Gothic, Meiryo, Osaka',
+    heading: '"HelsinkiGrotesk", Helvetica, Arial, MS Gothic, Meiryo, Osaka',
+    monospace: "Menlo, monospace"
+  }
+};
+
+const menu = [
+  "Introduction",
+  { name: "Get Started", menu: ["For designers", "For developers"] },
+  "Guidelines",
+  "Components",
+  "Community"
+];
+
+export default {
   dest: "/public",
   public: "/static",
   typescript: true,
-  themeConfig: {
-    showDarkModeSwitch: false
-  },
   files: "docs/**/*.{md,markdown,mdx}",
   title: "Helsinki Design System",
-  menu: [
-    "Introduction",
-    { name: "Get Started", menu: ["For designers", "For developers"] },
-    "Guidelines",
-    "Components",
-    "Community"
-  ]
+  themeConfig,
+  menu
 };
