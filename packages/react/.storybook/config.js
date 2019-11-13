3faace978a15a3ee8c3aaeea2ef9dce3f0defc19
lehtoinen <lehtovaaralainen@gmail.com>
2019-11-13 15:13:56 +0200

    Extended viewport addon entries for Storybook

--- packages/react/.storybook/config.js
+++ packages/react/.storybook/config.js
@@ -1,13 +1,31 @@
 import { configure, addDecorator, addParameters } from '@storybook/react';
 import { withA11y } from '@storybook/addon-a11y';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
 import './index.css';
+
+const viewports = {
+  ...INITIAL_VIEWPORTS,
+  ...{
+    narrow: {
+      name: 'Narrow full-height (320px)',
+      styles: {
+        width: '320px',
+        height: '100%',
+      },
+    },
+  },
+};
 
 addDecorator(withA11y);
 
 addParameters({
   options: {
     storySort: (a, b) => a[1].id.localeCompare(b[1].id),
+  },
+  viewport: {
+    viewports,
+    defaultViewport: 'narrow',
   },
 });
 
