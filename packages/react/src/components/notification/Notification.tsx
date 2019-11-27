40c64acd5de254b4eff2352454976b395886aa77
lehtoinen <lehtovaaralainen@gmail.com>
2019-11-27 15:34:52 +0200

    Remove close functionality from Notification

--- packages/react/src/components/notification/Notification.tsx
+++ packages/react/src/components/notification/Notification.tsx
@@ -1,29 +1,19 @@
 import React from 'react';
 
 import styles from './Notification.module.css';
-import IconClose from '../../icons/IconClose';
 import IconInfo from '../../icons/IconInfo';
 
 export type NotificationProps = React.PropsWithChildren<{
   labelText: string;
-  onClickClose?: () => void;
   alternative?: boolean;
 }>;
 
-export default ({ children, labelText, alternative = false, onClickClose = null }: NotificationProps) => {
-  return (
-    <div className={[styles.notification, alternative && styles.alternative].filter(e => e).join(' ')}>
-      <div className={styles.label}>
-        <IconInfo className={styles.iconInfo} />
-        <span className={styles.labelText}>{labelText}</span>
-        {onClickClose && (
-          <button type="button" className={styles.buttonClose} onClick={onClickClose}>
-            <IconClose />
-            <span className={styles.buttonCloseText}>close tooltip</span>
-          </button>
-        )}
-      </div>
-      <div className={styles.bodyText}>{children}</div>
+export default ({ children, labelText, alternative = false }: NotificationProps) => (
+  <div className={[styles.notification, alternative && styles.alternative].filter(e => e).join(' ')}>
+    <div className={styles.label}>
+      <IconInfo className={styles.iconInfo} />
+      <span className={styles.labelText}>{labelText}</span>
     </div>
-  );
-};
+    <div className={styles.bodyText}>{children}</div>
+  </div>
+);
