1b5c19ec598330a5359efa2a855902087a2bbefe
lehtoinen <lehtovaaralainen@gmail.com>
2019-11-27 15:30:01 +0200

    Refactored TextInputTooltip

--- packages/react/src/components/textinput/TextInputTooltip.tsx
+++ packages/react/src/components/textinput/TextInputTooltip.tsx
@@ -10,8 +10,8 @@ export type TextInputTooltipProps = React.PropsWithChildren<{
   alternative?: boolean;
 }>;
 
-export default ({ children, open, labelText, alternative = false }: TextInputTooltipProps) => {
-  const content = (
+export default ({ children, open, labelText, alternative = false }: TextInputTooltipProps) => (
+  <TooltipTransition open={open}>
     <div className={[styles.tooltip, alternative && styles.alternative].filter(e => e).join(' ')}>
       <div className={styles.label}>
         <IconInfo className={styles.iconInfo} />
@@ -19,17 +19,19 @@ export default ({ children, open, labelText, alternative = false }: TextInputToo
       </div>
       <div className={styles.bodyText}>{children}</div>
     </div>
-  );
+  </TooltipTransition>
+);
 
-  return (
-    <Transition
-      config={{ tension: 300, friction: 30 }}
-      items={open}
-      from={{ height: 0 }}
-      enter={{ height: 'auto' }}
-      leave={{ height: 0 }}
-    >
-      {show => show && (props => <div style={{ ...props, ...{ overflowY: 'hidden' } }}>{content}</div>)}
-    </Transition>
-  );
-};
+type TooltipTransitionProps = React.PropsWithChildren<{ open: boolean }>;
+
+const TooltipTransition = ({ children, open }: TooltipTransitionProps) => (
+  <Transition
+    config={{ tension: 300, friction: 30 }}
+    items={open}
+    from={{ height: 0 }}
+    enter={{ height: 'auto' }}
+    leave={{ height: 0 }}
+  >
+    {show => show && (props => <div style={{ ...props, ...{ overflowY: 'hidden' } }}>{children}</div>)}
+  </Transition>
+);
