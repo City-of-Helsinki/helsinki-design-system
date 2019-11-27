fe60e0f881d7137d1569184f611bae3014729772
lehtoinen <lehtovaaralainen@gmail.com>
2019-11-27 15:39:45 +0200

    Add DismissableNotification

---
+++ packages/react/src/components/notification/DismissableNotification.test.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import DismissableNotification from './DismissableNotification';
+
+describe('<DismissableNotification /> spec', () => {
+  it('renders the component', () => {
+    const { asFragment } = render(
+      <DismissableNotification labelText="This is the tool tip label text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.
+      </DismissableNotification>,
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
