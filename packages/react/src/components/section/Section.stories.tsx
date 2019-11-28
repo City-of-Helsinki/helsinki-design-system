3b1bc239caf0d063b181724040f03853ea9fc7df
lehtoinen <lehtovaaralainen@gmail.com>
2019-11-28 12:41:53 +0200

    Fix Section stories

--- packages/react/src/components/section/Section.stories.tsx
+++ packages/react/src/components/section/Section.stories.tsx
@@ -15,15 +15,21 @@ const placeholderText = (
 const placeholderImg = <img src={imageFile} alt="" style={{ display: 'block', width: '100%' }} />;
 
 storiesOf('Section', module)
+  .add('plain', () => (
+    <Section color="">
+      <h1>Lorem Ipsum</h1>
+      {placeholderText}
+    </Section>
+  ))
   .add('primary', () => (
-    <Section>
+    <Section color="primary">
       <h1>Lorem Ipsum</h1>
       {placeholderText}
     </Section>
   ))
   .add('primary with koros', () => (
     <>
-      <Section korosType="basic">
+      <Section color="primary" korosType="basic">
         <h1>Lorem Ipsum</h1>
         {placeholderText}
       </Section>
@@ -40,6 +46,21 @@ storiesOf('Section', module)
     <>
       {placeholderImg}
       <Section color="secondary" korosType="basic">
+        <h1>Lorem Ipsum</h1>
+        {placeholderText}
+      </Section>
+    </>
+  ))
+  .add('tertiary', () => (
+    <Section color="tertiary">
+      <h1>Lorem Ipsum</h1>
+      {placeholderText}
+    </Section>
+  ))
+  .add('tertiary with koros', () => (
+    <>
+      {placeholderImg}
+      <Section color="tertiary" korosType="basic">
         <h1>Lorem Ipsum</h1>
         {placeholderText}
       </Section>
