dbc1d52580969debf1709ade411cfe4d94d80fdd
lehtoinen <lehtovaaralainen@gmail.com>
2019-11-29 17:19:54 +0200

    Export DismissableNotification

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,6 +1,10 @@
 export { default as WallOfText } from './components/walloftext/WallOfText';
 export { default as TextInput, TextInputProps } from './components/textinput/TextInput';
 export { default as Notification, NotificationProps } from './components/notification/Notification';
+export {
+  default as DismissableNotification,
+  DismissableNotificationProps,
+} from './components/notification/DismissableNotification';
 export { default as Section, SectionProps } from './components/section/Section';
 export { default as Koros, KorosProps } from './components/koros/Koros';
 export { default as ImageWithCard, ImageWithCardProps } from './components/imagewithcard/ImageWithCard';
