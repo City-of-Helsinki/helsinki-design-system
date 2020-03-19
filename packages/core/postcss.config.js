d06a88ff73a4ffdccaedb4cfe3dce94e451f99f8
Niclas Liimatainen <niclas.liimatainen@digia.com>
2020-03-19 09:44:15 +0200

    Add postcss-mixins and postcss-nesting dev dependencies to core
    ++ Update yarn.lock

--- packages/core/postcss.config.js
+++ packages/core/postcss.config.js
@@ -1,10 +1,15 @@
 const postcssImport = require("postcss-import");
 const postcssPresetEnv = require("postcss-preset-env");
 const postcssInlineSVG = require("postcss-inline-svg");
+const postcssNesting = require("postcss-nesting");
+const postcssMixins = require("postcss-mixins");
+
 module.exports = {
   plugins: [
     postcssImport(),
     postcssPresetEnv({ browsers: "ie > 8" }),
-    postcssInlineSVG()
+    postcssInlineSVG(),
+    postcssMixins(),
+    postcssNesting()
   ]
 };
