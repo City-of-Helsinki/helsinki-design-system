e7597785afa29df4411baf2ffcd48c3dd85c4114
Niclas Liimatainen <niclas.liimatainen@digia.com>
2020-03-23 15:11:12 +0200

    Remove some core postcss plugins

--- packages/core/postcss.config.js
+++ packages/core/postcss.config.js
@@ -1,15 +1,11 @@
 const postcssImport = require("postcss-import");
 const postcssPresetEnv = require("postcss-preset-env");
 const postcssInlineSVG = require("postcss-inline-svg");
-const postcssNesting = require("postcss-nesting");
-const postcssMixins = require("postcss-mixins");
 
 module.exports = {
   plugins: [
     postcssImport(),
     postcssPresetEnv({ browsers: "ie > 8" }),
     postcssInlineSVG(),
-    postcssMixins(),
-    postcssNesting()
   ]
 };
