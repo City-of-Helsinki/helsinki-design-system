7d2f5f86f1080dac9daaed63b19fe38b849fcd05
Niclas Liimatainen <niclas.liimatainen@digia.com>
2020-04-02 13:12:54 +0300

    Add radio-button story

--- packages/react/src/components/radiobutton/RadioButton.stories.tsx
+++ packages/react/src/components/radiobutton/RadioButton.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { storiesOf } from '@storybook/react';
 
 import RadioButton from './RadioButton';
@@ -9,7 +9,28 @@ const WrapperDecorator = storyFn => <div style={{ padding: '20px' }}> {storyFn()
 
 storiesOf('RadioButton', module)
   .addDecorator(WrapperDecorator)
-  .add('default', () => <RadioButton id="radio" labelText="Label" />)
+  .add('unselected', () => <RadioButton id="radio" labelText="Label" />)
   .add('selected', () => <RadioButton id="radio" labelText="Label" checked />)
   .add('disabled', () => <RadioButton id="radio" labelText="Label" disabled />)
-  .add('selected disabled', () => <RadioButton id="radio" labelText="Label" checked disabled />);
+  .add('selected disabled', () => <RadioButton id="radio" labelText="Label" checked disabled />)
+  .add('default', () => {
+    const [radioValue, setRadioValue] = useState(null);
+    return (
+      <>
+        <RadioButton
+          id="radio"
+          value="foo"
+          labelText="Label"
+          onChange={event => setRadioValue((event.target as HTMLInputElement).value)}
+          checked={radioValue === 'foo'}
+        />
+        <RadioButton
+          id="radio2"
+          value="bar"
+          labelText="Label"
+          onChange={event => setRadioValue((event.target as HTMLInputElement).value)}
+          checked={radioValue === 'bar'}
+        />
+      </>
+    );
+  });
