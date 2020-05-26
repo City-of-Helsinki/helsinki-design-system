c319786fd876e01f3db17b0fa441f34c7d76da81
Niclas Liimatainen <niclas.liimatainen@digia.com>
2020-05-26 13:40:30 +0300

    Add examples

--- packages/core/src/icons/icons.stories.js
+++ packages/core/src/icons/icons.stories.js
@@ -5,15 +5,15 @@ import './icon.css';
 
 export default {
   title: 'Icons',
-  decorators: [(storyFn) => `<style>.hds-icon {margin-right: 1rem;}</style>${storyFn()}`],
+  decorators: [(storyFn) => `<style>.hds-icon {margin-right: var(--spacing-m);}</style>${storyFn()}`],
 };
 
 const iconStories = require(__filename);
-const req = require.context('.', false, /(icon\b-+).+?.css$/);
+const req = require.context('.', true, /(icon\b-+).+?.css$/);
 req.keys().forEach((fileName) => {
-  const icon = path.basename(fileName, '.css').substring(5);
+  const icon = path.basename(fileName, '.css').substring('icon-'.length);
   iconStories[icon] = () =>
-    [200, 100, 50, 25].reduce((acc, size) => {
+    [16, 24, 36, 48, 64].reduce((acc, size) => {
       acc += `<span class="hds-icon hds-icon--${icon}" style="width: ${size}px; height: ${size}px;"></span>`;
       return acc;
     }, '');
