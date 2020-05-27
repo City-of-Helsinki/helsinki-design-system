28a25c9eaeff9e9b2202034cfce76f869dcf5a37
Niclas Liimatainen <niclas.liimatainen@digia.com>
2020-05-27 14:33:26 +0300

    Add icon interface

---
+++ packages/react/src/icons/Icon.interface.ts
@@ -0,0 +1,12 @@
+import { CSSProperties } from 'react';
+
+export type IconProps = {
+  /**
+   * Additional class names to apply to the icon
+   */
+  className?: string;
+  /**
+   * Override or extend the styles applied to the component
+   */
+  style?: CSSProperties;
+};
