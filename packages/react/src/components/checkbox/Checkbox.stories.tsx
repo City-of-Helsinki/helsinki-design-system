27c9ba98b9b2c6071f6df0451e78d759de554425
Niclas Liimatainen <niclas.liimatainen@digia.com>
2020-05-28 11:41:36 +0300

    Update Checkbox story

--- packages/react/src/components/checkbox/Checkbox.stories.tsx
+++ packages/react/src/components/checkbox/Checkbox.stories.tsx
@@ -65,10 +65,8 @@ Custom.story = {
 export const RichLabel = () => {
   const label = (
     <span>
-      I agree to the{' '}
-      <a style={{ color: 'var(--color-bus)' }} href="/?path=/story/components-checkbox--rich-label">
-        Terms of service
-      </a>
+      I have read city&apos;s {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+      <a style={{ color: 'var(--color-bus)', textDecoration: 'underline' }}>data protection principles</a>.
     </span>
   );
   return <Checkbox id="radio4" label={label} />;
