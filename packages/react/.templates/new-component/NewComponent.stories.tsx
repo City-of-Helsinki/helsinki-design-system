bd8c4dab5120f697f87a0ec457ef975fd6773fd1
Niclas Liimatainen <niclas.liimatainen@digia.com>
2020-06-29 16:11:26 +0300

    Update component scaffolding story template

--- packages/react/.templates/new-component/NewComponent.stories.tsx
+++ packages/react/.templates/new-component/NewComponent.stories.tsx
@@ -4,7 +4,7 @@ import [-replace-name-capital-] from './[-replace-name-capital-]';
 
 export default {
   component: [-replace-name-capital-],
-  title: '[-replace-name-capital-]',
+  title: 'Components/[-replace-name-capital-]',
 };
 
 export const Example = () => <[-replace-name-capital-]>[-replace-name-capital-]</[-replace-name-capital-]>;
