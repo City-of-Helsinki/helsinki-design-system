4d6ad7e605c01418e602cfc7c98d532319745fa4
Niclas Liimatainen <niclas.liimatainen@digia.com>
2020-06-29 16:23:19 +0300

    Add Logo component

---
+++ packages/react/src/components/logo/Logo.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Logo from './Logo';
+
+export default {
+  component: Logo,
+  title: 'Components/Logo',
+};
+
+export const Default = () => <Logo />;
--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -18,3 +18,5 @@ export { default as Button, ButtonProps } from './components/button/Button';
 export { default as RadioButton, RadioButtonProps } from './components/radiobutton/RadioButton';
 export { default as Checkbox, CheckboxProps } from './components/checkbox/Checkbox';
 export { default as Dropdown, DropdownProps } from './components/dropdown/Dropdown';
+export { default as Logo, LogoProps } from './components/logo/Logo';
+export { default as Navigation, NavigationProps } from './components/navigation/Navigation';
