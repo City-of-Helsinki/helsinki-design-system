5e1cfd7a4b39cce3acd6e694f809f0d863eacb9f
Niclas Liimatainen <niclas.liimatainen@digia.com>
2020-08-30 15:25:48 +0300

    Fix bundle minification and bump selector specificity
    Bump selector specificity

--- packages/react/rollup.config.js
+++ packages/react/rollup.config.js
@@ -20,6 +20,8 @@ export default {
     'components/Dropdown/index': 'src/components/dropdown/index.ts',
     'components/ImageWithCard/index': 'src/components/imageWithCard/index.ts',
     'components/Koros/index': 'src/components/koros/index.ts',
+    'components/Logo/index': 'src/components/logo/index.ts',
+    'components/Navigation/index': 'src/components/navigation/index.ts',
     'components/Notification/index': 'src/components/notification/index.ts',
     'components/RadioButton/index': 'src/components/radioButton/index.ts',
     'components/Section/index': 'src/components/section/index.ts',
@@ -32,6 +34,7 @@ export default {
     {
       dir: 'lib',
       format: 'esm',
+      compact: true,
     },
   ],
   plugins: [
@@ -64,7 +67,10 @@ export default {
       ],
       plugins: [['@babel/plugin-transform-runtime', { useESModules: true }]],
     }),
-    terser(),
+    terser({
+      compress: false,
+      mangle: false,
+    }),
   ],
   external: [
     /@babel\/runtime/,
@@ -76,53 +82,3 @@ export default {
     '@react-aria/visually-hidden',
   ],
 };
-
-export default [
-  {
-    input: 'src/index.ts',
-    output: [
-      {
-        file: 'lib/umd/index.js',
-        format: 'umd',
-        name: 'hds-react',
-        globals: {
-          react: 'React',
-          'lodash.isequal': 'isEqual',
-          'lodash.uniqueid': 'uniqueId',
-          'react-spring': 'reactSpring',
-          '@react-aria/visually-hidden': 'visuallyHidden',
-        },
-      },
-    ],
-    ...commonConfig,
-  },
-  {
-    input: {
-      index: 'src/index.ts',
-      'components/index': 'src/components/index.ts',
-      'icons/index': 'src/icons/index.ts',
-      'components/Button/index': 'src/components/button/index.ts',
-      'components/Checkbox/index': 'src/components/checkbox/index.ts',
-      'components/Columns/index': 'src/components/columns/index.ts',
-      'components/Dropdown/index': 'src/components/dropdown/index.ts',
-      'components/ImageWithCard/index': 'src/components/imageWithCard/index.ts',
-      'components/Koros/index': 'src/components/koros/index.ts',
-      'components/Logo/index': 'src/components/logo/index.ts',
-      'components/Navigation/index': 'src/components/navigation/index.ts',
-      'components/Notification/index': 'src/components/notification/index.ts',
-      'components/RadioButton/index': 'src/components/radioButton/index.ts',
-      'components/Section/index': 'src/components/section/index.ts',
-      'components/StatusLabel/index': 'src/components/statusLabel/index.ts',
-      'components/TextInput/index': 'src/components/textInput/index.ts',
-      'components/Textarea/index': 'src/components/textarea/index.ts',
-      'components/Tooltip/index': 'src/components/tooltip/index.ts',
-    },
-    output: [
-      {
-        dir: 'lib',
-        format: 'esm',
-      },
-    ],
-    ...commonConfig,
-  },
-];
