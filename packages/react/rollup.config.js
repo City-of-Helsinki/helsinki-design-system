12a62ebafaaa6f7198aa95e29885247f0beaedcb
Niclas Liimatainen <niclas.liimatainen@digia.com>
2020-10-04 22:49:30 +0300

    Rollup config changes
    - Update rollup related dependencies
    - Add cjs bundle

--- packages/react/rollup.config.js
+++ packages/react/rollup.config.js
@@ -1,45 +1,36 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import babel from '@rollup/plugin-babel';
 import includePaths from 'rollup-plugin-includepaths';
-import postcss from 'rollup-plugin-postcss';
-import typescript from 'rollup-plugin-typescript2';
 import resolve from '@rollup/plugin-node-resolve';
+import ts from '@wessberg/rollup-plugin-ts';
+import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
+import postcss from 'rollup-plugin-postcss';
+import { sizeSnapshot } from 'rollup-plugin-size-snapshot';
 import { terser } from 'rollup-plugin-terser';
 
 const extensions = ['.js', '.jsx', '.ts', '.tsx'];
+const external = [
+  'react',
+  'react-dom',
+  'lodash.uniqueid',
+  'lodash.isequal',
+  'react-spring',
+  '@react-aria/visually-hidden',
+];
 
-export default {
-  input: {
-    index: 'src/index.ts',
-    'components/index': 'src/components/index.ts',
-    'icons/index': 'src/icons/index.ts',
-    'components/Button/index': 'src/components/button/index.ts',
-    'components/Checkbox/index': 'src/components/checkbox/index.ts',
-    'components/Columns/index': 'src/components/columns/index.ts',
-    'components/Dropdown/index': 'src/components/dropdown/index.ts',
-    'components/ImageWithCard/index': 'src/components/imageWithCard/index.ts',
-    'components/Koros/index': 'src/components/koros/index.ts',
-    'components/Logo/index': 'src/components/logo/index.ts',
-    'components/Navigation/index': 'src/components/navigation/index.ts',
-    'components/Notification/index': 'src/components/notification/index.ts',
-    'components/RadioButton/index': 'src/components/radioButton/index.ts',
-    'components/Section/index': 'src/components/section/index.ts',
-    'components/StatusLabel/index': 'src/components/statusLabel/index.ts',
-    'components/TextInput/index': 'src/components/textInput/index.ts',
-    'components/Textarea/index': 'src/components/textarea/index.ts',
-    'components/Tooltip/index': 'src/components/tooltip/index.ts',
-  },
-  output: [
-    {
-      dir: 'lib',
-      format: 'esm',
-      compact: true,
-    },
-  ],
+const getExternal = (format) => (format === 'esm' ? [...external, /@babel\/runtime/] : external);
+
+const getConfig = (format, getSizeSnapshot) => ({
   plugins: [
     includePaths({ paths: ['src'], extensions }),
     resolve(),
+    ts(),
+    format === 'esm' &&
+      babel({
+        babelHelpers: 'runtime',
+        exclude: 'node_modules/**',
+        extensions,
+      }),
     commonjs({
       include: '../../node_modules/**',
     }),
@@ -55,31 +46,59 @@ export default {
         ],
       },
     }),
-    typescript(),
-    babel({
-      babelrc: false,
-      babelHelpers: 'runtime',
-      exclude: 'node_modules/**',
-      extensions,
-      presets: [
-        ['@babel/preset-env', { targets: '>1%, not dead, not ie 11, not op_mini all' }],
-        '@babel/preset-react',
-        '@babel/preset-typescript',
-      ],
-      plugins: [['@babel/plugin-transform-runtime', { useESModules: true }]],
-    }),
-    terser({
-      compress: false,
-      mangle: false,
-    }),
+    getSizeSnapshot && sizeSnapshot(),
+    terser(),
   ],
-  external: [
-    /@babel\/runtime/,
-    'react',
-    'react-dom',
-    'lodash.uniqueid',
-    'lodash.isequal',
-    'react-spring',
-    '@react-aria/visually-hidden',
-  ],
-};
+  external: getExternal(format),
+});
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        dir: 'lib/cjs',
+        format: 'cjs',
+      },
+    ],
+    ...getConfig('cjs'),
+  },
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'lib/index.js',
+        format: 'esm',
+      },
+    ],
+    ...getConfig('esm'),
+  },
+  {
+    input: {
+      'components/index': 'src/components/index.ts',
+      'icons/index': 'src/icons/index.ts',
+      'components/Button/index': 'src/components/button/index.ts',
+      'components/Checkbox/index': 'src/components/checkbox/index.ts',
+      'components/Columns/index': 'src/components/columns/index.ts',
+      'components/Dropdown/index': 'src/components/dropdown/index.ts',
+      'components/ImageWithCard/index': 'src/components/imageWithCard/index.ts',
+      'components/Koros/index': 'src/components/koros/index.ts',
+      'components/Logo/index': 'src/components/logo/index.ts',
+      'components/Navigation/index': 'src/components/navigation/index.ts',
+      'components/Notification/index': 'src/components/notification/index.ts',
+      'components/RadioButton/index': 'src/components/radioButton/index.ts',
+      'components/Section/index': 'src/components/section/index.ts',
+      'components/StatusLabel/index': 'src/components/statusLabel/index.ts',
+      'components/TextInput/index': 'src/components/textInput/index.ts',
+      'components/Textarea/index': 'src/components/textarea/index.ts',
+      'components/Tooltip/index': 'src/components/tooltip/index.ts',
+    },
+    output: [
+      {
+        dir: 'lib',
+        format: 'esm',
+      },
+    ],
+    ...getConfig('esm', false),
+  },
+];
