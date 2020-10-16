a5439891e77eb42fc0ef0b490b9aafcc1181a6e4
Niclas Liimatainen <niclas.liimatainen@digia.com>
2020-10-16 14:55:03 +0300

    Update NavigationActions styles

--- packages/react/src/components/navigation/navigation-actions/NavigationActions.tsx
+++ packages/react/src/components/navigation/navigation-actions/NavigationActions.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import styles from './NavigationActions.module.css';
+import styles from './NavigationActions.module.scss';
 
 export const NavigationActions = ({ children }: React.PropsWithChildren<{}>) => (
   <div className={styles.navigationActions}>{children}</div>
