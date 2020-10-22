dfd41a9caa0b82a21d912d5608fa138deea8d347
Aleksi Kaistinen <aleksi.kaistinen@allu.io>
2020-10-22 11:25:56 +0300

    Update tooltip snapshot tests

--- packages/react/src/components/tooltip/Tooltip.test.tsx
+++ packages/react/src/components/tooltip/Tooltip.test.tsx
@@ -5,11 +5,35 @@ import { Tooltip } from './Tooltip';
 
 describe('<TooltipNew /> spec', () => {
   it('renders the component', () => {
-    const { asFragment } = render(<Tooltip />);
+    const { asFragment } = render(
+      <Tooltip
+        small
+        placement="auto"
+        buttonLabel="Tooltip button"
+        tooltipLabel="Tooltip label"
+        className="className"
+        buttonClassName="buttonClassName"
+        tooltipClassName="tooltipClassName"
+      >
+        Tooltip content
+      </Tooltip>,
+    );
     expect(asFragment()).toMatchSnapshot();
   });
   it('opens the tooltip when button is clicked', async () => {
-    const { asFragment, getByRole } = render(<Tooltip />);
+    const { asFragment, getByRole } = render(
+      <Tooltip
+        small
+        placement="auto"
+        buttonLabel="Tooltip button"
+        tooltipLabel="Tooltip label"
+        className="className"
+        buttonClassName="buttonClassName"
+        tooltipClassName="tooltipClassName"
+      >
+        Tooltip content
+      </Tooltip>,
+    );
     fireEvent.click(getByRole('button'));
     waitFor(() => getByRole('section'));
     expect(asFragment()).toMatchSnapshot();
