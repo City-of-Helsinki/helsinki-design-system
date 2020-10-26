76c6fb6eea9903a79b5022ad82fbb613ef4b0f50
Niclas Liimatainen <niclas.liimatainen@digia.com>
2020-10-26 17:14:55 +0200

    Fix NavigationDropdown typing

--- packages/react/src/components/navigation/navigationDropdown/NavigationDropdown.tsx
+++ packages/react/src/components/navigation/navigationDropdown/NavigationDropdown.tsx
@@ -2,7 +2,14 @@ import React from 'react';
 
 import { MenuButton, MenuButtonProps } from '../../../internal/menuButton/MenuButton';
 
-export const NavigationDropdown = ({ label, children, ...rest }: MenuButtonProps) => (
+export type NavigationDropdownProps = MenuButtonProps & {
+  /**
+   * If `true`, the item will be marked as active
+   */
+  active?: boolean;
+};
+
+export const NavigationDropdown = ({ label, children, ...rest }: NavigationDropdownProps) => (
   <MenuButton label={label} {...rest}>
     {children}
   </MenuButton>
