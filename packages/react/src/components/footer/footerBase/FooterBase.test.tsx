147a93144339d8647b3aee64e4a7f133774248eb
Niclas Liimatainen <niclas.liimatainen@digia.com>
2020-10-28 13:11:03 +0200

    Make it possible to hide the copyright texts in the base section

--- packages/react/src/components/footer/footerBase/FooterBase.test.tsx
+++ packages/react/src/components/footer/footerBase/FooterBase.test.tsx
@@ -6,7 +6,9 @@ import { FooterWrapper } from '../../../utils/test-utils';
 
 describe('<Footer.Base /> spec', () => {
   it('renders the component', () => {
-    const { asFragment } = render(<FooterBase />, { wrapper: FooterWrapper });
+    const { asFragment } = render(<FooterBase copyrightHolder="Copyright" copyrightText="All rights reserved" />, {
+      wrapper: FooterWrapper,
+    });
     expect(asFragment()).toMatchSnapshot();
   });
 });
