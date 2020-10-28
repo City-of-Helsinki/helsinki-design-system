951735b5eca90be4c4005f948c8742bc8055b4b4
Aleksi Kaistinen <aleksi.kaistinen@allu.io>
2020-10-28 13:25:26 +0200

    Enable custom theme for Card component

--- packages/react/src/components/card/Card.tsx
+++ packages/react/src/components/card/Card.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 // import core base styles
 import 'hds-core';
 import styles from './Card.module.scss';
 import classNames from '../../utils/classNames';
+import setComponentTheme from '../../utils/setComponentTheme';
+
+export interface CardCustomTheme {
+  '--background-color'?: string;
+  '--border-color'?: string;
+  '--border-width'?: string;
+  '--padding-horizontal': string;
+  '--padding-vertical': string;
+}
 
 export type CardProps = {
   /**
@@ -23,15 +32,30 @@ export type CardProps = {
    */
   className?: string;
   /**
+   * Custom theme styles
+   */
+  theme?: CardCustomTheme;
+  /**
    * Additional children to render inside the card.
    */
   children?: React.ReactNode;
 } & React.HTMLProps<HTMLDivElement>;
 
-export const Card = ({ border, heading, text, className, children, ...divProps }: CardProps) => {
+export const Card = ({ border, heading, text, className, theme, children, ...divProps }: CardProps) => {
+  // handle custom themes
+  useEffect(() => {
+    if (theme) {
+      setComponentTheme<CardCustomTheme>('Card', theme);
+    }
+  }, [theme]);
+
   const hasBody = !!heading || !!text;
   return (
-    <div className={classNames(styles.card, border && styles.border, className)} role="region" {...divProps}>
+    <div
+      className={classNames(styles.card, border && styles.border, className, theme && 'custom')}
+      role="region"
+      {...divProps}
+    >
       {hasBody && (
         <div className={styles.body}>
           {heading && (
