fe6255e1872ba3854d5bded7ae925d2ff4f558e0
Aleksi Kaistinen <aleksi.kaistinen@allu.io>
2020-11-20 13:55:16 +0200

    Implement ErrorSummary component

---
+++ packages/react/src/components/errorSummary/ErrorSummary.test.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import { ErrorSummary } from './ErrorSummary';
+
+describe('<ErrorSummary /> spec', () => {
+  it('renders the component', () => {
+    const { asFragment } = render(
+      <ErrorSummary
+        label="Form contains following errors"
+        className="customClass"
+        size="default"
+        style={{ marginBottom: 'var(--spacing-m)' }}
+      >
+        <ul>
+          <li>
+            Error 1: <a href="#field1">Please enter your first name</a>
+          </li>
+          <li>
+            Error 2: <a href="#field2">Please enter your last name</a>
+          </li>
+          <li>
+            Error 3: <a href="#field3">Please enter a valid email address</a>
+          </li>
+        </ul>
+      </ErrorSummary>,
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
