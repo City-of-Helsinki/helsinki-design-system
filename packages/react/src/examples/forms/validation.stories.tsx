b53dfaef8ad6f543e698b3401b79054875ccc6eb
Aleksi Kaistinen <aleksi.kaistinen@allu.io>
2020-11-25 11:49:54 +0200

    validation wip

--- packages/react/src/examples/forms/validation.stories.tsx
+++ packages/react/src/examples/forms/validation.stories.tsx
@@ -2,11 +2,63 @@ import React, { useEffect, useState } from 'react';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 
-import { Button, ErrorSummary, TextInput, Checkbox } from '../../components';
+import {
+  Button,
+  ErrorSummary,
+  TextInput,
+  Checkbox,
+  Select,
+  SelectionGroup,
+  RadioButton,
+  TextArea,
+} from '../../components';
 
 export default {
   title: 'Patterns/Form validation',
-  decorators: [(storyFn) => <div style={{ maxWidth: '400px' }}>{storyFn()}</div>],
+  decorators: [
+    (storyFn) => (
+      <div style={{ maxWidth: '800px' }}>
+        <style type="text/css">
+          {`
+            .hds-example-form__title {
+              color: var(--color-bus);
+              font-size: var(--fontsize-heading-l);
+              line-height: var(--lineheight-m);
+              margin: 0 0 var(--spacing-m) 0;
+            }
+            .hds-example-form__required-info {
+              font-size: var(--fontsize-body-m);
+              line-height: var(--lineheight-l);
+              margin: 0 0 var(--spacing-l) 0;
+            }
+            .hds-example-form__section {
+              margin-bottom: var(--spacing-xl);
+            }
+            .hds-example-form__section-title {
+              color: var(--color-bus);
+              font-size: var(--fontsize-heading-s);
+              line-height: var(--lineheight-m);
+              margin: 0 0 var(--spacing-m);
+            }
+            .hds-example-form__grid-6-6 {
+              display: grid;
+              grid-template-columns: 1fr 1fr;
+              gap: var(--spacing-m);
+            }
+            .hds-example-form__grid-8-4 {
+              display: grid;
+              grid-template-columns: 2fr 1fr;
+              gap: var(--spacing-m);
+            }
+            .hds-example-form__item {
+              margin-bottom: var(--spacing-m);
+            }
+          `}
+        </style>
+        {storyFn()}
+      </div>
+    ),
+  ],
   parameters: {
     controls: { hideNoControlsWarning: true },
   },
@@ -22,15 +74,23 @@ export const Static = () => {
 
   // Define the validation schema
   const schema = Yup.object().shape({
-    firstName: Yup.string()
-      .min(2, 'First name is too short')
-      .max(50, 'First name is too long')
-      .required('First name is required'),
-    lastName: Yup.string()
-      .min(2, 'Last name is too short')
-      .max(50, 'Last name is too long')
-      .required('Last name is required'),
+    firstName: Yup.string().required('First name is required'),
+    lastName: Yup.string().required('Last name is required'),
+    city: Yup.string().required('City is required'),
+    postalCode: Yup.string()
+      .matches(/^\d+$/, 'Postal code must contain numbers only')
+      .required('Postal code is required'),
     email: Yup.string().email('Email is invalid').required('Email is invalid'),
+    registerPlate: Yup.string()
+      .matches(/^\w{1,3}-\d{1,3}$/, 'Invalid register plate number')
+      .required('Register plate number is required'),
+    brand: Yup.string().required('Vehicle brand is required'),
+    model: Yup.string().required('Vehicle model is required'),
+    parkingPeriod: Yup.string().oneOf(
+      ['continuous', 'temporary'],
+      'Parking period must be either continuous or temporary',
+    ),
+
     acceptTerms: Yup.boolean().oneOf([true], 'You must accept the terms'),
   });
 
@@ -39,8 +99,15 @@ export const Static = () => {
     initialValues: {
       firstName: '',
       lastName: '',
+      city: '',
+      postalCode: '',
       email: '',
-      acceptTerms: false,
+      registerPlate: '',
+      brand: '',
+      model: '',
+      parkingPeriod: 'continuous',
+      permitEndDate: '',
+      additionalRequests: '',
     },
     validateOnChange: false, // Disable validation on field change
     validateOnBlur: false, // Disable validation on field blur
@@ -63,6 +130,8 @@ export const Static = () => {
 
   return (
     <form>
+      <h2 className="hds-example-form__title">Residental parking permit application</h2>
+
       {hasErrors && (
         <ErrorSummary label="Form contains following errors" style={{ marginBottom: 'var(--spacing-m)' }} autofocus>
           <ul>
@@ -74,52 +143,177 @@ export const Static = () => {
           </ul>
         </ErrorSummary>
       )}
-      <FormItem>
-        <TextInput
-          id="firstName"
-          name="firstName"
-          label="First name"
-          onChange={formik.handleChange}
-          value={formik.values.firstName}
-          invalid={!!(hasErrors && formik.errors.firstName)}
-          errorText={hasErrors && formik.errors.firstName}
-          required
-        />
-      </FormItem>
-      <FormItem>
-        <TextInput
-          id="lastName"
-          name="lastName"
-          label="Last name"
-          onChange={formik.handleChange}
-          value={formik.values.lastName}
-          invalid={!!(hasErrors && formik.errors.lastName)}
-          errorText={hasErrors && formik.errors.lastName}
-          required
-        />
-      </FormItem>
-      <FormItem>
-        <TextInput
-          id="email"
-          name="email"
-          label="Email"
-          onChange={formik.handleChange}
-          value={formik.values.email}
-          invalid={!!(hasErrors && formik.errors.email)}
-          errorText={hasErrors && formik.errors.email}
-          required
-        />
-      </FormItem>
-      <FormItem>
-        <Checkbox
-          id="acceptTerms"
-          name="acceptTerms"
-          label="I accept the terms of this service"
-          checked={!!formik.values.acceptTerms}
-          onChange={formik.handleChange}
-          errorText={hasErrors && formik.errors.acceptTerms}
-        />
-      </FormItem>
+
+      <p className="hds-example-form__required-info">All fields marked with * are required</p>
+
+      <div className="hds-example-form__section">
+        <h3 className="hds-example-form__section-title">Contact information</h3>
+        <div className="hds-example-form__grid-6-6">
+          <div className="hds-example-form__item">
+            <TextInput
+              id="firstName"
+              name="firstName"
+              label="First name"
+              onChange={formik.handleChange}
+              value={formik.values.firstName}
+              invalid={!!(hasErrors && formik.errors.firstName)}
+              errorText={hasErrors && formik.errors.firstName}
+              required
+            />
+          </div>
+          <div className="hds-example-form__item">
+            <TextInput
+              id="lastName"
+              name="lastName"
+              label="Last name"
+              onChange={formik.handleChange}
+              value={formik.values.lastName}
+              invalid={!!(hasErrors && formik.errors.lastName)}
+              errorText={hasErrors && formik.errors.lastName}
+              required
+            />
+          </div>
+        </div>
+        <div className="hds-example-form__grid-8-4">
+          <div className="hds-example-form__item">
+            <Select<{ label: string }>
+              id="city"
+              label="City"
+              options={[{ label: 'Espoo' }, { label: 'Helsinki' }, { label: 'Vantaa' }]}
+              onChange={(value: { label: string }) => {
+                formik.setFieldValue('city', value.label);
+              }}
+              value={{ label: formik.values.city }}
+              invalid={!!(hasErrors && formik.errors.city)}
+              error={hasErrors && formik.errors.city}
+              required
+            />
+          </div>
+          <div className="hds-example-form__item">
+            <TextInput
+              id="postalCode"
+              name="postalCode"
+              label="Postal code"
+              onChange={formik.handleChange}
+              value={formik.values.postalCode}
+              invalid={!!(hasErrors && formik.errors.postalCode)}
+              errorText={hasErrors && formik.errors.postalCode}
+              required
+            />
+          </div>
+        </div>
+        <div className="hds-example-form__item">
+          <TextInput
+            id="email"
+            name="email"
+            label="Email address"
+            onChange={formik.handleChange}
+            value={formik.values.email}
+            invalid={!!(hasErrors && formik.errors.email)}
+            errorText={hasErrors && formik.errors.email}
+            required
+          />
+        </div>
+      </div>
+      <div className="hds-example-form__section">
+        <h3 className="hds-example-form__section-title">Vehicle information</h3>
+        <div className="hds-example-form__grid-6-6">
+          <div className="hds-example-form__item">
+            <TextInput
+              id="registerPlate"
+              name="registerPlate"
+              label="Register plate number"
+              placeholder="E.g. ABC-123"
+              helperText="Use format XXX-NNN"
+              onChange={formik.handleChange}
+              value={formik.values.registerPlate}
+              invalid={!!(hasErrors && formik.errors.registerPlate)}
+              errorText={hasErrors && formik.errors.registerPlate}
+              required
+            />
+          </div>
+        </div>
+        <div className="hds-example-form__grid-6-6">
+          <div className="hds-example-form__item">
+            <TextInput
+              id="brand"
+              name="brand"
+              label="Vehicle brand"
+              placeholder="E.g. Skoda"
+              onChange={formik.handleChange}
+              value={formik.values.brand}
+              invalid={!!(hasErrors && formik.errors.brand)}
+              errorText={hasErrors && formik.errors.brand}
+              required
+            />
+          </div>
+          <div className="hds-example-form__item">
+            <TextInput
+              id="model"
+              name="model"
+              label="Vehicle model"
+              placeholder="E.g. Octavia"
+              onChange={formik.handleChange}
+              value={formik.values.model}
+              invalid={!!(hasErrors && formik.errors.model)}
+              errorText={hasErrors && formik.errors.model}
+              required
+            />
+          </div>
+        </div>
+        <div className="hds-example-form__grid-6-6">
+          <div className="hds-example-form__item">
+            <SelectionGroup
+              label="Parking period"
+              direction="horizontal"
+              required
+              errorText={hasErrors && formik.errors.parkingPeriod}
+            >
+              <RadioButton
+                id="parkingPeriodContinuous"
+                name="parkingPeriod"
+                value="continuous"
+                label="Continuous"
+                onChange={formik.handleChange}
+                checked={formik.values.parkingPeriod === 'continuous'}
+              />
+              <RadioButton
+                id="parkingPeriodTemporary"
+                name="parkingPeriod"
+                value="temporary"
+                label="Temporary"
+                onChange={formik.handleChange}
+                checked={formik.values.parkingPeriod === 'temporary'}
+              />
+            </SelectionGroup>
+          </div>
+          <div className="hds-example-form__item">
+            <TextInput
+              id="permitEndDate"
+              name="permitEndDate"
+              label="Permit end date"
+              helperText="Use format DD.MM.YYYY"
+              onChange={formik.handleChange}
+              value={formik.values.permitEndDate}
+              invalid={!!(hasErrors && formik.errors.permitEndDate)}
+              errorText={hasErrors && formik.errors.permitEndDate}
+              required
+            />
+          </div>
+        </div>
+        <div className="hds-example-form__item">
+          <TextArea
+            id="additionalRequests"
+            name="additionalRequests"
+            label="Additional requests"
+            placeholder="E.g. Octavia"
+            onChange={formik.handleChange}
+            value={formik.values.additionalRequests}
+            invalid={!!(hasErrors && formik.errors.additionalRequests)}
+            errorText={hasErrors && formik.errors.additionalRequests}
+          />
+        </div>
+      </div>
       <FormItem>
         <Button type="button" onClick={onSubmitButtonClick}>
           Submit
