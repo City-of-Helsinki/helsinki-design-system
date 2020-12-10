989886afc423593bb74a200f5b3313bdb46d6698
Aleksi Kaistinen <aleksi.kaistinen@allu.io>
2020-12-10 14:12:28 +0200

    Clean up accordion and add customization options

--- packages/react/src/components/accordion/Accordion.stories.tsx
+++ packages/react/src/components/accordion/Accordion.stories.tsx
@@ -14,18 +14,40 @@ export default {
     controls: { hideNoControlsWarning: true },
   },
   args: {
-    title: 'How to publish data?',
-    card: false,
+    heading: 'How to publish data?',
     children: 'To publish your data, open your profile settings and click buttton "Publish".',
   },
 };
 
 export const Default = (args) => <Accordion {...args} />;
 
-export const CardAccordion = (args) => <Accordion {...args} />;
+export const CardAccordion = (args) => (
+  <>
+    <Accordion {...args} card />
+    <Accordion {...args} card border />
+    <Accordion
+      {...args}
+      card
+      border
+      theme={{
+        '--background-color': 'var(--color-white)',
+        '--border-color': 'var(--color-coat-of-arms)',
+        '--padding-horizontal': 'var(--spacing-m)',
+        '--padding-vertical': 'var(--spacing-m)',
+        '--header-font-color': 'var(--color-black-90)',
+        '--header-font-size': 'var(--fontsize-heading-m)',
+        '--header-line-height': 'var(--lineheight-m)',
+        '--button-size': '28px',
+        '--button-border-color-hover': 'var(--color-coat-of-arms)',
+        '--content-font-size': 'var(--fontsize-body-m)',
+        '--content-line-height': 'var(--lineheight-l)',
+      }}
+    />
+  </>
+);
 CardAccordion.storyName = 'Card';
 CardAccordion.args = {
-  card: true,
+  style: { marginBottom: 'var(--spacing-m)' },
 };
 
 export const Custom = () => {
--- packages/react/src/components/accordion/Accordion.tsx
+++ packages/react/src/components/accordion/Accordion.tsx
@@ -8,31 +8,92 @@ import styles from './Accordion.module.scss';
 import classNames from '../../utils/classNames';
 import { IconAngleDown, IconAngleUp } from '../../icons';
 import { useAccordion } from './useAccordion';
+import { useTheme } from '../../hooks/useTheme';
 
-export type AccordionProps = React.PropsWithChildren<{
+export interface AccordionCustomTheme {
+  '--background-color'?: string;
+  '--border-color'?: string;
+  '--padding-horizontal'?: string;
+  '--padding-vertical'?: string;
+  '--header-font-color'?: string;
+  '--header-font-size'?: string;
+  '--header-line-height'?: string;
+  '--button-size'?: string;
+  '--button-border-color-hover'?: string;
+  '--content-font-size'?: string;
+  '--content-line-height'?: string;
+}
+
+export type CommonAccordionProps = React.PropsWithChildren<{
+  /**
+   * If `true` border will be drawn around the accordion card.
+   */
+  border?: false;
   /**
    * Use the card variant if `true`
    */
-  card?: boolean;
+  card?: false;
   /**
    * Additional class names for accordion
    */
   className?: string;
   /**
+   * Heading text.
+   */
+  heading?: string;
+  /**
+   * Heading level
+   * @default 2
+   */
+  headingLevel?: number;
+  /**
    * The id for the accordion element
    */
   id?: string;
   /**
-   * Accordion title
+   * Additional styles
    */
-  title: string;
+  style?: React.CSSProperties;
+  /**
+   * Custom theme styles
+   */
+  theme?: AccordionCustomTheme;
 }>;
 
-export const Accordion = ({ card = false, children, className, id, title }: AccordionProps) => {
+export type CardAccordionProps = Omit<CommonAccordionProps, 'card' | 'border'> & {
+  /**
+   * If `true` border will be drawn around the accordion card.
+   */
+  border?: boolean;
+  /**
+   * Use the card variant if `true`
+   */
+  card: true;
+};
+
+export type AccordionProps = CommonAccordionProps | CardAccordionProps;
+
+export const Accordion = ({
+  border = false,
+  card = false,
+  children,
+  className,
+  heading,
+  headingLevel = 2,
+  id,
+  style,
+  theme,
+}: AccordionProps) => {
+  // Create a unique id if not provided via prop
   const [accordionId] = useState(id || uniqueId('accordion-'));
 
+  // Custom theme
+  const customThemeClass = useTheme<AccordionCustomTheme>(styles.accordion, theme);
+
+  // Accordion logic
   const { isOpen, buttonProps, contentProps } = useAccordion({ initiallyOpen: false });
 
+  // Switch icon based on isOpen state
   const icon = isOpen ? (
     <IconAngleUp aria-hidden className={styles.accordionButtonIcon} />
   ) : (
@@ -41,12 +102,20 @@ export const Accordion = ({ card = false, children, className, id, title }: Acco
 
   return (
     <div
-      className={classNames(styles.accordion, card && styles.card, isOpen && styles.isOpen, className)}
+      className={classNames(
+        styles.accordion,
+        card && styles.card,
+        card && border && styles.border,
+        isOpen && styles.isOpen,
+        customThemeClass,
+        className,
+      )}
+      style={style}
       id={accordionId}
     >
       <div className={styles.accordionHeader}>
-        <div role="heading" aria-level={2} id={`${accordionId}-heading`}>
-          {title}
+        <div role="heading" aria-level={headingLevel} id={`${accordionId}-heading`}>
+          {heading}
         </div>
         <button
           type="button"
