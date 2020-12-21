130e9e415bd0859f66390090b786bbb8f68c4b7f
Aleksi Kaistinen <aleksi.kaistinen@allu.io>
2020-12-21 15:02:55 +0200

    Add Search Input as core component

---
+++ packages/core/src/components/search-input/search-input.stories.js
@@ -0,0 +1,29 @@
+import './search-input.css';
+import '../../icons/icon.css';
+
+export default {
+  title: 'Components/Search input',
+  decorators: [(storyFn) => `<div style="max-width: 420px;">${storyFn()}</div>`],
+};
+
+export const Default = () => `
+  <div class="hds-search-input">
+    <label for="example" class="hds-search-input__label">Search</label>
+    <div class="hds-search-input__input-wrapper">
+      <input
+        id="example"
+        class="hds-search-input__input"
+        type="text"
+      />
+      <div class="hds-search-input__buttons">
+        <button
+          type="button"
+          aria-label="Search"
+          class="hds-search-input__button"
+        >
+          <i class="hds-icon hds-icon--search hds-icon--size-s" aria-hidden="true"></i>
+      </div>
+    </div>
+    <span class="hds-search-input__helper-text">Assistive text</span>
+  </div>
+`;
