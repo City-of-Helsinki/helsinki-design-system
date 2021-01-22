fe925a3464e809e3a46ec7ae2c81f673fcf6c54a
Aleksi Kaistinen <aleksi.kaistinen@allu.io>
2021-01-22 14:34:00 +0200

    Implement another version of TimeInput

--- packages/react/src/components/timeInput/TimeInput.stories.tsx
+++ packages/react/src/components/timeInput/TimeInput.stories.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 
 import { TimeInput } from './TimeInput';
+import { TimeInputAlternative } from './TimeInputAlternative';
 
 export default {
-  component: TimeInput,
+  component: TimeInputAlternative,
   title: 'Components/TimeInput',
+  decorators: [(storyFn) => <div style={{ maxWidth: '400px' }}>{storyFn()}</div>],
 };
 
 export const Example = () => {
   return <TimeInput id="time" label="Label" helperText="Assistive text" />;
+};
+
+export const Alternative = () => {
+  return <TimeInputAlternative id="time" label="Label" helperText="Assistive text" />;
 };
