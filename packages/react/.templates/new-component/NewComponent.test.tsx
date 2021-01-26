64bfadd8fdfcefb5acd04ebb28b98536b2408398
Aleksi Kaistinen <aleksi.kaistinen@allu.io>
2021-01-26 08:42:19 +0200

    Improve scaffolding script for react components
    - Use SCSS instead of CSS
    - Update storybook template to use args
    - Add axe accessibility test

--- packages/react/.templates/new-component/NewComponent.test.tsx
+++ packages/react/.templates/new-component/NewComponent.test.tsx
@@ -7,6 +7,11 @@ import { [-replace-name-capital-] } from './[-replace-name-capital-]';
 describe('<[-replace-name-capital-] /> spec', () => {
   it('renders the component', () => {
     const { asFragment } = render(<[-replace-name-capital-] />);
-    expect(asFragment()).toMatchSnapshot(); 
-    });
+    expect(asFragment()).toMatchSnapshot();
+  });
+  it('should not have basic accessibility issues', async () => {
+    const { container } = render(<[-replace-name-capital-] />);
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
 });
