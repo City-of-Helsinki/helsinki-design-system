a4db777519a957eb2d892278fed76597bc4f912d
Jaakko Nenonen <jaakko@codelicious.fi>
2021-03-08 21:11:32 +0200

    move Visible to internal components

--- packages/react/src/components/navigation/navigationSearch/NavigationSearch.tsx
+++ packages/react/src/components/navigation/navigationSearch/NavigationSearch.tsx
@@ -6,7 +6,7 @@ import { NavigationContext } from '../NavigationContext';
 import classNames from '../../../utils/classNames';
 import getIsElementFocused from '../../../utils/getIsElementFocused';
 import getIsElementBlurred from '../../../utils/getIsElementBlurred';
-import { Visible } from '../../visible';
+import { Visible } from '../../../internal/visible/Visible';
 
 export type NavigationSearchProps = {
   /**
