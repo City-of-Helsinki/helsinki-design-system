a47101d7ca85db7b49f5443fe1d70e2295a86b83
Mika Nevalainen <mn.nevalainen@gmail.com>
2021-03-08 11:02:01 +0200

    Require hoursLabel and minutesLabel props when using time-iput

--- packages/react/src/components/timeInput/TimeInput.stories.tsx
+++ packages/react/src/components/timeInput/TimeInput.stories.tsx
@@ -12,6 +12,8 @@ export default {
   args: {
     id: 'time',
     label: 'Label',
+    hoursLabel: 'hours',
+    minutesLabel: 'minutes',
     helperText: 'Assistive text',
     defaultValue: '00:00',
     disabled: false,
