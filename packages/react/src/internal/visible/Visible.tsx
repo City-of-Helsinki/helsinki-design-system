b7ee7cb81fd756495e45acf8ad60bcb15d152696
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2021-03-09 15:15:51 +0200

    Apply own classNames util

--- packages/react/src/internal/visible/Visible.tsx
+++ packages/react/src/internal/visible/Visible.tsx
@@ -1,6 +1,6 @@
-import classNames from 'classnames';
 import React from 'react';
 
+import classNames from '../../utils/classNames';
 import styles from './visible.module.scss';
 
 type Breakpoint = 's' | 'm' | 'l' | 'xl';
@@ -16,10 +16,11 @@ const capitalize = (str?: string) => str?.toUpperCase() || '';
 export const Visible: React.FC<Props> = ({ above, below, children, className }) => {
   return (
     <div
-      className={classNames(className, {
-        [styles[`above${capitalize(above)}`]]: above,
-        [styles[`below${capitalize(below)}`]]: below,
-      })}
+      className={classNames(
+        className,
+        above && styles[`above${capitalize(above)}`],
+        below && styles[`below${capitalize(below)}`],
+      )}
     >
       {children}
     </div>
