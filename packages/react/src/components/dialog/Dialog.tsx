6e543767a4ec38b6bcca58a572feb5aa170293ca
Mika Nevalainen <mn.nevalainen@gmail.com>
2021-03-18 17:42:49 +0200

    Make Dialog themeable

--- packages/react/src/components/dialog/Dialog.tsx
+++ packages/react/src/components/dialog/Dialog.tsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom';
 
 // import core base styles
 import 'hds-core';
-
+import styles from './Dialog.module.scss';
+import classNames from '../../utils/classNames';
+import { useTheme } from '../../hooks/useTheme';
 import { DialogActionButtons } from './dialogActionButtons/DialogActionButtons';
 import { DialogHeader } from './dialogHeader/DialogHeader';
 import { DialogContent } from './dialogContent/DialogContent';
-import styles from './Dialog.module.scss';
 
 export interface DialogCustomTheme {
   '--accent-line'?: string;
+  '--color-overlay'?: string;
+  '--min-width'?: string;
 }
 
 export type DialogProps = React.PropsWithChildren<{
@@ -35,12 +38,22 @@ export type DialogProps = React.PropsWithChildren<{
    */
   close?: () => void;
   /**
+   * Custom theme styles
+   */
+  theme?: DialogCustomTheme;
+  /**
+   * Additional class names to apply to the dialog.
+   */
+  className?: string;
+  /**
    * Children to render inside the dialog element.
    */
   children: ReactNode | ReactNodeArray;
 }>;
 
-export const Dialog = ({ id, isOpen, children, close, ...props }: DialogProps) => {
+export const Dialog = ({ id, isOpen, children, close, className, theme, ...props }: DialogProps) => {
+  const customThemeClass = useTheme<DialogCustomTheme>(styles.dialog, theme);
+
   const { 'aria-labelledby': ariaLabelledby, 'aria-describedby': ariaDescribedby } = props;
 
   const onClose = (): void => {
@@ -66,14 +79,18 @@ export const Dialog = ({ id, isOpen, children, close, ...props }: DialogProps) =
   });
 
   const DialogComponent = (): JSX.Element => (
-    <>
+    <div className={styles.dialogContainer}>
       <div className={styles.dialogBackdrop} />
-      <div role="dialog" aria-modal="true" id={id} className={styles.dialogWrapper} aria-labelledby={ariaLabelledby}>
-        <div className={styles.dialog} aria-describedby={ariaDescribedby}>
-          {children}
-        </div>
+      <div
+        role="dialog"
+        aria-modal="true"
+        id={id}
+        className={classNames(styles.dialog, customThemeClass, className)}
+        aria-labelledby={ariaLabelledby}
+      >
+        <div aria-describedby={ariaDescribedby}>{children}</div>
       </div>
-    </>
+    </div>
   );
 
   return isOpen ? ReactDOM.createPortal(<DialogComponent />, document.body) : null;
