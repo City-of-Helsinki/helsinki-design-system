c65358dc4568ba2f26035a491bfa0df15df45bca
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2021-03-19 17:48:56 +0200

    Add unit tests to NumberInput component and update test jsdom as required by the new tests

--- packages/react/src/components/numberInput/NumberInput.test.tsx
+++ packages/react/src/components/numberInput/NumberInput.test.tsx
@@ -1,17 +1,41 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { axe } from 'jest-axe';
 
 import { NumberInput } from './NumberInput';
 
+const numberInputProps = {
+  id: 'numberInputTestId',
+  label: 'Testi label number input',
+  plusStepButtonAriaLabel: 'Add 10 euros',
+  minusStepButtonAriaLabel: 'Decrease 10 euros',
+};
+
 describe('<NumberInput /> spec', () => {
   it('renders the component', () => {
-    const { asFragment } = render(<NumberInput />);
+    const { asFragment } = render(<NumberInput {...numberInputProps} />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+  it('renders the component with steppers', () => {
+    const { asFragment } = render(<NumberInput step={10} {...numberInputProps} />);
     expect(asFragment()).toMatchSnapshot();
   });
   it('should not have basic accessibility issues', async () => {
-    const { container } = render(<NumberInput />);
+    const { container } = render(<NumberInput step={10} {...numberInputProps} />);
     const results = await axe(container);
     expect(results).toHaveNoViolations();
+  });
+  it('should increase value correct amount when user clicks step up button', async () => {
+    render(<NumberInput defaultValue={10} step={10} {...numberInputProps} />);
+    userEvent.click(screen.getByRole('button', { name: 'Add 10 euros' }));
+    const numberInput = screen.getByLabelText('Testi label number input', { selector: 'input' });
+    expect(numberInput).toHaveValue(20);
+  });
+  it('should decrease value correct amount when user clicks step down button', async () => {
+    render(<NumberInput defaultValue={10} step={10} {...numberInputProps} />);
+    userEvent.click(screen.getByRole('button', { name: 'Decrease 10 euros' }));
+    const numberInput = screen.getByLabelText('Testi label number input', { selector: 'input' });
+    expect(numberInput).toHaveValue(0);
   });
 });
