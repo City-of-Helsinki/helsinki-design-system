9ab5777e1e9e780fd4356bef6a3620a42b615f2d
Mika Nevalainen <mn.nevalainen@gmail.com>
2021-03-23 12:53:58 +0200

    Add initial tests

--- packages/react/src/components/dialog/Dialog.test.tsx
+++ packages/react/src/components/dialog/Dialog.test.tsx
@@ -3,14 +3,65 @@ import { render } from '@testing-library/react';
 import { axe } from 'jest-axe';
 
 import { Dialog } from './Dialog';
+import { Button } from '../button/Button';
+import { IconAlertCircle } from '../../icons';
+
+// Because Dialog uses React's createPortal we need to provide a container for render
+const renderDialog = (dialogComponent: JSX.Element) => render(dialogComponent, { container: document.body });
 
 describe('<Dialog /> spec', () => {
+  const id = 'test-dialog-id';
+  const titleId = 'test-dialog-title-id';
+  const contentId = 'test-dialog-description-id';
+  const isOpen = true;
+  const close = () => false;
+
   it('renders the component', () => {
-    const { asFragment } = render(<Dialog />);
+    const { asFragment } = renderDialog(
+      <Dialog id={id} aria-labelledby={titleId} aria-describedby={contentId} isOpen={isOpen} close={close}>
+        <Dialog.Header id={titleId} title="Confirmation dialog" iconLeft={<IconAlertCircle aria-hidden="true" />} />
+        <Dialog.Content id={contentId}>
+          <p>Confirmation Dialog</p>
+        </Dialog.Content>
+        <Dialog.ActionButtons>
+          <Button
+            onClick={() => {
+              close();
+            }}
+          >
+            Continue
+          </Button>
+          <Button onClick={close} variant="secondary">
+            Cancel
+          </Button>
+        </Dialog.ActionButtons>
+      </Dialog>,
+    );
+
     expect(asFragment()).toMatchSnapshot();
   });
+
   it('should not have basic accessibility issues', async () => {
-    const { container } = render(<Dialog />);
+    const { container } = renderDialog(
+      <Dialog id={id} aria-labelledby={titleId} aria-describedby={contentId} isOpen={isOpen} close={close}>
+        <Dialog.Header id={titleId} title="Confirmation dialog" iconLeft={<IconAlertCircle aria-hidden="true" />} />
+        <Dialog.Content id={contentId}>
+          <p>Confirmation Dialog</p>
+        </Dialog.Content>
+        <Dialog.ActionButtons>
+          <Button
+            onClick={() => {
+              close();
+            }}
+          >
+            Continue
+          </Button>
+          <Button onClick={close} variant="secondary">
+            Cancel
+          </Button>
+        </Dialog.ActionButtons>
+      </Dialog>,
+    );
     const results = await axe(container);
     expect(results).toHaveNoViolations();
   });
