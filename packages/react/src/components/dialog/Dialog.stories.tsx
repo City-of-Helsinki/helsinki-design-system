502a34216dc6f1f24ee3ac7093ceea97cc8833b4
Mika Nevalainen <mn.nevalainen@gmail.com>
2021-03-26 17:41:26 +0200

    Add scrollable dialog story

--- packages/react/src/components/dialog/Dialog.stories.tsx
+++ packages/react/src/components/dialog/Dialog.stories.tsx
@@ -125,8 +125,96 @@ export const Confirmation = (args) => {
 Confirmation.storyName = 'Confirmation';
 
 Confirmation.args = {
-  id: 'delete-dialog',
-  'aria-labelledby': 'delete-dialog-title',
-  'aria-describedby': 'delete-dialog-description',
-  focusAfterCloseId: 'open-confimation-dialog-button',
+  id: 'confirmation-dialog',
+  'aria-labelledby': 'confirmation-dialog-title',
+  'aria-describedby': 'confirmation-dialog-description',
+  focusAfterCloseId: 'open-confirmation-dialog-button',
+};
+
+export const ScrollableConfirmation = (args) => {
+  const [open, setOpen] = useState<boolean>(false);
+  const close = () => setOpen(false);
+
+  return (
+    <>
+      <Button id={args.focusAfterCloseId} onClick={() => setOpen(true)}>
+        Open Confirmation Dialog
+      </Button>
+      <Dialog
+        id={args.id}
+        aria-labelledby={args['aria-labelledby']}
+        aria-describedby={args['aria-describedby']}
+        isOpen={open}
+        close={close}
+        focusAfterCloseId={args.focusAfterCloseId}
+        scrollable
+      >
+        <Dialog.Header
+          id={args['aria-labelledby']}
+          title="Confirm dialog"
+          iconLeft={<IconAlertCircle aria-hidden="true" />}
+        />
+        <Dialog.Content>
+          <h3 id={args['aria-describedby']}>Are you sure you want to continue?</h3>
+          <p>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem
+            aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni
+            dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor
+            sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore
+            magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis
+            suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in
+            ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas
+            nulla pariatur?
+          </p>
+
+          <p>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem
+            aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni
+            dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor
+            sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore
+            magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis
+            suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in
+            ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas
+            nulla pariatur?
+          </p>
+
+          <p>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem
+            aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni
+            dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor
+            sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore
+            magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis
+            suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in
+            ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas
+            nulla pariatur?
+          </p>
+        </Dialog.Content>
+        <Dialog.ActionButtons>
+          <Button
+            onClick={() => {
+              // Add confirm operations here
+              close();
+            }}
+          >
+            Confirm
+          </Button>
+          <Button onClick={close} variant="secondary">
+            Cancel
+          </Button>
+        </Dialog.ActionButtons>
+      </Dialog>
+    </>
+  );
+};
+
+ScrollableConfirmation.storyName = 'Scrollable confirmation';
+
+ScrollableConfirmation.args = {
+  id: 'confirmation-scrollable-dialog',
+  'aria-labelledby': 'confirmation-scrollable-title',
+  'aria-describedby': 'confirmation-scrollable-description',
+  focusAfterCloseId: 'open-confirmation-scrollable-dialog-button',
 };
