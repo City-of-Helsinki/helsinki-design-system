79f6886e07e9e86e2fbe4fa1f296a6a841a4fd34
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2021-04-01 14:30:06 +0300

    Phone input - initial implementation

---
+++ packages/react/src/components/phoneInput/PhoneInput.stories.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+
+import { PhoneInput } from './PhoneInput';
+import { Combobox } from '../dropdown/combobox';
+
+export default {
+  component: PhoneInput,
+  title: 'Components/PhoneInput',
+  decorators: [(storyFn) => <div style={{ maxWidth: '544px' }}>{storyFn()}</div>],
+  parameters: {
+    controls: { expanded: true },
+  },
+  args: {},
+};
+
+export const Default = (args) => <PhoneInput {...args} />;
+Default.args = {
+  id: 'Default',
+  helperText: 'Assistive text',
+  label: 'Label',
+};
+
+export const Disabled = (args) => <PhoneInput {...args} />;
+Disabled.storyName = 'Disabled';
+Disabled.args = {
+  id: 'Disabled',
+  helperText: 'Assistive text',
+  defaultValue: '0451234567',
+  label: 'Label for disabled',
+  disabled: true,
+};
+
+export const WithDefaultValue = (args) => <PhoneInput {...args} />;
+WithDefaultValue.storyName = 'With a default value';
+WithDefaultValue.args = {
+  id: 'WithDefaultValue',
+  defaultValue: '0451234567',
+  helperText: 'Assistive text',
+  label: 'Label for default valued phone input',
+};
+
+export const WithCountryCode = (args) => {
+  const options = [{ label: 'Finland (+358)' }, { label: 'UK (+46)' }];
+  return (
+    <div style={{ display: 'grid', gridTemplateColumns: '200px 328px', columnGap: '16px' }}>
+      <Combobox
+        id="hds-select-1"
+        label="With country code"
+        helper="Assistive text"
+        options={options}
+        defaultValue={{ label: 'Finland (+358)' }}
+        onBlur={() => action('onBlur')}
+        onChange={(change) => action('onChange')(change)}
+        onFocus={() => action('onFocus')}
+        toggleButtonAriaLabel="Choose country code"
+      />
+      <PhoneInput style={{ marginTop: 'auto', marginBottom: 'auto' }} {...args} />
+    </div>
+  );
+};
+
+WithCountryCode.storyName = 'With select countrycode';
+WithCountryCode.args = {
+  id: 'WithCountryCode',
+  defaultValue: '0451234567',
+};
---
+++ packages/react/src/components/phoneInput/PhoneInput.tsx
@@ -0,0 +1,109 @@
+import React, { useEffect, useRef } from 'react';
+import isFunction from 'lodash.isfunction';
+
+import 'hds-core';
+import { InputWrapper } from '../../internal/input-wrapper/InputWrapper';
+import { TextInputProps } from '../textInput';
+import textInputStyles from '../textInput/TextInput.module.css';
+import comboseAriaDescribedBy from '../../utils/comboseAriaDescribedBy';
+
+export type PhoneInputProps = Omit<
+  TextInputProps,
+  'buttonIcon' | 'buttonAriaLabel' | 'onButtonClick' | 'children' | 'label'
+> & {
+  /**
+   * The label for the input
+   */
+  label?: string;
+};
+
+export const PhoneInput = React.forwardRef<HTMLInputElement, PhoneInputProps>(
+  (
+    {
+      className = '',
+      disabled = false,
+      defaultValue,
+      errorText,
+      helperText,
+      hideLabel,
+      invalid,
+      id,
+      label,
+      labelText,
+      onChange = () => null,
+      required,
+      style,
+      successText,
+      tooltipLabel,
+      tooltipText,
+      tooltipButtonLabel,
+      type = 'tel',
+      ...rest
+    }: PhoneInputProps,
+    ref?: React.Ref<HTMLInputElement>,
+  ) => {
+    const wrapperProps = {
+      className,
+      errorText,
+      helperText,
+      hideLabel,
+      id,
+      invalid,
+      label,
+      labelText,
+      required,
+      style,
+      successText,
+      tooltipLabel,
+      tooltipText,
+      tooltipButtonLabel,
+    };
+
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    /**
+     * Merge props.ref to the internal ref. This is needed because we need the ref ourself and cannot rely on
+     * component user to provide it.
+     */
+    useEffect(() => {
+      if (ref) {
+        if (isFunction(ref)) {
+          (ref as (instance: HTMLInputElement) => void)(inputRef.current);
+        } else {
+          // eslint-disable-next-line no-param-reassign
+          (ref as React.MutableRefObject<HTMLInputElement>).current = inputRef.current;
+        }
+      }
+      inputRef.current.addEventListener('input', () => {
+        inputRef.current.value = inputRef.current.value.replace(/[^0-9+]/g, '');
+      });
+
+      return () => {
+        inputRef.current.removeEventListener('input', () => {
+          inputRef.current.value = inputRef.current.value.replace(/[^0-9+]/g, '');
+        });
+      };
+    }, [inputRef, ref]);
+
+    // Compose aria-describedby attribute
+    const ariaDescribedBy = comboseAriaDescribedBy(id, helperText, errorText, successText);
+
+    return (
+      <InputWrapper {...wrapperProps}>
+        <input
+          className={textInputStyles.input}
+          defaultValue={defaultValue}
+          disabled={disabled}
+          id={id}
+          onChange={onChange}
+          ref={inputRef}
+          required={required}
+          type={type}
+          aria-describedby={ariaDescribedBy.length > 0 ? ariaDescribedBy : null}
+          autoComplete="tel"
+          {...rest}
+        />
+      </InputWrapper>
+    );
+  },
+);
