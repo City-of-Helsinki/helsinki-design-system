f4f668f82629cc003e117614bcc9f303f278e56a
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2021-04-06 12:38:54 +0300

    Update phone input unit tests

--- packages/react/src/components/phoneInput/PhoneInput.test.tsx
+++ packages/react/src/components/phoneInput/PhoneInput.test.tsx
@@ -6,11 +6,11 @@ import { PhoneInput } from './PhoneInput';
 
 describe('<PhoneInput /> spec', () => {
   it('renders the component', () => {
-    const { asFragment } = render(<PhoneInput />);
+    const { asFragment } = render(<PhoneInput id="phone-input" label="test label" />);
     expect(asFragment()).toMatchSnapshot();
   });
   it('should not have basic accessibility issues', async () => {
-    const { container } = render(<PhoneInput />);
+    const { container } = render(<PhoneInput id="phone-input" label="test label" />);
     const results = await axe(container);
     expect(results).toHaveNoViolations();
   });
