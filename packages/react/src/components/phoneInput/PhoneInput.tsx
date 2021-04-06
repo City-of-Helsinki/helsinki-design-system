f7f87ef7d3f242a55f35faeb181ea7ac8b5b1efd
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2021-04-06 12:05:06 +0300

    Allow same characters as native input with type="tel"

--- packages/react/src/components/phoneInput/PhoneInput.tsx
+++ packages/react/src/components/phoneInput/PhoneInput.tsx
@@ -75,17 +75,6 @@ export const PhoneInput = React.forwardRef<HTMLInputElement, PhoneInputProps>(
           (ref as React.MutableRefObject<HTMLInputElement>).current = inputRef.current;
         }
       }
-      inputRef.current.addEventListener('input', () => {
-        inputRef.current.value = inputRef.current.value.replace(/[^0-9+]/g, '');
-      });
-
-      const currentRef = inputRef.current;
-
-      return () => {
-        currentRef.removeEventListener('input', () => {
-          currentRef.value = currentRef.value.replace(/[^0-9+]/g, '');
-        });
-      };
     }, [inputRef, ref]);
 
     // Compose aria-describedby attribute
