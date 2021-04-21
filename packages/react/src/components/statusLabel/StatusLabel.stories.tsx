17ce690a81bc5b82be81f8ad74cbd8a8f56512d6
Mika Nevalainen <mn.nevalainen@gmail.com>
2021-04-21 11:35:58 +0300

    Add icon support in StatusLabel component

--- packages/react/src/components/statusLabel/StatusLabel.stories.tsx
+++ packages/react/src/components/statusLabel/StatusLabel.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { radios, text, withKnobs } from '@storybook/addon-knobs';
 
 import { StatusLabel } from './StatusLabel';
+import { IconCheckCircle, IconInfoCircle } from '../../icons';
 
 export default {
   component: StatusLabel,
@@ -22,6 +23,32 @@ export const Alert = () => <StatusLabel type="alert">Alert</StatusLabel>;
 
 export const Error = () => <StatusLabel type="error">Error</StatusLabel>;
 
+export const Icons = () => (
+  <>
+    <StatusLabel iconLeft={<IconInfoCircle />}>Default</StatusLabel>
+    <br />
+    <br />
+    <StatusLabel type="info" iconLeft={<IconInfoCircle />}>
+      Info
+    </StatusLabel>
+    <br />
+    <br />
+    <StatusLabel type="success" iconLeft={<IconCheckCircle />}>
+      Success
+    </StatusLabel>
+    <br />
+    <br />
+    <StatusLabel type="alert" iconLeft={<IconInfoCircle />}>
+      Alert
+    </StatusLabel>
+    <br />
+    <br />
+    <StatusLabel type="error" iconLeft={<IconInfoCircle />}>
+      Error
+    </StatusLabel>
+  </>
+);
+
 export const Playground = () => {
   const label = text('Label', 'Status');
   const type = radios(
@@ -37,15 +64,4 @@ export const Playground = () => {
   );
 
   return <StatusLabel type={type}>{label}</StatusLabel>;
-};
-
-Playground.parameters = {
-  previewTabs: {
-    'storybook/docs/panel': {
-      hidden: true,
-    },
-  },
-  docs: {
-    disable: true,
-  },
 };
