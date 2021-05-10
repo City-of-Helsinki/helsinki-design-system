e8ad9e5c0097560cf006f52e14dbb2e34182de8a
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2021-05-10 15:43:49 +0300

    Initial unit tests for password input

--- packages/react/src/components/passwordInput/PasswordInput.test.tsx
+++ packages/react/src/components/passwordInput/PasswordInput.test.tsx
@@ -6,11 +6,30 @@ import { PasswordInput } from './PasswordInput';
 
 describe('<PasswordInput /> spec', () => {
   it('renders the component', () => {
-    const { asFragment } = render(<PasswordInput id="passwordInputId" label="passwordInput label" />);
+    const { asFragment } = render(
+      <PasswordInput
+        defaultValue="0451234567"
+        helperText="Assistive text"
+        id="passwordInputId"
+        label="passwordInput label"
+        revealPasswordButtonAriaLabel="Show password"
+        concealPasswordButtonAriaLabel="Hide password"
+      />,
+    );
     expect(asFragment()).toMatchSnapshot();
   });
+
   it('should not have basic accessibility issues', async () => {
-    const { container } = render(<PasswordInput id="passwordInputId" label="passwordInput label" />);
+    const { container } = render(
+      <PasswordInput
+        defaultValue="0451234567"
+        helperText="Assistive text"
+        id="passwordInputId"
+        label="passwordInput label"
+        revealPasswordButtonAriaLabel="Show password"
+        concealPasswordButtonAriaLabel="Hide password"
+      />,
+    );
     const results = await axe(container);
     expect(results).toHaveNoViolations();
   });
