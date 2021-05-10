63236b951eaa99c4892bd88d2766d262a669f4ea
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2021-05-10 12:17:44 +0300

    Add stories: external show password button, invalid, success and with autocomplete

--- packages/react/src/components/passwordInput/PasswordInput.stories.tsx
+++ packages/react/src/components/passwordInput/PasswordInput.stories.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { PasswordInput } from './PasswordInput';
+import { Button } from '../button';
+import { IconEye, IconEyeCrossed } from '../../icons';
 
 export default {
   component: PasswordInput,
   title: 'Components/PasswordInput',
-  decorators: [(storyFn) => <div style={{ maxWidth: '300px' }}>{storyFn()}</div>],
+  decorators: [(storyFn) => <div style={{ maxWidth: '400px' }}>{storyFn()}</div>],
   parameters: {
     controls: { expanded: true },
   },
@@ -17,6 +19,8 @@ Default.args = {
   id: 'Default',
   helperText: 'Assistive text',
   label: 'Label',
+  revealPasswordButtonAriaLabel: 'Show password',
+  concealPasswordButtonAriaLabel: 'Hide password',
 };
 
 export const Disabled = (args) => <PasswordInput {...args} />;
@@ -27,4 +31,63 @@ Disabled.args = {
   defaultValue: '0451234567',
   label: 'Label for disabled',
   disabled: true,
+  revealPasswordButtonAriaLabel: 'Show password',
+  concealPasswordButtonAriaLabel: 'Hide password',
 };
+export const AutoCompleteOn = (args) => <PasswordInput {...args} />;
+AutoCompleteOn.storyName = 'Autocomplete on';
+AutoCompleteOn.args = {
+  id: 'Autocomplete on',
+  helperText: 'Assistive text',
+  label: 'Label for autocomplete on',
+  autoComplete: 'on',
+  revealPasswordButtonAriaLabel: 'Show password',
+  concealPasswordButtonAriaLabel: 'Hide password',
+};
+
+export const Success = (args) => <PasswordInput {...args} />;
+Success.storyName = 'Success';
+Success.args = {
+  id: 'Success',
+  helperText: 'Assistive text',
+  label: 'Label for success input',
+  successText: 'Success text',
+  revealPasswordButtonAriaLabel: 'Show password',
+  concealPasswordButtonAriaLabel: 'Hide password',
+};
+
+export const Invalid = (args) => <PasswordInput {...args} />;
+Invalid.storyName = 'Invalid';
+Invalid.args = {
+  id: 'Error',
+  invalid: true,
+  helperText: 'Assistive text',
+  label: 'Label for invalid input',
+  errorText: 'Error text',
+  revealPasswordButtonAriaLabel: 'Show password',
+  concealPasswordButtonAriaLabel: 'Hide password',
+};
+
+export const WithExternalShowPasswordButton = () => {
+  const [revealPassword, setRevealPassword] = useState<boolean>(false);
+  return (
+    <div style={{ display: 'grid', gridTemplateColumns: '400px 200px', gap: '20px' }}>
+      <PasswordInput
+        label="Label for external show password button"
+        id="external-password-button"
+        includeShowPasswordButton={false}
+        helperText="Assistive text"
+        type={revealPassword ? 'text' : 'password'}
+      />
+      <Button
+        variant="supplementary"
+        onClick={() => setRevealPassword(!revealPassword)}
+        iconLeft={revealPassword ? <IconEyeCrossed aria-hidden /> : <IconEye aria-hidden />}
+      >
+        {revealPassword ? 'Hide password' : 'Show password'}
+      </Button>
+    </div>
+  );
+};
+
+WithExternalShowPasswordButton.storyName = 'With external show password button';
--- packages/react/src/components/passwordInput/PasswordInput.test.tsx
+++ packages/react/src/components/passwordInput/PasswordInput.test.tsx
@@ -6,11 +6,11 @@ import { PasswordInput } from './PasswordInput';
 
 describe('<PasswordInput /> spec', () => {
   it('renders the component', () => {
-    const { asFragment } = render(<PasswordInput id="passwordInput id" label="passwordInput label" />);
+    const { asFragment } = render(<PasswordInput id="passwordInputId" label="passwordInput label" />);
     expect(asFragment()).toMatchSnapshot();
   });
   it('should not have basic accessibility issues', async () => {
-    const { container } = render(<PasswordInput id="passwordInput id" label="passwordInput label" />);
+    const { container } = render(<PasswordInput id="passwordInputId" label="passwordInput label" />);
     const results = await axe(container);
     expect(results).toHaveNoViolations();
   });
