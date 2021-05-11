db1b79042f7dd0a6fd9d5c73d426bc45b3bda70f
Mika Nevalainen <mn.nevalainen@gmail.com>
2021-05-11 14:58:29 +0300

    Add extra weeks when needed to ensure datepicker months have equal height

--- packages/react/src/components/dateInput/components/monthTable/MonthTable.tsx
+++ packages/react/src/components/dateInput/components/monthTable/MonthTable.tsx
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react';
+import getWeeksInMonth from 'date-fns/getWeeksInMonth';
 import eachWeekOfInterval from 'date-fns/eachWeekOfInterval';
+import add from 'date-fns/add';
 import endOfMonth from 'date-fns/endOfMonth';
 import startOfMonth from 'date-fns/startOfMonth';
 
@@ -15,10 +17,15 @@ export type MonthTableProps = {
 
 export const MonthTable = (props: MonthTableProps) => {
   const { locale } = useContext(DatePickerContext);
-  const { month } = props;
-
-  const weeksInterval = { start: startOfMonth(month), end: endOfMonth(month) };
-  const weeks = eachWeekOfInterval(weeksInterval, { locale });
+  const { month }: { month: Date } = props;
+  const start: Date = startOfMonth(month);
+  const end: Date = endOfMonth(month);
+  const monthWeeks: number = getWeeksInMonth(month, { weekStartsOn: 1 });
+  const amountOfVisibleWeeks = 6;
+  const weeksEnd: Date =
+    monthWeeks === amountOfVisibleWeeks ? end : add(end, { weeks: amountOfVisibleWeeks - monthWeeks }); // To ensure equal height month tables, we need to add extra week rows at the end of the table.
+  const weeksInterval: { start: Date; end: Date } = { start, end: weeksEnd };
+  const weeks: Date[] = eachWeekOfInterval(weeksInterval, { locale });
 
   return (
     <div>
