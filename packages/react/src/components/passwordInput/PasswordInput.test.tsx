119045b63d90b3e8e2bdf7b6463c5d5deb1db3b3
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2021-05-18 12:20:42 +0300

    Add unit test for toggle password button functionality

--- packages/react/src/components/passwordInput/PasswordInput.test.tsx
+++ packages/react/src/components/passwordInput/PasswordInput.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
 import { axe } from 'jest-axe';
 
 import { PasswordInput } from './PasswordInput';
@@ -32,5 +33,42 @@ describe('<PasswordInput /> spec', () => {
     );
     const results = await axe(container);
     expect(results).toHaveNoViolations();
+  });
+
+  it('should change input type when show password button is clicked', async () => {
+    const { container } = render(
+      <PasswordInput
+        data-test="password-input"
+        defaultValue="0451234567"
+        helperText="Assistive text"
+        id="passwordInputId"
+        label="passwordInput label"
+        revealPasswordButtonAriaLabel="Show password"
+        concealPasswordButtonAriaLabel="Hide password"
+      />,
+    );
+    expect(container.querySelector('[data-test="password-input"]').type).toBe('password');
+
+    await act(async () => {
+      const showPasswordButton = screen.getByLabelText('Show password');
+
+      if (!showPasswordButton) {
+        throw new Error('showPasswordButton not found');
+      }
+      await fireEvent.click(showPasswordButton);
+    });
+
+    expect(container.querySelector('[data-test="password-input"]').type).toBe('text');
+
+    await act(async () => {
+      const hidePasswordButton = screen.getByLabelText('Hide password');
+
+      if (!hidePasswordButton) {
+        throw new Error('hidePasswordButton not found');
+      }
+      await fireEvent.click(hidePasswordButton);
+    });
+
+    expect(container.querySelector('[data-test="password-input"]').type).toBe('password');
   });
 });
