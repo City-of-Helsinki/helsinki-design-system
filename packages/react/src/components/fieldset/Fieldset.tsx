6b9d2df65e7efc0c7a6c38d0f45f33fe34744aa1
Mika Nevalainen <mn.nevalainen@gmail.com>
2021-06-01 14:54:20 +0300

    Add initial Fieldset component

---
+++ packages/react/src/components/fieldset/Fieldset.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+// import core base styles
+import 'hds-core';
+import styles from './Fieldset.module.scss';
+import classNames from '../../utils/classNames';
+
+export type FieldsetProps = React.PropsWithChildren<{
+  /**
+   * Heading text inside legend element
+   */
+  heading: string;
+  /**
+   * If `true` border will be drawn around the fieldset.
+   */
+  border?: boolean;
+  /**
+   * Additional class names to apply to the card.
+   */
+  className?: string;
+}>;
+
+export const Fieldset = ({ heading, border, className, children }: FieldsetProps) => (
+  <fieldset className={classNames(styles.fieldset, border && styles.border, className)}>
+    <legend className={styles.legend}>{heading}</legend>
+    {children}
+  </fieldset>
+);
