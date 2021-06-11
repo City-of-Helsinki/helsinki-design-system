2a8cea2a72a40f5fc33605cacf6ec7a754a19551
Mika Nevalainen <mn.nevalainen@gmail.com>
2021-06-11 10:43:16 +0300

    Add toggle-button functional tests

--- packages/react/src/components/toggleButton/ToggleButton.test.tsx
+++ packages/react/src/components/toggleButton/ToggleButton.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { axe } from 'jest-axe';
 
 import { ToggleButton } from './ToggleButton';
@@ -14,5 +15,19 @@ describe('<ToggleButton /> spec', () => {
     const { container } = render(<ToggleButton id="toggle-button" value={false} label="Test label" />);
     const results = await axe(container);
     expect(results).toHaveNoViolations();
+  });
+
+  it('should set pressed true when user clicks the button', async () => {
+    render(<ToggleButton id="toggle-button" value={false} label="Test label" />);
+    const button = screen.getByRole('button', { pressed: false });
+    userEvent.click(button);
+    expect(button).toHaveAttribute('aria-pressed', 'true');
+  });
+
+  it('should set pressed false when user clicks the button', async () => {
+    render(<ToggleButton id="toggle-button" value label="Test label" />);
+    const button = screen.getByRole('button', { pressed: true });
+    userEvent.click(button);
+    expect(button).toHaveAttribute('aria-pressed', 'false');
   });
 });
