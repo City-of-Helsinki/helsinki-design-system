6d10b6311a0a31273127b1d7756cd3cd068718cb
Mika Nevalainen <mn.nevalainen@gmail.com>
2021-06-14 12:28:18 +0300

    Fix test

--- packages/react/src/components/toggleButton/ToggleButton.test.tsx
+++ packages/react/src/components/toggleButton/ToggleButton.test.tsx
@@ -7,27 +7,50 @@ import { ToggleButton } from './ToggleButton';
 
 describe('<ToggleButton /> spec', () => {
   it('renders the component', () => {
-    const { asFragment } = render(<ToggleButton id="toggle-button" value={false} label="Test label" />);
+    let checked = false;
+    const { asFragment } = render(
+      <ToggleButton
+        id="toggle-button"
+        checked={checked}
+        onChange={() => {
+          checked = !checked;
+        }}
+        label="Test label"
+      />,
+    );
     expect(asFragment()).toMatchSnapshot();
   });
 
   it('should not have basic accessibility issues', async () => {
-    const { container } = render(<ToggleButton id="toggle-button" value={false} label="Test label" />);
+    let checked = false;
+    const { container } = render(
+      <ToggleButton
+        id="toggle-button"
+        checked={checked}
+        onChange={() => {
+          checked = !checked;
+        }}
+        label="Test label"
+      />,
+    );
     const results = await axe(container);
     expect(results).toHaveNoViolations();
   });
 
-  it('should set pressed true when user clicks the button', async () => {
-    render(<ToggleButton id="toggle-button" value={false} label="Test label" />);
+  it('should call onChange on click', async () => {
+    let checked = false;
+    render(
+      <ToggleButton
+        id="toggle-button"
+        checked={checked}
+        onChange={() => {
+          checked = !checked;
+        }}
+        label="Test label"
+      />,
+    );
     const button = screen.getByRole('button', { pressed: false });
     userEvent.click(button);
-    expect(button).toHaveAttribute('aria-pressed', 'true');
-  });
-
-  it('should set pressed false when user clicks the button', async () => {
-    render(<ToggleButton id="toggle-button" value label="Test label" />);
-    const button = screen.getByRole('button', { pressed: true });
-    userEvent.click(button);
-    expect(button).toHaveAttribute('aria-pressed', 'false');
+    expect(checked).toBeTruthy();
   });
 });
