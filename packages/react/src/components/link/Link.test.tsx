846ddf55a23518bc71f1d31f66b1479ee6aca176
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2021-06-17 11:40:23 +0300

    Initial implementation of hds link component

---
+++ packages/react/src/components/link/Link.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { axe } from 'jest-axe';
+
+import { Link } from './Link';
+
+describe('<Link /> spec', () => {
+  it('renders the component', () => {
+    const { asFragment } = render(<Link href="/test">Test link</Link>);
+    expect(asFragment()).toMatchSnapshot();
+  });
+  it('should not have basic accessibility issues', async () => {
+    const { container } = render(<Link href="/test">Test link</Link>);
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
+});
