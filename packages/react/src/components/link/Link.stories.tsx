2f2cdecc06c0277a2dfe5811217bb2a1176c0ad0
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2021-06-17 12:16:48 +0300

    Fix story paragraphs font size

--- packages/react/src/components/link/Link.stories.tsx
+++ packages/react/src/components/link/Link.stories.tsx
@@ -97,7 +97,7 @@ visitedStylesDisabled.args = {
 
 export const inlineLink = (args) => {
   return (
-    <p>
+    <p style={{ fontSize: '14px' }}>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
       magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
       <Link {...args}>Inline link</Link>
@@ -115,7 +115,7 @@ export const standaloneLink = (args) => {
       <Link {...args} size="L" style={{ display: 'block', marginBottom: '20px' }}>
         Standalone link
       </Link>
-      <p style={{ display: 'inline' }}>
+      <p style={{ display: 'inline', fontSize: '14px' }}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
         magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
         consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
