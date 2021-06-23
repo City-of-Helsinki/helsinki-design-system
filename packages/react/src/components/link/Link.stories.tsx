f122ba57d1728f217bcda9be0169d4f659c52a39
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2021-06-23 18:28:53 +0300

    Remove paragraph from standalone link story

--- packages/react/src/components/link/Link.stories.tsx
+++ packages/react/src/components/link/Link.stories.tsx
@@ -132,18 +132,9 @@ inlineLinks.argTypes = {
 
 export const standaloneLink = (args) => {
   return (
-    <>
-      <Link {...args} size="L" style={{ display: 'block', marginBottom: '20px', width: 'fit-content' }}>
-        Standalone link
-      </Link>
-      <p style={{ display: 'inline', fontSize: '14px' }}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-        laborum.
-      </p>
-    </>
+    <Link {...args} size="L" style={{ display: 'block', marginBottom: '20px', width: 'fit-content' }}>
+      Standalone link
+    </Link>
   );
 };
 
