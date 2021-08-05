ef6827ba50d802b43899efbb82b987cf40bdc164
Mika Nevalainen <mn.nevalainen@gmail.com>
2021-08-05 12:07:54 +0300

    Update breaking-changes component styles and html

--- site/src/components/BreakingChangesComments.js
+++ site/src/components/BreakingChangesComments.js
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import ReactMarkdown from 'react-markdown';
-import { format, parseJSON } from 'date-fns'
+import { format, parseJSON } from 'date-fns';
+
+const DateText = ({ label, date }) => (
+  <p style={{ fontStyle: 'italic' }}>
+    {label}: <time dateTime={date}>{format(parseJSON(date), 'd.M.yyyy')}</time>
+  </p>
+);
 
 const BreakingChangesComments = () => {
   const [comments, setComments] = useState([]);
@@ -14,17 +20,17 @@ const BreakingChangesComments = () => {
   }, []);
 
   return (
-    <ul style={{ margin: '2rem 1rem 0', padding: '0px' }}>
-      {comments.map(
-        (comment) =>
-          console.log(parseJSON(comment.updated_at)) || (
-            <li key={comment.id} style={{ margin: '0 0 3rem', padding: '0px' }}>
-              <ReactMarkdown>{comment.body && comment.body}</ReactMarkdown>
-              <p><time dateTime={comment.updated_at}>{format(parseJSON(comment.updated_at),  'd.M.yyyy')}</time>
-              </p>
-            </li>
-          ),
-      )}
+    <ul style={{ margin: 'var(--spacing-xl) var(--spacing-l) 0', padding: '0' }}>
+      {comments.map((comment) => (
+        <li key={comment.id} style={{ margin: '0 0 var(--spacing-2-xl)', padding: '0' }}>
+          <ReactMarkdown>{comment.body && comment.body}</ReactMarkdown>
+          {comment.created_at === comment.updated_at ? (
+            <DateText label="Published" date={comment.created_at} />
+          ) : (
+            <DateText label="Updated" date={comment.updated_at} />
+          )}
+        </li>
+      ))}
     </ul>
   );
 };
