e64242338525b600b102f8f0eeaff015f8a6264b
Mika Nevalainen <mn.nevalainen@gmail.com>
2021-08-10 11:49:55 +0300

    Warn if languageSelector item is missing lang-attribute

--- packages/react/src/components/navigation/navigationLanguageSelector/NavigationLanguageSelector.tsx
+++ packages/react/src/components/navigation/navigationLanguageSelector/NavigationLanguageSelector.tsx
@@ -3,9 +3,19 @@ import React from 'react';
 import styles from './NavigationLanguageSelector.module.scss';
 import { MenuButton, MenuButtonProps } from '../../../internal/menuButton/MenuButton';
 
-export const NavigationLanguageSelector = ({ children, id = 'languageSelector', label, ...rest }: MenuButtonProps) => (
-  <MenuButton className={styles.languageDropdown} id={id} label={label} menuOffset={10} {...rest} closeOnItemClick>
-    {children}
-  </MenuButton>
-);
+export const NavigationLanguageSelector = ({ children, id = 'languageSelector', label, ...rest }: MenuButtonProps) => {
+  React.useEffect(() => {
+    React.Children.forEach(children, (child:React.ReactElement) => {
+      if (!child.props.lang) {
+        console.warn(`NavigationLanguageSelector item "${child.props.label}" is missing a lang property.`);
+      }
+    });
+  });
+
+  return (
+    <MenuButton className={styles.languageDropdown} id={id} label={label} menuOffset={10} {...rest} closeOnItemClick>
+      {children}
+    </MenuButton>
+  );
+};
 NavigationLanguageSelector.componentName = 'NavigationLanguageSelector';
