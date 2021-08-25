b30cda05d18b0dd2e5a24212da89efb53bf83178
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2021-08-25 16:18:03 +0300

    Update empty linkbox story examples to align with design

--- packages/react/src/components/linkbox/LinkboxEmpty.stories.tsx
+++ packages/react/src/components/linkbox/LinkboxEmpty.stories.tsx
@@ -5,7 +5,7 @@ import { Linkbox } from './Linkbox';
 export default {
   component: Linkbox,
   title: 'Components/Linkbox/Empty for custom content',
-  decorators: [(storyFn) => <div style={{ maxWidth: '320px' }}>{storyFn()}</div>],
+  decorators: [(storyFn) => <div style={{ maxWidth: '400px' }}>{storyFn()}</div>],
   parameters: {
     backgrounds: {
       default: 'gray',
@@ -16,37 +16,57 @@ export default {
 };
 
 export const Default = () => (
-  <Linkbox ariaLabel="Linkkiruutu: HDS" href="https://hds.hel.fi">
-    <div style={{ height: '106px' }} />
-  </Linkbox>
+  <div style={{ width: '320px' }}>
+    <Linkbox ariaLabel="Linkkiruutu: HDS" href="https://hds.hel.fi">
+      <div style={{ height: '224px' }} />
+    </Linkbox>
+  </div>
 );
 
 export const WithBorder = () => (
-  <Linkbox ariaLabel="Linkkiruutu: HDS" href="https://hds.hel.fi" withBorder>
-    <div style={{ height: '106px' }} />
-  </Linkbox>
+  <div style={{ width: '320px' }}>
+    <Linkbox ariaLabel="Linkkiruutu: HDS" href="https://hds.hel.fi" withBorder>
+      <div style={{ height: '224px' }} />
+    </Linkbox>
+  </div>
 );
 
 export const WithoutBackground = () => (
-  <Linkbox ariaLabel="Linkkiruutu: HDS" href="https://hds.hel.fi" noBackground>
-    <div style={{ height: '106px' }} />
-  </Linkbox>
+  <div style={{ width: '320px' }}>
+    <Linkbox ariaLabel="Linkkiruutu: HDS" href="https://hds.hel.fi" noBackground>
+      <div style={{ height: '224px' }} />
+    </Linkbox>
+  </div>
 );
 
 export const External = () => (
-  <Linkbox ariaLabel="Linkkiruutu: HDS" href="https://hds.hel.fi" external openInNewTab>
-    <div style={{ height: '106px' }} />
-  </Linkbox>
+  <div style={{ width: '320px' }}>
+    <Linkbox ariaLabel="Linkkiruutu: HDS" href="https://hds.hel.fi" external openInNewTab>
+      <div style={{ height: '224px' }} />
+    </Linkbox>
+  </div>
 );
 
 export const SmallSize = () => (
-  <Linkbox ariaLabel="Linkkiruutu: HDS" href="https://hds.hel.fi" size="small">
-    <div style={{ height: '106px' }} />
-  </Linkbox>
+  <div style={{ width: '288px' }}>
+    <Linkbox ariaLabel="Linkkiruutu: HDS" href="https://hds.hel.fi" size="small">
+      <div style={{ height: '192px' }} />
+    </Linkbox>
+  </div>
+);
+
+export const MediumSize = () => (
+  <div style={{ width: '320px' }}>
+    <Linkbox ariaLabel="Linkkiruutu: HDS" href="https://hds.hel.fi" size="medium">
+      <div style={{ height: '224px' }} />
+    </Linkbox>
+  </div>
 );
 
 export const LargeSize = () => (
-  <Linkbox ariaLabel="Linkkiruutu: HDS" href="https://hds.hel.fi" size="large">
-    <div style={{ height: '106px' }} />
-  </Linkbox>
+  <div style={{ width: '400px' }}>
+    <Linkbox ariaLabel="Linkkiruutu: HDS" href="https://hds.hel.fi" size="large">
+      <div style={{ height: '296px' }} />
+    </Linkbox>
+  </div>
 );
