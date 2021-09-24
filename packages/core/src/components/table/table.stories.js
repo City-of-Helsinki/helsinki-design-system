6cc6375d40dc6a348ad6de9b3316747cc6bccfe3
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2021-09-24 15:55:32 +0300

    Add missing combination; Light with vertical headers and zebra rows

--- packages/core/src/components/table/table.stories.js
+++ packages/core/src/components/table/table.stories.js
@@ -328,6 +328,60 @@ export const VerticalHeaderZebra = () => `
 	</div>
 `;
 
+export const VerticalHeaderZebraLight = () => `
+  <div class="hds-table-container" style="max-width: 486px">
+    <table class="hds-table hds-table--zebra hds-table--light hds-table--with-vertical-header">
+			<colgroup>
+				<col span="1" class="hds-table__vertical-header-column">
+			</colgroup>
+    	<tr class="hds-table__header-row">
+				<th></th>
+				<th scope="col">8-12</th>
+				<th scope="col">12-14</th>
+				<th scope="col">14-16</th>
+				<th scope="col">16-18</th>
+    	</tr>
+			<tbody class="hds-table__content hds-table__content--text-align-td-right">
+    		<tr>
+					<th scope="row">Monday</th>
+					<td>324</td>
+					<td>562</td>
+					<td>280</td>
+					<td>94</td>
+				</tr>
+    		<tr>
+					<th scope="row">Tuesday</th>
+					<td>341</td>
+					<td>688</td>
+					<td>425</td>
+					<td>113</td>
+				</tr>
+				<tr>
+					<th scope="row">Wednesday</th>
+					<td>294</td>
+					<td>492</td>
+					<td>280</td>
+					<td>67</td>
+				</tr>
+				<tr>
+					<th scope="row">Thursday</th>
+					<td>312</td>
+					<td>501</td>
+					<td>455</td>
+					<td>112</td>
+				</tr>
+				<tr>
+					<th scope="row">Friday</th>
+					<td>150</td>
+					<td>142</td>
+					<td>362</td>
+					<td>455</td>
+				</tr>
+			</tbody>
+  	</table>
+	</div>
+`;
+
 export const VerticalHeaderAndLines = () => `
   <div class="hds-table-container" style="max-width: 486px">
     <table class="hds-table hds-table--with-vertical-header hds-table--with-vertical-lines">
