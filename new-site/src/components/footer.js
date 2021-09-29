62fa15a098901de44a1428bc36d8095436bb7929
Mika Nevalainen <mn.nevalainen@gmail.com>
2021-09-29 12:05:13 +0300

    Add footer

---
+++ new-site/src/components/footer.js
@@ -0,0 +1,36 @@
+import * as React from "react"
+import PropTypes from "prop-types"
+import { Footer } from "hds-react"
+
+const PageFooter = ({
+  title,
+  footerTitle,
+  footerAriaLabel,
+  footerCopyrightLinks,
+}) => (
+  <Footer title={footerTitle || title} footerAriaLabel={footerAriaLabel}>
+    <Footer.Base copyrightHolder="Copyright">
+      {footerCopyrightLinks &&
+        footerCopyrightLinks.map(({ label, href }) => (
+          <Footer.Item label={label} href={href} />
+        ))}
+    </Footer.Base>
+  </Footer>
+)
+
+PageFooter.propTypes = {
+  footerTitle: PropTypes.string,
+  footerAriaLabel: PropTypes.string,
+  footerCopyrightLinks: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string,
+      href: PropTypes.string,
+    })
+  ),
+}
+
+PageFooter.defaultProps = {
+  title: `Helsinki Design System`,
+}
+
+export default PageFooter
