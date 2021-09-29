0a4e21c9836c6f07ddb02a1a48de29c5ec43f0d0
Mika Nevalainen <mn.nevalainen@gmail.com>
2021-09-29 10:51:04 +0300

    Use HDS Container as page content element

--- new-site/src/components/layout.js
+++ new-site/src/components/layout.js
@@ -8,6 +8,7 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import { Container } from 'hds-react';
 
 import Header from "./header"
 import "./layout.css"
@@ -26,24 +27,9 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      <Container id="content" style={{ margin: '0 auto'}}>
         <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
+      </Container>
     </>
   )
 }
