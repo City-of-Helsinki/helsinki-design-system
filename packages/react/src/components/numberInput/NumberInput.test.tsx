5824a641623f33ae29ee410754a2fc55864c4f44
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2021-10-05 10:55:08 +0300

    Allow reseting number input to an empty value

--- packages/react/src/components/numberInput/NumberInput.test.tsx
+++ packages/react/src/components/numberInput/NumberInput.test.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { axe } from 'jest-axe';
@@ -71,5 +71,31 @@ describe('<NumberInput /> spec', () => {
       userEvent.click(screen.getByRole('button', { name: 'Decrease 10 euros' }));
     });
     expect(onChange.mock.calls.length).toBe(1);
+  });
+
+  it'should be able to reset a controlled NumberInput to an empty value', async () => {
+    const TestComponent = () => {
+      const [value, setValue] = useState<'' | number>(10);
+      return (
+        <div>
+          <button
+            type="button"
+            onClick={() => {
+              setValue('');
+            }}
+          >
+            Reset number input
+          </button>
+          <NumberInput step={10} value={value} {...numberInputProps} />
+        </div>
+      );
+    };
+
+    render(<TestComponent />);
+    await act(async () => {
+      userEvent.click(screen.getByRole('button', { name: 'Reset number input' }));
+    });
+    const numberInput = screen.getByLabelText('Test label number input', { selector: 'input' });
+    expect(numberInput).toHaveValue(null); // Empty string is converted to null by react/html
   });
 });
