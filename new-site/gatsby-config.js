f47de218022c9aa6c374ff97e4abcc57239728d4
Mika Nevalainen <mn.nevalainen@gmail.com>
2021-10-08 17:27:09 +0300

    Create placeholder mdx-pages

--- new-site/gatsby-config.js
+++ new-site/gatsby-config.js
@@ -22,6 +22,28 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `docs`,
+        path: `${__dirname}/src/docs/`,
+      },
+    },
+    // This config is need if pages are are somewhere else than in pages folder.
+    {
+      resolve: "gatsby-plugin-page-creator",
+      options: {
+        path: `${__dirname}/src/docs`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        defaultLayouts: {
+          default: require.resolve("./src/components/layout.js"),
+        }
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
