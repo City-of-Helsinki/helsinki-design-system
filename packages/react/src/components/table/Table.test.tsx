1839ca573b666002dc5811d74f3a3359cf98d5c1
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2021-11-10 16:42:18 +0200

    Add initial unit tests for table

--- packages/react/src/components/table/Table.test.tsx
+++ packages/react/src/components/table/Table.test.tsx
@@ -1,13 +1,55 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import { axe } from 'jest-axe';
+
+import { DataTable } from './DataTable';
+
 describe('<Table /> spec', () => {
+  let cellConfig;
+  let rows;
+  let caption;
+
+  beforeEach(() => {
+    cellConfig = {
+      cols: [
+        { key: 'id', headerName: 'Not rendered' },
+        { key: 'firstName', headerName: 'First name' },
+        { key: 'surname', headerName: 'Surname' },
+        {
+          key: 'age',
+          headerName: 'Age',
+          transform: ({ age }) => {
+            return <div style={{ textAlign: 'right' }}>{age}</div>;
+          },
+        },
+        { key: 'profession', headerName: 'Profession' },
+      ],
+      indexKey: 'id',
+      renderIndexCol: false,
+    };
+
+    rows = [
+      { id: 1000, firstName: 'Lauri', surname: 'Kekkonen', age: 39, profession: 'Engineer' },
+      { id: 1001, firstName: 'Maria', surname: 'Sarasoja', age: 62, profession: 'Designer' },
+      { id: 1002, firstName: 'Anneli', surname: 'Routa', age: 50, profession: 'Meteorologist' },
+      { id: 1003, firstName: 'Osku', surname: 'Rausku', age: 18, profession: 'Mail Carrier' },
+    ];
+
+    caption = (
+      <span>
+        <b>Table 1</b>: Table description
+      </span>
+    );
+  });
+
   it('renders the component', () => {
-    expect(true).toBe(true);
-    // const { asFragment } = render(<Table />);
-    // expect(asFragment()).toMatchSnapshot();
+    const { asFragment } = render(<DataTable caption={caption} cellConfig={cellConfig} rows={rows} />);
+    expect(asFragment()).toMatchSnapshot();
   });
   it('should not have basic accessibility issues', async () => {
     expect(true).toBe(true);
-    // const { container } = render(<Table />);
-    // const results = await axe(container);
-    // expect(results).toHaveNoViolations();
+    const { container } = render(<DataTable caption={caption} cellConfig={cellConfig} rows={rows} />);
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
   });
 });
