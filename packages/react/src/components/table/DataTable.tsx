7bed2c64581b777956639efd8ba652ef1a9c1805
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2021-11-15 13:32:55 +0200

    Remove unneeded forwardRef injection

--- packages/react/src/components/table/DataTable.tsx
+++ packages/react/src/components/table/DataTable.tsx
@@ -94,220 +94,218 @@ function processRows(rows, order, sorting, cellConfig) {
   });
 }
 
-export const DataTable = React.forwardRef(
-  ({
-    cellConfig,
-    checkboxSelection,
-    ariaLabelCheckboxSelection,
-    initiallySelectedRows,
+export const DataTable = ({
+  cellConfig,
+  checkboxSelection,
+  ariaLabelCheckboxSelection,
+  initiallySelectedRows,
+  rows,
+  variant = 'dark',
+  heading,
+  headingAriaLevel = 2,
+  dense = false,
+  zebra = false,
+  verticalLines = false,
+  verticalHeaders,
+  textAlignContentRight = false,
+  setSelections,
+  caption,
+  theme,
+  customActionButtons,
+  ...rest
+}: DataTableProps) => {
+  if (cellConfig.renderIndexCol === undefined) {
+    // eslint-disable-next-line no-param-reassign
+    cellConfig.renderIndexCol = true;
+  }
+  if (verticalHeaders && verticalHeaders.length && checkboxSelection) {
+    // eslint-disable-next-line no-console
+    console.warn(
+      'Incompatible props verticalHeaders and checkboxSelection provided. Cannot use checkboxSelection when verticalHeaders is applied',
+    );
+    // eslint-disable-next-line no-param-reassign
+    checkboxSelection = false;
+  }
+
+  if (checkboxSelection && caption) {
+    // eslint-disable-next-line no-console
+    console.warn('Cannot use caption prop when checkboxSelection is set to true. Use heading prop instead');
+    // eslint-disable-next-line no-param-reassign
+    caption = undefined;
+  }
+
+  const [sorting, setSorting] = useState<string>(cellConfig.initialSortingColumnKey);
+  const [order, setOrder] = useState<'asc' | 'desc' | undefined>(cellConfig.initialSortingOrder);
+  const [selectedRows, setSelectedRows] = useState<any[]>(initiallySelectedRows || []);
+  const customThemeClass = useTheme<TableCustomTheme>(variant === 'dark' ? styles.dark : styles.light, theme);
+
+  function selectAllRows() {
+    const allRows = rows.map((row) => {
+      return row[cellConfig.indexKey];
+    });
+    setSelectedRows(allRows);
+  }
+
+  function deSelectAllRows() {
+    setSelectedRows([]);
+  }
+
+  useEffect(() => {
+    if (setSelections) {
+      setSelections(selectedRows);
+    }
+  }, [setSelections, selectedRows]);
+
+  const setSortingAndOrder = (colKey: string): void => {
+    if (sorting === colKey) {
+      setOrder(order === 'desc' ? 'asc' : 'desc');
+    } else {
+      setOrder('asc');
+    }
+    setSorting(colKey);
+  };
+
+  const processedRows = useMemo(() => processRows(rows, order, sorting, cellConfig), [
     rows,
-    variant = 'dark',
-    heading,
-    headingAriaLevel = 2,
-    dense = false,
-    zebra = false,
-    verticalLines = false,
-    verticalHeaders,
-    textAlignContentRight = false,
-    setSelections,
-    caption,
-    theme,
-    customActionButtons,
-    ...rest
-  }: DataTableProps) => {
-    if (cellConfig.renderIndexCol === undefined) {
-      // eslint-disable-next-line no-param-reassign
-      cellConfig.renderIndexCol = true;
+    sorting,
+    order,
+    cellConfig,
+  ]);
+
+  const firstRenderedColumnKey = cellConfig.cols.find((column) => {
+    if (!cellConfig.renderIndexCol) {
+      return column.key !== cellConfig.indexKey;
     }
-    if (verticalHeaders && verticalHeaders.length && checkboxSelection) {
-      // eslint-disable-next-line no-console
-      console.warn(
-        'Incompatible props verticalHeaders and checkboxSelection provided. Cannot use checkboxSelection when verticalHeaders is applied',
-      );
-      // eslint-disable-next-line no-param-reassign
-      checkboxSelection = false;
-    }
+    return true;
+  }).key;
 
-    if (checkboxSelection && caption) {
-      // eslint-disable-next-line no-console
-      console.warn('Cannot use caption prop when checkboxSelection is set to true. Use heading prop instead');
-      // eslint-disable-next-line no-param-reassign
-      caption = undefined;
-    }
-
-    const [sorting, setSorting] = useState<string>(cellConfig.initialSortingColumnKey);
-    const [order, setOrder] = useState<'asc' | 'desc' | undefined>(cellConfig.initialSortingOrder);
-    const [selectedRows, setSelectedRows] = useState<any[]>(initiallySelectedRows || []);
-    const customThemeClass = useTheme<TableCustomTheme>(variant === 'dark' ? styles.dark : styles.light, theme);
-
-    function selectAllRows() {
-      const allRows = rows.map((row) => {
-        return row[cellConfig.indexKey];
-      });
-      setSelectedRows(allRows);
-    }
-
-    function deSelectAllRows() {
-      setSelectedRows([]);
-    }
-
-    useEffect(() => {
-      if (setSelections) {
-        setSelections(selectedRows);
-      }
-    }, [setSelections, selectedRows]);
-
-    const setSortingAndOrder = (colKey: string): void => {
-      if (sorting === colKey) {
-        setOrder(order === 'desc' ? 'asc' : 'desc');
-      } else {
-        setOrder('asc');
-      }
-      setSorting(colKey);
-    };
-
-    const processedRows = useMemo(() => processRows(rows, order, sorting, cellConfig), [
-      rows,
-      sorting,
-      order,
-      cellConfig,
-    ]);
-
-    const firstRenderedColumnKey = cellConfig.cols.find((column) => {
-      if (!cellConfig.renderIndexCol) {
-        return column.key !== cellConfig.indexKey;
-      }
-      return true;
-    }).key;
-
-    return (
-      <>
-        {(checkboxSelection || heading) && (
-          <div className={styles.actionContainer}>
-            {heading && (
-              <div role="heading" aria-level={headingAriaLevel} className={styles.heading}>
-                {heading}
-              </div>
-            )}
-            {(checkboxSelection || (customActionButtons && customActionButtons.length > 0)) && (
-              <div className={styles.actionButtonContainer}>
-                {checkboxSelection && (
-                  <>
-                    <Button
-                      onClick={() => {
-                        selectAllRows();
-                      }}
-                      variant="secondary"
-                      size="small"
-                      disabled={selectedRows.length === rows.length}
-                    >
-                      Select all rows
-                    </Button>
-                    <Button
-                      onClick={() => {
-                        deSelectAllRows();
-                      }}
-                      variant="secondary"
-                      size="small"
-                      disabled={selectedRows.length === 0}
-                    >
-                      Clear selections
-                    </Button>
-                  </>
-                )}
-                {customActionButtons &&
-                  customActionButtons.length > 0 &&
-                  customActionButtons.map((actionButton) => {
-                    return actionButton;
-                  })}
-              </div>
-            )}
-          </div>
-        )}
-        <Table
-          variant={variant}
-          dense={dense}
-          zebra={zebra}
-          verticalLines={verticalLines}
-          customThemeClass={customThemeClass}
-          {...rest}
-        >
-          {caption && <caption className={styles.caption}>{caption}</caption>}
-          {verticalHeaders && verticalHeaders.length && <Table.VerticalHeaderColGroup />}
-          <thead>
-            <Table.HeaderRow>
-              {verticalHeaders && verticalHeaders.length && <td role="presentation" />}
-              {checkboxSelection && <td className={styles.checkboxHeader} />}
-              {cellConfig.cols.map((column) => {
+  return (
+    <>
+      {(checkboxSelection || heading) && (
+        <div className={styles.actionContainer}>
+          {heading && (
+            <div role="heading" aria-level={headingAriaLevel} className={styles.heading}>
+              {heading}
+            </div>
+          )}
+          {(checkboxSelection || (customActionButtons && customActionButtons.length > 0)) && (
+            <div className={styles.actionButtonContainer}>
+              {checkboxSelection && (
+                <>
+                  <Button
+                    onClick={() => {
+                      selectAllRows();
+                    }}
+                    variant="secondary"
+                    size="small"
+                    disabled={selectedRows.length === rows.length}
+                  >
+                    Select all rows
+                  </Button>
+                  <Button
+                    onClick={() => {
+                      deSelectAllRows();
+                    }}
+                    variant="secondary"
+                    size="small"
+                    disabled={selectedRows.length === 0}
+                  >
+                    Clear selections
+                  </Button>
+                </>
+              )}
+              {customActionButtons &&
+                customActionButtons.length > 0 &&
+                customActionButtons.map((actionButton) => {
+                  return actionButton;
+                })}
+            </div>
+          )}
+        </div>
+      )}
+      <Table
+        variant={variant}
+        dense={dense}
+        zebra={zebra}
+        verticalLines={verticalLines}
+        customThemeClass={customThemeClass}
+        {...rest}
+      >
+        {caption && <caption className={styles.caption}>{caption}</caption>}
+        {verticalHeaders && verticalHeaders.length && <Table.VerticalHeaderColGroup />}
+        <thead>
+          <Table.HeaderRow>
+            {verticalHeaders && verticalHeaders.length && <td role="presentation" />}
+            {checkboxSelection && <td className={styles.checkboxHeader} />}
+            {cellConfig.cols.map((column) => {
+              if (column.key === cellConfig.indexKey && !cellConfig.renderIndexCol) {
+                return null;
+              }
+              if (cellConfig.sortingEnabled) {
+                return (
+                  <Table.SortingHeaderCell
+                    key={column.key}
+                    colKey={column.key}
+                    title={column.headerName}
+                    ariaLabelSortButtonNeutral={cellConfig.ariaLabelSortButtonNeutral}
+                    ariaLabelSortButtonAscending={cellConfig.ariaLabelSortButtonAscending}
+                    ariaLabelSortButtonDescending={cellConfig.ariaLabelSortButtonDescending}
+                    setSortingAndOrder={setSortingAndOrder}
+                    order={sorting === column.key ? order : 'neutral'}
+                    sortIconType={column.sortIconType}
+                  />
+                );
+              }
+              return (
+                <th key={column.key} scope="col">
+                  {column.headerName}
+                </th>
+              );
+            })}
+          </Table.HeaderRow>
+        </thead>
+        <Table.TableBody textAlignContentRight={textAlignContentRight}>
+          {processedRows.map((row, index) => (
+            <tr key={row[cellConfig.indexKey]}>
+              {verticalHeaders && verticalHeaders.length && <th scope="row">{verticalHeaders[index].headerName}</th>}
+              {checkboxSelection && (
+                <td className={styles.checkboxData}>
+                  <Checkbox
+                    checked={selectedRows.includes(row[cellConfig.indexKey])}
+                    id={row[cellConfig.indexKey]}
+                    label=""
+                    aria-label={`${ariaLabelCheckboxSelection || 'Row selection'} ${row[firstRenderedColumnKey]}`}
+                    onChange={(e) => {
+                      if (e.target.checked) {
+                        setSelectedRows([...selectedRows, row[cellConfig.indexKey]]);
+                      } else {
+                        const result = [
+                          ...selectedRows.filter((selectedRow) => selectedRow !== row[cellConfig.indexKey]),
+                        ];
+                        setSelectedRows(result);
+                      }
+                    }}
+                    className={styles.checkbox}
+                  />
+                </td>
+              )}
+              {cellConfig.cols.map((column, cellIndex) => {
                 if (column.key === cellConfig.indexKey && !cellConfig.renderIndexCol) {
                   return null;
                 }
-                if (cellConfig.sortingEnabled) {
-                  return (
-                    <Table.SortingHeaderCell
-                      key={column.key}
-                      colKey={column.key}
-                      title={column.headerName}
-                      ariaLabelSortButtonNeutral={cellConfig.ariaLabelSortButtonNeutral}
-                      ariaLabelSortButtonAscending={cellConfig.ariaLabelSortButtonAscending}
-                      ariaLabelSortButtonDescending={cellConfig.ariaLabelSortButtonDescending}
-                      setSortingAndOrder={setSortingAndOrder}
-                      order={sorting === column.key ? order : 'neutral'}
-                      sortIconType={column.sortIconType}
-                    />
-                  );
-                }
                 return (
-                  <th key={column.key} scope="col">
-                    {column.headerName}
-                  </th>
+                  <td
+                    key={cellIndex} // eslint-disable-line react/no-array-index-key
+                  >
+                    {column.transform && column.transform(row)}
+                    {!column.transform && row[column.key]}
+                  </td>
                 );
               })}
-            </Table.HeaderRow>
-          </thead>
-          <Table.TableBody textAlignContentRight={textAlignContentRight}>
-            {processedRows.map((row, index) => (
-              <tr key={row[cellConfig.indexKey]}>
-                {verticalHeaders && verticalHeaders.length && <th scope="row">{verticalHeaders[index].headerName}</th>}
-                {checkboxSelection && (
-                  <td className={styles.checkboxData}>
-                    <Checkbox
-                      checked={selectedRows.includes(row[cellConfig.indexKey])}
-                      id={row[cellConfig.indexKey]}
-                      label=""
-                      aria-label={`${ariaLabelCheckboxSelection || 'Row selection'} ${row[firstRenderedColumnKey]}`}
-                      onChange={(e) => {
-                        if (e.target.checked) {
-                          setSelectedRows([...selectedRows, row[cellConfig.indexKey]]);
-                        } else {
-                          const result = [
-                            ...selectedRows.filter((selectedRow) => selectedRow !== row[cellConfig.indexKey]),
-                          ];
-                          setSelectedRows(result);
-                        }
-                      }}
-                      className={styles.checkbox}
-                    />
-                  </td>
-                )}
-                {cellConfig.cols.map((column, cellIndex) => {
-                  if (column.key === cellConfig.indexKey && !cellConfig.renderIndexCol) {
-                    return null;
-                  }
-                  return (
-                    <td
-                      key={cellIndex} // eslint-disable-line react/no-array-index-key
-                    >
-                      {column.transform && column.transform(row)}
-                      {!column.transform && row[column.key]}
-                    </td>
-                  );
-                })}
-              </tr>
-            ))}
-          </Table.TableBody>
-        </Table>
-      </>
-    );
-  },
-);
+            </tr>
+          ))}
+        </Table.TableBody>
+      </Table>
+    </>
+  );
+};
