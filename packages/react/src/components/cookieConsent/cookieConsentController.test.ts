104ad023ef60a4908e8d5b5c8ef6f888256e6f57
NikoHelle <niko.helle@hyperactive.fi>
2021-11-17 15:44:53 +0200

    Created a controller for cookie consents
    The controller handles cookie read/write actions and stores consents until user saves them to a cookie.
    
    Created cookie is accessible from *.hel.fi and *.hel.ninja (and any *.domain.suffix).
    
    Common conset 'names' are declared so same consents can be used between services and not asked again.
    
    Cookie's "secure" -property is set to 'false' so it is readable and settable without https and therefore in localhost.
    
    This is not considered as a security risk because of the insensitive content of the cookie and 'sameSite' is anyway set to 'strict' so the cookie is not sent to third party domains.
    
    Added a package for cookie read/write and some lodash helpers
    
    Added tests and mocks.

---
+++ packages/react/src/components/cookieConsent/cookieConsentController.test.ts
@@ -0,0 +1,396 @@
+/* eslint-disable jest/no-mocks-import */
+import { createUniversalCookieMockHelpers } from './__mocks__/mockUniversalCookie';
+import mockWindowLocation, { MockedWindowLocationActions } from './__mocks__/mockWindowLocation';
+import createConsentController, {
+  ConsentController,
+  ConsentList,
+  ConsentObject,
+  createStorage,
+} from './cookieConsentController';
+
+const mockCookieHelpers = createUniversalCookieMockHelpers();
+
+jest.mock(
+  'universal-cookie',
+  () =>
+    function universalCookieMockClass() {
+      return mockCookieHelpers.createMockedModule();
+    },
+);
+
+describe(`cookieConsentController.ts`, () => {
+  let controller: ConsentController;
+  let mockedWindowControls: MockedWindowLocationActions;
+  afterEach(() => {
+    mockCookieHelpers.reset();
+  });
+  beforeAll(() => {
+    mockedWindowControls = mockWindowLocation();
+  });
+  afterAll(() => {
+    mockedWindowControls.restore();
+  });
+  const defaultControllerTestData = {
+    requiredConsents: ['requiredConsent1', 'requiredConsent2'],
+    optionalConsents: ['optionalConsent1', 'optionalConsent2'],
+    cookie: {},
+  };
+  const createControllerAndInitCookie = ({
+    requiredConsents,
+    optionalConsents,
+    cookie = {},
+  }: {
+    requiredConsents?: ConsentList;
+    optionalConsents?: ConsentList;
+    cookie?: ConsentObject;
+  }) => {
+    mockCookieHelpers.setStoredCookie(cookie);
+    controller = createConsentController({
+      requiredConsents,
+      optionalConsents,
+    });
+  };
+  describe('createConsentController', () => {
+    describe('initialization', () => {
+      it('parses requiredConsents and optionalConsents correctly', () => {
+        createControllerAndInitCookie(defaultControllerTestData);
+        expect(controller.getRequired()).toEqual({
+          requiredConsent1: false,
+          requiredConsent2: false,
+        });
+        expect(controller.getOptional()).toEqual({
+          optionalConsent1: false,
+          optionalConsent2: false,
+        });
+      });
+      it('parses and stores unknown consents', () => {
+        const cookieDataWithUnknownConsents = {
+          unknownConsent1: true,
+          unknownConsent2: false,
+          unknownConsent3: true,
+        };
+        const otherConsents = {
+          requiredConsent1: false,
+          requiredConsent2: false,
+          optionalConsent1: false,
+          optionalConsent2: false,
+        };
+        createControllerAndInitCookie({
+          ...defaultControllerTestData,
+          cookie: cookieDataWithUnknownConsents,
+        });
+        expect(controller.save()).toEqual({
+          ...cookieDataWithUnknownConsents,
+          ...otherConsents,
+        });
+      });
+      it('parses props correctly without consents', () => {
+        createControllerAndInitCookie({});
+        expect(controller.getRequired()).toEqual({});
+        expect(controller.getOptional()).toEqual({});
+      });
+      it('throws when same consent is both optional and required', () => {
+        expect(() =>
+          createConsentController({
+            requiredConsents: ['consent1', 'consent2'],
+            optionalConsents: ['consent2', 'consent3'],
+          }),
+        ).toThrow();
+      });
+      it('restores saved consents correctly', () => {
+        const storedCookieData = {
+          requiredConsent1: true,
+          optionalConsent2: true,
+          unknownConsent1: true,
+        };
+        const allConsents = {
+          ...storedCookieData,
+          requiredConsent2: false,
+          optionalConsent1: false,
+        };
+        createControllerAndInitCookie({
+          ...defaultControllerTestData,
+          cookie: storedCookieData,
+        });
+        expect(controller.getRequired()).toEqual({
+          requiredConsent1: allConsents.requiredConsent1,
+          requiredConsent2: allConsents.requiredConsent2,
+        });
+        expect(controller.getOptional()).toEqual({
+          optionalConsent1: allConsents.optionalConsent1,
+          optionalConsent2: allConsents.optionalConsent2,
+        });
+        expect(controller.save()).toEqual(allConsents);
+      });
+      it('cookie is only read on init', () => {
+        createControllerAndInitCookie({});
+        expect(mockCookieHelpers.mockGet).toHaveBeenCalledTimes(1);
+        expect(mockCookieHelpers.mockSet).toHaveBeenCalledTimes(0);
+      });
+    });
+    describe('unknown consents', () => {
+      it('are parsed, stored and never changed', () => {
+        const cookieDataWithUnknownConsents = {
+          unknownConsent1: true,
+          unknownConsent2: false,
+          unknownConsent3: true,
+        };
+        const otherConsents = {
+          requiredConsent1: false,
+          requiredConsent2: false,
+          optionalConsent1: false,
+          optionalConsent2: false,
+        };
+        createControllerAndInitCookie({
+          ...defaultControllerTestData,
+          cookie: cookieDataWithUnknownConsents,
+        });
+        controller.approveAll();
+        controller.approveRequired();
+        controller.rejectAll();
+        expect(controller.save()).toEqual({
+          ...cookieDataWithUnknownConsents,
+          ...otherConsents,
+        });
+      });
+    });
+    describe('approveAll', () => {
+      it('sets all consents to true. Setting consents does not store a new cookie', () => {
+        createControllerAndInitCookie(defaultControllerTestData);
+        controller.approveAll();
+        expect(controller.getRequired()).toEqual({
+          requiredConsent1: true,
+          requiredConsent2: true,
+        });
+        expect(controller.getOptional()).toEqual({
+          optionalConsent1: true,
+          optionalConsent2: true,
+        });
+        expect(mockCookieHelpers.mockSet).toHaveBeenCalledTimes(0);
+      });
+    });
+    describe('approveRequired', () => {
+      it(`approves all required consents. 
+          Optional are not affected. 
+          Setting consents does not store a new cookie`, () => {
+        createControllerAndInitCookie(defaultControllerTestData);
+        controller.approveRequired();
+        expect(controller.getRequired()).toEqual({
+          requiredConsent1: true,
+          requiredConsent2: true,
+        });
+        expect(controller.getOptional()).toEqual({
+          optionalConsent1: false,
+          optionalConsent2: false,
+        });
+        expect(mockCookieHelpers.mockSet).toHaveBeenCalledTimes(0);
+      });
+    });
+    describe('rejectAll', () => {
+      it('rejects all consents. Setting consents does not store a new cookie', () => {
+        createControllerAndInitCookie({
+          ...defaultControllerTestData,
+          cookie: {
+            requiredConsent1: true,
+            requiredConsent2: true,
+            optionalConsent1: true,
+            optionalConsent2: true,
+          },
+        });
+
+        expect({
+          ...controller.getRequired(),
+          ...controller.getOptional(),
+        }).toEqual({
+          requiredConsent1: true,
+          requiredConsent2: true,
+          optionalConsent1: true,
+          optionalConsent2: true,
+        });
+
+        controller.rejectAll();
+
+        expect({
+          ...controller.getRequired(),
+          ...controller.getOptional(),
+        }).toEqual({
+          requiredConsent1: false,
+          requiredConsent2: false,
+          optionalConsent1: false,
+          optionalConsent2: false,
+        });
+
+        expect(mockCookieHelpers.mockSet).toHaveBeenCalledTimes(0);
+      });
+    });
+    describe('getUnhandledConsents', () => {
+      it('returns consents that are not defined in previously saved cookie.', () => {
+        createControllerAndInitCookie({
+          ...defaultControllerTestData,
+          cookie: {
+            requiredConsent1: true,
+            optionalConsent2: true,
+            unknownCookie1: false,
+          },
+        });
+        const unhandled = controller.getUnhandledConsents();
+        expect(unhandled.includes('requiredConsent2')).toBeTruthy();
+        expect(unhandled.includes('optionalConsent1')).toBeTruthy();
+        expect(unhandled.includes('unknownCookie1')).toBeFalsy();
+      });
+    });
+    describe('getRequiredWithoutConsent', () => {
+      it('returns required consents that are not currently approved', () => {
+        createControllerAndInitCookie({
+          ...defaultControllerTestData,
+          cookie: {
+            requiredConsent1: true,
+            optionalConsent2: true,
+          },
+        });
+        const unhandled = controller.getUnhandledConsents();
+        expect(unhandled.includes('requiredConsent2')).toBeTruthy();
+        expect(unhandled.includes('optionalConsent1')).toBeTruthy();
+      });
+    });
+    describe('update', () => {
+      const allConsents = [
+        ...defaultControllerTestData.requiredConsents,
+        ...defaultControllerTestData.optionalConsents,
+      ];
+      it('sets a consent true/false', () => {
+        createControllerAndInitCookie(defaultControllerTestData);
+
+        const values = [true, false];
+
+        values.forEach((value) => {
+          allConsents.forEach((consent) => {
+            controller.update(consent, value);
+          });
+          expect({
+            ...controller.getRequired(),
+            ...controller.getOptional(),
+          }).toEqual({
+            requiredConsent1: value,
+            requiredConsent2: value,
+            optionalConsent1: value,
+            optionalConsent2: value,
+          });
+        });
+      });
+      it('Throws when setting an unknown consent', () => {
+        createControllerAndInitCookie(defaultControllerTestData);
+        expect(() => controller.update('consentX', false)).toThrow();
+      });
+      it('Does not auto save the cookie', () => {
+        allConsents.forEach((consent) => {
+          controller.update(consent, true);
+        });
+        expect(mockCookieHelpers.mockSet).toHaveBeenCalledTimes(0);
+      });
+    });
+    describe('save', () => {
+      it('stores the data into a cookie', () => {
+        createControllerAndInitCookie(defaultControllerTestData);
+        expect(mockCookieHelpers.mockSet).toHaveBeenCalledTimes(0);
+        controller.approveRequired();
+        controller.save();
+        expect(mockCookieHelpers.mockSet).toHaveBeenCalledTimes(1);
+        expect(JSON.parse(mockCookieHelpers.getSetCookieArguments().data)).toEqual({
+          requiredConsent1: true,
+          requiredConsent2: true,
+          optionalConsent1: false,
+          optionalConsent2: false,
+        });
+      });
+      it('the domain of the cookie is set to <domain>.<suffix> so it is readable from *.hel.fi and *.hel.ninja', () => {
+        createControllerAndInitCookie(defaultControllerTestData);
+        mockedWindowControls.setUrl('https://subdomain.hel.fi');
+        controller.save();
+        expect(mockCookieHelpers.getSetCookieArguments().options.domain).toEqual('hel.fi');
+        mockedWindowControls.setUrl('http://profiili.hel.ninja:3000?foo=bar');
+        controller.save();
+        expect(mockCookieHelpers.getSetCookieArguments().options.domain).toEqual('hel.ninja');
+      });
+    });
+  });
+  describe('createStorage', () => {
+    const createTestStorage = () =>
+      createStorage({
+        required: {
+          requiredConsent1: false,
+          requiredConsent2: true,
+        },
+        optional: {
+          optionalConsent1: false,
+          optionalConsent2: true,
+        },
+        unknown: {
+          unknownConsent1: false,
+          unknownConsent2: true,
+        },
+      });
+
+    it('Updates given consent and returns a new copy of data without mutating old data', () => {
+      const storage = createTestStorage();
+      const initialVersion = storage.getAll();
+      let unknownConsents = initialVersion.unknown as ConsentObject;
+      expect(initialVersion.required.requiredConsent1).toBeFalsy();
+      expect(initialVersion.required.requiredConsent2).toBeTruthy();
+      expect(initialVersion.optional.optionalConsent1).toBeFalsy();
+      expect(initialVersion.optional.optionalConsent2).toBeTruthy();
+      expect(unknownConsents.unknownConsent1).toBeFalsy();
+      expect(unknownConsents.unknownConsent2).toBeTruthy();
+
+      const approvedVersion = storage.approve(['requiredConsent1', 'optionalConsent1']);
+      unknownConsents = approvedVersion.unknown as ConsentObject;
+      expect(approvedVersion.required.requiredConsent1).toBeTruthy();
+      expect(approvedVersion.required.requiredConsent2).toBeTruthy();
+      expect(approvedVersion.optional.optionalConsent1).toBeTruthy();
+      expect(approvedVersion.optional.optionalConsent2).toBeTruthy();
+      // initial version is unchanged
+      expect(initialVersion.required.requiredConsent1).toBeFalsy();
+      expect(initialVersion.optional.optionalConsent1).toBeFalsy();
+      // unknown consents are unchanged
+      expect(unknownConsents.unknownConsent1).toBeFalsy();
+      expect(unknownConsents.unknownConsent2).toBeTruthy();
+
+      const rejectedVersion = storage.reject([
+        'requiredConsent1',
+        'requiredConsent2',
+        'optionalConsent1',
+        'optionalConsent2',
+      ]);
+      unknownConsents = rejectedVersion.unknown as ConsentObject;
+      expect(rejectedVersion.required.requiredConsent1).toBeFalsy();
+      expect(rejectedVersion.required.requiredConsent2).toBeFalsy();
+      expect(rejectedVersion.optional.optionalConsent1).toBeFalsy();
+      expect(rejectedVersion.optional.optionalConsent2).toBeFalsy();
+      // previous version is unchanged
+      expect(approvedVersion.required.requiredConsent1).toBeTruthy();
+      expect(approvedVersion.required.requiredConsent2).toBeTruthy();
+      expect(approvedVersion.optional.optionalConsent1).toBeTruthy();
+      expect(approvedVersion.optional.optionalConsent2).toBeTruthy();
+      // unknown consents are unchanged
+      expect(unknownConsents.unknownConsent1).toBeFalsy();
+      expect(unknownConsents.unknownConsent2).toBeTruthy();
+    });
+    it('Throws when setting an unknown consent', () => {
+      const storage = createTestStorage();
+      expect(() => storage.approve(['consentX'])).toThrow();
+      expect(() => storage.reject(['consentX'])).toThrow();
+      expect(() => storage.approve(['unknownConsent1'])).toThrow();
+      expect(() => storage.reject(['unknownConsent2'])).toThrow();
+    });
+    it('getConsentByName returns true/false even for unknown consents. Unknown consents are false', () => {
+      const storage = createTestStorage();
+      expect(storage.getConsentByName('requiredConsent1')).toBeFalsy();
+      expect(storage.getConsentByName('requiredConsent2')).toBeTruthy();
+      expect(storage.getConsentByName('optionalConsent1')).toBeFalsy();
+      expect(storage.getConsentByName('optionalConsent2')).toBeTruthy();
+      expect(storage.getConsentByName('consentZ')).toBeFalsy();
+      expect(storage.getConsentByName('unknownConsent1')).toBeFalsy();
+      expect(storage.getConsentByName('unknownConsent2')).toBeFalsy();
+    });
+  });
+});
