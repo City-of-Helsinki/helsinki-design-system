f62596431fbec047b095d99b6f9c742cf0da2d7b
NikoHelle <niko.helle@hyperactive.fi>
2021-11-17 15:46:08 +0200

    Created React context for cookie consents
    The current state cannot be in one component, so context stores and shares the state.
    
    Status of the consents must be readable from multiple components like matomo, i18n and so on.
    
    Also some components may need to hide themselves from screen readers and prevent scrolling etc while dialog is open.
    
    The context also simplifies the usage of the cookie consent controller by providing helpers like "willRenderCookieConsentDialog" and "hasUserHandledAllConsents"

---
+++ packages/react/src/components/cookieConsent/CookieConsentContext.tsx
@@ -0,0 +1,78 @@
+import React, { createContext, useMemo, useState } from 'react';
+
+import create, { ConsentController, ConsentList, ConsentObject } from './cookieConsentController';
+
+export type CookieConsentContextType = {
+  getRequired: () => ConsentObject;
+  getOptional: () => ConsentObject;
+  update: ConsentController['update'];
+  approveRequired: ConsentController['approveRequired'];
+  approveAll: ConsentController['approveAll'];
+  save: ConsentController['save'];
+  willRenderCookieConsentDialog: boolean;
+  hasUserHandledAllConsents: () => boolean;
+};
+
+type CookieConsentContextProps = {
+  optionalConsents?: ConsentList;
+  requiredConsents?: ConsentList;
+  children: React.ReactNode | React.ReactNode[] | null;
+  onAllConsentsGiven?: (consents: ConsentObject) => void;
+  onConsentsParsed?: (consents: ConsentObject, hasUserHandledAllConsents: boolean) => void;
+};
+
+export const CookieConsentContext = createContext<CookieConsentContextType>({
+  getRequired: () => ({}),
+  getOptional: () => ({}),
+  update: () => undefined,
+  approveRequired: () => undefined,
+  approveAll: () => undefined,
+  save: () => ({}),
+  hasUserHandledAllConsents: () => false,
+  willRenderCookieConsentDialog: false,
+});
+
+export const Provider = ({
+  optionalConsents,
+  requiredConsents,
+  onAllConsentsGiven = () => undefined,
+  onConsentsParsed = () => undefined,
+  children,
+}: CookieConsentContextProps): React.ReactElement => {
+  const consentController = useMemo(() => create({ requiredConsents, optionalConsents }), [
+    requiredConsents,
+    optionalConsents,
+  ]);
+
+  const hasUserHandledAllConsents = () =>
+    consentController.getRequiredWithoutConsent().length === 0 && consentController.getUnhandledConsents().length === 0;
+
+  const [willRenderCookieConsentDialog, setWillRenderCookieConsentDialog] = useState<boolean>(
+    !hasUserHandledAllConsents(),
+  );
+
+  const mergeConsents = () => ({
+    ...consentController.getRequired(),
+    ...consentController.getOptional(),
+  });
+
+  const contextData: CookieConsentContextType = {
+    getRequired: () => consentController.getRequired(),
+    getOptional: () => consentController.getOptional(),
+    update: (key, value) => consentController.update(key, value),
+    approveRequired: () => consentController.approveRequired(),
+    approveAll: () => consentController.approveAll(),
+    save: () => {
+      const savedData = consentController.save();
+      if (hasUserHandledAllConsents()) {
+        setWillRenderCookieConsentDialog(false);
+        onAllConsentsGiven(mergeConsents());
+      }
+      return savedData;
+    },
+    willRenderCookieConsentDialog,
+    hasUserHandledAllConsents,
+  };
+  onConsentsParsed(mergeConsents(), hasUserHandledAllConsents());
+  return <CookieConsentContext.Provider value={contextData}>{children}</CookieConsentContext.Provider>;
+};
