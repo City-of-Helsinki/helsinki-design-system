84a16a4467ca3850fd6a1f669c272de08f9aca5a
NikoHelle <niko.helle@hyperactive.fi>
2021-11-17 15:44:53 +0200

    Created a controller for cookie consents
    The controller handles cookie read/write actions and stores consents until user saves them to a cookie.
    
    Created cookie is accessible from *.hel.fi and *.hel.ninja (and any *.domain.suffix).
    
    Common conset 'names' are declared so same consents can be used between services and not asked again.
    
    Cookie's "secure" -property is set to 'false' so it is readable and settable without https and therefore in localhost.
    
    This is not considered as a security risk because of the insensitive content of the cookie and 'sameSite' is anyway set to 'strict' so the cookie is not sent to third party domains.
    
    Added a package for cookie read/write and some lodash helpers
    
    Added tests and mocks.

---
+++ packages/react/src/components/cookieConsent/cookieConsentController.ts
@@ -0,0 +1,251 @@
+import _pick from 'lodash.pick';
+import _isObject from 'lodash.isobject';
+import _isUndefined from 'lodash.isundefined';
+import CookieController, { CookieSetOptions } from 'universal-cookie';
+
+export type ConsentList = string[];
+
+export type ConsentObject = { [x: string]: boolean };
+
+export type ConsentStorage = {
+  required: ConsentObject;
+  optional: ConsentObject;
+  unknown?: ConsentObject;
+};
+
+export type ConsentControllerProps = {
+  requiredConsents?: ConsentList;
+  optionalConsents?: ConsentList;
+};
+
+export type ConsentController = {
+  getRequired: () => ConsentObject;
+  getOptional: () => ConsentObject;
+  update: (key: string, value: boolean) => void;
+  approveRequired: () => void;
+  approveAll: () => void;
+  rejectAll: () => void;
+  getUnhandledConsents: () => string[];
+  getRequiredWithoutConsent: () => string[];
+  save: () => ConsentObject;
+};
+
+const COOKIE_NAME = 'city-of-helsinki-cookie-consents';
+
+export const commonConsents = {
+  matomo: 'matomo',
+  tunnistamo: 'tunnistamo',
+  language: 'language',
+  marketing: 'marketing',
+  preferences: 'preferences',
+};
+
+function convertStringArrayToKeyConsentObject(array: string[]): ConsentObject {
+  return array.reduce((current, key) => {
+    // eslint-disable-next-line no-param-reassign
+    current[key] = false;
+    return current;
+  }, {} as ConsentObject);
+}
+
+function mergeConsents(set1: ConsentObject, set2: ConsentObject, set3?: ConsentObject): ConsentObject {
+  return { ...set1, ...set2, ...set3 };
+}
+
+function parseConsents(jsonString: string | undefined): ConsentObject {
+  if (!jsonString || jsonString.length < 2 || jsonString.charAt(0) !== '{') {
+    return {};
+  }
+  try {
+    return JSON.parse(jsonString);
+  } catch (e) {
+    return {};
+  }
+}
+
+function createConsentsString(consents: ConsentObject): string {
+  if (!_isObject(consents)) {
+    return '{}';
+  }
+  return JSON.stringify(consents);
+}
+
+function createCookieController(): {
+  get: () => string;
+  set: (data: string) => void;
+} {
+  const cookieController = new CookieController();
+  const oneYearInSeconds = 60 * 60 * 24 * 365;
+  const defaultCookieSetOptions: CookieSetOptions = {
+    path: '/',
+    secure: false,
+    sameSite: 'strict',
+    maxAge: oneYearInSeconds,
+  };
+
+  const getCookieDomainFromUrl = (): string => window.location.hostname.split('.').slice(-2).join('.');
+
+  const createCookieOptions = (): CookieSetOptions => ({
+    ...defaultCookieSetOptions,
+    domain: getCookieDomainFromUrl(),
+  });
+
+  const get = (): string => cookieController.get(COOKIE_NAME, { doNotParse: true }) || '';
+
+  const set = (data: string): void => {
+    cookieController.set(COOKIE_NAME, data, createCookieOptions());
+  };
+  return {
+    get,
+    set,
+  };
+}
+
+export function createStorage(
+  initialValues: ConsentStorage,
+): {
+  getAll: () => ConsentStorage;
+  getConsentByName: (consentName: string) => boolean;
+  approve: (keys: string[]) => ConsentStorage;
+  reject: (keys: string[]) => ConsentStorage;
+} {
+  let storage: ConsentStorage = { ...initialValues };
+
+  const getStorage = () => storage;
+
+  const copyStorage = (): ConsentStorage => ({
+    optional: { ...storage.optional },
+    required: { ...storage.required },
+    unknown: { ...storage.unknown },
+  });
+
+  const updateStorage = (newStorage: ConsentStorage): void => {
+    storage = newStorage;
+  };
+
+  const findConsentSource = (consentName: string, targetStorage: ConsentStorage): ConsentObject | undefined => {
+    if (!_isUndefined(targetStorage.required[consentName])) {
+      return targetStorage.required;
+    }
+    if (!_isUndefined(targetStorage.optional[consentName])) {
+      return targetStorage.optional;
+    }
+    return undefined;
+  };
+
+  const updateConsent = (targetStorage: ConsentStorage, consentName: string, value: boolean): void => {
+    const target = findConsentSource(consentName, targetStorage);
+    if (!target) {
+      throw new Error(`Unknown consent ${consentName}`);
+    }
+    target[consentName] = value;
+  };
+
+  const setConsents = (keys: string[], value: boolean): ConsentStorage => {
+    const copiedStorage = copyStorage();
+    keys.forEach((key) => {
+      updateConsent(copiedStorage, key, value);
+    });
+    updateStorage(copiedStorage);
+    return copiedStorage;
+  };
+
+  const getConsentByName = (consentName: string): boolean => {
+    const target = findConsentSource(consentName, getStorage());
+    if (!target) {
+      return false;
+    }
+    return target[consentName];
+  };
+
+  const approve = (keys: string[]): ConsentStorage => setConsents(keys, true);
+
+  const reject = (keys: string[]): ConsentStorage => setConsents(keys, false);
+
+  return {
+    getAll: () => getStorage(),
+    getConsentByName,
+    approve,
+    reject,
+  };
+}
+
+function verifyConsentProps({ optionalConsents, requiredConsents }: ConsentControllerProps) {
+  if (!requiredConsents || !optionalConsents) {
+    return;
+  }
+  requiredConsents.forEach((consent) => {
+    if (optionalConsents.includes(consent)) {
+      throw new Error(`optional consent '${consent}' found in requiredConsents.`);
+    }
+  });
+}
+
+export default function createConsentController(props: ConsentControllerProps): ConsentController {
+  verifyConsentProps(props);
+  const { optionalConsents = [], requiredConsents = [] } = props;
+  const allConsents = [...optionalConsents, ...requiredConsents];
+  const cookieController = createCookieController();
+  const currentConsentsInCookie = parseConsents(cookieController.get());
+
+  const required = mergeConsents(
+    convertStringArrayToKeyConsentObject(requiredConsents),
+    _pick(currentConsentsInCookie, requiredConsents),
+  );
+
+  const optional = mergeConsents(
+    convertStringArrayToKeyConsentObject(optionalConsents),
+    _pick(currentConsentsInCookie, optionalConsents),
+  );
+
+  const unknownConsentKeys = Object.keys(currentConsentsInCookie).filter((key) => !allConsents.includes(key));
+
+  const unknown = unknownConsentKeys.length ? _pick(currentConsentsInCookie, unknownConsentKeys) : undefined;
+
+  const storage = createStorage({ required, optional, unknown });
+
+  const getConsents = () => storage.getAll();
+
+  const save = () => {
+    const currentVersion = getConsents();
+    const consents = mergeConsents(currentVersion.required, currentVersion.optional, currentVersion.unknown);
+    cookieController.set(createConsentsString(consents));
+    return consents;
+  };
+
+  const rejectAll = () => {
+    storage.reject(allConsents);
+  };
+
+  const approveRequired = () => {
+    storage.approve(requiredConsents);
+  };
+
+  const approveAll = () => {
+    storage.approve(allConsents);
+  };
+
+  const update = (consentName: string, value: boolean) => {
+    const arr = [consentName];
+    if (value) {
+      storage.approve(arr);
+    } else {
+      storage.reject(arr);
+    }
+  };
+
+  return {
+    getRequired: () => storage.getAll().required,
+    getOptional: () => storage.getAll().optional,
+    update,
+    approveAll,
+    approveRequired,
+    getRequiredWithoutConsent: () => requiredConsents.filter((key) => storage.getConsentByName(key) !== true),
+    rejectAll,
+    getUnhandledConsents: () => {
+      const storedCookies = parseConsents(cookieController.get());
+      return allConsents.filter((key) => _isUndefined(storedCookies[key]));
+    },
+    save,
+  };
+}
