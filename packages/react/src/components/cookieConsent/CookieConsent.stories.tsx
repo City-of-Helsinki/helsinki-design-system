c7743d0b40a5104efd90b525031216c070c1d8ba
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2021-11-29 14:12:36 +0200

    Open up Demo to show CookieConsentContext usage

--- packages/react/src/components/cookieConsent/CookieConsent.stories.tsx
+++ packages/react/src/components/cookieConsent/CookieConsent.stories.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import { commonConsents } from './cookieConsentController';
-import { Provider as CookieContextProvider } from './CookieConsentContext';
-import Demo from './__storybook__/Demo';
+import { CookieConsentContext, Provider as CookieContextProvider } from './CookieConsentContext';
+import { CookieConsent } from './CookieConsent';
+import classNames from '../../utils/classNames';
+import styles from './__storybook__/styles.module.scss';
 
 export default {
-  component: Demo,
+  component: CookieConsent,
   title: 'Components/CookieConsent',
   parameters: {
     controls: { expanded: true },
@@ -14,6 +16,33 @@ export default {
 };
 
 export const Example = () => {
+  const Application = () => {
+    const { willRenderCookieConsentDialog } = useContext(CookieConsentContext);
+    return (
+      <div
+        className={classNames(styles.application, willRenderCookieConsentDialog ? styles['no-scroll'] : '')}
+        aria-hidden={willRenderCookieConsentDialog ? 'true' : 'false'}
+      >
+        <h1>This is a dummy application</h1>
+        {willRenderCookieConsentDialog && (
+          <>
+            <p>
+              Cookie consent dialog is visible. &nbsp;
+              <a href="/" title="This is a dummy link">
+                Can&apos;t touch this!
+              </a>
+            </p>
+          </>
+        )}
+        {!willRenderCookieConsentDialog && (
+          <>
+            <p>Cookie consents have been given. Remove the cookie to see the dialog again.</p>
+          </>
+        )}
+      </div>
+    );
+  };
+
   return (
     <CookieContextProvider
       requiredConsents={[commonConsents.tunnistamo, commonConsents.language]}
@@ -34,7 +63,8 @@ export const Example = () => {
         }
       }}
     >
-      <Demo />
+      <CookieConsent />
+      <Application />
     </CookieContextProvider>
   );
 };
