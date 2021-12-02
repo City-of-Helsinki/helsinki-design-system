ae2cd9c10297f18f3f26cf0050c3efd0c6cb19cc
Mika Nevalainen <mn.nevalainen@gmail.com>
2021-12-02 16:20:08 +0200

    Add support for passing ref into native input element

--- packages/react/src/components/searchInput/SearchInput.tsx
+++ packages/react/src/components/searchInput/SearchInput.tsx
@@ -1,4 +1,4 @@
-import React, { KeyboardEvent, useState, useRef } from 'react';
+import React, { KeyboardEvent, useState, useRef, Ref, useEffect } from 'react';
 import { useCombobox } from 'downshift';
 
 // import core base styles
@@ -7,6 +7,7 @@ import 'hds-core';
 import styles from './SearchInput.module.scss';
 import { FieldLabel } from '../../internal/field-label/FieldLabel';
 import { DropdownMenu } from '../../internal/dropdownMenu/DropdownMenu';
+import mergeRefWithInternalRef from '../../utils/mergeRefWithInternalRef';
 import classNames from '../../utils/classNames';
 import { IconCrossCircle, IconSearch } from '../../icons';
 import { GetSuggestionsFunction, SUGGESTIONS_DEBOUNCE_VALUE, useSuggestions } from './useSuggestions';
@@ -66,6 +67,10 @@ export type SearchInputProps<SuggestionItem> = {
    */
   hideSearchButton?: boolean;
   /**
+   * The `ref` is forwarded to the native input element.
+   */
+  ref?: React.Ref<HTMLInputElement>;
+  /**
    * Override or extend the styles applied to the component
    */
   style?: React.CSSProperties;
@@ -81,144 +86,163 @@ export type SearchInputProps<SuggestionItem> = {
   visibleSuggestions?: number;
 };
 
-export const SearchInput = <SuggestionItem,>({
-  className,
-  clearButtonAriaLabel = 'Clear',
-  getSuggestions,
-  helperText,
-  highlightSuggestions = false,
-  label,
-  loadingSpinnerFinishedText = 'Finished loading suggestions',
-  loadingSpinnerText = 'Loading suggestions',
-  onSubmit,
-  searchButtonAriaLabel = 'Search',
-  hideSearchButton = false,
-  style,
-  suggestionLabelField,
-  visibleSuggestions = 8,
-}: SearchInputProps<SuggestionItem>) => {
-  const inputRef = useRef<HTMLInputElement>(null);
-  const [isSubmitted, setIsSubmitted] = useState<boolean>(false);
-  const [inputValue, setInputValue] = useState<string>('');
-  const { suggestions, isLoading } = useSuggestions<SuggestionItem>(inputValue, getSuggestions, isSubmitted);
-  const showLoadingSpinner = useShowLoadingSpinner(isLoading, 1500 - SUGGESTIONS_DEBOUNCE_VALUE);
-  const {
-    isOpen,
-    getLabelProps,
-    getMenuProps,
-    getInputProps,
-    getComboboxProps,
-    highlightedIndex,
-    getItemProps,
-    reset,
-  } = useCombobox<SuggestionItem>({
-    items: suggestions,
-    onInputValueChange: (e) => {
-      setInputValue(e.inputValue);
-    },
-    itemToString: (item) => (item ? `${item[suggestionLabelField]}` : ''),
-  });
+const SearchInputComponent = React.forwardRef(
+  <SuggestionItem,>(
+    {
+      className,
+      clearButtonAriaLabel = 'Clear',
+      getSuggestions,
+      helperText,
+      highlightSuggestions = false,
+      label,
+      loadingSpinnerFinishedText = 'Finished loading suggestions',
+      loadingSpinnerText = 'Loading suggestions',
+      onSubmit,
+      searchButtonAriaLabel = 'Search',
+      hideSearchButton = false,
+      style,
+      suggestionLabelField,
+      visibleSuggestions = 8,
+    }: SearchInputProps<SuggestionItem>,
+    ref: Ref<HTMLInputElement>,
+  ) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+    const [isSubmitted, setIsSubmitted] = useState<boolean>(false);
+    const [inputValue, setInputValue] = useState<string>('');
+    const { suggestions, isLoading } = useSuggestions<SuggestionItem>(inputValue, getSuggestions, isSubmitted);
+    const showLoadingSpinner = useShowLoadingSpinner(isLoading, 1500 - SUGGESTIONS_DEBOUNCE_VALUE);
+    const {
+      isOpen,
+      getLabelProps,
+      getMenuProps,
+      getInputProps,
+      getComboboxProps,
+      highlightedIndex,
+      getItemProps,
+      reset,
+    } = useCombobox<SuggestionItem>({
+      items: suggestions,
+      onInputValueChange: (e) => {
+        setInputValue(e.inputValue);
+      },
+      itemToString: (item) => (item ? `${item[suggestionLabelField]}` : ''),
+    });
 
-  const submit = () => {
-    setIsSubmitted(true);
-    onSubmit(inputValue);
-  };
+    const submit = () => {
+      setIsSubmitted(true);
+      onSubmit(inputValue);
+    };
 
-  const onInputKeyUp = (event: KeyboardEvent<HTMLInputElement>) => {
-    const key = event.key || event.keyCode;
-    if (key === 'Enter' || key === 13) {
-      submit();
-    } else {
-      setIsSubmitted(false);
-    }
-  };
+    const onInputKeyUp = (event: KeyboardEvent<HTMLInputElement>) => {
+      const key = event.key || event.keyCode;
+      if (key === 'Enter' || key === 13) {
+        submit();
+      } else {
+        setIsSubmitted(false);
+      }
+    };
 
-  const clear = () => {
-    reset();
-    inputRef.current.focus();
-  };
+    const clear = () => {
+      reset();
+      inputRef.current.focus();
+    };
 
-  return (
-    <div className={classNames(styles.root, isOpen && styles.open, className)} style={style}>
-      {label && <FieldLabel label={label} {...getLabelProps()} />}
-      <div className={classNames(styles.wrapper)} ref={getComboboxProps().ref}>
-        <input
-          {...getInputProps({
-            onKeyUp: onInputKeyUp,
-            ref: inputRef,
-            role: getComboboxProps().role,
-            'aria-expanded': getComboboxProps()['aria-expanded'],
-            'aria-haspopup': getComboboxProps()['aria-haspopup'],
-            'aria-owns': getComboboxProps()['aria-owns'],
-          })}
-          className={classNames(styles.input)}
-        />
-        <div className={styles.buttons}>
-          <>
-            {inputValue.length > 0 && (
-              <button
-                type="button"
-                aria-label={clearButtonAriaLabel}
-                className={classNames(styles.button)}
-                onClick={clear}
-              >
-                <IconCrossCircle className={styles.searchIcon} aria-hidden />
-              </button>
-            )}
-          </>
-          <>
-            {!hideSearchButton && (
-              <button
-                type="button"
-                aria-label={searchButtonAriaLabel}
-                className={classNames(styles.button)}
-                onClick={submit}
-              >
-                <IconSearch className={styles.searchIcon} aria-hidden />
-              </button>
-            )}
-          </>
-        </div>
-        {showLoadingSpinner && (
-          <div className={styles.loadingSpinnerContainer} aria-hidden>
-            <LoadingSpinner
-              loadingText={loadingSpinnerText}
-              loadingFinishedText={loadingSpinnerFinishedText}
-              small
-              className={styles.loadingSpinner}
-            />
-            {loadingSpinnerText}
+    /**
+     * Merge props.ref to the internal ref. This is needed because we need the ref ourself and cannot rely on
+     * component user to provide it.
+     */
+    useEffect(() => {
+      if (ref) {
+        mergeRefWithInternalRef(ref, inputRef);
+      }
+    }, [inputRef, ref]);
+
+    return (
+      <div className={classNames(styles.root, isOpen && styles.open, className)} style={style}>
+        {label && <FieldLabel label={label} {...getLabelProps()} />}
+        <div className={classNames(styles.wrapper)} ref={getComboboxProps().ref}>
+          <input
+            {...getInputProps({
+              onKeyUp: onInputKeyUp,
+              ref: inputRef,
+              role: getComboboxProps().role,
+              'aria-expanded': getComboboxProps()['aria-expanded'],
+              'aria-haspopup': getComboboxProps()['aria-haspopup'],
+              'aria-owns': getComboboxProps()['aria-owns'],
+            })}
+            className={classNames(styles.input)}
+          />
+          <div className={styles.buttons}>
+            <>
+              {inputValue.length > 0 && (
+                <button
+                  type="button"
+                  aria-label={clearButtonAriaLabel}
+                  className={classNames(styles.button)}
+                  onClick={clear}
+                >
+                  <IconCrossCircle className={styles.searchIcon} aria-hidden />
+                </button>
+              )}
+            </>
+            <>
+              {!hideSearchButton && (
+                <button
+                  type="button"
+                  aria-label={searchButtonAriaLabel}
+                  className={classNames(styles.button)}
+                  onClick={submit}
+                >
+                  <IconSearch className={styles.searchIcon} aria-hidden />
+                </button>
+              )}
+            </>
           </div>
-        )}
-        <DropdownMenu<SuggestionItem>
-          isOptionDisabled={() => false}
-          multiselect={false}
-          open={isOpen && !showLoadingSpinner}
-          selectedItem={null}
-          selectedItems={[]}
-          highlightValue={highlightSuggestions && inputValue.length >= 3 && inputValue}
-          visibleOptions={visibleSuggestions}
-          menuStyles={styles}
-          options={suggestions}
-          optionLabelField={`${suggestionLabelField}`}
-          menuProps={getMenuProps({
-            style: { maxHeight: DROPDOWN_MENU_ITEM_HEIGHT * visibleSuggestions },
-          })}
-          getItemProps={(item, index, selected, optionDisabled) =>
-            getItemProps({
-              item,
-              index,
-              disabled: optionDisabled,
-              className: classNames(
-                styles.menuItem,
-                selected && styles.selected,
-                highlightedIndex === index && styles.highlighted,
-              ),
-            })
-          }
-        />
+          {showLoadingSpinner && (
+            <div className={styles.loadingSpinnerContainer} aria-hidden>
+              <LoadingSpinner
+                loadingText={loadingSpinnerText}
+                loadingFinishedText={loadingSpinnerFinishedText}
+                small
+                className={styles.loadingSpinner}
+              />
+              {loadingSpinnerText}
+            </div>
+          )}
+          <DropdownMenu<SuggestionItem>
+            isOptionDisabled={() => false}
+            multiselect={false}
+            open={isOpen && !showLoadingSpinner}
+            selectedItem={null}
+            selectedItems={[]}
+            highlightValue={highlightSuggestions && inputValue.length >= 3 && inputValue}
+            visibleOptions={visibleSuggestions}
+            menuStyles={styles}
+            options={suggestions}
+            optionLabelField={`${suggestionLabelField}`}
+            menuProps={getMenuProps({
+              style: { maxHeight: DROPDOWN_MENU_ITEM_HEIGHT * visibleSuggestions },
+            })}
+            getItemProps={(item, index, selected, optionDisabled) =>
+              getItemProps({
+                item,
+                index,
+                disabled: optionDisabled,
+                className: classNames(
+                  styles.menuItem,
+                  selected && styles.selected,
+                  highlightedIndex === index && styles.highlighted,
+                ),
+              })
+            }
+          />
+        </div>
+        {helperText && <div className={styles.helperText}>{helperText}</div>}
       </div>
-      {helperText && <div className={styles.helperText}>{helperText}</div>}
-    </div>
-  );
-};
+    );
+  },
+);
+
+export const SearchInput = (SearchInputComponent as unknown) as <SuggestionItem>(
+  props: SearchInputProps<SuggestionItem> & { ref?: Ref<HTMLFormElement> },
+) => JSX.Element;
