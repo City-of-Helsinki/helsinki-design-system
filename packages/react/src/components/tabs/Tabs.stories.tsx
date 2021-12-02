80a8e5c53f348db0a2e17cd6279ba12ac94a18b4
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2021-12-02 15:59:33 +0200

    Enable customisation of Tabs component through theme prop

--- packages/react/src/components/tabs/Tabs.stories.tsx
+++ packages/react/src/components/tabs/Tabs.stories.tsx
@@ -74,3 +74,38 @@ export const Small = () => (
     </TabPanel>
   </Tabs>
 );
+
+export const WithCustomTheme = () => {
+  const theme = {
+    '--tab-color': 'var(--color-brick)',
+    '--tab-active-border-color': 'var(--color-brick)',
+  };
+
+  return (
+    <Tabs theme={theme}>
+      <TabList className="example-tablist">
+        <Tab>Daycare</Tab>
+        <Tab>Pre-school</Tab>
+        <Tab>Basic education</Tab>
+        <Tab>Upper secondary</Tab>
+        <Tab>University</Tab>
+      </TabList>
+      <TabPanel>Daytime care for people who cannot be fully independent, such as children or elderly people.</TabPanel>
+      <TabPanel>
+        A pre-school is an educational establishment offering early childhood education to children before they begin
+        compulsory education at primary school.
+      </TabPanel>
+      <TabPanel>
+        The objective of basic education in Finland is to support pupils&#39; growth towards humanity and ethically
+        responsible membership of society.
+      </TabPanel>
+      <TabPanel>
+        Upper secondary school studies last three to four years, preparing the students for the matriculation
+        examination.
+      </TabPanel>
+      <TabPanel>
+        A high-level educational institution in which students study for degrees and academic research is done.
+      </TabPanel>
+    </Tabs>
+  );
+};
--- packages/react/src/components/tabs/Tabs.tsx
+++ packages/react/src/components/tabs/Tabs.tsx
@@ -6,6 +6,21 @@ import styles from './Tabs.module.scss';
 import classNames from '../../utils/classNames';
 import { TabsContext } from './TabsContext';
 import { FCWithName } from '../../common/types';
+import { useTheme } from '../../hooks/useTheme';
+
+export interface TabsCustomTheme {
+  '--tablist-border-color'?: string;
+  '--tablist-border-size'?: string;
+  '--tab-color'?: string;
+  '--tab-font-size'?: string;
+  '--tab-height'?: string;
+  '--tab-min-width'?: string;
+  '--tab-spacing'?: string;
+  '--tab-active-border-size'?: string;
+  '--tab-active-border-color'?: string;
+  '--tab-focus-outline-size'?: string;
+  '--tab-focus-outline-color'?: string;
+}
 
 export type TabsProps = React.PropsWithChildren<{
   /**
@@ -13,11 +28,17 @@ export type TabsProps = React.PropsWithChildren<{
    * @default false
    */
   small?: boolean;
+  /**
+   * Defines the tabs theme
+   */
+  theme?: TabsCustomTheme;
 }>;
 
-export const Tabs = ({ children, small = false }: TabsProps) => {
+export const Tabs = ({ children, small = false, theme }: TabsProps) => {
   const [activeTab, setActiveTab] = useState<number>(0);
   const [focusedTab, setFocusedTab] = useState<number>(null);
+  // custom theme class that is applied to the root element
+  const customThemeClass = useTheme<TabsCustomTheme>(styles.tabs, theme);
 
   /**
    * Get the TabList from children
@@ -43,7 +64,7 @@ export const Tabs = ({ children, small = false }: TabsProps) => {
 
   return (
     <TabsContext.Provider value={{ activeTab, setActiveTab, focusedTab, setFocusedTab }}>
-      <div className={classNames(styles.tabs, small && styles.small)}>
+      <div className={classNames(styles.tabs, small && styles.small, theme && customThemeClass)}>
         {tabList}
         {tabPanels}
       </div>
