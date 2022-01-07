a2038a505b87dd9d14085f1673d1fbb7977eb792
NikoHelle <niko.helle@hyperactive.fi>
2022-01-07 17:25:05 +0200

    Accessibility fix: order of elements
    Tab order should be: heading, lang selection, content, action buttons, close.
    
    Changed elements' positions and moved contents of <Main /> to Content.
    
    Language should be after H1 in tab ordering, so injected it to the right place in dom.
    
    Visually it is never there, but avoided manual tabIndexes.
    
    <Main /> became obsolete, so removed.

--- packages/react/src/components/cookieConsent/content/Content.tsx
+++ packages/react/src/components/cookieConsent/content/Content.tsx
@@ -5,7 +5,6 @@ import Buttons from '../buttons/Buttons';
 import { IconAngleDown, IconAngleUp, IconCross } from '../../../icons';
 import { useAccordion } from '../../accordion';
 import Details from '../details/Details';
-import Main from '../main/Main';
 import styles from '../CookieConsent.module.scss';
 import { Card } from '../../card/Card';
 
@@ -25,23 +24,29 @@ function Content({ onClick }: ViewProps): React.ReactElement {
       id="cookie-consent-content"
       aria-live="assertive"
     >
-      <div className={styles['language-switcher-and-close']}>
+      <div className={styles['main-content']} data-testid="cookie-consent-information">
+        <span
+          className={styles['emulated-h1']}
+          role="heading"
+          aria-level={1}
+          id="cookie-consent-active-heading"
+          // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
+          tabIndex={0}
+        >
+          Evästesuostumukset
+        </span>
         <div className={styles['language-switcher']} data-testid="cookie-consent-language-switcher">
-          <a href="/" title="This is a dummy language switcher" onClick={(e) => e.preventDefault()}>
+          <a href="/" tabIndex={0} title="This is a dummy language switcher" onClick={(e) => e.preventDefault()}>
             FI
           </a>
         </div>
-        <button
-          type="button"
-          title="Hyväksy valitut ja pakolliset evästeet"
-          className={styles['close-button']}
-          data-testid="cookie-consent-close-button"
-          onClick={() => onClick('approveSelectedAndRequired')}
-        >
-          <IconCross aria-hidden />
-        </button>
+        <p id="cookie-consent-description">
+          Tämä sivusto käyttää välttämättömiä evästeitä suorituskyvyn varmistamiseksi sekä yleisen käytön seurantaan.
+          Lisäksi käytämme kohdennusevästeitä käyttäjäkokemuksen parantamiseksi, analytiikkaan ja kohdistetun sisällön
+          näyttämiseen. Jatkamalla sivuston käyttöä ilman asetusten muuttamista hyväksyt välttämättömien evästeiden
+          käytön.
+        </p>
       </div>
-      <Main />
       <button type="button" className={styles['accordion-button']} {...buttonProps}>
         <Icon aria-hidden />
         <span>{settingsButtonText}</span>
@@ -56,6 +61,15 @@ function Content({ onClick }: ViewProps): React.ReactElement {
         <Details onClick={onClick} />
       </Card>
       <Buttons onClick={onClick} hasOptionalConsents={isOpen} />
+      <button
+        type="button"
+        title="Hyväksy valitut ja pakolliset evästeet"
+        className={styles['close-button']}
+        data-testid="cookie-consent-close-button"
+        onClick={() => onClick('approveSelectedAndRequired')}
+      >
+        <IconCross aria-hidden />
+      </button>
     </div>
   );
 }
