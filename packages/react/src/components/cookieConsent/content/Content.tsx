c4154aba009aa32463d3f0226a525e7fea54c04a
NikoHelle <niko.helle@hyperactive.fi>
2022-01-10 10:22:39 +0200

    Autofocus to H1 and fix heading styles
    Removed dialog role and aria-live. Blocks accessibility instead of helping.
    
    Removed unnecessary ids, aria-labelledby and aria-describedby as element is not a dialog anymore.

--- packages/react/src/components/cookieConsent/content/Content.tsx
+++ packages/react/src/components/cookieConsent/content/Content.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import { ViewProps } from '../types';
 import Buttons from '../buttons/Buttons';
@@ -12,26 +12,25 @@ function Content({ onClick }: ViewProps): React.ReactElement {
   const { isOpen, buttonProps, contentProps } = useAccordion({
     initiallyOpen: false,
   });
+  const titleRef = useRef<HTMLHeadingElement>();
   const Icon = isOpen ? IconAngleUp : IconAngleDown;
   const settingsButtonText = isOpen ? 'Piilota asetukset' : 'Näytä asetukset';
 
+  useEffect(() => {
+    if (titleRef.current) {
+      titleRef.current.focus();
+    }
+  }, [titleRef]);
   return (
-    <div
-      className={styles.content}
-      role="dialog"
-      aria-labelledby="cookie-consent-active-heading"
-      aria-describedby="cookie-consent-description"
-      id="cookie-consent-content"
-      aria-live="assertive"
-    >
+    <div className={styles.content} id="cookie-consent-content">
       <div className={styles['main-content']} data-testid="cookie-consent-information">
         <span
           className={styles['emulated-h1']}
           role="heading"
           aria-level={1}
-          id="cookie-consent-active-heading"
           // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
           tabIndex={0}
+          ref={titleRef}
         >
           Evästesuostumukset
         </span>
@@ -40,7 +39,7 @@ function Content({ onClick }: ViewProps): React.ReactElement {
             FI
           </a>
         </div>
-        <p id="cookie-consent-description">
+        <p>
           Tämä sivusto käyttää välttämättömiä evästeitä suorituskyvyn varmistamiseksi sekä yleisen käytön seurantaan.
           Lisäksi käytämme kohdennusevästeitä käyttäjäkokemuksen parantamiseksi, analytiikkaan ja kohdistetun sisällön
           näyttämiseen. Jatkamalla sivuston käyttöä ilman asetusten muuttamista hyväksyt välttämättömien evästeiden
