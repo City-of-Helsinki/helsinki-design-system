8bb1ab88390035e79f5e43c23abc3b62b5b9d472
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2022-01-20 14:49:08 +0200

    Add optional variant of Stepper with step heading included

--- packages/react/src/components/stepper/Stepper.tsx
+++ packages/react/src/components/stepper/Stepper.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 // import core base styles
 import 'hds-core';
 import styles from './Stepper.module.scss';
 import { Step, StepProps } from './Step';
-import { IconAngleLeft } from '../../icons';
 import classNames from '../../utils/classNames';
 
 type Language = 'en' | 'fi' | 'sv' | string;
@@ -22,6 +21,18 @@ export type StepperProps = React.ComponentPropsWithoutRef<'button'> & {
   renderCustomStateAriaLabel?: (state) => string;
   customSelectedAriaLabel?: string;
   onStepClick?: (event, number: number) => void;
+  stepHeading?: boolean;
+  headingClassName?: string;
+  stepHeadingAriaLevel?: number;
+  renderCustomStepHeading?: (step, totalNumberOfSteps, label) => string;
+};
+
+const getStepHeading = (language: Language, step: number, totalNumberOfSteps: number, label: string) => {
+  return {
+    en: `Step ${step}/${totalNumberOfSteps}: ${label}`,
+    fi: `Vaihe ${step}/${totalNumberOfSteps}: ${label}`,
+    sv: `Steg ${step}/${totalNumberOfSteps}: ${label}`,
+  }[language];
 };
 
 export const Stepper = ({
@@ -37,48 +48,73 @@ export const Stepper = ({
   renderCustomStateAriaLabel,
   customSelectedAriaLabel,
   onStepClick,
+  stepHeading,
+  stepHeadingAriaLevel = 2,
+  headingClassName,
 }: StepperProps) => {
+  const stepHeadingRef = useRef(null);
+
+  useEffect(() => {
+    if (stepHeadingRef.current) {
+      stepHeadingRef.current.focus();
+    }
+  }, [selectedStep]);
+
   return (
-    <div className={classNames(styles.stepper, small && styles.small)}>
-      <div
-        className={styles.line}
-        aria-hidden
-        style={{
-          width: `max( calc(100% - var(--hds-step-width)), calc(${stepsTotal} * var(--hds-step-width) - var(--hds-step-width) ))`,
-        }}
-      >
+    <>
+      <div className={classNames(styles.stepper, small && styles.small)}>
+        <div
+          className={styles.line}
+          aria-hidden
+          style={{
+            width: `max( calc(100% - var(--hds-step-width)), calc(${stepsTotal} * var(--hds-step-width) - var(--hds-step-width) ))`,
+          }}
+        >
+          {labels.map((label, index) => {
+            if (index === labels.length - 1) {
+              return null;
+            }
+            return (
+              <div
+                key={`${label}-${index}`} // eslint-disable-line react/no-array-index-key
+                style={{ width: `calc( 100% / ${stepsTotal - 1})` }}
+                className={states[index + 1] === 'disabled' ? styles.disabledLine : styles.enabledLine}
+              />
+            );
+          })}
+        </div>
         {labels.map((label, index) => {
-          if (index === labels.length - 1) {
-            return null;
-          }
           return (
-            <div
-              style={{ width: `calc( 100% / ${stepsTotal - 1})` }}
-              className={states[index + 1] === 'disabled' ? styles.disabledLine : styles.enabledLine}
+            <Step
+              key={`${index}-${label}`} // eslint-disable-line react/no-array-index-key
+              label={label}
+              language={language}
+              number={index + 1}
+              small={small}
+              stepsTotal={stepsTotal}
+              selected={selectedStep === index + 1}
+              state={states[index]}
+              onStepClick={(event, number) => onStepClick(event, number)}
+              attentionIcon={attentionIcon}
+              pauseIcon={pauseIcon}
+              renderCustomStepCountLabel={renderCustomStepCountLabel}
+              renderCustomStateAriaLabel={renderCustomStateAriaLabel}
+              customSelectedAriaLabel={customSelectedAriaLabel}
             />
           );
         })}
       </div>
-      {labels.map((label, index) => {
-        return (
-          <Step
-            key={`${index}-${label}`} // eslint-disable-line react/no-array-index-key
-            label={label}
-            language={language}
-            number={index + 1}
-            small={small}
-            stepsTotal={stepsTotal}
-            selected={selectedStep === index + 1}
-            state={states[index]}
-            onStepClick={(event, number) => onStepClick(event, number)}
-            attentionIcon={attentionIcon}
-            pauseIcon={pauseIcon}
-            renderCustomStepCountLabel={renderCustomStepCountLabel}
-            renderCustomStateAriaLabel={renderCustomStateAriaLabel}
-            customSelectedAriaLabel={customSelectedAriaLabel}
-          />
-        );
-      })}
-    </div>
+      {stepHeading && (
+        <div
+          tabIndex={-1}
+          ref={stepHeadingRef}
+          role="heading"
+          aria-level={stepHeadingAriaLevel}
+          className={classNames(styles.heading, headingClassName)}
+        >
+          {getStepHeading(language, selectedStep, stepsTotal, labels[selectedStep - 1])}
+        </div>
+      )}
+    </>
   );
 };
