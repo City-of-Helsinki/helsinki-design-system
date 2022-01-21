d86f328a44026a2c25b3cb1d0f35ff55acce8cb5
Mika Nevalainen <mn.nevalainen@gmail.com>
2022-01-21 17:00:33 +0200

    Fix snapshot and axe test

--- packages/react/src/components/cookieConsent/test.util.ts
+++ packages/react/src/components/cookieConsent/test.util.ts
@@ -30,13 +30,27 @@ export function extractSetCookieArguments(
 
 export const getContent = (): Content => {
   return {
+    mainTitle: 'Evästesuostumukset',
+    mainText: `Tämä sivusto käyttää välttämättömiä evästeitä suorituskyvyn varmistamiseksi sekä yleisen käytön seurantaan.
+      Lisäksi käytämme kohdennusevästeitä käyttäjäkokemuksen parantamiseksi, analytiikkaan ja kohdistetun sisällön
+      näyttämiseen. Jatkamalla sivuston käyttöä ilman asetusten muuttamista hyväksyt välttämättömien evästeiden
+      käytön.`,
+    detailsTitle: 'Tietoa sivustolla käytetyistä evästeistä',
+    detailsText: `Sivustolla käytetyt evästeet on luokiteltu käyttötarkoituksen mukaan. Alla voit lukea tietoa jokaisesta
+      kategoriasta ja sallia tai kieltää evästeiden käytön.`,
+    requiredConsentsTitle: 'Välttämättömät evästeet',
+    requiredConsentsText:
+      'Välttämättömien evästeiden käyttöä ei voi kieltää. Ne mahdollistavat sivuston toiminnan ja vaikuttavat sivuston käyttäjäystävällisyyteen.',
+    optionalConsentsTitle: 'Muut evästeet',
+    optionalConsentsText: 'Voit hyväksyä tai jättää hyväksymättä muut evästeet.',
     consents: {},
-    approveAllConsents: 'approveAllConsents',
-    approveRequiredAndSelectedConsents: 'approveRequiredAndSelectedConsents',
-    approveOnlyRequiredConsents: 'approveOnlyRequiredConsents',
-    showSettings: 'showSettings',
-    hideSettings: 'hideSettings',
-    language: 'af',
-    languageOptions: [{ code: 'af', label: 'AF' }],
+    approveAllConsents: 'Hyväksy kaikki evästeet',
+    approveRequiredAndSelectedConsents: 'Hyväksy valitut ja pakolliset evästeet',
+    approveOnlyRequiredConsents: 'Hyväksy vain pakolliset evästeet',
+    showSettings: 'Näytä asetukset',
+    hideSettings: 'Piilota asetukset',
+    language: 'fi',
+    languageOptions: [{ code: 'fi', label: 'Suomeksi (FI)' }],
+    languageSelectorAriaLabel: 'Kieli: Suomi. Vaihda kieli. Change language. Ändra språk.',
   } as Content;
 };
