f25b21702b3d5c4758c9b0f9d84a51c8cddc05be
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2022-01-24 16:55:20 +0200

    Add basic unit tests

--- packages/react/src/components/stepper/Stepper.test.tsx
+++ packages/react/src/components/stepper/Stepper.test.tsx
@@ -2,15 +2,29 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { axe } from 'jest-axe';
 
-import { Stepper } from './MultiPageFormStepper';
+import { Stepper, StepperProps } from './Stepper';
 
-describe('<MultiPageFormStepper /> spec', () => {
+describe('<Stepper /> spec', () => {
+  let state;
+  let labels;
+
+  beforeEach(() => {
+    state = {
+      activeStep: 1,
+      states: ['available', 'disabled', 'disabled', 'disabled', 'disabled'] as StepperProps['states'],
+    };
+    labels = ['Step 1', 'Step 2', 'Step 3', 'Step 4 - longer text', 'Step 5'];
+  });
   it('renders the component', () => {
-    const { asFragment } = render(<Stepper />);
+    const { asFragment } = render(
+      <Stepper labels={labels} language="en" states={state.states} selectedStep={state.activeStep} stepsTotal={5} />,
+    );
     expect(asFragment()).toMatchSnapshot();
   });
   it('should not have basic accessibility issues', async () => {
-    const { container } = render(<Stepper />);
+    const { container } = render(
+      <Stepper labels={labels} language="en" states={state.states} selectedStep={state.activeStep} stepsTotal={5} />,
+    );
     const results = await axe(container);
     expect(results).toHaveNoViolations();
   });
