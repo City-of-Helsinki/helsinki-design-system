23567d93744e6934094fa422eb2136eb73ed2b39
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2022-02-03 17:12:02 +0200

    Update unit tests

--- packages/react/src/components/stepper/Stepper.test.tsx
+++ packages/react/src/components/stepper/Stepper.test.tsx
@@ -2,58 +2,87 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { axe } from 'jest-axe';
 
-import { Stepper, StepperProps } from './Stepper';
+import { Stepper } from './Stepper';
+import { State } from './Step';
 
 describe('<Stepper /> spec', () => {
   let state;
-  let labels;
 
   beforeEach(() => {
     // Need to mock scrollIntoView for jest
     window.HTMLElement.prototype.scrollIntoView = function () {}; // eslint-disable-line
     state = {
-      activeStep: 1,
-      states: ['available', 'disabled', 'disabled', 'disabled', 'disabled'] as StepperProps['states'],
+      activeStepIndex: 0,
+      steps: [
+        {
+          state: State.available,
+          label: 'Step 1',
+        },
+        {
+          state: State.disabled,
+          label: 'Step 2',
+        },
+        {
+          state: State.disabled,
+          label: 'Step 3',
+        },
+        {
+          state: State.disabled,
+          label: 'Step 4 - longer text',
+        },
+        {
+          state: State.disabled,
+          label: 'Step 5',
+        },
+      ],
     };
-    labels = ['Step 1', 'Step 2', 'Step 3', 'Step 4 - longer text', 'Step 5'];
   });
 
   it('renders the component', () => {
-    const { asFragment } = render(
-      <Stepper labels={labels} language="en" states={state.states} selectedStep={state.activeStep} stepsTotal={5} />,
-    );
+    const { asFragment } = render(<Stepper steps={state.steps} language="en" selectedStep={state.activeStepIndex} />);
     expect(asFragment()).toMatchSnapshot();
   });
 
   it('should be able to select available step', () => {
     const { container, rerender } = render(
-      <Stepper
-        labels={labels}
-        language="en"
-        states={['available', 'available', 'disabled', 'disabled', 'disabled']}
-        selectedStep={state.activeStep}
-        stepsTotal={5}
-      />,
+      <Stepper steps={state.steps} language="en" selectedStep={state.activeStepIndex} />,
     );
     const availableButton = container.querySelector('[data-testid="hds-stepper-step-1"]');
     expect(availableButton).toHaveAttribute('aria-current', 'false');
 
     rerender(
       <Stepper
-        labels={labels}
+        steps={[
+          {
+            state: State.available,
+            label: 'Step 1',
+          },
+          {
+            state: State.available,
+            label: 'Step 2',
+          },
+          {
+            state: State.disabled,
+            label: 'Step 3',
+          },
+          {
+            state: State.disabled,
+            label: 'Step 4 - longer text',
+          },
+          {
+            state: State.disabled,
+            label: 'Step 5',
+          },
+        ]}
         language="en"
-        states={['available', 'available', 'disabled', 'disabled', 'disabled']}
-        selectedStep={2}
-        stepsTotal={5}
+        selectedStep={1}
       />,
     );
     expect(availableButton).toHaveAttribute('aria-current', 'step');
   });
 
   it('should not have basic accessibility issues', async () => {
-    const { container } = render(
-      <Stepper labels={labels} language="en" states={state.states} selectedStep={state.activeStep} stepsTotal={5} />,
-    );
+    const { container } = render(<Stepper language="en" steps={state.steps} selectedStep={state.activeStepIndex} />);
     const results = await axe(container);
     expect(results).toHaveNoViolations();
   });
