c0a76fa050476a8875c3ad4d6cefacd22a95e354
Roni Jaakkola <roni.jaakkola@gmail.com>
2022-02-09 12:29:26 +0200

    Added spacing token documentation page to the new doc site

---
+++ new-site/src/docs/elements/design-tokens/spacing/components/layout-tokens-table.tsx
@@ -0,0 +1,27 @@
+import React from "react"
+
+import { Table } from 'hds-react';
+
+const cols = [
+  { key: 'cssVariable', headerName: 'CSS variable' },
+  { key: 'sassVariable', headerName: 'SASS variable' },
+  { key: 'pxValue', headerName: 'PX value' },
+  { key: 'remValue', headerName: 'REM value' },
+  { key: 'example', headerName: 'Example' },
+];
+
+const rows = [
+  { cssVariable: <code>--spacing-layout-2-xs</code>, sassVariable: <code>$spacing-layout-2-xs</code>, pxValue: '16px', remValue: '1rem', example: '' },
+  { cssVariable: <code>--spacing-layout-xs</code>, sassVariable: <code>$spacing-layout-xs</code>, pxValue: '24px', remValue: '1.5rem', example: '' },
+  { cssVariable: <code>--spacing-layout-s</code>, sassVariable: <code>$spacing-layout-s</code>, pxValue: '32px', remValue: '2rem', example: '' },
+  { cssVariable: <code>--spacing-layout-m</code>, sassVariable: <code>$spacing-layout-m</code>, pxValue: '48px', remValue: '3rem', example: '' },
+  { cssVariable: <code>--spacing-layout-l</code>, sassVariable: <code>$spacing-layout-l</code>, pxValue: '64px', remValue: '4rem', example: '' },
+  { cssVariable: <code>--spacing-layout-xl</code>, sassVariable: <code>$spacing-layout-xl</code>, pxValue: '96px', remValue: '6rem', example: '' },
+  { cssVariable: <code>--spacing-layout-2-xl</code>, sassVariable: <code>$spacing-layout-2-xl</code>, pxValue: '128px', remValue: '8rem', example: '' },
+];
+
+export default function LayoutTokensTable() {
+  return (
+    <Table cols={cols} rows={rows} heading="Layout tokens" headingAriaLevel={3} indexKey="id" renderIndexCol={false} />
+  );
+}
