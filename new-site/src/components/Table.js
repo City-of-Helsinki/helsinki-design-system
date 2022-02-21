4ab62ab0ee705bf8737583204df2ed625f52011c
Mika Nevalainen <mn.nevalainen@gmail.com>
2022-02-21 14:22:45 +0200

    Add markdown-table caption prosessing

--- new-site/src/components/Table.js
+++ new-site/src/components/Table.js
@@ -1,12 +1,52 @@
 import React from 'react';
 
-const Table = (props) => (
-  <div className="hds-table-container docTable">
-    <table className="hds-table hds-table--dark" aria-label="Service users (dark variant)">
-      {props.children}
-    </table>
-  </div>
-);
+const captionPrefix = '[';
+const captionSuffix = ']';
+
+const isCaption = (cellContent) =>
+  typeof cellContent === 'string' && cellContent.startsWith(captionPrefix) && cellContent.endsWith(captionSuffix);
+
+const resolveCaptionStrings = (captionString) => {
+  const withoutSuffixes = captionString.replace(captionPrefix, '').replace(captionSuffix, '');
+  return withoutSuffixes.split(':');
+};
+
+const Table = (props) => {
+  const thead = (props.children || []).find(({ props }) => props.originalType === 'thead');
+  const tbody = (props.children || []).find(({ props }) => props.originalType === 'tbody');
+  const tbodyRows = tbody.props?.children || [];
+  const [rows, captionString] = tbodyRows.reduce(
+    (acc, row, i, arr) => {
+      if (
+        i >= arr.length - 1 &&
+        row.props.children &&
+        row.props.children[0] &&
+        isCaption(row.props.children[0].props.children)
+      ) {
+        return [acc[0], row.props.children[0].props.children];
+      } else {
+        return [[...acc[0], row], acc[1]];
+      }
+    },
+    [[], ''],
+  );
+
+  const tableChildrenWithoutCaption = [thead, { ...tbody, props: { ...tbody.props, children: rows } }];
+  const [tableName, caption] = captionString ? resolveCaptionStrings(captionString) : [undefined, undefined];
+
+  return (
+    <div className="hds-table-container docTable">
+      <table className="hds-table hds-table--dark" aria-label={caption}>
+        {tableName && caption && (
+          <caption className="hds-table__caption">
+            <b>{tableName}</b>: {caption}
+          </caption>
+        )}
+        {tableChildrenWithoutCaption}
+      </table>
+    </div>
+  );
+};
 
 const Thead = (props) => {
   const rowWithClassName = {
