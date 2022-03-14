a1b7647c5d69bd9083a5e8414d3876acb1d198a8
Roni Jaakkola <roni.jaakkola@gmail.com>
2022-03-14 13:33:54 +0200

    Transformed Colour token accessibility tables from React to Markdown

---
+++ new-site/src/components/examples/FocusExample.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const FocusExample = ({ color, background, style = {}, children }) => (
+  <div
+    style={{
+      fontSize: 'var(--fontsize-body-l)',
+      width: 'var(--spacing-2-xl)',
+      height: 'var(--spacing-2-xl)',
+      alignItems: 'center',
+      justifyContent: 'center',
+      display: 'flex',
+      background: `${background}`,
+      ...style,
+    }}
+  >
+    <div
+      style={{
+        width: '60%',
+        height: '60%',
+        border: '3px solid',
+        borderColor: `${color}`,
+      }}>
+    </div>
+  </div>
+);
+
+FocusExample.propTypes = {
+  color: PropTypes.string.isRequired,
+  background: PropTypes.string.isRequired,
+  style: PropTypes.object,
+};
+
+export default FocusExample;
