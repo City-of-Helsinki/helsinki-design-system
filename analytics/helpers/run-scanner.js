f368a59e800d88cf1f7ff896f6bcb4bb09b28435
Mika Nevalainen <mn.nevalainen@gmail.com>
2022-04-06 13:42:13 +0300

    Update result file name format

--- analytics/helpers/run-scanner.js
+++ analytics/helpers/run-scanner.js
@@ -15,7 +15,7 @@ async function runScanner (offset, limit, resultsDir) {
     const totalStrWithoutMessage = totalLines.splice(1, totalLines.length).join('\n');
     const dateString = new Date().toJSON().split(/[:.]/).join('-');
     fs.writeFileSync(`${resultsDir}/${dateString}_HDS_counts_in_${limit - offset}_repos.json`, `${totalStrWithoutMessage}}`);
-    fs.writeFileSync(`${resultsDir}/${dateString}_HDS_components_and_props_in_${offset}-${limit}_repos.json`, `{${componentsStr}`);
+    fs.writeFileSync(`${resultsDir}/${dateString}_HDS_components_and_props_in_${limit - offset}_repos.json`, `{${componentsStr}`);
     console.log(successMessage)
     return stdout;
   } catch (e) {
