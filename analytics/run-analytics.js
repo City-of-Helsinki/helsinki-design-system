2095565bcc3d0096661b59aa2dfcb6d64cec360b
Mika Nevalainen <mn.nevalainen@gmail.com>
2022-04-06 13:11:34 +0300

    Reformat logging

--- analytics/run-analytics.js
+++ analytics/run-analytics.js
@@ -6,30 +6,31 @@ const reposDir = './tmp';
 const resultsDir = './lib';
 
 const runAnalytics = async () => {
-  console.log('Searching repos');
+  console.log('Searching repos...');
   const repos = await searchRepos();
   const validRepos = repos.filter(({ name }) => name !== 'helsinki-design-system');
   const sortedRepos = validRepos.sort((a, b) => a.name.localeCompare(b.name));
   const offset = process.env.OFFSET || 0;
   const limit = process.env.LIMIT || sortedRepos.length;
-  console.log('Repos total:', sortedRepos.length);
+  console.log(`Found ${sortedRepos.length} repo(s).`);
 
   const reposToClone = sortedRepos.slice(offset, limit);
-  console.log('Starting to clone repos. Total number of repos to clone:', reposToClone.length);
-  const names = await cloneRepos(reposToClone, reposDir);
-  console.log('Cloned repos, total:', names.length);
+  console.log(`Starting to clone ${reposToClone.length} repos...`);
+  const clonedRepoNames = await cloneRepos(reposToClone, reposDir);
+  console.log(`Cloned ${clonedRepoNames.length} repo(s).`);
 
-  console.log('Starting to scan repos');
+  console.log('Starting to scan repos. This might take a while...');
   await runScanner(offset, limit, resultsDir);
-  console.log('Scanner completed');
+  console.log('Scanning completed.');
 
   if (fs.existsSync(reposDir)) {
-    console.log('Starting to remove repo-folder', reposDir);
+    console.log(`Starting to remove temporary repo-folder ${reposDir} ...`);
     fs.rmSync(reposDir, { recursive: true, force: true });
-    console.log('Repo-folder removed');
+    console.log('Repo-folder removed.');
   }
 
-  console.log(`Results generated in the ${resultsDir} folder. Process completed`);
+  console.log(`Results are generated into ${resultsDir} folder.`);
+  console.log(`Process completed.`);
 };
 
 runAnalytics();
