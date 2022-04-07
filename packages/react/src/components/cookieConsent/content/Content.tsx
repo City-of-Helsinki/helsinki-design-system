288e40f5db5b416ce45e28f83af35b054ddde5bc
NikoHelle <niko.helle@hyperactive.fi>
2022-04-07 13:09:40 +0300

    cookie-consent Removed close button

--- packages/react/src/components/cookieConsent/content/Content.tsx
+++ packages/react/src/components/cookieConsent/content/Content.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from 'react';
 
 import { ViewProps } from '../types';
 import Buttons from '../buttons/Buttons';
-import { IconAngleDown, IconAngleUp, IconCross } from '../../../icons';
+import { IconAngleDown, IconAngleUp } from '../../../icons';
 import { useAccordion } from '../../accordion';
 import Details from '../details/Details';
 import styles from '../CookieConsent.module.scss';
@@ -16,12 +16,11 @@ function Content({ onClick }: ViewProps): React.ReactElement {
   });
   const content = useCookieConsentContent();
   const { sections, ui } = content.texts;
-  const { hideSettings, showSettings, approveRequiredAndSelectedConsents, approveOnlyRequiredConsents } = ui;
+  const { hideSettings, showSettings } = ui;
   const { title, text } = sections.main;
   const titleRef = useRef<HTMLHeadingElement>();
   const Icon = isOpen ? IconAngleUp : IconAngleDown;
   const settingsButtonText = isOpen ? hideSettings : showSettings;
-  const closeButtonTitle = isOpen ? approveRequiredAndSelectedConsents : approveOnlyRequiredConsents;
   useEffect(() => {
     if (titleRef.current) {
       titleRef.current.focus();
@@ -64,15 +63,6 @@ function Content({ onClick }: ViewProps): React.ReactElement {
         <Details />
       </Card>
       <Buttons onClick={onClick} hasOptionalConsents={isOpen} />
-      <button
-        type="button"
-        title={closeButtonTitle}
-        className={styles['close-button']}
-        data-testid="cookie-consent-close-button"
-        onClick={() => onClick('approveSelectedAndRequired')}
-      >
-        <IconCross aria-hidden />
-      </button>
     </div>
   );
 }
