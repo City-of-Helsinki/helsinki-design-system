06f9daddcada4680a37da37a78be2163c91ac492
NikoHelle <niko.helle@hyperactive.fi>
2022-05-16 14:11:45 +0300

    cookie-consent Better stories

--- packages/react/src/components/cookieConsent/CookieConsent.stories.tsx
+++ packages/react/src/components/cookieConsent/CookieConsent.stories.tsx
@@ -15,11 +15,248 @@ export default {
   args: {},
 };
 
-const getContentSource = (options: Partial<ContentSource>): ContentSource => {
-  const { language, currentLanguage, onAllConsentsGiven, onConsentsParsed } = options;
-  return {
+// args is required for docs tab to show source code
+// eslint-disable-next-line no-unused-vars,@typescript-eslint/no-unused-vars
+export const ModalVersion = (args) => {
+  const [language, setLanguage] = useState<SupportedLanguage>('fi');
+  const onLanguageChange = (newLang) => setLanguage(newLang);
+  const focusedElementAfterCloseId = 'focused-element-after-cookie-consent-closed';
+  const contentSource: ContentSource = {
+    siteName: 'Sivusto XXX',
+    currentLanguage: language,
+    groups: [
+      {
+        commonGroup: 'essential',
+        required: true,
+        cookies: [
+          {
+            commonCookie: 'tunnistamo',
+          },
+          {
+            id: 'loadbalancer',
+            name: 'Load balancer',
+            hostName: 'Host name',
+            path: 'Path',
+            description:
+              'Description lectus lacinia sed. Phasellus purus nisi, imperdiet id volutpat vel, pellentesque in ex. In pretium maximus finibus',
+            expiration: '1h',
+          },
+          {
+            commonCookie: 'language-i18n',
+          },
+        ],
+      },
+      {
+        commonGroup: 'marketing',
+        required: false,
+        cookies: [
+          {
+            commonCookie: 'marketing',
+          },
+        ],
+      },
+      {
+        commonGroup: 'preferences',
+        required: false,
+        cookies: [
+          {
+            id: 'preferences1',
+            name: 'Preference 1',
+            hostName: 'Host name',
+            path: 'Path',
+            description:
+              'Description lectus lacinia sed. Phasellus purus nisi, imperdiet id volutpat vel, pellentesque in ex. In pretium maximus finibus',
+            expiration: '1h',
+          },
+          {
+            id: 'preferences2',
+            name: 'Preference 2',
+            hostName: 'Host name',
+            path: 'Path',
+            description:
+              'Description lectus lacinia sed. Phasellus purus nisi, imperdiet id volutpat vel, pellentesque in ex. In pretium maximus finibus',
+            expiration: '1 years',
+          },
+          {
+            id: 'preferences3',
+            name: 'Preference 3',
+            hostName: 'Host name',
+            path: 'Path',
+            description:
+              'Description lectus lacinia sed. Phasellus purus nisi, imperdiet id volutpat vel, pellentesque in ex. In pretium maximus finibus',
+            expiration: '2h',
+          },
+        ],
+      },
+      {
+        commonGroup: 'statistics',
+        required: false,
+        cookies: [
+          {
+            commonCookie: 'matomo',
+          },
+          {
+            id: 'someOtherConsent',
+            name: 'Other consent',
+            hostName: 'Host name',
+            path: 'Path',
+            description:
+              'Description lectus lacinia sed. Phasellus purus nisi, imperdiet id volutpat vel, pellentesque in ex. In pretium maximus finibus',
+            expiration: '1h',
+          },
+        ],
+      },
+    ],
+    language: {
+      onLanguageChange,
+    },
+    onAllConsentsGiven: (consents) => {
+      if (consents.matomo) {
+        //  start tracking
+        // window._paq.push(['setConsentGiven']);
+        // window._paq.push(['setCookieConsentGiven']);
+      }
+      const focusEl = document.getElementById(focusedElementAfterCloseId);
+      if (focusEl) {
+        focusEl.focus();
+      }
+    },
+    onConsentsParsed: (consents, hasUserHandledAllConsents) => {
+      if (consents.matomo === undefined) {
+        // tell matomo to wait for consent:
+        // window._paq.push(['requireConsent']);
+        // window._paq.push(['requireCookieConsent']);
+      } else if (consents.matomo === false) {
+        // tell matomo to forget conset
+        // window._paq.push(['forgetConsentGiven']);
+      }
+      if (hasUserHandledAllConsents) {
+        // cookie consent modal will not be shown
+      }
+    },
+  };
+
+  const MatomoCookieTracker = () => {
+    const isMatomoCookieApproved = getConsentStatus('matomo');
+    return (
+      <div>
+        <p>Example how to track single consent.</p>
+        <p>Matomo cookie is {!isMatomoCookieApproved && <strong>NOT</strong>} set.*</p>
+        <small>* This won&apos;t change in real time</small>
+      </div>
+    );
+  };
+
+  const ForcePageScrollBarForModalTesting = () => {
+    return (
+      <div>
+        <div style={{ height: '100vh' }}>&nbsp;</div>
+        <p style={{ opacity: '0' }}>Bottom page</p>
+      </div>
+    );
+  };
+
+  const Application = () => {
+    const consentsInSource = pickConsentIdsFromContentSource(contentSource);
+    const willRenderCookieConsentDialog = !hasHandledAllConsents(
+      consentsInSource.required || [],
+      consentsInSource.optional || [],
+    );
+    return (
+      <div>
+        {/* eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex */}
+        <h1 id={focusedElementAfterCloseId} tabIndex={0}>
+          This is an example application with cookie consent modal
+        </h1>
+        <p>The modal will be shown when required. If user has given consents, it will not be shown.</p>
+        {willRenderCookieConsentDialog ? (
+          <>
+            <p>Consents are not given. Modal is shown.</p>
+          </>
+        ) : (
+          <>
+            <p>Cookie consents have been given. Remove the cookie to see the modal again.</p>
+          </>
+        )}
+        <MatomoCookieTracker />
+        <ForcePageScrollBarForModalTesting />
+      </div>
+    );
+  };
+
+  return (
+    <>
+      <ConsentsInModal contentSource={contentSource} />
+      <Application />
+    </>
+  );
+};
+
+// args is required for docs tab to show source code
+// eslint-disable-next-line no-unused-vars,@typescript-eslint/no-unused-vars
+export const SimpleModalVersion = (args) => {
+  const [language, setLanguage] = useState<SupportedLanguage>('fi');
+  const onLanguageChange = (newLang) => setLanguage(newLang);
+  const focusedElementAfterCloseId = 'focused-element-after-cookie-consent-closed';
+  const contentSource: ContentSource = {
+    siteName: `Site title ${language}`,
+    currentLanguage: language,
+    cookies: [
+      {
+        commonCookie: 'tunnistamo',
+        required: false,
+        commonGroup: 'essential',
+      },
+    ],
+    language: {
+      onLanguageChange,
+    },
+  };
+
+  const Application = () => {
+    const consentsInSource = pickConsentIdsFromContentSource(contentSource);
+    const willRenderCookieConsentDialog = !hasHandledAllConsents(
+      consentsInSource.required || [],
+      consentsInSource.optional || [],
+    );
+    return (
+      <div>
+        {/* eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex */}
+        <h1 id={focusedElementAfterCloseId} tabIndex={0}>
+          Simplest cookie consent example
+        </h1>
+        <p>This is an example how the modal is shown with minimal content parameters.</p>
+        <p>Note: it the cookie.required would be true, the modal would not be shown.</p>
+        {!willRenderCookieConsentDialog && (
+          <>
+            <p>Cookie consents have been given. Remove the cookie to see the modal again.</p>
+          </>
+        )}
+      </div>
+    );
+  };
+
+  return (
+    <>
+      <ConsentsInModal contentSource={contentSource} />
+      <Application />
+    </>
+  );
+};
+
+// args is required for docs tab to show source code
+// eslint-disable-next-line no-unused-vars,@typescript-eslint/no-unused-vars
+export const PageVersion = (args) => {
+  const contentSource: ContentSource = {
     siteName: 'Sivuston XXX',
-    currentLanguage,
+    currentLanguage: 'fi',
+    texts: {
+      sections: {
+        main: {
+          title: 'Cookie consents as a page!',
+        },
+      },
+    },
     groups: [
       {
         commonGroup: 'essential',
@@ -36,11 +273,9 @@ const getContentSource = (options: Partial<ContentSource>): ContentSource => {
             description:
               'Kuvaus lectus lacinia sed. Phasellus purus nisi, imperdiet id volutpat vel, pellentesque in ex. In pretium maximus finibus',
             expiration: 'Voimassaoloaika',
-            required: true,
           },
           {
             commonCookie: 'language-i18n',
-            required: true,
           },
         ],
       },
@@ -86,122 +321,152 @@ const getContentSource = (options: Partial<ContentSource>): ContentSource => {
         ],
       },
     ],
-    language,
-    onAllConsentsGiven,
-    onConsentsParsed,
-  };
-};
-
-// args is required for docs tab to show source code
-// eslint-disable-next-line no-unused-vars,@typescript-eslint/no-unused-vars
-export const ModalVersion = (args) => {
-  const [language, setLanguage] = useState<SupportedLanguage>('fi');
-  const onLanguageChange = (newLang) => setLanguage(newLang);
-  const focusedElementAfterCloseId = 'focused-element-after-cookie-consent-closed';
-
-  const contentSource = getContentSource({
-    currentLanguage: language,
-    language: {
-      onLanguageChange,
-    },
     onAllConsentsGiven: (consents) => {
-      if (consents.matomo) {
-        //  start tracking
-        // window._paq.push(['setConsentGiven']);
-        // window._paq.push(['setCookieConsentGiven']);
-      }
-      const focusEl = document.getElementById(focusedElementAfterCloseId);
-      if (focusEl) {
-        focusEl.focus();
+      // called when consents are saved
+      // handle changes like:
+      if (!consents.matomo) {
+        // stop matomo tracking
       }
     },
-    onConsentsParsed: (consents, hasUserHandledAllConsents) => {
-      if (consents.matomo === undefined) {
-        // tell matomo to wait for consent:
-        // window._paq.push(['requireConsent']);
-        // window._paq.push(['requireCookieConsent']);
-      } else if (consents.matomo === false) {
-        // tell matomo to forget conset
-        // window._paq.push(['forgetConsentGiven']);
-      }
-      if (hasUserHandledAllConsents) {
-        // cookie consent dialog will not be shown
-      }
-    },
-  });
-
-  const MatomoCookieTracker = () => {
-    const isMatomoCookieApproved = getConsentStatus('matomo');
-    return (
-      <div>
-        <p>Matomo cookie is {!isMatomoCookieApproved && <strong>NOT</strong>} set.*</p>
-        <small>* This won&apos;t change in real time</small>
-      </div>
-    );
-  };
-
-  const ForcePageScrollBarForModalTesting = () => {
-    return (
-      <div>
-        <div style={{ height: '100vh' }}>&nbsp;</div>
-        <p>Bottom page</p>
-      </div>
-    );
-  };
-
-  const Application = () => {
-    const consentsInSource = pickConsentIdsFromContentSource(contentSource);
-    const willRenderCookieConsentDialog = !hasHandledAllConsents(
-      consentsInSource.required || [],
-      consentsInSource.optional || [],
-    );
-    return (
-      <div>
-        {/* eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex */}
-        <h1 id={focusedElementAfterCloseId} tabIndex={0}>
-          This is a dummy application
-        </h1>
-        {willRenderCookieConsentDialog ? (
-          <>
-            <p>Cookie consent modal will be shown.</p>
-          </>
-        ) : (
-          <>
-            <p>Cookie consents have been given. Remove the cookie to see the dialog again.</p>
-          </>
-        )}
-        <MatomoCookieTracker />
-        <ForcePageScrollBarForModalTesting />
-      </div>
-    );
   };
 
   return (
-    <>
-      <ConsentsInModal contentSource={contentSource} />
-      <Application />
-    </>
+    <main>
+      <ConsentsInPage contentSource={contentSource} />
+    </main>
   );
 };
 
 // args is required for docs tab to show source code
 // eslint-disable-next-line no-unused-vars,@typescript-eslint/no-unused-vars
-export const PageVersion = (args) => {
-  const Application = () => {
-    const [language, setLanguage] = useState<SupportedLanguage>('fi');
-    const onLanguageChange = (newLang) => setLanguage(newLang);
-
-    const contentSource = getContentSource({ currentLanguage: language, language: { onLanguageChange } });
-    return (
-      <main>
-        <ConsentsInPage contentSource={contentSource} />
-      </main>
-    );
+export const CustomContentVersion = (args) => {
+  const contentSource: ContentSource = {
+    siteName: 'Not shown if main title is overridden',
+    currentLanguage: 'fi',
+    texts: {
+      sections: {
+        main: {
+          title: 'Custom main title',
+          text: 'Custom main preferences',
+        },
+        details: {
+          title: 'Custom details title',
+          text: 'Custom details preferences',
+        },
+      },
+      ui: {
+        showSettings: 'Custom show settings',
+        hideSettings: 'Custom hide settings',
+        approveAllConsents: 'Custom approve all consents',
+        approveRequiredAndSelectedConsents: 'Custom approve required and selected consents',
+        approveOnlyRequiredConsents: 'Custom approve only required consents',
+        settingsSaved: 'Custom settings saved text',
+      },
+      tableHeadings: {
+        name: 'Cookie custom name',
+        hostName: 'Host',
+        path: 'Where its saved',
+        description: 'Cookie purpose',
+        expiration: 'Lifespan',
+      },
+    },
+    requiredCookies: {
+      title: 'Overridden title of required cookies',
+      text: 'Overridden text of required cookies. The default text is not shown.',
+      checkboxAriaDescription: 'This is the overridden checkbox aria description of required cookies',
+      groups: [
+        {
+          commonGroup: 'essential',
+          title: 'Essential cookies custom title',
+          text: 'Essential cookies custom text',
+          checkboxAriaDescription: 'Custom checkbox aria description for essential cookies group',
+          expandAriaLabel: 'Custom expand button aria label for essential cookies group',
+          cookies: [
+            {
+              commonCookie: 'tunnistamo',
+              title: 'Tunnistamo cookie custom title',
+              text: 'Tunnistamo cookie custom text',
+            },
+            {
+              id: 'custom-cookie',
+              name: 'Custom cookie',
+              hostName: 'Host name',
+              path: 'Path',
+              description:
+                'Description lectus lacinia sed. Phasellus purus nisi, imperdiet id volutpat vel, pellentesque in ex. In pretium maximus finibus',
+              expiration: '1000 years',
+            },
+          ],
+        },
+        {
+          title: 'Custom cookie group title',
+          text: 'Custom cookie group text',
+          checkboxAriaDescription: 'Custom checkbox aria description for custom cookies group',
+          expandAriaLabel: 'Custom expand button aria label for custom cookies group',
+          cookies: [
+            {
+              commonCookie: 'marketing',
+            },
+          ],
+        },
+      ],
+    },
+    optionalCookies: {
+      title: 'Overridden title of optional cookies',
+      text: 'Overridden text of optional cookies. The default text is not shown.',
+      checkboxAriaDescription: 'This is the overridden checkbox aria description of optional cookies',
+      groups: [
+        {
+          title: 'Custom cookies group title',
+          text: 'Custom cookies group text',
+          checkboxAriaDescription: 'Custom checkbox aria description for custom cookies group',
+          expandAriaLabel: 'Custom expand button aria label for custom cookies group',
+          cookies: [
+            {
+              id: 'some-cookie',
+              name: 'Setting 1',
+              hostName: 'Host name',
+              path: 'Path',
+              description:
+                'Description lectus lacinia sed. Phasellus purus nisi, imperdiet id volutpat vel, pellentesque in ex. In pretium maximus finibus',
+              expiration: 'Sessio',
+            },
+            {
+              id: 'some-cookie2',
+              name: 'Setting 2',
+              hostName: 'Host name',
+              path: 'Path',
+              description:
+                'Description lectus lacinia sed. Phasellus purus nisi, imperdiet id volutpat vel, pellentesque in ex. In pretium maximus finibus',
+              expiration: 'Sessio',
+            },
+          ],
+        },
+        {
+          commonGroup: 'statistics',
+          cookies: [
+            {
+              commonCookie: 'matomo',
+              text: 'Custom matomo text',
+            },
+            {
+              id: 'someOtherConsent',
+              name: 'Other cookie',
+              hostName: 'Host name',
+              path: 'Path',
+              description:
+                'Description lectus lacinia sed. Phasellus purus nisi, imperdiet id volutpat vel, pellentesque in ex. In pretium maximus finibus',
+              expiration: 'Sessio',
+            },
+          ],
+        },
+      ],
+    },
   };
 
   return (
-    <>
-      <Application />
-    </>
+    <main>
+      <ConsentsInPage contentSource={contentSource} />
+    </main>
   );
 };
