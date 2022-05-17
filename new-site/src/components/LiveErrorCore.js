dede7c123730add81670c80b164772de1c856a80
Mika Nevalainen <mn.nevalainen@gmail.com>
2022-05-17 17:40:39 +0300

    Disable html-validate in Safari due missing regexp lookbehind support

--- new-site/src/components/LiveErrorCore.js
+++ new-site/src/components/LiveErrorCore.js
@@ -1,33 +1,61 @@
 import React, { useState, useEffect } from 'react';
-import { StaticConfigLoader, HtmlValidate } from "html-validate/dist/cjs/browser";
-import { default as html5 } from "html-validate/elements/html5.json";
+import { StaticConfigLoader, HtmlValidate } from 'html-validate/dist/cjs/browser';
+import { default as html5 } from 'html-validate/elements/html5.json';
 
-import './LiveErrorCore.scss'
+import './LiveErrorCore.scss';
 
-const loader = new StaticConfigLoader({
-  extends: ["html-validate:standard"],
-  elements: [html5],
-});
-const htmlvalidate = new HtmlValidate(loader);
+// https://stackoverflow.com/a/65896524
+const testRegexpLookAheadLookBehind = () => {
+  try {
+    return (
+      'foobarfoofoo'
+        .replace(new RegExp('(?<=foo)foo', 'g'), 'ahead')
+        .replace(new RegExp('foo(?!bar)', 'g'), 'behind') === 'foobarbehindahead'
+    );
+  } catch (error) {
+    return false;
+  }
+};
 
-const LiveErrorCore = ({code}) => {
+const getValidator = () => {
+  const loader = new StaticConfigLoader({
+    extends: ['html-validate:standard'],
+    elements: [html5],
+  });
+  return new HtmlValidate(loader);
+};
+
+/*
+ Html-validate library uses regexp lookbehind feature which is not supported in Safari at the moment (17.5.2022).
+ We need to disable validation in browsers which do not support lookbehind.
+ - https://caniuse.com/js-regexp-lookbehind
+ - https://gitlab.com/html-validate/html-validate/-/issues/147
+ */
+
+const htmlValidate = testRegexpLookAheadLookBehind() ? getValidator() : undefined;
+const isValidatorSupported = !!htmlValidate;
+
+const LiveErrorCore = ({ code }) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    const report = htmlvalidate.validateString(code);
-    if (report.valid) {
-      setError(null);
-    } else {
-      setError(report.results[0].messages[0]);
+    if (isValidatorSupported) {
+      const report = htmlValidate.validateString(code);
+      if (report.valid) {
+        setError(null);
+      } else {
+        setError(report.results[0].messages[0]);
+      }
     }
-  }, [code])
+  }, [code]);
 
+  if (!isValidatorSupported) {
+    return null;
+  }
   if (!error) {
     return null;
   }
-  return (
-    <pre className="live-error-core">SyntaxError: {`${error.message} (${error.line}:${error.column})`}</pre>
-  );
+  return <pre className="live-error-core">SyntaxError: {`${error.message} (${error.line}:${error.column})`}</pre>;
 };
 
 export default LiveErrorCore;
