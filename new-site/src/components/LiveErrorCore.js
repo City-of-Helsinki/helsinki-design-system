f9bc45a5568243204eafedaada4b27759af127a9
Mika Nevalainen <mn.nevalainen@gmail.com>
2022-05-18 08:49:16 +0300

    Refactor code to make it work in builds too

--- new-site/src/components/LiveErrorCore.js
+++ new-site/src/components/LiveErrorCore.js
@@ -1,45 +1,42 @@
 import React, { useState, useEffect } from 'react';
-import { StaticConfigLoader, HtmlValidate } from 'html-validate/dist/cjs/browser';
 import { default as html5 } from 'html-validate/elements/html5.json';
 
 import './LiveErrorCore.scss';
 
 // https://stackoverflow.com/a/65896524
-const testRegexpLookAheadLookBehind = () => {
+const supportsRegexpLookBehind = () => {
   try {
-    return (
-      'foobarfoofoo'
-        .replace(new RegExp('(?<=foo)foo', 'g'), 'ahead')
-        .replace(new RegExp('foo(?!bar)', 'g'), 'behind') === 'foobarbehindahead'
-    );
+    return 'foobarfoofoo'.replace(new RegExp('(?<=foo)foo', 'g'), 'behind') === 'foobarfoobehind';
   } catch (error) {
     return false;
   }
 };
 
-const getValidator = () => {
-  const loader = new StaticConfigLoader({
-    extends: ['html-validate:standard'],
-    elements: [html5],
-  });
-  return new HtmlValidate(loader);
-};
-
-/*
- Html-validate library uses regexp lookbehind feature which is not supported in Safari at the moment (17.5.2022).
- We need to disable validation in browsers which do not support lookbehind.
- - https://caniuse.com/js-regexp-lookbehind
- - https://gitlab.com/html-validate/html-validate/-/issues/147
- */
-
-const htmlValidate = testRegexpLookAheadLookBehind() ? getValidator() : undefined;
-const isValidatorSupported = !!htmlValidate;
-
 const LiveErrorCore = ({ code }) => {
   const [error, setError] = useState(null);
+  const [htmlValidate, setHtmlValidate] = useState(undefined);
+
+  /*
+    TODO: Remove lookbehind support test and async import once Safari supports regexp lookbehind.
+    Html-validate library uses regexp lookbehind feature which is not supported in Safari at the moment (17.5.2022).
+    We need to disable validation in browsers that do not support lookbehind.
+    - https://caniuse.com/js-regexp-lookbehind
+    - https://gitlab.com/html-validate/html-validate/-/issues/147
+ */
+  useEffect(() => {
+    if (supportsRegexpLookBehind()) {
+      import('html-validate/dist/cjs/browser').then(({ StaticConfigLoader, HtmlValidate }) => {
+        const loader = new StaticConfigLoader({
+          extends: ['html-validate:standard'],
+          elements: [html5],
+        });
+        setHtmlValidate(new HtmlValidate(loader));
+      });
+    }
+  }, []);
 
   useEffect(() => {
-    if (isValidatorSupported) {
+    if (htmlValidate) {
       const report = htmlValidate.validateString(code);
       if (report.valid) {
         setError(null);
@@ -49,7 +46,7 @@ const LiveErrorCore = ({ code }) => {
     }
   }, [code]);
 
-  if (!isValidatorSupported) {
+  if (!htmlValidate) {
     return null;
   }
   if (!error) {
