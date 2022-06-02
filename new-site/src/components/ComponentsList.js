b2ee2350f267da8b071b07eb59b4bf21f2ee58fd
Roni Jaakkola <roni.jaakkola@gmail.com>
2022-06-02 15:52:58 +0300

    Shortened component overview linkbox descriptions

--- new-site/src/components/ComponentsList.js
+++ new-site/src/components/ComponentsList.js
@@ -22,14 +22,14 @@ const ComponentsList = ({ data }) => {
     },
     {
       name: 'Card',
-      text: 'Cards can be used to divide and organise interface content for better understandability and readability.',
+      text: 'Cards can be used to divide and organise interface content.',
       linkboxAriaLabel: 'Card component',
       linkAriaLabel: 'Go to the Card component page',
       href: '/components/card'
     },
     {
       name: 'Checkbox',
-      text: 'Checkboxes are used to pick an option when one or more options can be chosen.',
+      text: 'Checkboxes are used to select from one of more options.',
       linkboxAriaLabel: 'Checkbox component',
       linkAriaLabel: 'Go to the Checkbox component page',
       href: '/components/checkbox'
@@ -57,28 +57,28 @@ const ComponentsList = ({ data }) => {
     },
     {
       name: 'Fieldset',
-      text: 'Fieldset groups multiple related form input fields together by giving them a common legend heading.',
+      text: 'Fieldset groups multiple related form input fields together.',
       linkboxAriaLabel: 'Fieldset component',
       linkAriaLabel: 'Go to the Fieldset component page',
       href: '/components/fieldset'
     },
     {
       name: 'FileInput',
-      text: 'A file input helps the user to browse and select one or multiple files to be uploaded to the service.',
+      text: 'A file input helps to browse and select files to be uploaded to the service.',
       linkboxAriaLabel: 'FileInput component',
       linkAriaLabel: 'Go to the FileInput component page',
       href: '/components/file-input'
     },
     {
       name: 'Footer',
-      text: 'The Footer component is located at the bottom of the page below the main body content.',
+      text: 'The footer is located at the bottom of the page below the main body content.',
       linkboxAriaLabel: 'Footer component',
       linkAriaLabel: 'Go to the Footer component page',
       href: '/components/footer'
     },
     {
       name: 'Icon',
-      text: 'Icons are used for providing visual cues and highlighting actions, denoting types and classes of information, and giving additional meaning to content.',
+      text: 'Icons are used for providing visual cues and highlighting actions.',
       linkboxAriaLabel: 'Icon component',
       linkAriaLabel: 'Go to the Icon component page',
       href: '/components/icon'
@@ -92,35 +92,35 @@ const ComponentsList = ({ data }) => {
     },
     {
       name: 'Link',
-      text: 'Links are used as navigational elements and can be used on their own or in inline with text.',
+      text: 'Links are used as navigational elements and can be standalone or inline.',
       linkboxAriaLabel: 'Link component',
       linkAriaLabel: 'Go to the Link component page',
       href: '/components/link'
     },
     {
       name: 'Linkbox',
-      text: 'Linkboxes are used to combine multiple elements into a single interactable element.',
+      text: 'Linkboxes are used to combine elements into a single interactable element.',
       linkboxAriaLabel: 'Linkbox component',
       linkAriaLabel: 'Go to the Linkbox component page',
       href: '/components/linkbox'
     },
     {
       name: 'LoadingSpinner',
-      text: 'Loading spinner is used for notifying users that their action is being processed or data retrieved from server.',
+      text: 'Loading spinner is used for notifying users that their action is being processed.',
       linkboxAriaLabel: 'LoadingSpinner component',
       linkAriaLabel: 'Go to the LoadingSpinner component page',
       href: '/components/loading-spinner'
     },
     {
       name: 'Logo',
-      text: 'The logo is used to identify the site or application as an official City of Helsinki service.',
+      text: 'The logo is used to identify the service as an official City of Helsinki service.',
       linkboxAriaLabel: 'Logo component',
       linkAriaLabel: 'Go to the Logo component page',
       href: '/components/logo'
     },
     {
       name: 'Navigation',
-      text: 'A navigation component is the main way for users to navigate and locate themselves when using a service.',
+      text: 'A navigation component is the main way to navigate and locate in the service.',
       linkboxAriaLabel: 'Navigation component',
       linkAriaLabel: 'Go to the Navigation component page',
       href: '/components/navigation'
@@ -141,7 +141,7 @@ const ComponentsList = ({ data }) => {
     },
     {
       name: 'PasswordInput',
-      text: 'A password field is a foundational input field component that the user can interact with and specifically input password data.',
+      text: 'A password field is an input field component that is used to input password data.',
       linkboxAriaLabel: 'PasswordInput component',
       linkAriaLabel: 'Go to the PasswordInput component page',
       href: '/components/password-input'
@@ -169,14 +169,14 @@ const ComponentsList = ({ data }) => {
     },
     {
       name: 'SelectionGroup',
-      text: 'Selection group allows grouping related form selection elements (such as Checkboxes) to each other.',
+      text: 'Selection group allows grouping form selection elements to each other.',
       linkboxAriaLabel: 'SelectionGroup component',
       linkAriaLabel: 'Go to the SelectionGroup component page',
       href: '/components/selection-group'
     },
     {
       name: 'SideNavigation',
-      text: 'A side navigation is an additional navigation element that is located at the left-hand side of the viewport.',
+      text: 'A side navigation is an additional navigation located on the left of the viewport.',
       linkboxAriaLabel: 'SideNavigation component',
       linkAriaLabel: 'Go to the SideNavigation component page',
       href: '/components/side-navigation'
@@ -190,7 +190,7 @@ const ComponentsList = ({ data }) => {
     },
     {
       name: 'Stepper',
-      text: 'If a form is divided into multiple pages or steps, the Form progress component helps the user to understand the progression and to navigate between form steps.',
+      text: 'The stepper helps to see the progression and to navigate between form steps.',
       linkboxAriaLabel: 'Stepper component',
       linkAriaLabel: 'Go to the Stepper component page',
       href: '/components/stepper'
@@ -204,7 +204,7 @@ const ComponentsList = ({ data }) => {
     },
     {
       name: 'Tabs',
-      text: 'Tabs are used to help the user browse and navigate between information contents that have a relation.',
+      text: 'Tabs are used to navigate between contents that have a relation.',
       linkboxAriaLabel: 'Tabs component',
       linkAriaLabel: 'Go to the Tabs component page',
       href: '/components/tabs'
@@ -218,14 +218,14 @@ const ComponentsList = ({ data }) => {
     },
     {
       name: 'TextArea',
-      text: 'A text area is a specialised version of the text input component. Text areas can be used for multiline answers.',
+      text: 'A text area is an input used for multiline answers.',
       linkboxAriaLabel: 'TextArea component',
       linkAriaLabel: 'Go to the TextArea component page',
       href: '/components/text-area'
     },
     {
       name: 'TextInput',
-      text: 'A text field is an input field that the user can interact with and input content and data.',
+      text: 'A text field is an input field that the user can use to input content and data.',
       linkboxAriaLabel: 'TextInput component',
       linkAriaLabel: 'Go to the TextInput component page',
       href: '/components/text-input'
@@ -239,7 +239,7 @@ const ComponentsList = ({ data }) => {
     },
     {
       name: 'ToggleButton',
-      text: 'Toggle button allows the user to switch between two distinct states such as "On" and "Off".',
+      text: 'Toggle button allows to switch between two states such as "On" and "Off".',
       linkboxAriaLabel: 'ToggleButton component',
       linkAriaLabel: 'Go to the ToggleButton component page',
       href: '/components/toggle-button'
