98406176da48482887edd642db3452400523b020
NikoHelle <niko.helle@hyperactive.fi>
2022-06-03 11:39:00 +0300

    cookie-consent) cookie-consent Renamed example components used in stories
    Should have "Cookie" mentioned.

--- packages/react/src/components/cookieConsent/cookieModal/CookieModal.tsx
+++ packages/react/src/components/cookieConsent/cookieModal/CookieModal.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 
-import { Page } from '../page/Page';
+import { Modal } from '../modal/Modal';
 import { ContentSource } from '../content.builder';
 import { Provider as CookieContextProvider } from '../CookieConsentContext';
 
-export function ConsentsInPage(props: {
-  contentSource: ContentSource;
-  cookieDomain?: string;
-}): React.ReactElement | null {
+export function CookieModal(props: { contentSource: ContentSource; cookieDomain?: string }): React.ReactElement | null {
   const { cookieDomain, contentSource } = props;
   return (
     <CookieContextProvider contentSource={contentSource} cookieDomain={cookieDomain}>
-      <Page />
+      <Modal />
     </CookieContextProvider>
   );
 }
