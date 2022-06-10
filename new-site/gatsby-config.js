7102a44cdb978387da39c9e7e5403a7cb5e3f892
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2022-06-10 13:02:34 +0300

    Add robots.txt to new documentation site to improve seo

--- new-site/gatsby-config.js
+++ new-site/gatsby-config.js
@@ -198,5 +198,11 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        policy: [{userAgent: '*', disallow: '/v1'}]
+      }
+    }
   ],
 };
