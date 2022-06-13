b010833ae7af26281ec64d40c1a829b95418fe32
Mika Nevalainen <mn.nevalainen@gmail.com>
2022-06-13 17:10:45 +0300

    Add hero component

---
+++ new-site/src/components/Hero.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { withPrefix } from 'gatsby';
+import { Koros } from 'hds-react';
+
+import './Hero.scss';
+import PropTypes from 'prop-types';
+
+const Hero = ({ children, backgroundImageUrl, title, text }) => {
+  return (
+    <div className={`hero`} style={{ backgroundImage: `url(${backgroundImageUrl})`}}>
+      <div className="hero-content">
+        <div className="hero-content-text">
+          <div className="hero-content-shape" />
+          <h1 className="hero-title">{title}</h1>
+          <p className="hero-text">{text}</p>
+          {children}
+        </div>
+      </div>
+      <Koros className="hero-koros hero-koros-horizontal" flipHorizontal />
+      <Koros className="hero-koros hero-koros-rotated" flipHorizontal rotate="45deg" />
+    </div>
+  );
+};
+
+Hero.propTypes = {
+  className: PropTypes.string,
+  backgroundImageUrl: PropTypes.string,
+  title: PropTypes.string,
+  text: PropTypes.string,
+  children: PropTypes.node,
+};
+
+export default Hero;
