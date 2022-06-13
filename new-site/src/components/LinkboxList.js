aae584c53bd4e45a49a1114e744ceff8974b263d
Mika Nevalainen <mn.nevalainen@gmail.com>
2022-06-13 17:47:37 +0300

    Add possibility to pass a className to linkboxlist

--- new-site/src/components/LinkboxList.js
+++ new-site/src/components/LinkboxList.js
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import { navigate } from 'gatsby';
 import { Linkbox } from 'hds-react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 import './LinkboxList.scss';
 
-const LinkboxList = ({ data}) => {
+const LinkboxList = ({ data, className }) => {
   return (
-    <div className="linkbox-list">
-      {data.map(( item ) => {
+    <div className={`linkbox-list ${className || ''}`}>
+      {data.map((item) => {
         return (
           <div key={item.name} className="linkbox-list-item">
             <Linkbox
@@ -33,12 +33,15 @@ const LinkboxList = ({ data}) => {
 };
 
 LinkboxList.propTypes = {
-  data: PropTypes.arrayOf(PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    linkboxAriaLabel: PropTypes.string.isRequired,
-    href: PropTypes.string.isRequired,
-    text: PropTypes.string.isRequired,
-  }))
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      linkboxAriaLabel: PropTypes.string.isRequired,
+      href: PropTypes.string.isRequired,
+      text: PropTypes.string.isRequired,
+    }),
+  ),
+  className: PropTypes.string,
 };
 
 export default LinkboxList;
