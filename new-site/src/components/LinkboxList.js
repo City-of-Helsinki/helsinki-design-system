0430e7381e483c4c4623d5e6a963ff21c68825e3
Mika Nevalainen <mn.nevalainen@gmail.com>
2022-06-14 10:42:08 +0300

    Add support for adding image to LinkBoxlist

--- new-site/src/components/LinkboxList.js
+++ new-site/src/components/LinkboxList.js
@@ -1,39 +1,38 @@
 import * as React from 'react';
-import { navigate } from 'gatsby';
+import { navigate, withPrefix } from 'gatsby';
 import { Linkbox } from 'hds-react';
 import PropTypes from 'prop-types';
 
 import './LinkboxList.scss';
 
-const LinkboxList = ({ data, className }) => {
-  return (
-    <div className={`linkbox-list ${className || ''}`}>
-      {data.map((item) => {
-        return (
-          <div key={item.name} className="linkbox-list-item">
-            <Linkbox
-              withBorder
-              className="linkbox-list-link"
-              size="small"
-              linkboxAriaLabel={item.linkboxAriaLabel}
-              linkAriaLabel={item.linkAriaLabel}
-              href={item.href}
-              external={item.external}
-              heading={item.name}
-              text={item.text}
-              onClick={(event) => {
-                if(!item.external) {
-                  event.preventDefault();
-                  navigate(item.href);
-                }
-              }}
-            />
-          </div>
-        );
-      })}
-    </div>
-  );
-};
+const LinkboxList = ({ data, className }) => (
+  <div className={`linkbox-list ${className || ''}`}>
+    {data.map((item) => {
+      return (
+        <div key={item.name} className="linkbox-list-item">
+          <Linkbox
+            withBorder
+            className="linkbox-list-link"
+            size="small"
+            linkboxAriaLabel={item.linkboxAriaLabel}
+            linkAriaLabel={item.linkAriaLabel}
+            href={item.href}
+            external={item.external}
+            heading={item.name}
+            text={item.text}
+            imgProps={item.imgProps ? { ...item.imgProps, src: withPrefix(item.imgProps.src) } : undefined}
+            onClick={(event) => {
+              if (!item.external) {
+                event.preventDefault();
+                navigate(item.href);
+              }
+            }}
+          />
+        </div>
+      );
+    })}
+  </div>
+);
 
 LinkboxList.propTypes = {
   data: PropTypes.arrayOf(
@@ -43,6 +42,11 @@ LinkboxList.propTypes = {
       href: PropTypes.string.isRequired,
       text: PropTypes.string.isRequired,
       external: PropTypes.bool,
+      imgProps: PropTypes.shape({
+        src: PropTypes.string.isRequired,
+        width: PropTypes.number.isRequired,
+        height: PropTypes.number.isRequired,
+      }),
     }),
   ),
   className: PropTypes.string,
