db1c9eea67d06cd6cbc03ba2c2bc4db849525b77
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2022-06-15 11:39:40 +0300

    Add favicon to new documentation site

--- new-site/gatsby-config.js
+++ new-site/gatsby-config.js
@@ -191,7 +191,8 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `static/favicon/transparent_light_favicon_package/favicon-32x32.png`, // This path is relative to the root of the site.
+        include_favicon: false,
       },
     },
     `gatsby-plugin-sass`,
