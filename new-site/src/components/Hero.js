072bf39f16e1955319ade32741db9cd035708bb6
Mika Nevalainen <mn.nevalainen@gmail.com>
2022-06-15 14:47:08 +0300

    Make hero to take all the available screen space in larger screens

--- new-site/src/components/Hero.js
+++ new-site/src/components/Hero.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import { withPrefix } from 'gatsby';
-import { Koros } from 'hds-react';
+import { Koros, Container } from 'hds-react';
 import PropTypes from 'prop-types';
 
 import './Hero.scss';
 
 const Hero = ({ children, backgroundImageUrl, title, text }) => {
   return (
-    <div className={`hero`} style={{ backgroundImage: `url(${withPrefix(backgroundImageUrl)})` }}>
-      <div className="hero-content">
-        <div className="hero-content-text">
-          <div className="hero-content-shape" />
-          <h1 className="hero-title">{title}</h1>
-          <p className="hero-text">{text}</p>
-          {children}
+    <div className="hero-container">
+      <Container className="hero-wrapper">
+        <div className="hero">
+          <div className="hero-content">
+            <div className="hero-content-text">
+              <div className="hero-content-shape" />
+              <h1 className="hero-title">{title}</h1>
+              <p className="hero-text">{text}</p>
+              {children}
+            </div>
+          </div>
+          <Koros className="hero-koros hero-koros-rotated" flipHorizontal rotate="45deg" />
+          <Koros className="hero-koros hero-koros-horizontal" flipHorizontal />
         </div>
-      </div>
-      <Koros className="hero-koros hero-koros-rotated" flipHorizontal rotate="45deg" />
-      <Koros className="hero-koros hero-koros-horizontal" flipHorizontal />
+      </Container>
+      <div className="hero-bg-image" style={{ backgroundImage: `url(${withPrefix(backgroundImageUrl)})` }} />
     </div>
   );
 };
