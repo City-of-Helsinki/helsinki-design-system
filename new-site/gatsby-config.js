0fff92b104652ad7370ad8097b76d224c6dc8257
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2022-06-20 12:45:40 +0300

    Fix manifest icon path

--- new-site/gatsby-config.js
+++ new-site/gatsby-config.js
@@ -193,12 +193,12 @@ module.exports = {
         display: `minimal-ui`,
         icons: [
           {
-            src: "static/favicon/transparent_light_favicon_package/android-chrome-192x192.png",
+            src: `${process.env.PATH_PREFIX ? process.env.PATH_PREFIX : ''}/favicon/transparent_light_favicon_package/android-chrome-192x192.png`,
             sizes: "192x192",
             type: "image/png"
           },
           {
-            src: "static/favicon/transparent_light_favicon_package/android-chrome-256x256.png",
+            src: `${process.env.PATH_PREFIX ? process.env.PATH_PREFIX : ''}/favicon/transparent_light_favicon_package/android-chrome-256x256.png`,
             sizes: "256x256",
             type: "image/png"
           }
