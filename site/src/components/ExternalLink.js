414137591c60cd812c51928b059dd83519447877
Mika Nevalainen <mn.nevalainen@gmail.com>
2022-06-28 14:27:15 +0300

    Add support for size to site's ExternalLink

--- site/src/components/ExternalLink.js
+++ site/src/components/ExternalLink.js
@@ -2,11 +2,17 @@ import React from 'react';
 import { Link } from 'hds-react';
 import PropTypes from 'prop-types';
 
-const ExternalLink = ({ href, children, openInNewTab }) => {
+const ExternalLink = ({ href, children, openInNewTab, size }) => {
   const openInNewTabProps = openInNewTab ? { openInNewTab, openInNewTabAriaLabel: 'Opens in a new tab.' } : {};
 
   return (
-    <Link href={href} openInExternalDomainAriaLabel="Opens a different website." external {...openInNewTabProps}>
+    <Link
+      href={href}
+      openInExternalDomainAriaLabel="Opens a different website."
+      external
+      {...openInNewTabProps}
+      size={size}
+    >
       {children}
     </Link>
   );
@@ -16,6 +22,7 @@ ExternalLink.propTypes = {
   href: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
   openInNewTab: PropTypes.bool,
+  size: PropTypes.string,
 };
 
 export default ExternalLink;
