af66ee1043610b6480d6d6c6145026c42c1eb85c
Niko Helle <niko.hele@hyperactive.fi>
2022-08-10 15:18:55 +0300

    Revert Modal to its simple version without React.portal

--- packages/react/src/components/cookieConsent/modal/Modal.tsx
+++ packages/react/src/components/cookieConsent/modal/Modal.tsx
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import ReactDOM from 'react-dom';
 import { VisuallyHidden } from '@react-aria/visually-hidden';
 
 import classNames from '../../../utils/classNames';
 import styles from '../CookieConsent.module.scss';
 import { useCookieContentContext, useUiTexts } from '../contexts/ContentContext';
-import { useCookieConsentContext, forceFocusToElement } from '../contexts/ConsentContext';
+import { useCookieConsentContext } from '../contexts/ConsentContext';
 import { Content } from '../content/Content';
 
 export function Modal(): React.ReactElement | null {
@@ -19,47 +18,9 @@ export function Modal(): React.ReactElement | null {
   // if hasUserHandledAllConsents() was false at first and then later true, user must have saved consents.
   const showScreenReaderSaveNotification = isModalInitiallyShown && !shouldShowModal;
   const { settingsSaved } = useUiTexts();
-  const containerId = 'HdsCookieConsentContainer';
-  const getContainerElement = (): HTMLElement | null => document.getElementById(containerId);
-  const [isDomReady, setIsDomReady] = useState<boolean>(false);
-
   useEffect(() => {
     setTimeout(() => setPopupTimerComplete(true), popupDelayInMs);
   }, []);
-
-  useEffect(() => {
-    const containerElement = getContainerElement();
-    if (shouldShowModal && !isDomReady) {
-      if (!containerElement) {
-        const htmlContainer = document.createElement('div');
-        htmlContainer.setAttribute('id', containerId);
-        htmlContainer.setAttribute('data-testid', 'html-cookie-consent-container');
-        document.body.insertBefore(htmlContainer, document.body.firstChild);
-      }
-      setIsDomReady(true);
-    } else if (!shouldShowModal && isDomReady) {
-      if (containerElement) {
-        document.body.removeChild(containerElement);
-      }
-      setIsDomReady(false);
-    }
-  }, [shouldShowModal, isDomReady, setIsDomReady]);
-
-  // focus target selector on esc key press
-  useEffect(() => {
-    const handleEscKey = (event: KeyboardEvent) => {
-      const key = event.key || event.keyCode;
-      if (key === 'Escape' || key === 'Esc' || key === 27) {
-        forceFocusToElement(contentContext.focusTargetSelector);
-      }
-    };
-    if (isDomReady) {
-      document.addEventListener('keyup', handleEscKey);
-    }
-    return () => {
-      document.removeEventListener('keyup', handleEscKey);
-    };
-  }, [isDomReady, contentContext.focusTargetSelector]);
 
   if (showScreenReaderSaveNotification) {
     return (
@@ -71,11 +32,11 @@ export function Modal(): React.ReactElement | null {
     );
   }
 
-  if (!shouldShowModal || !isDomReady) {
+  if (!shouldShowModal) {
     return null;
   }
 
-  const renderModal = (): JSX.Element => (
+  return (
     <div
       className={classNames(styles.container, styles.modal, popupTimerComplete && styles.animateIn)}
       data-testid="cookie-consent"
@@ -83,6 +44,4 @@ export function Modal(): React.ReactElement | null {
       <div className={styles.aligner}>{popupTimerComplete && <Content />}</div>
     </div>
   );
-
-  return ReactDOM.createPortal(renderModal(), getContainerElement());
 }
