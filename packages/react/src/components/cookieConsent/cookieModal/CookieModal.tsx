9fa394cb6c2e5221095f9ad261068fb54be07819
Niko Helle <niko.hele@hyperactive.fi>
2022-08-22 13:22:15 +0300

    Use new Context component instead of CookieContextProvider

--- packages/react/src/components/cookieConsent/cookieModal/CookieModal.tsx
+++ packages/react/src/components/cookieConsent/cookieModal/CookieModal.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 import { Modal } from '../modal/Modal';
 import { ContentSource } from '../content.builder';
-import { Provider as CookieContextProvider } from '../CookieConsentContext';
+import { Context } from '../contexts/ContextComponent';
 
 export function CookieModal(props: { contentSource: ContentSource; cookieDomain?: string }): React.ReactElement | null {
   const { cookieDomain, contentSource } = props;
   return (
-    <CookieContextProvider contentSource={contentSource} cookieDomain={cookieDomain}>
+    <Context contentSource={contentSource} cookieDomain={cookieDomain}>
       <Modal />
-    </CookieContextProvider>
+    </Context>
   );
 }
