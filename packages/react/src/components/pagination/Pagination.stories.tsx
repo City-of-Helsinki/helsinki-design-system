e53ab65c6c8432473c715d7ad110c8577a501aba
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2022-08-22 17:06:23 +0300

    Add possibility to show pagination without prev and next buttons

--- packages/react/src/components/pagination/Pagination.stories.tsx
+++ packages/react/src/components/pagination/Pagination.stories.tsx
@@ -39,6 +39,25 @@ export const WithTruncation = (args) => (
 
 // args is required for docs tab to show source code
 // eslint-disable-next-line no-unused-vars,@typescript-eslint/no-unused-vars
+export const withoutPrevAndNextButtons = (args) => {
+  return (
+    <Pagination
+      hideNextButton
+      hidePrevButton
+      onChange={() => null}
+      paginationAriaLabel="Pagination"
+      pageCount={68}
+      pageIndex={7}
+      pageHref={() => '#'}
+      language="en"
+    />
+  );
+};
+
+withoutPrevAndNextButtons.storyName = 'Without prev and next buttons';
+
+// args is required for docs tab to show source code
+// eslint-disable-next-line no-unused-vars,@typescript-eslint/no-unused-vars
 export const InteractableExample = (args) => {
   const [pageIndex, setPageIndex] = useState<number>(5);
 
