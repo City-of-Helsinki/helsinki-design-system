f8defeb097ccec924e168886c921ff3c2a144758
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2022-08-22 11:59:52 +0300

    Initial implementation of the react pagination component

---
+++ packages/react/src/components/pagination/Pagination.stories.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+
+import { Pagination } from './Pagination';
+
+export default {
+  component: Pagination,
+  title: 'Components/Pagination',
+  parameters: {
+    controls: { expanded: true },
+  },
+  args: {},
+};
+
+// args is required for docs tab to show source code
+// eslint-disable-next-line no-unused-vars,@typescript-eslint/no-unused-vars
+export const Basic = (args) => (
+  <Pagination
+    onChange={() => null}
+    paginationAriaLabel="Pagination"
+    pageCount={5}
+    pageIndex={0}
+    pageHref={() => '#'}
+    language="en"
+  />
+);
+
+// args is required for docs tab to show source code
+// eslint-disable-next-line no-unused-vars,@typescript-eslint/no-unused-vars
+export const WithTruncation = (args) => (
+  <Pagination
+    onChange={() => null}
+    paginationAriaLabel="Pagination"
+    pageCount={68}
+    pageIndex={7}
+    pageHref={() => '#'}
+    language="en"
+  />
+);
+
+// args is required for docs tab to show source code
+// eslint-disable-next-line no-unused-vars,@typescript-eslint/no-unused-vars
+export const InteractableExample = (args) => {
+  const [pageIndex, setPageIndex] = useState<number>(12);
+
+  return (
+    <Pagination
+      onChange={(event, index) => {
+        event.preventDefault();
+        setPageIndex(index);
+      }}
+      paginationAriaLabel="Pagination"
+      pageCount={25}
+      pageIndex={pageIndex}
+      pageHref={() => '#'}
+      siblingCount={1}
+      language="en"
+    />
+  );
+};
