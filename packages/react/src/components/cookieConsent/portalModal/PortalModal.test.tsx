b0a69bb076bf33f41685369437d4406bf50a13f6
Niko Helle <niko.hele@hyperactive.fi>
2022-09-05 12:41:10 +0300

    Merged Portal.test to PortalModal.test

--- packages/react/src/components/cookieConsent/portalModal/PortalModal.test.tsx
+++ packages/react/src/components/cookieConsent/portalModal/PortalModal.test.tsx
@@ -30,9 +30,16 @@ const mockedCookieControls = mockDocumentCookie();
 
 let content: Content;
 
+const testIds = {
+  content: 'cookie-consent',
+  closeButton: 'cookie-consent-approve-button',
+  target: 'PortalTarget',
+  targetWithChildren: 'PortalTargetWithChildren',
+};
+
 const renderCookieConsent = (
   { requiredConsents = [], optionalConsents = [], consents = {}, contentSourceOverrides }: TestConsentData,
-  withRealTimers = false,
+  portalProps: { rootId?: string; withRealTimers?: boolean } = { withRealTimers: false },
 ): RenderResult => {
   // inject unknown consents to verify those are
   // stored and handled, but not required or optional
@@ -40,16 +47,21 @@ const renderCookieConsent = (
     ...consents,
     ...unknownConsents,
   };
+  const { rootId } = portalProps;
   const contentSource = getContentSource(requiredConsents, optionalConsents, contentSourceOverrides);
   content = createContent(contentSource);
   jest.useFakeTimers();
   mockedCookieControls.init({ [COOKIE_NAME]: JSON.stringify(consentCookieWithInjectedUnknowns) });
   const result = render(
     <div>
-      <PortalModal contentSource={contentSource} />
+      <PortalModal contentSource={contentSource} rootId={rootId} />
       <button id="focus-target" type="button">
         Focus me
       </button>
+      <div id={testIds.target} data-testid={testIds.target} />
+      <div id={testIds.targetWithChildren} data-testid={testIds.targetWithChildren}>
+        <p>I am here to prevent auto-removal</p>
+      </div>
     </div>,
   );
   act(() => {
@@ -57,7 +69,7 @@ const renderCookieConsent = (
   });
 
   // For example, axe uses timers so sometimes the test must use real ones
-  if (withRealTimers) {
+  if (portalProps.withRealTimers) {
     jest.useRealTimers();
   }
 
@@ -79,7 +91,7 @@ describe('<PortalModal /> spec', () => {
   });
 
   it('should not have basic accessibility issues', async () => {
-    const result = renderCookieConsent(defaultConsentData, true);
+    const result = renderCookieConsent(defaultConsentData, { withRealTimers: true });
     await openAccordion(result, dataTestIds.settingsToggler);
     await openAllAccordions(result, content, dataTestIds);
     const results = await axe(result.container);
@@ -127,16 +139,73 @@ describe('<PortalModal /> ', () => {
 
   describe('Auto-focusing works also with portal and focus is', () => {
     it('shifted to the modal heading level 1 when modal is rendered', () => {
-      const result = renderCookieConsent(defaultConsentData, true);
+      const result = renderCookieConsent(defaultConsentData, { withRealTimers: true });
       const modalH1 = screen.queryByRole('heading', { level: 1 });
       expect(getActiveElement(result.container)).toEqual(modalH1);
     });
 
     it('shifted to the element defined in content.focusTargetSelector when modal is closed', async () => {
-      const result = renderCookieConsent(defaultConsentData, true);
+      const result = renderCookieConsent(defaultConsentData, { withRealTimers: true });
       const elementGetter = () => result.container.querySelector(content.focusTargetSelector as string);
       clickElement(result, dataTestIds.approveButton);
       await waitForElementFocus(elementGetter);
+    });
+  });
+  describe('Is rendered to the given element. Other elements in the dom are left alone.', () => {
+    it('If element with given id (rootId) is found, portal and its children are rendered there', () => {
+      const result = renderCookieConsent(defaultConsentData, { rootId: testIds.target });
+      const targetElement = result.getByTestId(testIds.target);
+      const contentElement = result.getByTestId(testIds.content);
+      // not using expect(elementA).toBe(elementB), if it fails it throws unrelated error
+      // https://github.com/testing-library/dom-testing-library/issues/875
+      expect(contentElement.parentElement === targetElement).toBeTruthy();
+      expect(() => result.getByTestId(testIds.targetWithChildren)).not.toThrow();
+    });
+    it('If element with given id (rootId) is not found, the element is created and portal and its children are rendered there', async () => {
+      const result = renderCookieConsent(defaultConsentData);
+      const targetElement = result.getByTestId(dataTestIds.htmlContainer);
+      const contentElement = result.getByTestId(testIds.content);
+      expect(contentElement.parentElement === targetElement).toBeTruthy();
+      expect(() => result.getByTestId(testIds.targetWithChildren)).not.toThrow();
+    });
+    it('Added portal is the first element in the dom', async () => {
+      const result = renderCookieConsent(defaultConsentData);
+      const targetElement = result.getByTestId(dataTestIds.htmlContainer);
+      expect(!!targetElement).toBeTruthy();
+      expect(targetElement === result.baseElement.childNodes[0]).toBeTruthy();
+    });
+  });
+  describe('If modal should not be shown', () => {
+    const onlyRequiredConsents = {
+      requiredConsents: [['requiredConsent1', 'requiredConsent2'], ['requiredConsent3']],
+    };
+    it('The container element is removed and content is not rendered', () => {
+      const result = renderCookieConsent(onlyRequiredConsents, { rootId: testIds.target });
+      expect(() => result.getByTestId(testIds.target)).toThrow();
+      expect(() => result.getByTestId(testIds.content)).toThrow();
+      expect(() => result.getByTestId(testIds.targetWithChildren)).not.toThrow();
+    });
+    it('A new container is not added and content is not rendered', () => {
+      const result = renderCookieConsent(onlyRequiredConsents);
+      expect(() => result.getByTestId('NewElement')).toThrow();
+      expect(() => result.getByTestId(testIds.content)).toThrow();
+      expect(() => result.getByTestId(testIds.targetWithChildren)).not.toThrow();
+    });
+    it('If an existing element is used as container and it has children, it is not removed. Even if content was not rendered.', () => {
+      const result = renderCookieConsent(onlyRequiredConsents, { rootId: testIds.targetWithChildren });
+      expect(() => result.getByTestId(testIds.targetWithChildren)).not.toThrow();
+      expect(() => result.getByTestId(testIds.content)).toThrow();
+    });
+  });
+  describe('If modal is closed', () => {
+    it('the portal and container are not removed. There is a screen reader notification.', async () => {
+      const result = renderCookieConsent(defaultConsentData, { rootId: testIds.target });
+      clickElement(result, testIds.closeButton);
+      await waitFor(() => {
+        expect(() => result.getByTestId(testIds.content)).toThrow();
+      });
+      expect(() => result.getByTestId(testIds.target)).not.toThrow();
+      expect(() => result.getByTestId(testIds.targetWithChildren)).not.toThrow();
     });
   });
 });
