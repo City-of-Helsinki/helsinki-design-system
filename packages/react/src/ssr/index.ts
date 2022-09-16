b058bd21609a7e50b60b4a11295eb710ee88f727
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2022-09-16 13:45:47 +0300

    Get rid of patch-package. Take necessary code from used-styles

--- packages/react/src/ssr/index.ts
+++ packages/react/src/ssr/index.ts
@@ -1,4 +1,32 @@
-import { getCriticalRules, loadStyleDefinitions } from 'used-styles';
+import { extractAllUnmatchable } from './extractAllUnmatchable';
+import { getRawCriticalRules, assertIsReady } from './getRawCriticalRules';
+import { loadStyleDefinitions } from './loadStyleDefinitions';
+import { StyleDefinition, StyleSelector, SelectionFilter } from './types';
+
+const createUsedFilter = () => {
+  const usedSelectors = new Set<string>();
+
+  return (_: never, rule: StyleSelector) => {
+    if (usedSelectors.has(rule.hash)) {
+      return false;
+    }
+
+    usedSelectors.add(rule.hash);
+
+    return true;
+  };
+};
+
+/**
+ * returns critical rules(selector) used in a given HTML code
+ */
+const getCriticalRules = (html: string, def: StyleDefinition, filter: SelectionFilter = createUsedFilter()): string => {
+  assertIsReady(def);
+
+  return [...extractAllUnmatchable(def, filter), ...getRawCriticalRules(html, def, filter)]
+    .map(({ css, file }) => `\n/* ${file} */\n${css}`)
+    .join('');
+};
 
 export async function getCriticalHDSRules(markup, allHDSStyles) {
   if (markup && markup.length > 0 && allHDSStyles && allHDSStyles.length > 0) {
