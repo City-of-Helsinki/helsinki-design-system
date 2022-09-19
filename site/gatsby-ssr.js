d8fb55444540f2b74e9984b90411c514e503d7ac
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2022-09-19 17:19:01 +0300

    Improve naming

--- site/gatsby-ssr.js
+++ site/gatsby-ssr.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { getCriticalHDSRules, hdsStyles } from 'hds-react';
+import { getCriticalHdsRules, hdsStyles } from 'hds-react';
 import { renderToString } from 'react-dom/server';
 
 export const replaceRenderer = async ({ bodyComponent, setHeadComponents }) => {
   const bodyHTML = renderToString(bodyComponent);
 
   if (hdsStyles && hdsStyles.length > 0 && bodyHTML && bodyHTML.length > 0) {
-    const cssRules = await getCriticalHDSRules(bodyHTML, hdsStyles);
+    const cssRules = await getCriticalHdsRules(bodyHTML, hdsStyles);
     const HeadComponents = [
       <style data-used-styles dangerouslySetInnerHTML={{ __html: cssRules }} />
     ];
