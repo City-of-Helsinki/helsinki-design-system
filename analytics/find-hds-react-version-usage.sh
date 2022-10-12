8169908a5ef19b72018cca684de7783b89865218
harriplappalainen <harri.lappalainen@solita.fi>
2022-10-12 09:36:22 +0300

    Save hds react version usage file in same place and with same file naming style as the other analytics files

--- analytics/find-hds-react-version-usage.sh
+++ analytics/find-hds-react-version-usage.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
+
+# Path to file
+
+# jos toimii, nii kokeile suoraan stringissä $()
+date=$(date -u +"%Y-%m-%dT%H-%M-%SZ");
+echo $date;
+FILE_NAME="./results/${date}_HDS_react_versions_in_use.json";
+
 # Call to Github API to find all occurrences of "hds-react" under City-of-Helsinki org.
-curl -H "Accept: application/vnd.github+json"  -H "Authorization: token $TOKEN" "https://api.github.com/search/code?q=hds-react+in:file+filename:package.json+org:City-of-Helsinki&per_page=100" | jq '. | .total_count as $total_count | { total_count: $total_count, repositories: [.items[] | {name: .repository.name, url: .repository.html_url, package_url: .git_url, path: .path}]}' > hds-react-version-usage.json
+curl -H  --retry 5 --retry-connrefused --retry-max-time 60 -H "Accept: application/vnd.github+json"  -H "Authorization: token $TOKEN" "https://api.github.com/search/code?q=hds-react+in:file+filename:package.json+org:City-of-Helsinki&per_page=100" | jq '. | .total_count as $total_count | { total_count: $total_count, repositories: [.items[] | {name: .repository.name, url: .repository.html_url, package_url: .git_url, path: .path}]}' > $FILE_NAME
 
-
-# count variable is used to access the correct object inside hds-react-version-usage.json array
+# count variable is used to access the correct object inside the file array
 count=0;
-# Iterate through hds-react-version-usage.json file
-jq -c '.repositories[]' hds-react-version-usage.json | while read i; do
+# Iterate through the generated file
+jq -c '.repositories[]' $FILE_NAME | while read i; do
 
     # Read the git url for package json
     url=$(echo $i | jq -r '.package_url');
@@ -16,10 +23,10 @@ jq -c '.repositories[]' hds-react-version-usage.json | while read i; do
     version=$(jq '.dependencies."hds-react"' <<< "${content}")
     # If version is null, read it from devDependencies instead
     [[ "$version" = "null" ]] && properVersion=$(jq '.devDependencies."hds-react"' <<< "${content}") || properVersion=$(jq '.dependencies."hds-react"' <<< "${content}");
-    # Insert the key value pair "version": "xx.xx" into the hds-react-version-usage.json with sponge
-    jq --argjson properVersion "$properVersion" --argjson count "$count" '.repositories[$count] += { version: $properVersion }' hds-react-version-usage.json | sponge hds-react-version-usage.json;
+    # Insert the key value pair "version": "xx.xx" into the json file with sponge
+    jq --argjson properVersion "$properVersion" --argjson count "$count" '.repositories[$count] += { version: $properVersion }' $FILE_NAME | sponge $FILE_NAME;
     
     ((count++))
 done
 # Remove package helsinki-design-system from the list
-jq 'del(.repositories[] | select(.name == "helsinki-design-system"))' hds-react-version-usage.json | sponge hds-react-version-usage.json
+jq 'del(.repositories[] | select(.name == "helsinki-design-system"))' $FILE_NAME | sponge $FILE_NAME
