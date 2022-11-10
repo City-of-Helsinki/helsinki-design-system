006669c5a478309062aeacdc1c74bdbdb254ce88
harriplappalainen <harri.lappalainen@solita.fi>
2022-11-10 13:28:33 +0200

    Remove theme support from Header component and styles that might not be relevant here

--- packages/react/src/components/header/Header.tsx
+++ packages/react/src/components/header/Header.tsx
@@ -6,8 +6,6 @@ import styles from './Header.module.scss';
 import classNames from '../../utils/classNames';
 import { HeaderContext, HeaderContextProps } from './HeaderContext';
 import { useMobile } from '../../hooks/useMobile';
-import { HeaderTheme } from './Header.interface';
-import { useTheme } from '../../hooks/useTheme';
 
 export type HeaderProps = React.PropsWithChildren<{
   /**
@@ -18,21 +16,14 @@ export type HeaderProps = React.PropsWithChildren<{
    * ID of the header element.
    */
   id?: string;
-  /**
-   * Defines the header theme.
-   * @default 'white'
-   */
-  theme?: HeaderTheme;
 }>;
 
-export const Header = ({ children, className, theme = 'light' }: HeaderProps) => {
+export const Header = ({ children, className }: HeaderProps) => {
   const isMobile = useMobile();
-  // Custom theme class that is applied to the root element
-  const customThemeClass = useTheme<HeaderTheme>(styles.header, theme);
   const context: HeaderContextProps = { isMobile };
   return (
     <HeaderContext.Provider value={context}>
-      <div className={classNames(styles.header, styles[`theme-${theme}`], customThemeClass, className)}>
+      <div className={classNames(styles.header, className)}>
         <div className={styles.headerBackgroundWrapper}>{children}</div>
       </div>
     </HeaderContext.Provider>
