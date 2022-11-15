9d0cf4208d9cb55776413d0377cb74ddeec8a863
harriplappalainen <harri.lappalainen@solita.fi>
2022-11-15 09:22:01 +0200

    Custom hook for using useEffect in SSR to avoid big warning and useLayoutEffect in CSR

---
+++ packages/react/src/hooks/useIsomorphicLayoutEffect.ts
@@ -0,0 +1,8 @@
+// use-isomorphic-layout-effect.js
+import { useLayoutEffect, useEffect } from 'react';
+
+/**
+ * If rendering on client side, use the useLayoutEffect hook. If SSR, use useEffect to avoid a big warning.
+ */
+const useIsomorphicLayoutEffect = typeof window !== 'undefined' && window.document ? useLayoutEffect : useEffect;
+export default useIsomorphicLayoutEffect;
