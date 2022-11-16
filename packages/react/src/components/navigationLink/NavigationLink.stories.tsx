066ae57e38b9b664993985b96f993a394373883a
harriplappalainen <harri.lappalainen@solita.fi>
2022-11-16 13:23:37 +0200

    NavigationLink component for use at least in UniversalBar and Footer

---
+++ packages/react/src/components/navigationLink/NavigationLink.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { NavigationLink } from './NavigationLink';
+
+export default {
+  component: NavigationLink,
+  title: 'Components/NavigationLink',
+  parameters: {
+    controls: { expanded: true },
+  },
+  args: {},
+};
+
+export const Default = (args) => <NavigationLink {...args}>NavigationLink</NavigationLink>;
---
+++ packages/react/src/components/navigationLink/NavigationLink.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+// import core base styles
+import 'hds-core';
+import styles from './NavigationLink.module.scss';
+import classNames from '../../utils/classNames';
+import { Link } from '../link';
+
+export type NavigationLinkProps = React.PropsWithChildren<{
+  /**
+   * Link content.
+   */
+  children: string;
+  /**
+   * Additional class names to apply.
+   */
+  className?: string;
+  /**
+   * Hypertext Reference of the link.
+   */
+  href: string;
+  /**
+   * Size of the link.
+   */
+  size?: 'S' | 'M' | 'L';
+}>;
+
+export const NavigationLink = ({ children, className, href, size, ...rest }: NavigationLinkProps) => {
+  return (
+    <Link className={classNames(styles.navigationLink, className)} href={href} size={size} {...rest}>
+      {children}
+    </Link>
+  );
+};
