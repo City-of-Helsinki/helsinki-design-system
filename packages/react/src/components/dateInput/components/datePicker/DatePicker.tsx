4486aabbaaa7663c93c3f4d6fd50b1988d142a1d
Mika Nevalainen <mn.nevalainen@gmail.com>
2022-12-16 08:44:35 +0200

    Add comment

--- packages/react/src/components/dateInput/components/datePicker/DatePicker.tsx
+++ packages/react/src/components/dateInput/components/datePicker/DatePicker.tsx
@@ -302,6 +302,7 @@ export const DatePicker = (providedProps: DayPickerProps) => {
 
   const currentMonthAvailableDays: number[] = currentMonthAvailableDates.map((date) => date.getDate());
 
+  // We need to wait for the popper to count the position before we show the datepicker. The initial position might be off and repositioning causes the picker to move after the user opens it.
   const beforePopperWrite: Partial<Modifier<string, object>> = useMemo(
     () => ({
       name: 'beforePopperWrite',
