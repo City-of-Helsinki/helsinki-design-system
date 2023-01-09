68c154c4b26759c1f8b8efdb109cfaae2055563f
Niko Helle <niko.hele@hyperactive.fi>
2023-01-09 13:50:11 +0200

    Simple version

--- packages/react/src/components/breadcrumb/Breadcrumb.tsx
+++ packages/react/src/components/breadcrumb/Breadcrumb.tsx
@@ -3,9 +3,36 @@ import React from 'react';
 // import core base styles
 import 'hds-core';
 import styles from './Breadcrumb.module.scss';
+import { Link } from '../link';
 
-export type BreadcrumbProps = React.PropsWithChildren<{}>;
+export type BreadcrumbInfo = { title: string; path: string };
+export type BreadcrumbProps = { list: BreadcrumbInfo[] };
 
-export const Breadcrumb = ({ children }: BreadcrumbProps) => {
-  return <div className={styles.breadcrumb}>{children}</div>;
+const BreadcrumbItem = ({ item, active }: { item: BreadcrumbInfo; active: boolean }) => {
+  return (
+    <div className={styles.item}>
+      {active ? <span className={styles.active}>{item.title}</span> : <Link href={item.path}>{item.title}</Link>}
+    </div>
+  );
+};
+
+const Separator = () => {
+  return <span className={styles.separator}>{'\u003e'}</span>;
+};
+
+export const Breadcrumb = ({ list }: BreadcrumbProps) => {
+  if (list.length < 2) {
+    return null;
+  }
+
+  return (
+    <div className={styles.breadcrumb}>
+      {list.map((item, index) => (
+        <>
+          {index > 0 && <Separator />}
+          <BreadcrumbItem item={item} active={index === list.length - 1} />
+        </>
+      ))}
+    </div>
+  );
 };
