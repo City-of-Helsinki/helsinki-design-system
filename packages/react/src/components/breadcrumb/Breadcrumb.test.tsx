1a073b15626f3d5e58d8323380951485fcdc5dab
Niko Helle <niko.hele@hyperactive.fi>
2023-01-11 14:41:37 +0200

    Added tests

--- packages/react/src/components/breadcrumb/Breadcrumb.test.tsx
+++ packages/react/src/components/breadcrumb/Breadcrumb.test.tsx
@@ -5,13 +5,44 @@ import { axe } from 'jest-axe';
 import { Breadcrumb } from './Breadcrumb';
 
 describe('<Breadcrumb /> spec', () => {
+  const defaultList = [
+    { title: 'Home', path: '/' },
+    { title: 'Level 1', path: '/level1' },
+    { title: 'Level 2', path: '/level2' },
+    { title: 'Level 3', path: '/level3' },
+    { title: 'Current', path: null },
+  ];
   it('renders the component', () => {
-    const { asFragment } = render(<Breadcrumb />);
+    const { asFragment } = render(<Breadcrumb list={defaultList} />);
     expect(asFragment()).toMatchSnapshot();
   });
   it('should not have basic accessibility issues', async () => {
-    const { container } = render(<Breadcrumb />);
+    const { container } = render(<Breadcrumb list={defaultList} />);
     const results = await axe(container);
     expect(results).toHaveNoViolations();
+  });
+  it('renders all items. Last item with a path (#3) is also in mobile view and therefore rendered twice.', async () => {
+    const { getAllByText, getByText, container } = render(<Breadcrumb list={defaultList} />);
+    expect(container.querySelectorAll('nav')).toHaveLength(1);
+    defaultList.forEach((item) => {
+      if (!item.path) {
+        expect(() => getByText(item.title, { selector: 'span' })).not.toThrow();
+      } else if (item.path === defaultList[3].path) {
+        expect(getAllByText(item.title, { selector: 'a' })).toHaveLength(2);
+      } else {
+        expect(() => getByText(item.title, { selector: 'a' })).not.toThrow();
+      }
+    });
+  });
+  it('returns null if there are no items with paths.', async () => {
+    const { container } = render(
+      <Breadcrumb
+        list={[
+          { title: 'Home', path: null },
+          { title: 'Current', path: null },
+        ]}
+      />,
+    );
+    expect(container.querySelectorAll('nav')).toHaveLength(0);
   });
 });
