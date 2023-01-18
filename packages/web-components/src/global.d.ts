f8dbc425e20f21f05cdd03ad82d4619a5bcbec81
Mika Nevalainen <mn.nevalainen@gmail.com>
2023-01-18 12:06:10 +0200

    Fix global types

--- packages/web-components/src/global.d.ts
+++ packages/web-components/src/global.d.ts
@@ -1,11 +1,5 @@
-export {}
-
 declare module '*.jpg';
 declare module '*.scss' {
   const styles: { [className: string]: string };
   export default styles;
-}
-
-declare global {
-  type SizeType = 's' | 'm' | 'l';
 }
