dd03e25da0b2d3ec9a58c4e3b175587143fb5a99
Mika Nevalainen <mn.nevalainen@gmail.com>
2023-01-24 13:08:22 +0200

    Update styles and layout

--- site/src/docs/about/this-is-hds/index.jsx
+++ site/src/docs/about/this-is-hds/index.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
-import { Accordion, useAccordion, Button, Card, Container, IconAngleDown, IconAngleUp, ImageWithCard, Link, Linkbox, Table } from 'hds-react';
+import {
+  Accordion,
+  useAccordion,
+  Button,
+  Card,
+  Container,
+  IconAngleDown,
+  IconAngleUp,
+  ImageWithCard,
+  Link,
+  Linkbox,
+  Table,
+} from 'hds-react';
 import Hero from '../../../components/Hero';
 import Seo from '../../../components/Seo';
 
@@ -29,8 +41,6 @@ const TableBasicInfo = () => {
     { id: 'competition', about: 'Best Design System' },
     { id: 'tech', about: 'React, html+css (core)' },
     { id: 'design', about: 'Sketch, Abstract' },
-    
-    
   ];
   const verticalHeaders = [
     { key: 'name', headerName: 'Name' },
@@ -40,7 +50,6 @@ const TableBasicInfo = () => {
     { key: 'competition', headerName: 'Competition category' },
     { key: 'tech', headerName: 'Technologies used' },
     { key: 'design', headerName: 'Design tools used' },
-    
   ];
   const caption = (
     <span>
@@ -60,9 +69,6 @@ const TableBasicInfo = () => {
     </div>
   );
 };
-
-
-//<script type="text/javascript">!function(){"use strict";window.addEventListener("message",(function(e){if(void 0!==e.data["datawrapper-height"]){var t=document.querySelectorAll("iframe");for(var a in e.data["datawrapper-height"])for(var r=0;r<t.length;r++){if(t[r].contentWindow===e.source)t[r].style.height=e.data["datawrapper-height"][a]+"px"}}}))}();</script>
 
 const DemoPage = () => {
   return (
@@ -90,95 +96,174 @@ const DemoPage = () => {
       </Hero>
 
       <Container className="info-page-content text-body">
-
-        <ImageWithCard cardLayout="hover" src="/images/homepage/amos58.jpg" style={{ height: '106px', marginTop: 'var(--spacing-m)', backgroundColor: 'var(--silver-medium-light !important'}}>
-         <h2 className="heading-l">Background</h2>
-          <p >
-          The City of Helsinki offers thousands of digital and non-digital services for different purposes. 
-          The common goal is to meet the needs of the users.
-          </p>
-        </ImageWithCard>
-
-        <h2 >For example, citizens of Helsinki need to</h2>
-        <div class="exampleContainer" style = {{ display: 'grid', columnGap: 'var(--spacing-s)', rowGap: ' var(--spacing-m)', gridTemplateColumns : "50% 50%" }} >
-          <Linkbox 
-            linkboxAriaLabel="Linkbox: Asti" 
-            linkAriaLabel="Asti" 
-            href="https://asti.hel.fi"  
-            heading = "Apply to daycare"
-            external
-            imgProps={{ src: "/images/foundation/visual-assets/placeholders/image-m@3x.png", width: 384, height: 245 }}
-            style={{backgroundColor: 'var(--color-coat-of-arms-light)', marginTop: 'var(--spacing-m)'}}>
-          </Linkbox>
-
-          <Linkbox 
-          linkboxAriaLabel="Linkbox: Events Helsinki" 
-          linkAriaLabel="Events Helsinki" 
-          href="https://hds.hel.fihttps://tapahtumat.hel.fi/en/home" 
-          external
-          heading = "Find events in Helsinki"
-          style={{backgroundColor: 'var(--color-metro)',marginTop: 'var(--spacing-m)'}}>
-                <div style={{ height: '106px', marginTop: 'var(--spacing-m)' }}><h3>Find events in Helsinki</h3></div>
-          </Linkbox>
-
-          <Linkbox 
-          linkboxAriaLabel="Linkbox: Boat Berts" 
-          linkAriaLabel="Boat Berths" 
-          href="https://hds.hel.fi" 
-          heading="Rent boat berth"
-          external
-          style={{backgroundColor: 'var(--color-suomenlinna-medium-light)', marginTop: 'var(--spacing-m)'}} >
-                <div style={{ height: '106px', marginTop: 'var(--spacing-m)' }}><h3>Rent boat berths</h3></div>
-          </Linkbox>
-
-          <Linkbox 
-          linkboxAriaLabel="Linkbox: Culture Kids" 
-          linkAriaLabel="Asti" 
-          href="https://kummilapset.hel.fi/en/" 
-          heading="Become Culture Kid"
-          external
-          style={{backgroundColor: 'var(--color-tram-light)', marginTop: 'var(--spacing-m)'}}>
-          </Linkbox>
-      
-          <Linkbox 
-          linkboxAriaLabel="Linkbox: hel.fi main site" 
-          linkAriaLabel="hel.fi" 
-          href="https://hel.fi" 
-          heading = "Visit the official site hel.fi"
-          external
-          style={{backgroundColor: 'var(--color-engel-light)', marginTop: 'var(--spacing-m)'}} >
-          </Linkbox>
-    
-          <Linkbox 
-          linkboxAriaLabel="Linkbox: Buy resident parking permits" 
-          linkAriaLabel="Resident parking permits" 
-          href="https://www.hel.fi/en/urban-environment-and-traffic/parking/resident-parking" 
-          heading = "Buy resident parking permits"
-          external
-          style={{backgroundColor: 'var(--color-metro-light)', marginTop: 'var(--spacing-m)'}} >
-          </Linkbox>
-        
-          
-      
-          
+        <div className="info-page-image-card-container">
+          <ImageWithCard
+            className="info-page-image-card"
+            cardLayout="hover"
+            cardAlignment="left"
+            src="/images/homepage/amos58.jpg"
+          >
+            <h2 className="heading-l">Background</h2>
+            <p>
+              The City of Helsinki offers thousands of digital and non-digital services for different purposes. The
+              common goal is to meet the needs of the users.
+            </p>
+          </ImageWithCard>
         </div>
 
-        <ImageWithCard cardLayout="hover" src="/images/homepage/amos58.jpg" color="var(--silver-medium-light)">
-         <h2 className="heading-l">Goals</h2>
-          <p >
-          The Helsinki Design System is a shared point of reference for designers, developers and product owners working for the City of Helsinki. HDS helps teams work and communicate more efficiently and assists the City of Helsinki in fulfilling its vision in the digital world. </p>
-        </ImageWithCard>
-        
+        <h2>For example, citizens of Helsinki need to:</h2>
+        <div className="info-page-card-grid">
+          <Card
+            style={{ backgroundImage: 'url(/images/about/this-is-hds/hki-illustration-daycare.svg)' }}
+            theme={{ '--background-color': 'var(--color-coat-of-arms-medium-light)' }}
+            className="info-page-card-grid-item"
+            heading="Apply to daycare"
+          >
+            <Link
+              ariaLabel="Learn more about applying to daycare"
+              openInNewTabAriaLabel="Opens in new tab"
+              openInExternalDomainAriaLabel="Directs to another site"
+              className="info-page-card-grid-link-button"
+              href="https://asti.hel.fi"
+              style={{ color: 'var(--color-black)' }}
+              external
+            >
+              Learn more
+            </Link>
+          </Card>
+          <Card
+            style={{ backgroundImage: 'url(/images/about/this-is-hds/hki-illustration-dj.svg)' }}
+            theme={{ '--background-color': 'var(--color-metro)' }}
+            className="info-page-card-grid-item"
+            heading="Find events in Helsinki"
+          >
+            <Link
+              ariaLabel="Learn more about events"
+              openInNewTabAriaLabel="Opens in new tab"
+              openInExternalDomainAriaLabel="Directs to another site"
+              className="info-page-card-grid-link-button"
+              href="https://hds.hel.fihttps://tapahtumat.hel.fi/en/home"
+              external
+              style={{ color: 'var(--color-white)' }}
+            >
+              Learn more
+            </Link>
+          </Card>
+          <Card
+            style={{ backgroundImage: 'url(/images/about/this-is-hds/hki-illustration-boat.svg)' }}
+            theme={{ '--background-color': 'var(--color-suomenlinna)' }}
+            className="info-page-card-grid-item"
+            heading="Rent a place for a boat"
+          >
+            <Link
+              ariaLabel="Learn more about renting a place for a boat"
+              openInNewTabAriaLabel="Opens in new tab"
+              openInExternalDomainAriaLabel="Directs to another site"
+              className="info-page-card-grid-link-button"
+              href="https://hds.hel.fi"
+              external
+              style={{ color: 'var(--color-black)' }}
+            >
+              Learn more
+            </Link>
+          </Card>
+          <Card
+            style={{ backgroundImage: 'url(/images/about/this-is-hds/hki-illustration-culture-kids.svg)' }}
+            theme={{ '--background-color': 'var(--color-copper-light)' }}
+            className="info-page-card-grid-item"
+            heading="Become a Culture Kid"
+          >
+            <Link
+              ariaLabel="Learn more about becoming a culture kid"
+              openInNewTabAriaLabel="Opens in new tab"
+              openInExternalDomainAriaLabel="Directs to another site"
+              className="info-page-card-grid-link-button"
+              href="https://kummilapset.hel.fi/en/"
+              external
+              style={{ color: 'var(--color-black)' }}
+            >
+              Learn more
+            </Link>
+          </Card>
+          <Card
+            style={{ backgroundImage: 'url(/images/about/this-is-hds/hki-illustration-havis-amanda.svg)' }}
+            theme={{ '--background-color': 'var(--color-engel-light)' }}
+            className="info-page-card-grid-item"
+            heading="Visit the official site hel.fi"
+          >
+            <Link
+              ariaLabel="Learn more about hel.fi site"
+              openInNewTabAriaLabel="Opens in new tab"
+              openInExternalDomainAriaLabel="Directs to another site"
+              className="info-page-card-grid-link-button"
+              href="https://hel.fi"
+              external
+              style={{ color: 'var(--color-black)' }}
+            >
+              Learn more
+            </Link>
+          </Card>
+          <Card
+            style={{ backgroundImage: 'url(/images/about/this-is-hds/hki-illustration-truck.svg)' }}
+            theme={{ '--background-color': 'var(--color-gold-light)' }}
+            className="info-page-card-grid-item"
+            heading="Buy a resident parking permit"
+          >
+            <Link
+              ariaLabel="Learn more about parking permits"
+              openInNewTabAriaLabel="Opens in new tab"
+              openInExternalDomainAriaLabel="Directs to another site"
+              className="info-page-card-grid-link-button"
+              href="https://www.hel.fi/en/urban-environment-and-traffic/parking/resident-parking"
+              external
+              style={{ color: 'var(--color-black)' }}
+            >
+              Learn more
+            </Link>
+          </Card>
+        </div>
+
+        <div className="info-page-image-card-container">
+          <ImageWithCard
+            className="info-page-image-card"
+            cardLayout="hover"
+            cardAlignment="left"
+            src="/images/homepage/amos58.jpg"
+          >
+            <h2 className="heading-l">Goals</h2>
+            <p>
+              The Helsinki Design System is a shared point of reference for designers, developers and product owners
+              working for the City of Helsinki. HDS helps teams work and communicate more efficiently and assists the
+              City of Helsinki in fulfilling its vision in the digital world.{' '}
+            </p>
+          </ImageWithCard>
+        </div>
+
         <h2 className="heading-l">HDS is part of the city strategy</h2>
         <p>
-        HDS is a tool for building the digital presence and brand of city of Helsinki. HDS supports creating equal and functional city services.</p>
+          HDS is a tool for building the digital presence and brand of city of Helsinki. HDS supports creating equal and
+          functional city services.
+        </p>
 
-        <p>Digitalisation is a key enabler to city services and the new city strategy. The city strategy emphasises creating accessible services for all citizens, regardless the time and place. HDS provides building blocks for such services and enables projects to solve actual user problems instead of rebuilding basic elements. HDS has accessibility baked in so that the end user projects using the ready components are one step closer on accessible services.</p>
+        <p>
+          Digitalisation is a key enabler to city services and the new city strategy. The city strategy emphasises
+          creating accessible services for all citizens, regardless the time and place. HDS provides building blocks for
+          such services and enables projects to solve actual user problems instead of rebuilding basic elements. HDS has
+          accessibility baked in so that the end user projects using the ready components are one step closer on
+          accessible services.
+        </p>
 
-        <p>HDS ensures accessibility in every stage of component development. HDS components always meet at least WCAG 2.1 AA level and often also reach the AAA level. HDS components and patterns are tested with the newest NVDA on Windows and the newest VoiceOver on Mac and iOS. </p>
+        <p>
+          HDS ensures accessibility in every stage of component development. HDS components always meet at least WCAG
+          2.1 AA level and often also reach the AAA level. HDS components and patterns are tested with the newest NVDA
+          on Windows and the newest VoiceOver on Mac and iOS.{' '}
+        </p>
 
-        <p>The brand of Helsinki is present in physical world and in digital world. These two should represent the same city and feel familiar and recognisable. HDS is a tool to build digital presence according to city of Helsinki brand.</p>
-        
+        <p>
+          The brand of Helsinki is present in physical world and in digital world. These two should represent the same
+          city and feel familiar and recognisable. HDS is a tool to build digital presence according to city of Helsinki
+          brand.
+        </p>
 
         <h2 className="heading-l">How we work</h2>
         <p>
@@ -186,7 +271,7 @@ const DemoPage = () => {
           Helsinki. City of Helsinki didn't buy ready design system but rather decided to gather the best professionals
           in the same team. This way City of Helsinki owns and continues to maintain the design system. When the people
           change, it is important that the processes but also the actual design system are well documented, so that it
-          is possible to transfer the knowledge. 
+          is possible to transfer the knowledge.
         </p>
 
         <p>
@@ -215,14 +300,23 @@ const DemoPage = () => {
           handle discussion and requests from the projects across the city organization.
         </p>
 
-        <ImageWithCard cardLayout="hover" src="/images/homepage/amos58.jpg" style={{ height: '106px', marginTop: 'var(--spacing-m)', backgroundColor: 'var(--silver-medium-light !important'}}>
-         <h2 className="heading-l">Results</h2>
-          <p >
-          Helsinki Design System is gaining more users and that way larger impact on the city of Helsinki digital user experiene.
+        <ImageWithCard
+          cardLayout="hover"
+          src="/images/homepage/amos58.jpg"
+          style={{
+            height: '106px',
+            marginTop: 'var(--spacing-m)',
+            backgroundColor: 'var(--silver-medium-light !important',
+          }}
+        >
+          <h2 className="heading-l">Results</h2>
+          <p>
+            Helsinki Design System is gaining more users and that way larger impact on the city of Helsinki digital user
+            experiene.
           </p>
         </ImageWithCard>
 
-        <h2 class="heading-l">Some of the key metrics</h2>
+        <h2 className="heading-l">Some of the key metrics</h2>
 
         <Card
           border
@@ -230,21 +324,24 @@ const DemoPage = () => {
           text="React component usage in 6 different times."
           style={{
             marginTop: 'var(--spacing-s)',
-
           }}
         >
-          <iframe title="HDS React component usage" aria-label="Interactive bar chart" id="datawrapper-chart-X0PtN" src="https://datawrapper.dwcdn.net/X0PtN/8/" scrolling="no" frameborder="0" data-external="1"
+          <iframe
+            title="HDS React component usage"
+            aria-label="Interactive bar chart"
+            id="datawrapper-chart-X0PtN"
+            src="https://datawrapper.dwcdn.net/X0PtN/8/"
+            scrolling="no"
+            frameBorder="0"
+            data-external="1"
             style={{
-              width: "100%", 
+              width: '100%',
               //min-width: "100% !important",
-              border: "none", 
-              height:"620",
+              border: 'none',
+              height: '620',
             }}
-          >
-          </iframe>
-          
+          ></iframe>
         </Card>
-
 
         <Card
           border
@@ -252,11 +349,9 @@ const DemoPage = () => {
           text="Usage of Design kit"
           style={{
             marginTop: 'var(--spacing-s)',
-            width: '45%'
-
+            width: '45%',
           }}
         />
-
 
         <Card
           border
@@ -264,23 +359,24 @@ const DemoPage = () => {
           text="Unique visitors on the documentation site between June 2020 to end of December 2022."
           style={{
             marginTop: 'var(--spacing-s)',
-
           }}
         >
-          <iframe title="Unique visitors on hds.hel.fi" aria-label="Interactive line chart" id="datawrapper-chart-LLonN" src="https://datawrapper.dwcdn.net/LLonN/3/" scrolling="no" frameborder="0" data-external="1"
+          <iframe
+            title="Unique visitors on hds.hel.fi"
+            aria-label="Interactive line chart"
+            id="datawrapper-chart-LLonN"
+            src="https://datawrapper.dwcdn.net/LLonN/3/"
+            scrolling="no"
+            frameBorder="0"
+            data-external="1"
             style={{
-              width: "100%", 
+              width: '100%',
               //min-width: "100% !important",
-              border: "none", 
-              height:"620",
+              border: 'none',
+              height: '620',
             }}
-          >
-          </iframe>
-          
+          ></iframe>
         </Card>
-        
-          
-
 
         <h2 className="heading-l">Basic Info</h2>
         <TableBasicInfo />
@@ -290,4 +386,3 @@ const DemoPage = () => {
 };
 
 export default DemoPage;
-
