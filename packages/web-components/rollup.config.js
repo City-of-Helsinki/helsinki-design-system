b2d03b28c778932be60a8bd2bce6fe6a22dbe55d
Mika Nevalainen <mn.nevalainen@gmail.com>
2023-02-01 18:18:40 +0200

    Fix Web Components build

--- packages/web-components/rollup.config.js
+++ packages/web-components/rollup.config.js
@@ -4,23 +4,17 @@ import resolve from '@rollup/plugin-node-resolve';
 import ts from 'rollup-plugin-ts';
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
-import postcss from 'rollup-plugin-postcss';
-import { terser } from 'rollup-plugin-terser';
-import del from 'rollup-plugin-delete';
-import cssText from 'rollup-plugin-css-text';
+import litcss from 'rollup-plugin-lit-css';
+import postcss from 'postcss';
+import postcssModules from 'postcss-modules';
 
-const insertCssEsm = () => {
-  return {
-    name: 'insert-css-esm',
-    buildEnd: () => {
-      fs.appendFileSync(
-        `${__dirname}/lib/index.js`,
-        `import CSS_TEXT from './index.css-text'; export { CSS_TEXT as hdsStyles };`,
-      );
-    },
-  };
-};
+const processor = postcss(
+  postcssModules({
+    global: true,
+    generateScopedName: '[local]',
+    getJSON: () => {},
+  }),
+);
 
 const insertCssCjs = () => {
   return {
@@ -34,33 +28,12 @@ const insertCssCjs = () => {
   };
 };
 
-const moveCss = () => {
-  return {
-    name: 'move-css',
-    closeBundle: () => {
-      fs.renameSync(`${__dirname}/lib/tmp/index.css`, `${__dirname}/lib/index.css`);
-      fs.renameSync(`${__dirname}/lib/tmp/index.css-text.d.ts`, `${__dirname}/lib/index.css-text.d.ts`);
-      fs.renameSync(`${__dirname}/lib/tmp/index.css-text.js`, `${__dirname}/lib/index.css-text.js`);
-    },
-  };
-};
-
 const extensions = ['.js', '.ts'];
-const external = [
-  'crc-32',
-  'kashe',
-  'memoize-one',
-  'postcss',
-  'lodash.uniqueid',
-  'lodash.isequal',
-  'lodash.isfunction',
-  '@juggle/resize-observer',
-  '@popperjs/core',
-];
+const external = ['postcss', 'lodash.uniqueid', 'lodash.isequal', 'lodash.isfunction', 'lodash.toString'];
 
 const getExternal = (format) => (format === 'esm' ? [...external, /@babel\/runtime/] : external);
 
-const getPlugins = (format, extractCSS) => {
+const getPlugins = (format) => {
   const babelPlugin =
     format === 'esm' &&
     babel({
@@ -69,47 +42,22 @@ const getPlugins = (format, extractCSS) => {
       extensions,
     });
 
-  const postCssPlugin = postcss({
-    extract: extractCSS ? 'index.css' : undefined,
-    modules: true,
-    use: ['sass'],
-    minimize: {
-      preset: [
-        'default',
-        {
-          calc: false,
-          discardUnused: true,
-          mergeIdents: true,
-          reduceIdents: true,
-        },
-      ],
-    },
-  });
-
   const plugins = [
     includePaths({ paths: ['src'], extensions }),
     resolve(),
     ts(),
     babelPlugin,
     commonjs({ include: ['../../node_modules/**', 'node_modules/**'] }),
-    json(),
-    postCssPlugin,
-    terser(),
+    litcss({
+      include: ['/**/*.css', '/**/*.scss'],
+      transform: (css, { filePath }) => processor.process(css, { from: filePath }).then((res) => res.css),
+    }),
   ];
 
-  if (extractCSS) {
-    plugins.push(
-      cssText(),
-      moveCss(),
-      insertCssEsm(),
-      del({
-        targets: 'lib/tmp',
-        hook: 'closeBundle',
-      }),
-    );
+  if (format === 'cjs') {
+    plugins.push(insertCssCjs());
   }
 
-  if (format === 'cjs') plugins.push(insertCssCjs());
   return plugins;
 };
 
@@ -118,37 +66,25 @@ const getConfig = (format, extractCSS) => ({
   external: getExternal(format),
 });
 
-const esmInput = {
-  index: 'src/index.ts',
-  'components/index': 'src/components/index.ts',
-  'icons/index': 'src/icons/index.ts',
-  'components/Accordion/index': 'src/components/accordion/index.ts',
-};
-
 export default [
   {
-    input: esmInput,
+    input: {
+      index: 'src/index.ts',
+      'components/index': 'src/components/index.ts',
+      'icons/index': 'src/icons/index.ts',
+      'components/Accordion/index': 'src/components/accordion/index.ts',
+    },
     output: [
       {
         dir: 'lib',
         format: 'esm',
-      },
-    ],
-    ...getConfig('esm', false),
-  },
-  {
-    input: esmInput,
-    output: [
-      {
-        dir: 'lib/tmp',
-        format: 'esm',
         exports: 'named',
       },
     ],
-    ...getConfig('esm', true),
+    ...getConfig('esm'),
   },
   {
-    input: ['src/index.ts', 'lib/index.css-text.js'],
+    input: ['src/index.ts'],
     output: [
       {
         dir: 'lib/cjs',
