b65001a1f59d205c31f49351416a2eae81c2e770
harriplappalainen <harri.lappalainen@solita.fi>
2023-02-08 10:00:23 +0200

    Explicitly set viewport width in useMediaQuery tests and test the value changing

--- packages/react/src/hooks/__tests__/useMediaQuery.test.tsx
+++ packages/react/src/hooks/__tests__/useMediaQuery.test.tsx
@@ -3,22 +3,66 @@ import { renderHook } from '@testing-library/react-hooks';
 
 import { useMediaQueryLessThan, useMediaQueryGreaterThan } from '../useMediaQuery';
 
+/* This test suite changes window width but it's set to default 1024 after this is done. */
 describe('useMediaQuery', () => {
-  beforeEach(cleanup);
+  afterAll(cleanup);
+
   it('useMediaQueryLessThan returns correct value', () => {
-    // By default the window.innerWidth is 1024px
-    const isSmallView = renderHook(() => useMediaQueryLessThan('s'));
-    const isLargeView = renderHook(() => useMediaQueryLessThan('xl'));
+    // Change the viewport to 500px
+    global.innerWidth = 500;
+    // Trigger the window resize event
+    global.dispatchEvent(new Event('resize'));
 
-    expect(isSmallView.result.current).toBe(false);
-    expect(isLargeView.result.current).toBe(true);
+    const isSmallView = renderHook(() => useMediaQueryLessThan('m'));
+    const isMobileView = renderHook(() => useMediaQueryLessThan('xs'));
+
+    expect(isSmallView.result.current).toBe(true);
+    expect(isMobileView.result.current).toBe(false);
   });
-  it('useMediaQueryGreaterThan returns correct value', () => {
-    // By default the window.innerWidth is 1024px
-    const isLargeView = renderHook(() => useMediaQueryGreaterThan('s'));
-    const isXLargeView = renderHook(() => useMediaQueryGreaterThan('xl'));
 
-    expect(isLargeView.result.current).toBe(true);
-    expect(isXLargeView.result.current).toBe(false);
+  it('useMediaQueryLessThan reacts to window resize', () => {
+    // Set the viewport width again just to be safe
+    global.innerWidth = 600;
+    // Trigger the window resize event
+    global.dispatchEvent(new Event('resize'));
+
+    const isMediumView = renderHook(() => useMediaQueryLessThan('m'));
+    expect(isMediumView.result.current).toBe(true);
+
+    // Set the viewport width again just to be safe
+    global.innerWidth = 1000;
+    // Trigger the window resize event
+    global.dispatchEvent(new Event('resize'));
+
+    expect(isMediumView.result.current).toBe(false);
+  });
+
+  it('useMediaQueryGreaterThan returns correct value', () => {
+    // Set the viewport width again just to be safe
+    global.innerWidth = 500;
+    // Trigger the window resize event
+    global.dispatchEvent(new Event('resize'));
+    const isSmallView = renderHook(() => useMediaQueryGreaterThan('xs'));
+    const isLargeView = renderHook(() => useMediaQueryGreaterThan('l'));
+
+    expect(isSmallView.result.current).toBe(true);
+    expect(isLargeView.result.current).toBe(false);
+  });
+
+  it('useMediaQueryGreaterThan reacts to window resize', () => {
+    // Set the viewport width again just to be safe
+    global.innerWidth = 800;
+    // Trigger the window resize event
+    global.dispatchEvent(new Event('resize'));
+
+    const isMediumView = renderHook(() => useMediaQueryGreaterThan('m'));
+    expect(isMediumView.result.current).toBe(true);
+
+    // Set the viewport width again just to be safe
+    global.innerWidth = 500;
+    // Trigger the window resize event
+    global.dispatchEvent(new Event('resize'));
+
+    expect(isMediumView.result.current).toBe(false);
   });
 });
