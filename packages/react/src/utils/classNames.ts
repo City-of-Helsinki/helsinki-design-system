019edb65b46d15b74f347ab70d318a7b374e07fe
Tuomas Hatakka <tuomas.hatakka@gmail.com>
2023-02-11 00:02:07 +0200

    Add possibility to bind scss import object to the classNames fnc

--- packages/react/src/utils/classNames.ts
+++ packages/react/src/utils/classNames.ts
@@ -1,27 +1,30 @@
 const hasOwn = {}.hasOwnProperty;
 
-const parseObject = (obj) => {
+const parseObject = (obj, styles = null) => {
   if (obj.toString !== Object.prototype.toString && !obj.toString.toString().includes('[native code]')) {
     return obj.toString();
   }
 
-  return Object.keys(obj)
-    .filter((key) => hasOwn.call(obj, key) && !!obj[key])
+  const iterable = { ...obj };
+  if (styles) {
+    Object.keys(styles)
+      .filter((key) => !!obj[key])
+      .forEach((key) => {
+        iterable[styles[key]] = true;
+      });
+  }
+
+  return Object.keys(iterable)
+    .filter((key) => hasOwn.call(iterable, key) && !!iterable[key])
     .join(' ');
 };
 
 function parseClassName(arg) {
-  if (!arg) return null;
   const argType = typeof arg;
 
-  if (argType === 'string') {
-    return arg;
-  }
-
-  if (argType === 'number') {
-    return arg;
-  }
-
+  if (!arg) return null;
+  if (argType === 'string') return arg;
+  if (argType === 'number') return arg;
   if (Array.isArray(arg)) {
     if (arg.length)
       return arg
@@ -30,12 +33,28 @@ function parseClassName(arg) {
         .join(' ');
     return null;
   }
-
-  if (argType === 'object') {
-    return parseObject(arg);
-  }
-
+  if (argType === 'object') return parseObject(arg, this);
   return null;
 }
 
+/**
+ * Generate a class name string for given arguments. Accepts an array of strings or objects
+ * from which a space delimited string is generated as follows:
+ * - Falsy values are filtered out.
+ * - Strings are appended to the resulting string as given.
+ * - For objects, every key with a truthy value is appended to the resulting string.
+ * @returns string A space delimited string parsed from the given arguments.
+ */
 export default (...args) => args.map(parseClassName).filter(Boolean).join(' ');
+
+/**
+ * Generate a classNames function that is bound to a set of class names imported from a css file.
+ * Whenever an object is passed to the returned bound classNames function the css file's class names are
+ * included in parsed object as keys and tested against object's values for the same
+ * @param styles A css class name object.
+ * @returns A classNames function bound to the given css class name object.
+ */
+export const styleBoundClassNames = (styles) => {
+  const boundClassNames = parseClassName.bind(styles);
+  return (...args) => args.map(boundClassNames).filter(Boolean).join(' ');
+};
