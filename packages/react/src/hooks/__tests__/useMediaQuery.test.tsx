b3e280ce8049556aa06c4c36047f4f42175654c1
harriplappalainen <harri.lappalainen@solita.fi>
2023-02-22 12:21:05 +0200

    Fix test warning, setting default innerWidth and use fireEvent instead of global.dispatchEvent

--- packages/react/src/hooks/__tests__/useMediaQuery.test.tsx
+++ packages/react/src/hooks/__tests__/useMediaQuery.test.tsx
@@ -1,17 +1,23 @@
-import { cleanup } from '@testing-library/react';
-import { renderHook } from '@testing-library/react-hooks';
+import { cleanup, fireEvent } from '@testing-library/react';
+import { act, renderHook } from '@testing-library/react-hooks';
 
 import { useMediaQueryLessThan, useMediaQueryGreaterThan } from '../useMediaQuery';
 
 /* This test suite changes window width but it's set to default 1024 after this is done. */
 describe('useMediaQuery', () => {
-  afterAll(cleanup);
+  const defaultInnerWidth = global.innerWidth;
+  afterAll(() => {
+    cleanup();
+    global.innerWidth = defaultInnerWidth;
+  });
 
   it('useMediaQueryLessThan returns correct value', () => {
-    // Change the viewport to 500px
-    global.innerWidth = 500;
     // Trigger the window resize event
-    global.dispatchEvent(new Event('resize'));
+    act(() => {
+      // Change the viewport to 500px
+      global.innerWidth = 500;
+      fireEvent(window, new Event('resize'));
+    });
 
     const isSmallView = renderHook(() => useMediaQueryLessThan('m'));
     const isMobileView = renderHook(() => useMediaQueryLessThan('xs'));
@@ -21,27 +27,33 @@ describe('useMediaQuery', () => {
   });
 
   it('useMediaQueryLessThan reacts to window resize', () => {
-    // Set the viewport width again just to be safe
-    global.innerWidth = 600;
     // Trigger the window resize event
-    global.dispatchEvent(new Event('resize'));
+    act(() => {
+      // Set the viewport width to be under medium breakpoint
+      global.innerWidth = 600;
+      fireEvent(window, new Event('resize'));
+    });
 
     const isMediumView = renderHook(() => useMediaQueryLessThan('m'));
     expect(isMediumView.result.current).toBe(true);
 
-    // Set the viewport width again just to be safe
-    global.innerWidth = 1000;
     // Trigger the window resize event
-    global.dispatchEvent(new Event('resize'));
+    act(() => {
+      // Set the viewport width to over medium breakpoint
+      global.innerWidth = 1000;
+      fireEvent(window, new Event('resize'));
+    });
 
     expect(isMediumView.result.current).toBe(false);
   });
 
   it('useMediaQueryGreaterThan returns correct value', () => {
-    // Set the viewport width again just to be safe
-    global.innerWidth = 500;
     // Trigger the window resize event
-    global.dispatchEvent(new Event('resize'));
+    act(() => {
+      // Set the viewport width to small
+      global.innerWidth = 500;
+      fireEvent(window, new Event('resize'));
+    });
     const isSmallView = renderHook(() => useMediaQueryGreaterThan('xs'));
     const isLargeView = renderHook(() => useMediaQueryGreaterThan('l'));
 
@@ -50,18 +62,22 @@ describe('useMediaQuery', () => {
   });
 
   it('useMediaQueryGreaterThan reacts to window resize', () => {
-    // Set the viewport width again just to be safe
-    global.innerWidth = 800;
     // Trigger the window resize event
-    global.dispatchEvent(new Event('resize'));
+    act(() => {
+      // Set the viewport width to over medium
+      global.innerWidth = 800;
+      fireEvent(window, new Event('resize'));
+    });
 
     const isMediumView = renderHook(() => useMediaQueryGreaterThan('m'));
     expect(isMediumView.result.current).toBe(true);
 
-    // Set the viewport width again just to be safe
-    global.innerWidth = 500;
     // Trigger the window resize event
-    global.dispatchEvent(new Event('resize'));
+    act(() => {
+      // Set the viewport width to small
+      global.innerWidth = 500;
+      fireEvent(window, new Event('resize'));
+    });
 
     expect(isMediumView.result.current).toBe(false);
   });
