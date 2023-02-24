ff1160fae9df204c0e24261e52bc601b294794df
Mika Nevalainen <mn.nevalainen@gmail.com>
2023-02-24 14:13:06 +0200

    Inject styles into top of the head in production build

--- packages/react/rollup.config.js
+++ packages/react/rollup.config.js
@@ -87,6 +87,9 @@ const getConfig = (format, extractCSS) => ({
     json(),
     postcss({
       extract: extractCSS ? 'index.css' : undefined,
+      inject: {
+        injectAt: 'top'
+      },
       modules: true,
       use: ['sass'],
       minimize: {
