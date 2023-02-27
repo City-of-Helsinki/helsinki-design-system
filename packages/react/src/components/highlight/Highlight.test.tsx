9f6ac998bde9e4c075572f94869517ce4f022298
Minna Salonen <minsalon@Minnas-MBP.home>
2023-02-27 18:00:42 +0200

    removed unneeded imports, fixed css formatting + updated reference snapshots

--- packages/react/src/components/highlight/Highlight.test.tsx
+++ packages/react/src/components/highlight/Highlight.test.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { axe } from 'jest-axe';
 
 import { Highlight } from './Highlight';
 
 describe('<Highlight /> spec', () => {
   it('renders the Highlight', () => {
-    const { asFragment } = render(<Highlight text="You may select an highlight from the article to be displayed here. Select an excerpt that you want the user to pay attention to." />);
+    const { asFragment } = render(
+      <Highlight text="You may select an highlight from the article to be displayed here. Select an excerpt that you want the user to pay attention to." />,
+    );
     expect(asFragment()).toMatchSnapshot();
   });
   it('renders the Quote', () => {
-    const { asFragment } = render(<Highlight text="Add an interesting quote here" type="quote" reference="First name Last name. Title."/>);
+    const { asFragment } = render(
+      <Highlight text="Add an interesting quote here" type="quote" reference="First name Last name. Title." />,
+    );
     expect(asFragment()).toMatchSnapshot();
   });
 });
