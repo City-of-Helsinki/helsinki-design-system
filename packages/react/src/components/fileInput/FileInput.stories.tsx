49d1aa2ac26ac278aa714598517c22f5b992da49
Tuomo Kivinen <mrtuomok@gmail.com>
2023-02-28 14:17:35 +0200

    remove Jest-tests, add Loki reference images

--- packages/react/src/components/fileInput/FileInput.stories.tsx
+++ packages/react/src/components/fileInput/FileInput.stories.tsx
@@ -88,7 +88,7 @@ export const WithDefaultValue = (args) => {
 };
 
 WithDefaultValue.args = {
-  accept: '.txt',
+  accept: '',
   defaultValue: [
     new File(['string content'], 'dummy.txt', { type: 'text/plain' }),
     new File(['string content with more text'], 'anotherDummy.txt', { type: 'text/plain' }),
