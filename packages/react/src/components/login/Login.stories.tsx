4521a6759b83c38bd33b1781cd7894613375520c
Niko Helle <niko.hele@hyperactive.fi>
2023-04-16 15:34:57 +0300

    Added a warning for using Login stories in an iframe
    Tunnistamo does not allow iframes.
    
    Also Iframe redirects the iframe to the login end point and then the callback is also rendered in an iframe.
    
    Then there is an iframe in an iframe.
    
    This can also cause CORS, session cookie and session storage problems

--- packages/react/src/components/login/Login.stories.tsx
+++ packages/react/src/components/login/Login.stories.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Button } from '../button/Button';
 import { LoginContextProvider, useOidcClient } from './index';
 import { OidcClientProps } from './client/oidcClient';
+import { Notification } from '../notification/Notification';
 
 export default {
   component: LoginContextProvider,
@@ -22,13 +23,35 @@ const loginProps: OidcClientProps = {
   },
 };
 
+const IFrameWarning = () => {
+  const openWindowInTop = () => {
+    if (window.top) {
+      window.top.location.href = `/iframe.html${window.location.search}`;
+    }
+  };
+  if (window.top !== window.self) {
+    return (
+      <Notification type="error">
+        <p>Login components should not be used in an iframe</p>
+        <Button onClick={openWindowInTop}>Open this story without and iframe</Button>
+      </Notification>
+    );
+  }
+  return null;
+};
+
 const LoginComponent = () => {
   const oidcClient = useOidcClient();
   const onButtonClick = () => {
     oidcClient.login();
   };
 
-  return <Button onClick={onButtonClick}>Login</Button>;
+  return (
+    <div>
+      <IFrameWarning />
+      <Button onClick={onButtonClick}>Login</Button>
+    </div>
+  );
 };
 
 export const Example = () => {
