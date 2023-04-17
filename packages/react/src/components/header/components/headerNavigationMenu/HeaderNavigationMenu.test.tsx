39ff5c080c77d6a9ec4fab5658f1cc3f43aa699b
harriplappalainen <harri.lappalainen@solita.fi>
2023-04-17 14:48:27 +0300

    Fix HeaderNavigationMenu test

--- packages/react/src/components/header/components/headerNavigationMenu/HeaderNavigationMenu.test.tsx
+++ packages/react/src/components/header/components/headerNavigationMenu/HeaderNavigationMenu.test.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { render } from '@testing-library/react';
 import { axe } from 'jest-axe';
 
 import { HeaderNavigationMenu } from './HeaderNavigationMenu';
-import { NavigationLink } from '../navigationLink/NavigationLink';
-import { HeaderNavigationMenuWrapper } from '../../../../utils/test-utils';
 
 describe('<HeaderNavigationMenu /> spec', () => {
   it('renders the component', () => {
@@ -17,38 +14,5 @@ describe('<HeaderNavigationMenu /> spec', () => {
     const { container } = render(<HeaderNavigationMenu />);
     const results = await axe(container);
     expect(results).toHaveNoViolations();
-  });
-  it('should open dropdown when the button is clicked', async () => {
-    render(
-      <NavigationLink
-        href="#"
-        label="Link"
-        dropdownLinks={[
-          <NavigationLink href="#" label="Test" dropdownLinks={[<NavigationLink href="#" label="Test" />]} />,
-        ]}
-      />,
-      { wrapper: HeaderNavigationMenuWrapper },
-    );
-    userEvent.click(screen.getByTestId('dropdown-button-main-nav-0'));
-
-    expect(screen.getByTestId('dropdown-menu-main-nav-0')).toBeVisible();
-  });
-  it('should have only one main dropdown active', async () => {
-    render(
-      <>
-        <NavigationLink href="#" label="Link 1" dropdownLinks={[<NavigationLink href="#" label="Link 1 nested" />]} />
-        <NavigationLink href="#" label="Link 2" dropdownLinks={[<NavigationLink href="#" label="Link 2 nested" />]} />
-      </>,
-      { wrapper: HeaderNavigationMenuWrapper },
-    );
-    // Click the first main navigation link's dropdown button
-    userEvent.click(screen.getByTestId('dropdown-button-main-nav-0'));
-    expect(screen.getByTestId('dropdown-menu-main-nav-0')).toBeVisible();
-    // Click the second main navigation link's dropdown button
-    userEvent.click(screen.getByTestId('dropdown-button-main-nav-1'));
-    // Now the second dropdown should show
-    expect(screen.getByTestId('dropdown-menu-main-nav-1')).toBeVisible();
-    // The first dropdown should not be visible. This can only be tested if display: none; is set inline!
-    expect(screen.getByTestId('dropdown-menu-main-nav-0')).not.toBeVisible();
   });
 });
