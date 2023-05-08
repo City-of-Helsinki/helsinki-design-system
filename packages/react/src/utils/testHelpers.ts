1d948919a786fd6b3eb483fd3e48244784e4c2ee
Niko Helle <niko.hele@hyperactive.fi>
2023-05-08 14:42:05 +0300

    Test helper for get data from jest.Mock

---
+++ packages/react/src/utils/testHelpers.ts
@@ -0,0 +1,25 @@
+export function getMockCalls(func: jest.Mock | jest.SpyInstance) {
+  return func.mock ? func.mock.calls : [];
+}
+
+export function getAllMockCallArgs(func: jest.Mock | jest.SpyInstance) {
+  const calls = getMockCalls(func);
+  return calls.map((mockArgs) => mockArgs);
+}
+
+export function getMockCallArgs(func: jest.Mock | jest.SpyInstance, index = 0) {
+  const calls = getMockCalls(func);
+  return calls[index];
+}
+
+export function getLastMockCallArgs(func: jest.Mock | jest.SpyInstance) {
+  const calls = getMockCalls(func);
+  return getMockCallArgs(func, calls.length - 1);
+}
+
+export function filterMockCallArgs(func: jest.Mock | jest.SpyInstance, filter: (...args: unknown[]) => boolean) {
+  const calls = getMockCalls(func);
+  return calls.filter((call) => {
+    return filter(...call);
+  });
+}
