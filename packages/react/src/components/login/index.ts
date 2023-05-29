fd88ac9f2daaae0f8fc72c1db4756467db4e0044
Niko Helle <niko.hele@hyperactive.fi>
2023-05-29 23:13:35 +0300

    Proper index.ts
    No need to export everything. There is so much anyway.

--- packages/react/src/components/login/index.ts
+++ packages/react/src/components/login/index.ts
@@ -1,3 +1,116 @@
-export * from './LoginContext';
+// components
+export * from './LoginProvider';
+export * from './LoginButton';
+export * from './LoginCallbackHandler';
+export * from './WithAuthenticatedUser';
+export * from './WithAuthentication';
+export * from './WithoutAuthenticatedUser';
+export * from './SessionEndedHandler';
+
+// hooks
 export * from './client/hooks';
+export * from './beacon/hooks';
+export * from './apiTokensClient/hooks';
+export * from './sessionPoller/hooks';
+
+// types
 export * from './types';
+export { ApiTokenClientProps, TokenData, ApiTokenClient, ApiTokensClientEvent } from './apiTokensClient/index';
+export { SessionPoller, SessionPollerEvent, SessionPollerOptions } from './sessionPoller/sessionPoller';
+export {
+  OidcClient,
+  OidcClientEvent,
+  OidcClientProps,
+  OidcClientState,
+  LoginProps,
+  LogoutProps,
+  Amr,
+} from './client/index';
+export {
+  SignalType,
+  SignalNamespace,
+  SignalPayload,
+  ConnectedModule,
+  Beacon,
+  SignalContext,
+  Signal,
+  SignalListener,
+  SignalTriggerProps,
+  SignalTrigger,
+  SignalListenerSource,
+  Disposer,
+} from './beacon/beacon';
+export {
+  NamespacedBeacon,
+  ErrorPayload,
+  ErrorSignal,
+  EventType,
+  EventData,
+  EventPayload,
+  EventSignal,
+  StateChangeType,
+  StateChangeSignalPayload,
+  StateChangeSignal,
+} from './beacon/signals';
+
+// modules
+export * from './client/oidcClient';
+export * from './apiTokensClient/apiTokensClient';
+export { createSessionPoller } from './sessionPoller/sessionPoller';
+
+// beacon
+export { createBeacon, createSignalTrigger } from './beacon/beacon';
+
+// errors
+export * from './apiTokensClient/apiTokensClientError';
+export * from './client/oidcClientError';
+export * from './sessionPoller/sessionPollerError';
+
+// events
+export { apiTokensClientEvents } from './apiTokensClient/index';
+export { oidcClientEvents } from './client/index';
+export { sessionPollerEvents } from './sessionPoller/sessionPoller';
+
+// stateChanges
+export { oidcClientStates } from './client/index';
+
+// plain js
+export { createLoginSystem } from './createLoginSystem';
+
+// namespaces
+export { apiTokensClientNamespace } from './apiTokensClient/index';
+export { oidcClientNamespace } from './client/index';
+export { sessionPollerNamespace } from './sessionPoller/sessionPoller';
+
+// signals
+export * from './apiTokensClient/signals';
+export * from './client/signals';
+export * from './sessionPoller/signals';
+export {
+  initSignalType,
+  errorSignalType,
+  eventSignalType,
+  stateChangeSignalType,
+  createNamespacedBeacon,
+  createErrorSignal,
+  createEventSignal,
+  createStateChangeTriggerProps,
+  createTriggerPropsForAllSignals,
+  createTriggerForAllSignalTypes,
+  createTriggerForAllNamespaces,
+  convertSignalToTrigger,
+  filterSignals,
+  isErrorSignal,
+  isEventSignal,
+  isInitSignal,
+  isStateChangeSignal,
+  isNamespacedSignal,
+  getEventSignalPayload,
+  getStateChangeSignalPayload,
+  getErrorSignalPayload,
+  createInitTriggerProps,
+  createEventTriggerProps,
+  createErrorTriggerProps,
+  createStateChangeSignal,
+  waitForSignals,
+} from './beacon/signals';
--- packages/react/src/components/login/types.ts
+++ packages/react/src/components/login/types.ts
@@ -1,8 +1,4 @@
 import { User as OidcClientUser } from 'oidc-client-ts';
 
-import { TokenData, ApiTokenClient as ATC } from './apiTokensClient/index';
-
 export type User = OidcClientUser;
 export type Profile = OidcClientUser['profile'];
-export type ApiTokens = TokenData;
-export type ApiTokenClient = ATC;
