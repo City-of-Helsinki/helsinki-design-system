a3fd40a29bc015c4642110fb244dea6e14c5a6d9
nlaitine <niko.laitinen@nitor.com>
2023-06-27 10:51:27 +0300

    postcss-inline-svg removed

--- packages/core/postcss.config.js
+++ packages/core/postcss.config.js
@@ -4,7 +4,6 @@ module.exports = ({ env }) => ({
     require('postcss-preset-env')({
       browsers: '> 0.2%, not iOS < 15, not Safari < 14.5, not dead, not op_mini all, not Android >= 0, not and_uc >= 0',
     }),
-    require('postcss-inline-svg')(),
     require('autoprefixer')({}),
     env === 'minify'
       ? require('cssnano')({
