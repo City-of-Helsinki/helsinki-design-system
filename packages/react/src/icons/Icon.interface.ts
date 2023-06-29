7f0106e6d6e5770aabedc0d27c1bc1da986281b1
nlaitine <niko.laitinen@nitor.com>
2023-06-29 10:54:24 +0300

    Icon component interface updated

--- packages/react/src/icons/Icon.interface.ts
+++ packages/react/src/icons/Icon.interface.ts
@@ -1,8 +1,14 @@
-import { SVGAttributes } from 'react';
+import { CSSProperties, SVGAttributes } from 'react';
 
 export type IconProps = SVGAttributes<SVGElement> & {
+  ariaLabel?: string;
+  ariaLabelledby?: string;
+  ariaHidden?: boolean;
+  className?: string;
+  color?: string;
   /**
    * Icon size
    */
   size?: 'xs' | 's' | 'm' | 'l' | 'xl';
+  style?: CSSProperties;
 };
