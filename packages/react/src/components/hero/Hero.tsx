cb7c3cae61fa6f06127080ac58fb13933c564482
Niko Helle <niko.hele@hyperactive.fi>
2023-06-29 15:59:56 +0300

    Change old hds-core style imports
    Imports in other components were already changed in another PR.
    
    These components were missed, because their PR were open at the same time.

--- packages/react/src/components/hero/Hero.tsx
+++ packages/react/src/components/hero/Hero.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
-// import core base styles
-import 'hds-core';
+// import base styles
+import '../../styles/base.css';
+
 import styles from './Hero.module.scss';
 import classNames from '../../utils/classNames';
 import { useTheme } from '../../hooks/useTheme';
