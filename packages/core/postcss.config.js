6990ed83d81a914044217a239d51e15ae36eaac6
nlaitine <niko.laitinen@nitor.com>
2023-07-05 19:20:46 +0300

    Inline svg icons to css

--- packages/core/postcss.config.js
+++ packages/core/postcss.config.js
@@ -4,6 +4,7 @@ module.exports = ({ env }) => ({
     require('postcss-preset-env')({
       browsers: '> 0.2%, not iOS < 15, not Safari < 14.5, not dead, not op_mini all, not Android >= 0, not and_uc >= 0',
     }),
+    require('postcss-inline-svg')(),
     require('autoprefixer')({}),
     env === 'minify'
       ? require('cssnano')({
