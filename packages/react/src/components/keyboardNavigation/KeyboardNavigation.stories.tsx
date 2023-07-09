6d51cea159dae98a66bd7aab3775141b3c6c95a5
Niko Helle <niko.hele@hyperactive.fi>
2023-07-09 11:17:36 +0300

    Example how to restore focus after re-render / re-mount

--- packages/react/src/components/keyboardNavigation/KeyboardNavigation.stories.tsx
+++ packages/react/src/components/keyboardNavigation/KeyboardNavigation.stories.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import { KeyboardNavigation } from './KeyboardNavigation';
+import { Button } from '../button/Button';
+import { useKeyboardNavigation } from '../../hooks/useKeyboardNavigation';
 
 export default {
   component: KeyboardNavigation,
@@ -26,6 +28,15 @@ const StoryStyles = () => {
       }
       .nav li a {
         padding: var(--spacing-xs);
+      }
+      a:focus, a:focus-visible {
+        border: 1px solid var(--color-bus);
+      }
+      .buttons{
+        display:flex;
+      }
+      .buttons > * {
+        margin-right: var(--spacing-xs);
       }
     `}
     </style>
@@ -65,6 +76,73 @@ export const Example = () => {
         </ul>
         <p>Once focused, you can navigate with arrow and tab keys.</p>
       </KeyboardNavigation>
+    </div>
+  );
+};
+
+export const KeepAndResetFocusWithHook = () => {
+  const selectedElementIndexPathRef = useRef<number[]>([]);
+  const [show, setShow] = useState(true);
+  const [key, setKey] = useState(Math.random());
+
+  const { ref, getTracker } = useKeyboardNavigation({
+    onChange: (type, tracker, path) => {
+      if (type === 'focusChange') {
+        // store selected item, so it can be focused after re-render
+        selectedElementIndexPathRef.current = path ? path.map((data) => data.index) : [];
+      }
+    },
+  });
+  useEffect(() => {
+    if (show && selectedElementIndexPathRef.current.length) {
+      const tracker = getTracker();
+      if (tracker) {
+        tracker.setFocusedElementByIndex(selectedElementIndexPathRef.current);
+      }
+    }
+  });
+
+  return (
+    <div>
+      <StoryStyles />
+      {show && (
+        <div ref={ref} key={key}>
+          <ul className="nav">
+            <li>
+              <a href="/" onClick={onLinkClick} tabIndex={0}>
+                Item 1
+              </a>
+            </li>
+            <li>
+              <a href="/" onClick={onLinkClick} tabIndex={0}>
+                Item 2
+              </a>
+            </li>
+            <li>
+              <a href="/" onClick={onLinkClick} tabIndex={0}>
+                Item 3
+              </a>
+            </li>
+            <li>
+              <a href="/" onClick={onLinkClick} tabIndex={0}>
+                Item 4
+              </a>
+            </li>
+          </ul>
+        </div>
+      )}
+      <div className="buttons">
+        <Button type="button" onClick={() => setShow(!show)}>
+          {show ? 'Remove menu' : 'Show menu'}
+        </Button>
+        {show && (
+          <Button type="button" onClick={() => setKey(Math.random())}>
+            Re-render menu
+          </Button>
+        )}
+      </div>
+      <p>Once focused, you can navigate with arrow and tab keys.</p>
+      <p>Focus is restored, when component re-renders.</p>
     </div>
   );
 };
