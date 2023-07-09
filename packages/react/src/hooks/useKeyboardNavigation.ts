15b7ba90bdd756b559416d874d79c52d3ff6d3e4
Niko Helle <niko.hele@hyperactive.fi>
2023-07-09 10:44:35 +0300

    Added useKeyboardNavigation hook
    Just a hook version of the keyboard tracker.
    
    Few basic functions are returned and also element ref. All tracker functions can be accessed with getTracker().
    
    onChange-function will always receive tracker as a parameter, so there is no real need to expose all tracker's functions through the hook.

---
+++ packages/react/src/hooks/useKeyboardNavigation.ts
@@ -0,0 +1,54 @@
+import React, { useCallback, useEffect, useRef } from 'react';
+
+import { createKeyboardTracker } from './keyboardNavigation/createKeyboardTracker';
+import { KeyboardTracker, KeyboardTrackerProps } from './keyboardNavigation';
+
+type RefListener = (element: HTMLElement | null) => React.MutableRefObject<HTMLElement | null>;
+
+export function useKeyboardNavigation(trackerProps: KeyboardTrackerProps = {}) {
+  const observedElementRef = useRef<HTMLElement | null>(null);
+  const tracker = useRef<KeyboardTracker | null>(null);
+  const refListener: RefListener = useCallback(
+    (observedElement: HTMLElement | null) => {
+      observedElementRef.current = observedElement;
+      if (!observedElement || observedElement !== observedElementRef.current) {
+        if (tracker.current) {
+          tracker.current.dispose();
+          tracker.current = null;
+        }
+        observedElementRef.current = null;
+      }
+      if (observedElement) {
+        tracker.current = createKeyboardTracker(observedElement, trackerProps);
+      }
+      return observedElementRef;
+    },
+    [observedElementRef],
+  );
+
+  const cleanUp = useCallback(() => {
+    if (tracker.current) {
+      tracker.current.dispose();
+      tracker.current = null;
+    }
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      cleanUp();
+    };
+  }, [cleanUp]);
+
+  return {
+    getElement: () => {
+      return observedElementRef.current;
+    },
+    getTracker: () => {
+      return tracker.current;
+    },
+    ref: refListener,
+    refresh: () => {
+      return tracker.current ? tracker.current.refresh() : undefined;
+    },
+  };
+}
