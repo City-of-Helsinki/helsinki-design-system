45b3de96f8d8f1e9d64bf7061c85e2dac6d357dd
Minna Salonen <minsalon@Minnas-MBP.home>
2023-08-01 15:18:10 +0300

    hds-1803: header dark theme

--- packages/react/src/components/header/Header.stories.tsx
+++ packages/react/src/components/header/Header.stories.tsx
@@ -141,6 +141,128 @@ export const WithFullFeatures = (args) => (
   </>
 );
 
+export const WithFullFeaturesDarkTheme = (args) => (
+  <>
+    <Header {...args} onDidChangeLanguage={languageChangedAction}>
+      <Header.UniversalBar primaryLinkText="Helsingin kaupunki" primaryLinkHref="#">
+        <Header.NavigationLink href="#" label="Link 1" />
+        <Header.NavigationLink href="#" label="Link 2" />
+        <Header.NavigationLink href="#" label="Link 3" />
+      </Header.UniversalBar>
+
+      <Header.ActionBar
+        title="Helsingin kaupunki"
+        titleAriaLabel="Helsingin kaupunki"
+        titleUrl="https://hel.fi"
+        titleStyle={TitleStyleType.normal}
+        logoAriaLabel="Service logo"
+        logoUrl="https://hel.fi"
+        menuButtonAriaLabel="Menu"
+      >
+        <Header.NavigationLanguageSelector languages={languages}>
+          <h3>Tietoa muilla kielillä</h3>
+          <Link external href="www.example.com">
+            Selkosuomi
+          </Link>
+          <Link external href="www.example.com">
+            Viittomakieli
+          </Link>
+        </Header.NavigationLanguageSelector>
+
+        <Header.ActionBarItem fullWidth label="Haku" icon={IconSearch} id="action-bar-search">
+          <Header.NavigationSearch label="Hae palvelusta" onChange={searchChangeAction} onSubmit={searchSubmitAction} />
+        </Header.ActionBarItem>
+      </Header.ActionBar>
+
+      <Header.NavigationMenu>
+        <Header.NavigationLink
+          href="#"
+          label="Link 1"
+          onClick={(event) => event.preventDefault()}
+          active
+          dropdownLinks={[
+            <Header.NavigationLink
+              href="#"
+              label="Test"
+              active
+              dropdownLinks={[
+                <Header.NavigationLink href="#" label="Nested" />,
+                <Header.NavigationLink href="#" label="Nested" />,
+                <Header.NavigationLink href="#" label="Nested" />,
+              ]}
+            />,
+            <Header.NavigationLink
+              href="#"
+              label="Test"
+              dropdownLinks={[
+                <Header.NavigationLink href="#" label="Nested" />,
+                <Header.NavigationLink href="#" label="Nested" />,
+              ]}
+            />,
+          ]}
+        />
+        <Header.NavigationLink
+          href="#"
+          label="Link 2"
+          dropdownLinks={[
+            <Header.NavigationLink
+              href="#"
+              label="Test"
+              active
+              dropdownLinks={[
+                <Header.NavigationLink href="#" label="Nested" />,
+                <Header.NavigationLink href="#" label="Nested" />,
+                <Header.NavigationLink href="#" label="Nested" />,
+              ]}
+            />,
+            <Header.NavigationLink
+              href="#"
+              label="Test"
+              dropdownLinks={[
+                <Header.NavigationLink href="#" label="Nested" />,
+                <Header.NavigationLink href="#" label="Nested" />,
+              ]}
+            />,
+          ]}
+        />
+        <Header.NavigationLink href="#" label="Link 3" />
+        <Header.NavigationLink href="#" label="Link 4" />
+        <Header.NavigationLink href="#" label="Link 5" />
+        <Header.NavigationLink href="#" label="Link 6" />
+        <Header.NavigationLink href="#" label="Link 7" />
+        <Header.NavigationLink
+          href="#"
+          label="Link 8"
+          dropdownLinks={[
+            <Header.NavigationLink
+              href="#"
+              label="Test"
+              active
+              dropdownLinks={[
+                <Header.NavigationLink href="#" label="Nested" />,
+                <Header.NavigationLink href="#" label="Nested" />,
+                <Header.NavigationLink href="#" label="Nested" />,
+              ]}
+            />,
+            <Header.NavigationLink
+              href="#"
+              label="Test"
+              dropdownLinks={[
+                <Header.NavigationLink href="#" label="Nested" />,
+                <Header.NavigationLink href="#" label="Nested" />,
+              ]}
+            />,
+          ]}
+        />
+      </Header.NavigationMenu>
+    </Header>
+  </>
+);
+
+WithFullFeaturesDarkTheme.args = {
+  theme: 'dark',
+};
+
 export const Minimal = (args) => {
   return (
     <Header {...args} onDidChangeLanguage={languageChangedAction}>
@@ -171,6 +293,47 @@ export const Minimal = (args) => {
       </Header.NavigationMenu>
     </Header>
   );
+};
+
+export const MinimalWithDarkTheme = (args) => {
+  return (
+    <Header {...args} onDidChangeLanguage={languageChangedAction}>
+      <Header.ActionBar
+        title="Helsingin kaupunki"
+        titleAriaLabel="Helsingin kaupunki"
+        titleUrl="https://hel.fi"
+        titleStyle={TitleStyleType.black}
+      >
+        <Header.NavigationLanguageSelector languages={languages}>
+          <h3>Tietoa muilla kielillä</h3>
+          <Link external href="www.example.com">
+            Selkosuomi
+          </Link>
+          <Link external href="www.example.com">
+            Viittomakieli
+          </Link>
+        </Header.NavigationLanguageSelector>
+
+        <Header.ActionBarItem fullWidth label="Haku" icon={IconSearch} id="action-bar-search">
+          <Header.NavigationSearch onChange={searchChangeAction} onSubmit={searchSubmitAction} label="Haku" />
+        </Header.ActionBarItem>
+        <hr style={{ order: 9 }} />
+        <Header.ActionBarItem label="Kirjaudu" icon={IconUser} style={{ order: 10 }} id="action-bar-login">
+          <h3>Kirjautumisvalinnat</h3>
+        </Header.ActionBarItem>
+      </Header.ActionBar>
+
+      <Header.NavigationMenu>
+        <Header.NavigationLink href="#" label="Link 1" />
+        <Header.NavigationLink href="#" label="Link 2" />
+        <Header.NavigationLink href="#" label="Link 3" />
+      </Header.NavigationMenu>
+    </Header>
+  );
+};
+
+MinimalWithDarkTheme.args = {
+  theme: 'dark',
 };
 
 export const MinimalWithLocalization = (args) => {
@@ -249,4 +412,8 @@ export const MinimalWithLocalization = (args) => {
       </Header.NavigationMenu>
     </Header>
   );
+};
+
+WithSkipLink.args = {
+  theme: 'dark',
 };
