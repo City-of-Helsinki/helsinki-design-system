c42613b508d0e8eb14aeab4c1a8007232ac2aeef
Minna Salonen <minsalon@Minnas-MBP.home>
2023-08-02 15:18:42 +0300

    hds-1813: use webpack5 in core

--- packages/core/.storybook/main.js
+++ packages/core/.storybook/main.js
@@ -1,4 +1,7 @@
 module.exports = {
+  core: {
+    builder: 'webpack5',
+  },
   stories: ['../src/**/*.stories.@(js|mdx)'],
   addons: [
     '@storybook/addon-docs',
@@ -6,6 +9,26 @@ module.exports = {
     '@storybook/addon-backgrounds',
     '@storybook/addon-viewport',
     '@storybook/addon-storysource',
+    'storybook-preset-inline-svg'
   ],
   staticDirs: ['../src/fonts'],
+  webpackFinal: async (config) => {
+    return {
+      ...config,
+      module: { 
+        ...config.module, 
+        rules: [
+          ...config.module.rules, 
+          {
+            test: /\.svg$/,
+            resourceQuery: /svgr/,
+            use: [
+              {
+                loader: "@svgr/webpack",
+              },
+            ],
+          }
+      ] },
+    };
+  },  
 };
