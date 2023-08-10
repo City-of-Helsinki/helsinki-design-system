e1cd2e6cce26822d2f273d03d86f6c8dad18ffc7
harriplappalainen <harri.lappalainen@solita.fi>
2023-08-10 15:04:26 +0300

    Export SkipLinkTheme and TitleStyleType from header to the build

--- packages/react/src/components/header/index.ts
+++ packages/react/src/components/header/index.ts
@@ -1,2 +1,4 @@
 export * from './Header';
 export { LanguageOption } from './LanguageContext';
+export { SkipLinkTheme } from '../../internal/skipLink';
+export { TitleStyleType } from './components/headerActionBar';
