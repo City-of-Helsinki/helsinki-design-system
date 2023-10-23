664213880edbc51d67a2f3001c7a728c571a5ed6
Tuomo Kivinen <mrtuomok@gmail.com>
2023-10-23 08:56:38 +0300

    (HDS-1763) fix tag styles to match designs, add more examples, update reference images,

--- packages/core/src/components/tag/tag.stories.js
+++ packages/core/src/components/tag/tag.stories.js
@@ -4,19 +4,19 @@ export default {
   title: 'Components/Tag',
 };
 
-export const Default = () => `
+export const DefaultTag = () => `
   <div class="hds-tag">
     <span class="hds-tag__label">Label</span>
   </div>
 `;
 
-export const Clickable = () => `
+export const DefaultTagClickable = () => `
   <div class="hds-tag" role="link" tabindex="0" onclick="">
     <span class="hds-tag__label">Label</span>
   </div>
 `;
 
-export const Deletable = () => `
+export const DefaultTagDeletable = () => `
   <div class="hds-tag">
     <span class="hds-tag__label" aria-hidden="true">Label</span>
     <button aria-label="Delete item: Label" class="hds-tag__delete-button button-reset">
@@ -25,11 +25,47 @@ export const Deletable = () => `
   </div>
 `;
 
-export const LargeDeletable = () => `
+export const DefaultTagWithLongText = () => `
+  <div class="hds-tag" style="max-width: 300px;">
+    <span class="hds-tag__label">Label - This is a tag with a very long text which is not advisable and might span into multiple lines</span>
+  </div>
+`;
+
+export const DefaultTagWithLongTextAndDeletable = () => `
+  <div class="hds-tag" style="max-width: 300px;">
+  <span class="hds-tag__label" aria-hidden="true">Label - This is a tag with a very long text which is not advisable and might span into multiple lines</span>
+    <button aria-label="Delete item: Label" class="hds-tag__delete-button button-reset">
+      <span aria-hidden="true" class="hds-icon hds-icon--cross"></span>
+    </button>
+  </div>
+`;
+
+export const LargeTag = () => `
+  <div class="hds-tag hds-tag--large">
+    <span class="hds-tag__label">Label</span>
+  </div>
+`;
+
+export const LargeTagDeletable = () => `
   <div class="hds-tag hds-tag--large">
     <span class="hds-tag__label" aria-hidden="true">Label</span>
     <button aria-label="Delete item: Label" class="hds-tag__delete-button button-reset">
         <span aria-hidden="true" class="hds-icon hds-icon--cross"></span>
+    </button>
+  </div>
+`;
+
+export const LargeTagWithLongText = () => `
+  <div class="hds-tag hds-tag--large" style="max-width: 300px;">
+    <span class="hds-tag__label" aria-hidden="true">Label - This is a tag with a very long text which is not advisable and might span into multiple lines</span>
+  </div>
+`;
+
+export const LargeTagWithLongTextAndDeletable = () => `
+  <div class="hds-tag hds-tag--large" style="max-width: 300px;">
+    <span class="hds-tag__label" aria-hidden="true">Label - This is a tag with a very long text which is not advisable and might span into multiple lines</span>
+    <button aria-label="Delete item: Label" class="hds-tag__delete-button button-reset">
+      <span aria-hidden="true" class="hds-icon hds-icon--cross"></span>
     </button>
   </div>
 `;
