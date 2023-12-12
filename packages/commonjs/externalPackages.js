9adc56cec9c6350af2adc12a807cf650e54d2ee2
NikoHelle <niko.helle@hyperactive.fi>
2023-12-12 15:05:58 +0200

    (hds-1742) Base files for hds-js module
    base-package.json is used when creating actual package.json with rollup
    
    externalPackages.js lists packages that should not be bundled. Same as 'external' in rollup.config
    
    index.ts list files to export. Currently test files are listed.

---
+++ packages/commonjs/externalPackages.js
@@ -0,0 +1,6 @@
+/**
+ * List packages that should NOT be bundled to the complied js.
+ * Listed packages are listed in the 'dependencies' of the generated package.json
+ */
+const dependencies = ['cookie'];
+module.exports = dependencies;
---
+++ packages/commonjs/index.ts
@@ -0,0 +1,3 @@
+export * from '../react/src/utils/cancellablePromise';
+export * from '../react/src/components/cookieConsent/cookieConsentController';
+export * from '../react/src/components/cookieConsent/cookieController';
