8fb5194e29a8aeda2ee9a288ef5868dda18a6870
NikoHelle <niko.helle@hyperactive.fi>
2024-01-04 14:02:30 +0200

    (hds-1742) Fix lib refs and login exports
    Paths to the bundle were wrong.
    
    Exported all login related code byt creating a new index file to export vanilla js code only.

--- packages/hds-js/index.ts
+++ packages/hds-js/index.ts
@@ -1,4 +1,4 @@
 export * from '../react/src/components/cookieConsent/cookieConsentController';
 export * from '../react/src/components/cookieConsent/cookieController';
 export * from '../react/src/components/cookieConsent/getContent';
-export { createLoginSystem } from '../react/src/components/login/createLoginSystem';
+export * from '../react/src/components/login/index.vanilla-js';
