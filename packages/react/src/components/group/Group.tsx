5714aa3216bb0bac8aca80dee8f0401dcd3d2b26
NikoHelle <niko.helle@hyperactive.fi>
2024-01-23 11:24:36 +0200

    (hds-2069) Group component
    The component creates a controller and renders children with data from the controller.

---
+++ packages/react/src/components/group/Group.tsx
@@ -0,0 +1,66 @@
+import React, { isValidElement, useMemo } from 'react';
+
+import { getChildrenAsArray } from '../../utils/getChildren';
+import useForceRender from '../../hooks/useForceRender';
+import { createController } from './utils/controller';
+import { Controller, GroupChild, GroupProps } from './utils';
+import { isForwardController } from './utils/forwardController';
+
+export function pickGroupId(props: React.HtmlHTMLAttributes<HTMLElement>) {
+  return props['data-hds-group-id'] || props.id;
+}
+
+export const renderGroupChild = (
+  child: React.FunctionComponentElement<GroupChild> | React.ReactElement,
+  controller: Controller,
+) => {
+  const groupId = pickGroupId(child.props);
+  const props = groupId ? controller.getProps({ id: groupId, elementProps: child.props }) : {};
+  if (isForwardController(child)) {
+    props.forwardedController = controller;
+  }
+  return React.cloneElement(child, props);
+};
+
+export function renderGroupChildren(children: React.ReactNode | null, controller: Controller): React.ReactNode[] {
+  return getChildrenAsArray(children).map((child) => {
+    if (!child) {
+      return null;
+    }
+    const childType = typeof child;
+    if (childType === 'string' || childType === 'number') {
+      return child;
+    }
+    // in this case the child is a plain function accepting controller in argument object
+    if (childType === 'function') {
+      const renderResult = React.cloneElement(
+        (child as React.FC<{ controller: Controller }>)({ controller }) as React.ReactElement,
+      );
+      return renderGroupChild(renderResult, controller);
+    }
+    if (!isValidElement(child)) {
+      return null;
+    }
+    return renderGroupChild(child as React.ReactElement, controller);
+  });
+}
+
+export const RenderWithController = ({ children, controller }: React.PropsWithChildren<{ controller: Controller }>) => {
+  return <>{renderGroupChildren(children, controller)}</>;
+};
+
+export function Group({ children, propSetter, initialData, metaData, onChange = () => undefined }: GroupProps) {
+  const reRenderer = useForceRender();
+  const controller = useMemo(
+    () =>
+      createController({
+        reRenderer,
+        propSetter,
+        initialData,
+        metaData,
+        onChange,
+      }),
+    [initialData],
+  );
+  return <>{renderGroupChildren(children, controller)}</>;
+}
