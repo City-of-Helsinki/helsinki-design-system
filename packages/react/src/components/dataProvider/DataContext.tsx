e7223395495ff240bdc34b4cbcbc2111d9c9954d
NikoHelle <niko.helle@hyperactive.fi>
2024-02-22 11:51:37 +0200

    (hds-2069) DataProvider/Context for sharing data
    DataStorage is for storing given props like options, state, disabled, open
    
    MetaDataStorage is for storing internal state and data, like clickedElement, element refs, ids.
    
    Handlers are for updating storages and triggering changes.
    
    When data changes, the component is usually re-rendered.
    
    When metaData changes, re-rendering does not usually happen, but can.
    
    When a ChangeEvent is triggered, the onChange-function is called. If the function returns true, the context is re-rendered.
    
    onChange-function is memoized to force it to be a pure function and to avoid unnecessary renders.
    
    data and metadata storages are also memoized, but given data and metaData props are memoization dependencies.
    
    So changing data props between renders will create a new data storage. Same with metaData.
    
    DataProvider wraps the DataContext and enables re-rendering.

---
+++ packages/react/src/components/dataProvider/DataContext.tsx
@@ -0,0 +1,48 @@
+import { createContext, SyntheticEvent } from 'react';
+
+import { createStorage, Storage, StorageData } from './storage';
+
+export type ChangeEventPayload = { value?: unknown; originalEvent?: SyntheticEvent };
+export type ChangeEvent = { id: string; type?: string; payload?: ChangeEventPayload };
+export type ChangeHandler<D = StorageData, M = StorageData> = (
+  event: ChangeEvent,
+  dataHandlers: DataHandlers<D, M>,
+) => boolean;
+export type ChangeTrigger = (event: ChangeEvent) => void;
+
+export type DataContextProps<D = StorageData, M = StorageData> = {
+  dataStorage: Storage<D>;
+  metaDataStorage: Storage<M>;
+};
+
+export type DataHandlers<D = StorageData, M = StorageData> = {
+  getData: () => D;
+  updateData: (newData: Partial<D>) => D;
+  getMetaData: () => M;
+  updateMetaData: (newData: Partial<M>) => M;
+  asyncRequestWithTrigger: (promise: Promise<ChangeEvent>) => void;
+  trigger: ChangeTrigger;
+};
+
+export type DataContextContent<D = StorageData, M = StorageData> = DataContextProps<D, M> & {
+  dataHandlers: DataHandlers<D, M>;
+};
+
+const defaultStorage = createStorage({});
+
+export const createDataHandlers = (dataStorage: Storage, metaDataStorage: Storage): DataHandlers => {
+  return {
+    getData: () => dataStorage.get(),
+    updateData: (newData: StorageData) => dataStorage.set(newData),
+    getMetaData: () => metaDataStorage.get(),
+    updateMetaData: (newData: StorageData) => metaDataStorage.set(newData),
+    asyncRequestWithTrigger: () => ({}),
+    trigger: () => ({}),
+  };
+};
+
+export const DataContext = createContext<DataContextContent>({
+  dataStorage: defaultStorage,
+  metaDataStorage: defaultStorage,
+  dataHandlers: createDataHandlers(defaultStorage, defaultStorage),
+});
