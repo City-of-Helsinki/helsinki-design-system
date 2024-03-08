1b1a0b4c2debdc0ad40ee038c92cb7eab9d92699
NikoHelle <niko.helle@hyperactive.fi>
2024-03-08 18:35:17 +0200

    (hds-2069) Select component

---
+++ packages/react/src/components/select/Select.stories.tsx
@@ -0,0 +1,161 @@
+import React from 'react';
+
+import { SelectProps } from './types';
+import { IconLocation } from '../../icons';
+import { Select } from './Select';
+
+export default {
+  component: Select,
+  title: 'Components/Select',
+};
+
+const capitalise = (str: string) => str[0].toUpperCase() + str.slice(1);
+
+const fruitsAndVegetables = [
+  'apple',
+  'banana',
+  'orange',
+  'grape',
+  'strawberry',
+  'watermelon',
+  'kiwi',
+  'pineapple',
+  'mango',
+  'peach',
+  'carrot',
+  'broccoli',
+  'potato',
+  'tomato',
+  'cucumber',
+  'lettuce',
+  'spinach',
+  'bell pepper',
+  'eggplant',
+  'zucchini',
+  'blueberry',
+  'raspberry',
+  'blackberry',
+  'avocado',
+  'pear',
+  'lemon',
+  'lime',
+  'cherry',
+  'pumpkin',
+  'apricot',
+  'cranberry',
+  'fig',
+  'grapefruit',
+  'guava',
+  'honeydew',
+  'kale',
+  'nectarine',
+  'olive',
+  'papaya',
+  'pea',
+  'plum',
+  'radish',
+  'rutabaga',
+  'tangerine',
+  'turnip',
+  'watercress',
+  'yam',
+  'yucca',
+];
+
+const adjectives = [
+  'sweet',
+  'juicy',
+  'ripe',
+  'fresh',
+  'tasty',
+  'delicious',
+  'crisp',
+  'tangy',
+  'fragrant',
+  'colorful',
+  'nutritious',
+  'succulent',
+  'flavorful',
+  'sour',
+  'satisfying',
+  'exotic',
+  'vibrant',
+  'healthy',
+  'aromatic',
+  'wholesome',
+  'tender',
+  'zesty',
+  'bitter',
+  'spicy',
+  'crunchy',
+  'mellow',
+  'sugary',
+  'scented',
+  'sapid',
+  'mouthwatering',
+];
+
+function generateOptionLabels(count = -1): string[] {
+  // Arrays containing names of fruits and vegetables and adjectives
+
+  const length = count > 0 ? count : Math.floor(Math.random() * 20) + 1;
+
+  // Using set to avoid duplicates
+  const randomSet: Set<string> = new Set();
+  while (randomSet.size < length) {
+    // Randomly select a fruit or vegetable and an adjective
+    const randomFruitIndex = Math.floor(Math.random() * fruitsAndVegetables.length);
+    const randomAdjectiveIndex = Math.floor(Math.random() * adjectives.length);
+    const randomFruit = fruitsAndVegetables[randomFruitIndex];
+    const randomAdjective = adjectives[randomAdjectiveIndex];
+    const description = `${capitalise(randomAdjective)} ${randomFruit}`;
+    randomSet.add(description);
+  }
+
+  return Array.from(randomSet);
+}
+
+export const Example = () => {
+  const options = generateOptionLabels(20);
+  return <Select options={options} label="Label" placeholder="Choose one" icon={<IconLocation />} />;
+};
+
+export const OptionsAsHtml = () => {
+  return (
+    <Select label="Label" placeholder="Choose one">
+      <optgroup label="Group 1">
+        <option value="opt1">Option 1</option>
+        <option value="opt2">Option 2</option>
+      </optgroup>
+      <optgroup label="Group 2">
+        <option value="opt3">Option 3</option>
+        <option value="opt4">Option 4</option>
+      </optgroup>
+    </Select>
+  );
+};
+
+export const WithGroups = () => {
+  const groups: SelectProps['groups'] = [
+    {
+      label: 'Healthy choices',
+      options: generateOptionLabels(40),
+    },
+    {
+      label: 'Bad choices',
+      options: [
+        { value: 'Candy cane', label: 'Candy cane' },
+        { value: 'Sugar bomb', label: 'Sugar bomb' },
+        { value: 'Dr. Pepper', label: 'Dr. Pepper' },
+      ],
+    },
+  ];
+
+  return (
+    <Select groups={groups} label="Label" placeholder="Choose one" icon={<IconLocation />}>
+      <optgroup label="Group label">
+        <option value="label">Text</option>
+      </optgroup>
+    </Select>
+  );
+};
