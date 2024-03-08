b77bd46548d6c3a94402a082df8327dfbb6c58e9
NikoHelle <niko.helle@hyperactive.fi>
2024-03-08 18:33:38 +0200

    (hds-2069) Main Select button with selected options

---
+++ packages/react/src/components/select/components/selectedOptions/SelectedOptionsContainer.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import styles from '../../Select.module.scss';
+import { DivElementProps } from '../../types';
+import classNames from '../../../../utils/classNames';
+import { ButtonWithSelectedOptions } from './ButtonWithSelectedOptions';
+import { ArrowButton } from './ArrowButton';
+import { ClearButton } from './ClearButton';
+
+function createContainerProps(props: Partial<DivElementProps>): DivElementProps {
+  return {
+    ...props,
+    className: classNames(styles.selectedOptionsContainer),
+  };
+}
+
+export function SelectedOptionsContainer(props: Partial<DivElementProps>) {
+  const attr = createContainerProps(props);
+
+  return (
+    <div {...attr}>
+      <ButtonWithSelectedOptions />
+      <ClearButton />
+      <ArrowButton />
+    </div>
+  );
+}
