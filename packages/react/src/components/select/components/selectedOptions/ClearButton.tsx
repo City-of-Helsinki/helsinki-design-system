0a3cde0e4aa1fd8c622a9d6f28d6102a578ec207
NikoHelle <niko.helle@hyperactive.fi>
2024-03-08 18:33:38 +0200

    (hds-2069) Main Select button with selected options

---
+++ packages/react/src/components/select/components/selectedOptions/ClearButton.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import styles from '../../Select.module.scss';
+import { ButtonElementProps, SelectDataHandlers } from '../../types';
+import { IconCrossCircle } from '../../../../icons';
+import classNames from '../../../../utils/classNames';
+import { createOnClickListener, getSelectedOptions } from '../../utils';
+import { eventTypes, eventIds } from '../../events';
+import { useSelectDataHandlers } from '../../hooks/useSelectDataHandlers';
+
+const createClearButtonProps = ({ getData, getMetaData, trigger }: SelectDataHandlers): ButtonElementProps | null => {
+  const { elementIds } = getMetaData();
+  const { groups } = getData();
+  const selectedOptions = getSelectedOptions(groups);
+  if (!selectedOptions.length) {
+    return null;
+  }
+  return {
+    className: classNames(styles.button, styles.icon),
+    ...createOnClickListener({ id: eventIds.clearButton, type: eventTypes.click, trigger }),
+    id: elementIds.clearButton,
+    'aria-label': 'Remove all selections',
+  };
+};
+
+export function ClearButton() {
+  const props = createClearButtonProps(useSelectDataHandlers());
+  if (!props) {
+    return null;
+  }
+  return (
+    <button type="button" {...props}>
+      <IconCrossCircle className={styles.angleIcon} aria-hidden />
+    </button>
+  );
+}
