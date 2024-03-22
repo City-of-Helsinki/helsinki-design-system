4a736b913d8d50119bfb8711b2452600dc7f0a41
NikoHelle <niko.helle@hyperactive.fi>
2024-03-22 15:13:38 +0200

    (hds-2069) Screenreader notifications

---
+++ packages/react/src/components/select/components/list/searchAndFilter/ScreenReaderNotifications.tsx
@@ -0,0 +1,158 @@
+import React, { useCallback, useEffect, useRef } from 'react';
+
+import styles from '../../../Select.module.scss';
+import useForceRender from '../../../../../hooks/useForceRender';
+
+type ScreenReaderAlertData = {
+  pendingNotification?: string;
+  currentText?: string;
+  lastRenderedNotification?: string;
+  timeoutId?: ReturnType<typeof setTimeout>;
+};
+
+type Props = {
+  search?: string;
+  filter?: string;
+  isSearching: boolean;
+  resultCount: number;
+};
+
+export function ScreenReaderNotifications({ search, filter, isSearching, resultCount }: Props) {
+  const forceRender = useForceRender();
+  const dataRef = useRef<ScreenReaderAlertData>({});
+  const delayTime = 500;
+  const updateDataRef = useCallback(
+    (newProps: Partial<ScreenReaderAlertData>) => {
+      dataRef.current = {
+        ...dataRef.current,
+        ...newProps,
+      };
+    },
+    [dataRef],
+  );
+  const isPreviousNotification = useCallback(
+    (newNotification: string) => {
+      return (
+        dataRef.current.pendingNotification === newNotification ||
+        dataRef.current.currentText === newNotification ||
+        dataRef.current.lastRenderedNotification === newNotification
+      );
+    },
+    [dataRef],
+  );
+
+  const clearTimer = useCallback(() => {
+    const { timeoutId } = dataRef.current;
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    dataRef.current.timeoutId = undefined;
+  }, [dataRef]);
+
+  const clearPendingNotification = useCallback(() => {
+    clearTimer();
+    updateDataRef({ pendingNotification: undefined });
+  }, [clearTimer, updateDataRef]);
+
+  const setPendingNotification = useCallback(
+    (newNotification: string) => {
+      if (isPreviousNotification(newNotification)) {
+        return;
+      }
+      clearTimer();
+      updateDataRef({
+        pendingNotification: newNotification,
+      });
+      forceRender();
+    },
+    [dataRef, updateDataRef, forceRender, clearTimer, isPreviousNotification],
+  );
+
+  const showPendingNotification = useCallback(() => {
+    updateDataRef({
+      currentText: dataRef.current.pendingNotification,
+      pendingNotification: undefined,
+      timeoutId: undefined,
+    });
+  }, [dataRef, updateDataRef]);
+
+  const startPendingNotificationTimer = useCallback(() => {
+    clearTimeout(dataRef.current.timeoutId);
+    dataRef.current.timeoutId = setTimeout(() => {
+      showPendingNotification();
+      forceRender();
+    }, delayTime);
+  }, [dataRef, delayTime, forceRender, showPendingNotification]);
+
+  const shouldRender = () => {
+    if (!dataRef.current.currentText) {
+      return false;
+    }
+    if (dataRef.current.lastRenderedNotification === dataRef.current.currentText) {
+      return false;
+    }
+    return true;
+  };
+
+  const screenReaderTexts = {
+    none: '',
+    isSearching: `Searching for "${search}"`,
+    filteredResults: `Filtered results for "${filter}". Found ${resultCount} options.`,
+    noResults: `No options found for "${search || filter}". Try a different term.`,
+    searchResults: `Found ${resultCount} options for search term "${search}".`,
+  };
+
+  const getScreenReaderTextType = (): keyof typeof screenReaderTexts => {
+    if (isSearching) {
+      return 'isSearching';
+    }
+    if (filter) {
+      return 'filteredResults';
+    }
+    if (search && !isSearching) {
+      return 'searchResults';
+    }
+    if (resultCount === 0) {
+      return 'noResults';
+    }
+    return 'none';
+  };
+
+  const textType = getScreenReaderTextType();
+  const notification = screenReaderTexts[textType];
+  const debouncedTextTypes: Array<ReturnType<typeof getScreenReaderTextType>> = ['filteredResults', 'isSearching'];
+  const isPendingTextType = debouncedTextTypes.includes(textType);
+
+  if (!isPendingTextType) {
+    if (dataRef.current.pendingNotification) {
+      clearPendingNotification();
+    }
+    dataRef.current.currentText = notification;
+  } else {
+    setPendingNotification(notification);
+  }
+
+  useEffect(() => {
+    if (dataRef.current.pendingNotification && !dataRef.current.timeoutId) {
+      startPendingNotificationTimer();
+    }
+  });
+  useEffect(() => {
+    return () => {
+      clearPendingNotification();
+      dataRef.current = {};
+    };
+  }, [dataRef, clearPendingNotification, startPendingNotificationTimer]);
+
+  if (!shouldRender()) {
+    return null;
+  }
+
+  updateDataRef({ lastRenderedNotification: dataRef.current.currentText });
+  console.log('RENDER--->', dataRef.current.currentText);
+  return (
+    <div aria-live="polite" className={styles.screenReaderNotifications}>
+      {dataRef.current.currentText}
+    </div>
+  );
+}
--- packages/react/src/components/select/components/list/searchAndFilter/SearchAndFilterInfo.tsx
+++ packages/react/src/components/select/components/list/searchAndFilter/SearchAndFilterInfo.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 import styles from '../../../Select.module.scss';
 import classNames from '../../../../../utils/classNames';
@@ -6,43 +6,51 @@ import { countVisibleOptions } from '../../../utils';
 import { LoadingSpinner } from '../../../../loadingSpinner';
 import { DivElementProps } from '../../../types';
 import { useSelectDataHandlers } from '../../../hooks/useSelectDataHandlers';
+import { ScreenReaderNotifications } from './ScreenReaderNotifications';
 
-const createSearchAndFilterInfoProps = (): DivElementProps => {
+const createSearchAndFilterInfoProps = (forScreenReaderOnly: boolean): DivElementProps => {
   return {
-    className: classNames(styles.searchAndFilterInfoContainer),
+    className: classNames(styles.searchAndFilterInfoContainer, forScreenReaderOnly && styles.screenReaderOnly),
   };
 };
 
 export function SearchAndFilterInfo() {
   const { getData, getMetaData } = useSelectDataHandlers();
-
-  const { groups } = getData();
+  const shouldRenderScreenReaderNotificationsRef = useRef(false);
+  const { groups, open } = getData();
   const { isSearching, search, filter } = getMetaData();
   const count = countVisibleOptions(groups);
+  if (!shouldRenderScreenReaderNotificationsRef.current && (isSearching || filter)) {
+    shouldRenderScreenReaderNotificationsRef.current = true;
+  }
   const noResultsTexts =
     !isSearching && !count && (search || filter)
       ? [`No options found for "${search || filter}"`, 'Try a different term']
       : [];
   const loadingText = isSearching ? 'Loading options' : '';
+  const shouldBeVisible = noResultsTexts.length > 0 || loadingText;
 
-  const attr = createSearchAndFilterInfoProps();
-  if (!noResultsTexts?.length && !loadingText) {
+  if (!open || (!shouldBeVisible && !shouldRenderScreenReaderNotificationsRef.current)) {
     return null;
   }
+
+  const attr = createSearchAndFilterInfoProps(!shouldBeVisible && shouldRenderScreenReaderNotificationsRef.current);
+
   return (
     <div {...attr}>
       {loadingText && (
         <>
-          <LoadingSpinner small />
-          <span>{loadingText}</span>
+          <LoadingSpinner small loadingText="" loadingFinishedText="" />
+          <span aria-hidden>{loadingText}</span>
         </>
       )}
       {!!noResultsTexts.length && (
         <>
-          <span>{noResultsTexts[0]}</span>
-          <span>{noResultsTexts[1]}</span>
+          <span aria-hidden>{noResultsTexts[0]}</span>
+          <span aria-hidden>{noResultsTexts[1]}</span>
         </>
       )}
+      <ScreenReaderNotifications isSearching={isSearching} search={search} filter={filter} resultCount={count} />
     </div>
   );
 }
