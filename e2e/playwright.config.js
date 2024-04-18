f9a87efedb20487219ab4e74928b489d5368c14e
Tero Virtanen <tero.virtanen@mavericks.fi>
2024-04-18 07:43:25 +0300

    suggestions from review

--- e2e/playwright.config.js
+++ e2e/playwright.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './e2e/tests',
+  testDir: './tests',
   timeout: 180 * 1000,
   /* Run tests in files in parallel */
   fullyParallel: false,
