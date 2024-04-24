76d82db57fe3853ad8d46af6a3ee5736f257f922
NikoHelle <niko.helle@hyperactive.fi>
2024-04-24 13:04:21 +0300

    (hds-2235) New exports to hds-js

--- packages/hds-js/index.ts
+++ packages/hds-js/index.ts
@@ -1,4 +1,5 @@
 export * from '../react/src/components/cookieConsent/cookieConsentController';
 export * from '../react/src/components/cookieConsent/cookieController';
 export * from '../react/src/components/cookieConsent/getContent';
+export * from '../react/src/components/cookieConsent/classes/testClass';
 export * from '../react/src/components/login/index.vanilla-js';
