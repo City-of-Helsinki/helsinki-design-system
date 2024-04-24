b1038b38525aad835db4b38a0e0729d9a215eeb4
Mikko Tapionlinna <mikko.tapionlinna@siili.com>
2024-04-24 15:47:56 +0300

    (HDS-1854) Add initial version of cookie consent core

---
+++ packages/react/src/components/cookieConsentCore/template.js
@@ -0,0 +1,147 @@
+/**
+ * Generates the HTML for the cookie banner.
+ *
+ * @param {Object} translations - The translations object containing the text for the banner.
+ * @param {string} translations.bannerAriaLabel - The aria-label for the banner.
+ * @param {string} translations.heading - The heading text for the banner.
+ * @param {string} translations.description - The description text for the banner.
+ * @param {string} translations.showDetails - The text for the "Show Details" button.
+ * @param {string} translations.hideDetails - The text for the "Hide Details" button.
+ * @param {string} translations.form_heading - The heading text for the form.
+ * @param {string} translations.form_text - The text for the form.
+ * @param {string} translations.approveAllConsents - The text for the "Approve All Consents" button.
+ * @param {string} translations.approveRequiredAndSelectedConsents - The text for the "Approve Required and Selected Consents" button.
+ * @param {string} translations.approveOnlyRequiredConsents - The text for the "Approve Only Required Consents" button.
+ * @param {string} groupsHtml - The HTML for the consent groups.
+ * @param {boolean} isBanner - Indicates if the code is rendered as banner or part of a page.
+ * @return {string} The HTML for the cookie banner.
+ */
+export function getCookieBannerHtml(translations, groupsHtml, isBanner = true) {
+  return `
+<div id="hds-cc" class="hds-cc ${isBanner ? 'hds-cc--banner' : 'hds-cc--page'}" tabindex="-1" role="region" aria-label="${translations.bannerAriaLabel}">
+  <div class="hds-cc__container">
+    <div class="hds-cc__aligner">
+
+      <h2 class="hds-cc__heading">
+        ${translations.heading}
+      </h2>
+      <p class="hds-cc__description">
+        ${translations.description}
+      </p>
+
+      <button
+        type="button"
+        class="hds-cc__accordion-button hds-cc__accordion-button--details hds-button hds-button--small hds-button--supplementary"
+        onclick="this.setAttribute('aria-expanded', this.getAttribute('aria-expanded') === 'false' ? 'true' : 'false');"
+        aria-controls="hds-cc-form"
+        aria-expanded="false"
+        aria-live="polite">
+        <span aria-hidden="true" class="hds-icon hds-icon--angle-down"></span>
+        <span class="hds-cc__accordion-button-show hds-button__label">${translations.showDetails}</span>
+        <span class="hds-cc__accordion-button-hide hds-button__label">${translations.hideDetails}</span>
+      </button>
+
+      <form action="" class="hds-cc__form" id="hds-cc-form">
+        <div class="hds-cc__form__animator">
+          <h3>${translations.form_heading}</h3>
+          <p>${translations.form_text}</p>
+
+          <div class="hds-cc__groups">
+            ${groupsHtml}
+          </div>
+        </div>
+      </form>
+      <div class="hds-cc__buttons">
+        <button type="submit" class="hds-button hds-button--secondary hds-cc__selected-cookies-button" data-approved="selected">
+        <span class="hds-button__label">${translations.approveRequiredAndSelectedConsents}</span>
+        </button>
+        <button type="submit" class="hds-button hds-button--secondary hds-cc__all-cookies-button" data-approved="all">
+          <span class="hds-button__label">${translations.approveAllConsents}</span>
+        </button>
+        <button type="submit" class="hds-button hds-button--secondary hds-cc__required-cookies-button" data-approved="required">
+          <span class="hds-button__label">${translations.approveOnlyRequiredConsents}</span>
+        </button>
+      </div>
+    </div>
+  </div>
+</div>`;
+}
+
+/**
+ * Generates the HTML for a cookie group.
+ *
+ * @param {Object} translations - The translations object containing the text for the group.
+ * @param {string} groupId - The ID of the cookie group.
+ * @param {string} groupUniqueId - The unique ID of the cookie group.
+ * @param {string} tableRowsHtml - The HTML for the table rows.
+ * @param {boolean} groupRequired - Indicates if the group is required.
+ * @param {boolean} isAccepted - Indicates if the group is accepted.
+ * @return {string} The generated HTML for the cookie group.
+ */
+export function getGroupHtml(translations, groupId, groupUniqueId, tableRowsHtml, groupRequired, isAccepted) {
+  const required = groupRequired ? ' checked disabled' : '';
+  const accepted = isAccepted ? 'checked' : '';
+  return `
+            <div class="hds-cc__group">
+              <div class="hds-checkbox">
+                <input type="checkbox" id="${groupId}-cookies" class="hds-checkbox__input"${required}${accepted} data-group="${groupId}" />
+                <label for="${groupId}-cookies" class="hds-checkbox__label">${translations.title}</label>
+              </div>
+              <p>${translations.description}</p>
+
+              <button
+                type="button"
+                class="hds-cc__accordion-button hds-cc__accordion-button--group-details hds-button hds-button--small hds-button--supplementary"
+                onclick="this.setAttribute('aria-expanded', this.getAttribute('aria-expanded') === 'false' ? 'true' : 'false');"
+                aria-controls="hds-cc-group-details-${groupUniqueId}"
+                aria-expanded="false"
+                aria-live="polite">
+                <span aria-hidden="true" class="hds-icon hds-icon--angle-down"></span>
+                <span class="hds-cc__accordion-button-show hds-button__label">${translations.showCookieSettings}</span>
+                <span class="hds-cc__accordion-button-hide hds-button__label">${translations.hideCookieSettings}</span>
+              </button>
+              <div class="hds-cc__group-details" id="hds-cc-group-details-${groupUniqueId}">
+                <div class="hds-cc__group-details__animator">
+                  <div class="hds-table-container" tabindex="0" role="region">
+                    <table class="hds-table hds-table--dark">
+                      <thead>
+                        <tr class="hds-table__header-row">
+                        <th scope="col">${translations.tableHeadingsName}</th>
+                        <th scope="col">${translations.tableHeadingsHostName}</th>
+                        <th scope="col">${translations.tableHeadingsDescription}</th>
+                        <th scope="col">${translations.tableHeadingsExpiration}</th>
+                        <th scope="col">${translations.tableHeadingsType}</th>
+                        </tr>
+                      </thead>
+                      <tbody class="hds-table__content">
+                        ${tableRowsHtml}
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>`;
+}
+
+/**
+ * Generates HTML for a table row based on the provided row data.
+ *
+ * @param {Object} rowData - The data for the table row.
+ * @param {string} rowData.name - Cookie key string
+ * @param {string} rowData.host - The host of the cookie.
+ * @param {string} rowData.description - The description of the cookie.
+ * @param {string} rowData.expiration - The expiration of the cookie.
+ * @param {string} rowData.type - The type of the cookie/storage item.
+ * @return {string} The HTML for the table row.
+ */
+export function getTableRowHtml(rowData) {
+  return `
+                    <tr>
+                      <td>${rowData.name}</td>
+                      <td>${rowData.host}</td>
+                      <td>${rowData.description}</td>
+                      <td>${rowData.expiration}</td>
+                      <td>${rowData.type}</td>
+                    </tr>
+                    `;
+}
---
+++ packages/react/src/components/cookieConsentCore/translations.js
@@ -0,0 +1,172 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-template-curly-in-string */
+
+const translations = {
+  bannerAriaLabel: {
+    fi: 'Evästeasetukset',
+    sv: 'Inställningar för kakor',
+    en: 'Cookie settings',
+  },
+  heading: {
+    fi: '${siteName} käyttää evästeitä',
+    sv: '${siteName} använder kakor',
+    en: '${siteName} uses cookies',
+  },
+  description: {
+    fi: 'Tämä sivusto käyttää välttämättömiä evästeitä sivun perustoimintojen ja suorituskyvyn varmistamiseksi. Lisäksi käytämme kohdennusevästeitä käyttäjäkokemuksen parantamiseksi, analytiikkaan ja yksilöidyn sisällön näyttämiseen.',
+    sv: 'Denna webbplats använder obligatoriska kakor för att säkerställa de grundläggande funktionerna och prestandan. Dessutom använder vi inriktningskakor för bättre användarupplevelse, analytik och individualiserat innehåll.',
+    en: 'This website uses required cookies to ensure the basic functionality and performance. In addition, we use targeting cookies to improve the user experience, perform analytics and display personalised content.',
+  },
+  showDetails: {
+    fi: 'Näytä yksityiskohdat',
+    sv: 'Visa detaljer',
+    en: 'Show details',
+  },
+  hideDetails: {
+    fi: 'Piilota yksityiskohdat',
+    sv: 'Stänga detaljer',
+    en: 'Hide details',
+  },
+  form_heading: {
+    fi: 'Tietoa sivustolla käytetyistä evästeistä',
+    sv: 'Information om kakor som används på webbplatsen',
+    en: 'About the cookies used on the website',
+  },
+  form_text: {
+    fi: 'Sivustolla käytetyt evästeet on luokiteltu käyttötarkoituksen mukaan. Alla voit lukea eri luokista ja sallia tai kieltää evästeiden käytön.',
+    sv: 'Kakorna som används på webbplatsen har klassificerats enligt användningsändamål. Du kan läsa om de olika klasserna och acceptera eller förbjuda användningen av kakor.',
+    en: 'The cookies used on the website have been classified according to their intended use. Below, you can read about the various categories and accept or reject the use of cookies.',
+  },
+  showCookieSettings: {
+    fi: 'Näytä evästeasetukset',
+    sv: 'Visa kakinställningarna',
+    en: 'Show cookie settings',
+  },
+  hideCookieSettings: {
+    fi: 'Piilota evästeasetukset',
+    sv: 'Stänga kakinställningarna',
+    en: 'Hide cookie settings',
+  },
+  tableHeadingsName: {
+    fi: 'Nimi',
+    sv: 'Namn',
+    en: 'Name',
+  },
+  tableHeadingsHostName: {
+    fi: 'Evästeen asettaja',
+    sv: 'Den som lagrat kakan',
+    en: 'Cookie set by',
+  },
+  tableHeadingsDescription: {
+    fi: 'Käyttötarkoitus',
+    sv: 'Användning',
+    en: 'Purpose of use',
+  },
+  tableHeadingsExpiration: {
+    fi: 'Voimassaoloaika',
+    sv: 'Giltighetstid',
+    en: 'Period of validity',
+  },
+  tableHeadingsType: {
+    fi: 'Tyyppi',
+    sv: 'Typ',
+    en: 'Type',
+  },
+  approveAllConsents: {
+    fi: 'Hyväksy kaikki evästeet',
+    sv: 'Acceptera alla kakor',
+    en: 'Accept all cookies',
+  },
+  approveRequiredAndSelectedConsents: {
+    fi: 'Hyväksy valitut evästeet',
+    sv: 'Acceptera valda kakor',
+    en: 'Accept selected cookies',
+  },
+  approveOnlyRequiredConsents: {
+    fi: 'Hyväksy vain välttämättömät evästeet',
+    sv: 'Acceptera endast nödvändiga',
+    en: 'Accept required cookies only',
+  },
+  settingsSaved: {
+    fi: 'Asetukset tallennettu!',
+    sv: 'Inställningar sparade!',
+    en: 'Settings saved!',
+  },
+  type_1: {
+    fi: 'Eväste',
+    sv: 'Kakan',
+    en: 'Cookie',
+  },
+  type_2: 'localStorage',
+  type_3: 'sessionStorage',
+};
+
+/**
+ * Retrieves a translation based on the provided key, language, and parameters.
+ *
+ * @param {string} key - The translation key.
+ * @param {string} lang - The language code.
+ * @param {object} parameters - The parameters used to replace placeholders in the translation.
+ * @return {string} - The translated string.
+ * @throws {Error} - If the translation is missing for the provided key and language.
+ */
+export function getTranslation(key, lang, parameters) {
+  // Normal strings as template strings, use like:
+  // index({ a: 1, b: 2 }, "${a} is smaller than ${b}")
+  // returns: 1 is smaller than 2
+  // https://stackoverflow.com/a/41540381
+  function index(obj, is, value) {
+    if (typeof is === 'string') {
+      is = is.split('.');
+    }
+    if (is.length === 1 && value !== undefined) {
+      obj[is[0]] = value;
+      return value;
+    }
+    if (is.length === 0) {
+      return obj;
+    }
+    return index(obj[is[0]], is.slice(1), value);
+  }
+
+  // Find translation based on key, fallback to English
+  let translation = null;
+  if (translations[key]) {
+    if (typeof translations[key] === 'string') {
+      translation = translations[key];
+    } else if (typeof translations[key] === 'object') {
+      translation = translations[key][lang] || translations[key].en || null;
+    }
+  }
+
+  if (translation) {
+    // Replace dollar strings in translation with corresponding data from parameters
+    return translation.replace(/\$\{.+?\}/g, (match) => {
+      const stripDollarAndParenthesis = match.replace(/(^\$\{|\}$)/g, '');
+      const parameter = index(parameters, stripDollarAndParenthesis);
+
+      // Parameters may be either string or an language object
+      if (typeof parameter === 'object') {
+        if (parameter[lang]) {
+          return parameter[lang];
+        }
+
+        // Use English as fallback language where possible
+        if (parameter.en) {
+          return parameter.en;
+        }
+      }
+      return parameter;
+    });
+  }
+  throw new Error(`Missing translation: ${key}:${lang}`);
+}
+
+/**
+ * Retrieves the translation keys from the translations object.
+ * @return {string[]} An array of translation keys.
+ */
+export function getTranslationKeys() {
+  // console.log('getTranslationKeys', Object.keys(translations));
+  return Object.keys(translations);
+}
