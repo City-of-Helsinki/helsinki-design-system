c03dc7c1d7cc80787d3796f82284818024a3acd2
NikoHelle <niko.helle@hyperactive.fi>
2024-04-26 14:58:51 +0300

    (HDS-1854) Static json and css files

--- packages/react/.storybook/main.js
+++ packages/react/.storybook/main.js
@@ -20,7 +20,11 @@ module.exports = {
     '@storybook/addon-measure',
     '@storybook/addon-outline',
   ],
-  staticDirs: ['../src/fonts', { from: '../src/components/login/storybookStatic', to: '/static-login' }],
+  staticDirs: [
+    '../src/fonts',
+    { from: '../src/components/login/storybookStatic', to: '/static-login' },
+    { from: '../src/components/cookieConsentCore/example', to: '/static-cookie-consent' },
+  ],
   webpack: async (config) => ({
     ...config,
     resolve: {
