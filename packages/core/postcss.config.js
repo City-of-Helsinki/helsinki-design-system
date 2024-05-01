340b2c4c56c22d885a1f89396ad434582388a34b
NikoHelle <niko.helle@hyperactive.fi>
2024-05-01 22:47:37 +0300

    (new-cookie) (hds-2241) Updated core configs
    Same rules as React packages has

--- packages/core/postcss.config.js
+++ packages/core/postcss.config.js
@@ -1,5 +1,7 @@
 module.exports = ({ env }) => ({
+  syntax: require('postcss-scss'),
   plugins: [
+    require('@csstools/postcss-sass')(),
     require('postcss-import')(),
     require('postcss-preset-env')({
       browsers: '> 0.2%, not iOS < 15, not Safari < 14.5, not dead, not op_mini all, not Android >= 0, not and_uc >= 0',
