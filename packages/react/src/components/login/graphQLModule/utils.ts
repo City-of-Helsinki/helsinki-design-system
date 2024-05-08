1d7b8dbc58f3c420585f6dd1d26abe0ed1efdff3
NikoHelle <niko.helle@hyperactive.fi>
2024-05-08 09:23:49 +0300

    (hds-2246) Utils for common Query modifications

---
+++ packages/react/src/components/login/graphQLModule/utils.ts
@@ -0,0 +1,31 @@
+import { QueryOptions } from '@apollo/client/core';
+import { merge } from 'lodash';
+
+type PartialContext = Partial<QueryOptions['context']>;
+type QueryHeaders = Record<string, unknown>;
+
+function mergeQueryOptionsContexts(target: PartialContext, overrides: PartialContext): PartialContext {
+  return merge(target, overrides);
+}
+
+export function mergeQueryOptionContexts(options: QueryOptions, overrideContext: PartialContext): QueryOptions {
+  const context: PartialContext = options.context || {};
+  // eslint-disable-next-line no-param-reassign
+  options.context = mergeQueryOptionsContexts(context, overrideContext);
+
+  return options;
+}
+
+export function mergeHeadersToQueryOptions(options: QueryOptions, headers: QueryHeaders): QueryOptions {
+  const context: PartialContext = {
+    headers,
+  };
+  return mergeQueryOptionContexts(options, context);
+}
+
+export function mergeAuthorizationHeaderToQueryOptions(options: QueryOptions, authHeader: string): QueryOptions {
+  const headers: QueryHeaders = {
+    authorization: authHeader,
+  };
+  return mergeHeadersToQueryOptions(options, headers);
+}
