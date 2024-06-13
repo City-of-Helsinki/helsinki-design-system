f75acf0b32c2b0f9291fdf976b5ca1a4f7a3c816
Mikko Tapionlinna <mikko.tapionlinna@siili.com>
2024-06-13 10:49:09 +0300

    (HDS-1854) Move all translations to siteSettings.json

--- packages/react/src/components/cookieConsentCore/translations.js
+++ packages/react/src/components/cookieConsentCore/translations.js
@@ -1,118 +1,21 @@
 /* eslint-disable no-param-reassign */
-/* eslint-disable no-template-curly-in-string */
-
-const translations = {
-  bannerAriaLabel: {
-    fi: 'Evästeasetukset',
-    sv: 'Inställningar för kakor',
-    en: 'Cookie settings',
-  },
-  heading: {
-    fi: '${siteName} käyttää evästeitä',
-    sv: '${siteName} använder kakor',
-    en: '${siteName} uses cookies',
-  },
-  description: {
-    fi: 'Tämä sivusto käyttää välttämättömiä evästeitä sivun perustoimintojen ja suorituskyvyn varmistamiseksi. Lisäksi käytämme kohdennusevästeitä käyttäjäkokemuksen parantamiseksi, analytiikkaan ja yksilöidyn sisällön näyttämiseen.',
-    sv: 'Denna webbplats använder obligatoriska kakor för att säkerställa de grundläggande funktionerna och prestandan. Dessutom använder vi inriktningskakor för bättre användarupplevelse, analytik och individualiserat innehåll.',
-    en: 'This website uses required cookies to ensure the basic functionality and performance. In addition, we use targeting cookies to improve the user experience, perform analytics and display personalised content.',
-  },
-  showDetails: {
-    fi: 'Näytä yksityiskohdat',
-    sv: 'Visa detaljer',
-    en: 'Show details',
-  },
-  hideDetails: {
-    fi: 'Piilota yksityiskohdat',
-    sv: 'Stänga detaljer',
-    en: 'Hide details',
-  },
-  form_heading: {
-    fi: 'Tietoa sivustolla käytetyistä evästeistä',
-    sv: 'Information om kakor som används på webbplatsen',
-    en: 'About the cookies used on the website',
-  },
-  form_text: {
-    fi: 'Sivustolla käytetyt evästeet on luokiteltu käyttötarkoituksen mukaan. Alla voit lukea eri luokista ja sallia tai kieltää evästeiden käytön.',
-    sv: 'Kakorna som används på webbplatsen har klassificerats enligt användningsändamål. Du kan läsa om de olika klasserna och acceptera eller förbjuda användningen av kakor.',
-    en: 'The cookies used on the website have been classified according to their intended use. Below, you can read about the various categories and accept or reject the use of cookies.',
-  },
-  showCookieSettings: {
-    fi: 'Näytä evästeasetukset',
-    sv: 'Visa kakinställningarna',
-    en: 'Show cookie settings',
-  },
-  hideCookieSettings: {
-    fi: 'Piilota evästeasetukset',
-    sv: 'Stänga kakinställningarna',
-    en: 'Hide cookie settings',
-  },
-  tableHeadingsName: {
-    fi: 'Nimi',
-    sv: 'Namn',
-    en: 'Name',
-  },
-  tableHeadingsHostName: {
-    fi: 'Evästeen asettaja',
-    sv: 'Den som lagrat kakan',
-    en: 'Cookie set by',
-  },
-  tableHeadingsDescription: {
-    fi: 'Käyttötarkoitus',
-    sv: 'Användning',
-    en: 'Purpose of use',
-  },
-  tableHeadingsExpiration: {
-    fi: 'Voimassaoloaika',
-    sv: 'Giltighetstid',
-    en: 'Period of validity',
-  },
-  tableHeadingsType: {
-    fi: 'Tyyppi',
-    sv: 'Typ',
-    en: 'Type',
-  },
-  approveAllConsents: {
-    fi: 'Hyväksy kaikki evästeet',
-    sv: 'Acceptera alla kakor',
-    en: 'Accept all cookies',
-  },
-  approveRequiredAndSelectedConsents: {
-    fi: 'Hyväksy valitut evästeet',
-    sv: 'Acceptera valda kakor',
-    en: 'Accept selected cookies',
-  },
-  approveOnlyRequiredConsents: {
-    fi: 'Hyväksy vain välttämättömät evästeet',
-    sv: 'Acceptera endast nödvändiga',
-    en: 'Accept required cookies only',
-  },
-  settingsSaved: {
-    fi: 'Asetukset tallennettu!',
-    sv: 'Inställningar sparade!',
-    en: 'Settings saved!',
-  },
-  type_1: {
-    fi: 'Eväste',
-    sv: 'Kakan',
-    en: 'Cookie',
-  },
-  type_2: 'localStorage',
-  type_3: 'sessionStorage',
-  type_4: 'IndexedDB',
-  type_5: 'Cache Storage',
-};
 
 /**
  * Retrieves a translation based on the provided key, language, and parameters.
  *
+ * @param {object} translations - The translations object.
  * @param {string} key - The translation key.
  * @param {string} lang - The language code.
  * @param {object} parameters - The parameters used to replace placeholders in the translation.
  * @return {string} - The translated string.
  * @throws {Error} - If the translation is missing for the provided key and language.
  */
-export function getTranslation(key, lang, parameters) {
+export function getTranslation(translations, key, lang, parameters) {
+  // Debug mode, return key instead of translation
+  if (lang === 'key') {
+    return key;
+  }
+
   // Normal strings as template strings, use like:
   // index({ a: 1, b: 2 }, "${a} is smaller than ${b}")
   // returns: 1 is smaller than 2
@@ -162,13 +65,4 @@ export function getTranslation(key, lang, parameters) {
     });
   }
   throw new Error(`Missing translation: ${key}:${lang}`);
-}
-
-/**
- * Retrieves the translation keys from the translations object.
- * @return {string[]} An array of translation keys.
- */
-export function getTranslationKeys() {
-  // console.log('getTranslationKeys', Object.keys(translations));
-  return Object.keys(translations);
 }
