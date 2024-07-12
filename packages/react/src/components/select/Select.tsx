6899a3e66b276925013f14939c9f4a35f214ba39
NikoHelle <niko.helle@hyperactive.fi>
2024-07-12 15:45:09 +0300

    (hds-2069) Update components for new types
    Also new tests for Select

--- packages/react/src/components/select/Select.tsx
+++ packages/react/src/components/select/Select.tsx
@@ -5,7 +5,7 @@ import { SelectProps, SelectMetaData, SelectData } from './types';
 import { Container } from './components/Container';
 import { Label } from './components/Label';
 import { changeChandler } from './dataUpdater';
-import { propsToGroups, childrenToGroups, getSelectedOptions } from './utils';
+import { propsToGroups, childrenToGroups, getSelectedOptions, getElementIds } from './utils';
 import { DataProvider } from '../dataProvider/DataProvider';
 import { SelectedOptionsContainer } from './components/selectedOptions/SelectedOptionsContainer';
 import { SelectionsAndListsContainer } from './components/SelectionsAndListsContainer';
@@ -14,33 +14,33 @@ import { ListAndInputContainer } from './components/list/ListAndInputContainer';
 import { ArrowButton } from './components/selectedOptions/ArrowButton';
 import { ButtonWithSelectedOptions } from './components/selectedOptions/ButtonWithSelectedOptions';
 import { ClearButton } from './components/selectedOptions/ClearButton';
+import { ErrorNotification } from './components/Error';
+import { AssistiveText } from './components/AssistiveText';
+import { createTextProvider } from './texts';
 
 export function Select({
   options,
   open,
   groups,
-  placeholder,
   icon,
-  label,
   required,
   onChange,
   children,
   id,
   disabled,
+  texts,
   invalid,
 }: SelectProps) {
   const initialData = useMemo<SelectData>(() => {
     return {
       groups: propsToGroups({ options, groups }) || childrenToGroups(children) || [],
-      label,
       open: !!open,
       required: !!required,
       invalid: !!invalid,
       disabled: !!disabled,
-      placeholder: placeholder || '',
       onChange,
     };
-  }, [options, label, open, groups, onChange, placeholder, disabled, invalid, required]);
+  }, [options, open, groups, onChange, disabled, invalid, required]);
 
   const metaData = useMemo((): SelectMetaData => {
     const containerId = `${id || uniqueId('hds-select-')}`;
@@ -55,15 +55,8 @@ export function Select({
         selectionButton: createRef<HTMLButtonElement>(),
       },
       selectedOptions: getSelectedOptions(initialData.groups),
-      elementIds: {
-        container: containerId,
-        button: `${containerId}-main-button`,
-        list: `${containerId}-list`,
-        clearButton: `${containerId}-clear-button`,
-        arrowButton: `${containerId}-arrow-button`,
-        label: `${containerId}-label`,
-        selectionsAndListsContainer: `${containerId}-sl-container`,
-      },
+      elementIds: getElementIds(containerId),
+      textProvider: createTextProvider(texts),
     };
   }, [id, initialData.groups]);
 
--- packages/react/src/components/select/components/Label.test.tsx
+++ packages/react/src/components/select/components/Label.test.tsx
@@ -1,47 +1,58 @@
-import React from 'react';
-import { render, RenderResult } from '@testing-library/react';
-
+import { mockedContainer, testUtilAfterAll, testUtilBeforeAll, initTests } from '../testUtil';
+import { SelectMetaData } from '../types';
 import { Label } from './Label';
-// eslint-disable-next-line jest/no-mocks-import
-import {
-  updateMockData,
-  resetMockData,
-  mockUseSelectDataHandlersContents,
-  getCurrentMockMetaData,
-  resetMockMetaData,
-} from '../hooks/__mocks__/useSelectDataHandlers';
 
-jest.mock('../hooks/useSelectDataHandlers', () => ({
-  useSelectDataHandlers: () => mockUseSelectDataHandlersContents,
-}));
+jest.mock('./Container', () => {
+  return {
+    __esModule: true,
+    Container: () => mockedContainer(),
+  };
+});
 
 describe('<Label />', () => {
-  beforeEach(() => {
-    resetMockData();
-    resetMockMetaData();
+  beforeAll(() => {
+    testUtilBeforeAll(Label);
   });
-  const getLabelElement = (result: RenderResult) => {
-    return result.container.querySelector('label') as HTMLLabelElement;
+  afterAll(() => {
+    testUtilAfterAll();
+  });
+  const label = 'This is the label';
+
+  const getLabelElementById = (container: HTMLElement, metaData: SelectMetaData) => {
+    const id = metaData.elementIds.label;
+    return container.querySelector(`#${id}`) as HTMLElement;
   };
-  it('Label value is set as the label in the getData()', () => {
-    const data = { label: 'Label text' };
-    updateMockData(data);
-    const result = render(<Label />);
-    const html = getLabelElement(result).innerHTML;
-    expect(html).toBe(data.label);
-    expect(html.includes('*')).toBeFalsy();
+
+  it('is rendered without errors and with "*" when data.required is true', async () => {
+    const { asFragment } = initTests({
+      renderComponentOnly: true,
+      selectProps: { required: true, texts: { label } },
+    });
+    expect(asFragment()).toMatchSnapshot();
   });
-  it("Label's id is set as the id in the getMetaData()", () => {
-    const metaData = getCurrentMockMetaData();
-    const result = render(<Label />);
-    expect(getLabelElement(result).getAttribute('id')).toBe(metaData.elementIds.label);
+  it('is rendered without errors and without "*" when data.required is false', async () => {
+    const { asFragment } = initTests({
+      renderComponentOnly: true,
+      selectProps: { texts: { label } },
+    });
+    expect(asFragment()).toMatchSnapshot();
   });
-  it('Label is marked as required when value is true', () => {
-    const data = { label: 'Label text', required: true };
-    updateMockData(data);
-    const result = render(<Label />);
-    const html = getLabelElement(result).innerHTML;
-    expect(html.includes(data.label)).toBeTruthy();
-    expect(html.includes('*')).toBeTruthy();
+
+  it('Component is rendered when texts.label is set', async () => {
+    const { getByText } = initTests({ renderComponentOnly: true, selectProps: { texts: { label } } });
+    expect(() => getByText(label)).not.toThrow();
+  });
+  it('Component is not rendered when texts.label is not set', async () => {
+    const { getByText } = initTests({ renderComponentOnly: true, selectProps: { texts: { label: '' } } });
+    expect(() => getByText(label)).toThrow();
+  });
+  it("Label's id is set as the id in the getMetaData()", async () => {
+    const { container, getMetaDataFromElement } = initTests({
+      renderComponentOnly: false,
+      selectProps: { texts: { label } },
+    });
+    const metaData = getMetaDataFromElement();
+    const element = getLabelElementById(container, metaData);
+    expect(element.getAttribute('id')).toBe(metaData.elementIds.label);
   });
 });
