3541ee7ecdeba44b95bedcbb4db817d0978a57d3
NikoHelle <niko.helle@hyperactive.fi>
2024-07-12 15:45:09 +0300

    (hds-2069) Update components for new types
    Also new tests for Select

--- packages/react/src/components/select/components/Label.tsx
+++ packages/react/src/components/select/components/Label.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 
 import { FieldLabel } from '../../../internal/field-label/FieldLabel';
 import { useSelectDataHandlers } from '../hooks/useSelectDataHandlers';
+import { getTextFromDataHandlers } from '../texts';
 import { SelectDataHandlers } from '../types';
 
 type FieldLabelProps = Parameters<typeof FieldLabel>[0];
 
 const createLabelProps = (dataHandlers: SelectDataHandlers): FieldLabelProps => {
   const { getData, getMetaData } = dataHandlers;
-  const { required, label } = getData();
+  const { required } = getData();
   const { elementIds } = getMetaData();
+  const label = getTextFromDataHandlers('label', dataHandlers);
   return {
     required,
     label,
