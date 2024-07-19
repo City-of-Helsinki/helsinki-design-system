0a137294e3cfc76ad6831c902ba0be5c6ff4946e
NikoHelle <niko.helle@hyperactive.fi>
2024-07-19 14:27:57 +0300

    (hds-2269) Container tests to include props checks

--- packages/react/src/components/container/Container.test.tsx
+++ packages/react/src/components/container/Container.test.tsx
@@ -3,15 +3,34 @@ import { render } from '@testing-library/react';
 import { axe } from 'jest-axe';
 
 import { Container } from './Container';
+import { getCommonElementTestProps, getElementAttributesMisMatches } from '../../utils/testHelpers';
 
 describe('<Container /> spec', () => {
   it('renders the component', () => {
-    const { asFragment } = render(<Container>Foo</Container>);
+    const { asFragment } = render(
+      <Container className="extra-class" alignWithHeader>
+        Foo
+      </Container>,
+    );
     expect(asFragment()).toMatchSnapshot();
   });
   it('should not have basic accessibility issues', async () => {
-    const { container } = render(<Container>Foo</Container>);
+    const { container } = render(
+      <Container className="extra-class" alignWithHeader>
+        Foo
+      </Container>,
+    );
     const results = await axe(container);
     expect(results).toHaveNoViolations();
+  });
+  it('Native html props are passed to the element', async () => {
+    const divProps = getCommonElementTestProps('div');
+    const { getByTestId } = render(
+      <Container className="extra-class" alignWithHeader {...divProps}>
+        Bar
+      </Container>,
+    );
+    const element = getByTestId(divProps['data-testid']);
+    expect(getElementAttributesMisMatches(element, divProps)).toHaveLength(0);
   });
 });
