74a1b752c5ce773230b785f75c38270d5104b6ee
NikoHelle <niko.helle@hyperactive.fi>
2024-07-19 16:01:30 +0300

    (hds-2269) Dialog and its children to accept all element props
    Dialog, DialogActionButton and DialogContent can have any div element props
    
    DialogHeader can have any heading element props
    
    Removed some unnecessary prop picking.

--- packages/react/src/components/dialog/dialogContent/DialogContent.tsx
+++ packages/react/src/components/dialog/dialogContent/DialogContent.tsx
@@ -3,19 +3,24 @@ import React, { useContext } from 'react';
 import styles from './DialogContent.module.scss';
 import classNames from '../../../utils/classNames';
 import { DialogContext } from '../DialogContext';
+import { AllElementPropsWithoutRef } from '../../../utils/elementTypings';
 
-export type DialogContentProps = React.PropsWithChildren<{
-  /**
-   * The id of the content element.
-   */
-  id?: string;
-}>;
-
-export const DialogContent = ({ id, children }: DialogContentProps) => {
+export type DialogContentProps = React.PropsWithChildren<
+  AllElementPropsWithoutRef<'div'> & {
+    /**
+     * The id of the content element.
+     */
+    id?: string;
+  }
+>;
+export const DialogContent = ({ children, className, ...rest }: DialogContentProps) => {
   const { scrollable } = useContext(DialogContext);
 
   return (
-    <div id={id} className={classNames(styles.dialogContent, scrollable && styles.dialogContentScrollable)}>
+    <div
+      className={classNames(styles.dialogContent, scrollable && styles.dialogContentScrollable, className)}
+      {...rest}
+    >
       {children}
     </div>
   );
