e0e79de7947b355c83f06dc9c2762c3f9148e34a
Tuomo Kivinen <mrtuomok@gmail.com>
2024-07-19 09:38:15 +0300

    release-4.0.0) (HDS-2331) Add initial Playwright tests

--- e2e/playwright.config.js
+++ e2e/playwright.config.js
@@ -1,16 +1,17 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  snapshotPathTemplate: '{testDir}/{testFilePath}-snapshots/{arg}{ext}',
   testDir: './tests',
-  timeout: 180 * 1000,
+  timeout: 60 * 1000,
   /* Run tests in files in parallel */
   fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  workers: 1,
+  workers: 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['junit', { outputFile: 'report/e2e-junit-results.xml' }],
@@ -22,22 +23,30 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env.E2E_TESTS_ENV_URL ?? "https://hds.hel.fi",
     ignoreHTTPSErrors: true,
-    screenshot: {
-      fullPage: true,
-      mode: "only-on-failure"
-    },
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-
     // https://playwright.dev/docs/videos
-    video: 'on-first-retry', 
     contextOptions: {recordVideo: { dir: "./report/videos/"}}
   },
 
   projects: [
     {
-      name: 'logged-out',
-      testMatch: [/pages/],
-    }
+      name: 'Desktop',
+      testMatch: [/tests/],
+      use: {
+        ...devices['Desktop Chrome'],
+        viewport: { width: 1280, height: 720 },
+        isMobile: false,
+      },
+    },
+    {
+      name: 'Mobile',
+      testMatch: [/tests/],
+      use: {
+        ...devices['Desktop Chrome'],
+        viewport: { width: 320, height: 576 },
+        isMobile: true,
+      },
+    },
   ],
 });
