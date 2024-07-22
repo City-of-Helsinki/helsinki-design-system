0b8d15351f58c7d90e2580cd050ae706bb008c2b
NikoHelle <niko.helle@hyperactive.fi>
2024-07-22 16:12:20 +0300

    (hds-2269) Hero and its children to accept all element props
    Added tests. Fixed KorosPros where old props remained.

--- packages/react/src/components/hero/Text.tsx
+++ packages/react/src/components/hero/Text.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 import styles from './Hero.module.scss';
 import classNames from '../../utils/classNames';
+import { AllElementPropsWithoutRef } from '../../utils/elementTypings';
 
-export const Text = (props: React.HtmlHTMLAttributes<HTMLParagraphElement>): React.ReactElement => {
+export const Text = (props: AllElementPropsWithoutRef<'p'>): React.ReactElement => {
   const { className, children, ...elementProps } = props;
   const classList = classNames(styles.text, className);
   return (
