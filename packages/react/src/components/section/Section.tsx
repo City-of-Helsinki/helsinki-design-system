efa346ac3cbb409623240ae8e20eaa952b760c7c
NikoHelle <niko.helle@hyperactive.fi>
2024-07-23 00:14:23 +0300

    (release-3.10.0) (hds-2269) Section to accept div element props

--- packages/react/src/components/section/Section.tsx
+++ packages/react/src/components/section/Section.tsx
@@ -3,21 +3,24 @@ import React from 'react';
 import '../../styles/base.module.css';
 import styles from './Section.module.css';
 import classNames from '../../utils/classNames';
+import { AllElementPropsWithoutRef } from '../../utils/elementTypings';
 import { Koros, KorosType } from '../koros';
 
 export type SectionColor = 'primary' | 'secondary' | 'tertiary' | 'plain';
 
-export type SectionProps = React.PropsWithChildren<{
-  color?: SectionColor;
-  className?: string;
-  korosType?: KorosType;
-}>;
+export type SectionProps = React.PropsWithChildren<
+  AllElementPropsWithoutRef<'div'> & {
+    color?: SectionColor;
+    className?: string;
+    korosType?: KorosType;
+  }
+>;
 
-export const Section = ({ children, className = '', color = 'plain', korosType = null }: SectionProps) => {
+export const Section = ({ children, className = '', color = 'plain', korosType = null, ...rest }: SectionProps) => {
   const withKoros = korosType !== null;
 
   return (
-    <div className={classNames(styles.section, styles[color], withKoros && styles.withKoros, className)}>
+    <div {...rest} className={classNames(styles.section, styles[color], withKoros && styles.withKoros, className)}>
       {withKoros && <Koros type={korosType} className={`${styles.koros} ${styles.topKoros} ${styles[korosType]}`} />}
       <div className={styles.content}>{children}</div>
       {withKoros && (
