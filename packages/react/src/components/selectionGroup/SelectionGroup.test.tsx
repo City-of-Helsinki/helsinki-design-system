ba4e824af47d048872a5a27b7a8de131b2410a7f
NikoHelle <niko.helle@hyperactive.fi>
2024-07-23 00:18:32 +0300

    (hds-2269) Native props tests to SelectionGroup

--- packages/react/src/components/selectionGroup/SelectionGroup.test.tsx
+++ packages/react/src/components/selectionGroup/SelectionGroup.test.tsx
@@ -4,6 +4,7 @@ import { axe } from 'jest-axe';
 
 import { SelectionGroup } from './SelectionGroup';
 import { Checkbox } from '../checkbox';
+import { getCommonElementTestProps, getElementAttributesMisMatches } from '../../utils/testHelpers';
 
 describe('<SelectionGroup /> spec', () => {
   it('renders the component', () => {
@@ -22,5 +23,15 @@ describe('<SelectionGroup /> spec', () => {
     );
     const results = await axe(container);
     expect(results).toHaveNoViolations();
+  });
+  it('native html props are passed to the element', async () => {
+    const fieldSetProps = getCommonElementTestProps<'fieldset'>('fieldset');
+    const { getByTestId } = render(
+      <SelectionGroup {...fieldSetProps}>
+        <Checkbox id="checkbox" label="Foo" />
+      </SelectionGroup>,
+    );
+    const element = getByTestId(fieldSetProps['data-testid']);
+    expect(getElementAttributesMisMatches(element, fieldSetProps)).toHaveLength(0);
   });
 });
