d08b21dc12a6ebd7d7ddfbfd2ded84a1d94e0074
NikoHelle <niko.helle@hyperactive.fi>
2024-07-22 23:49:29 +0300

    (release-3.10.0) (hds-2269) TextInput to accept input element props

--- packages/react/src/components/textInput/TextInput.tsx
+++ packages/react/src/components/textInput/TextInput.tsx
@@ -6,122 +6,126 @@ import { InputWrapper } from '../../internal/input-wrapper/InputWrapper';
 import classNames from '../../utils/classNames';
 import composeAriaDescribedBy from '../../utils/composeAriaDescribedBy';
 import { IconCrossCircle } from '../../icons';
+import { AllElementPropsWithoutRef, MergeAndOverrideProps } from '../../utils/elementTypings';
 
-export type TextInputProps = {
-  /**
-   * Additional class names to apply to the text input
-   */
-  className?: string;
-  /**
-   * The aria-label for the clear button.
-   * @default 'Clear'
-   */
-  clearButtonAriaLabel?: string;
-  /**
-   * Use clear button
-   */
-  clearButton?: boolean;
-  /**
-   * Additional children to render after the input.
-   */
-  children?: React.ReactNode;
-  /**
-   * The default input element value. Use when the component is not controlled
-   */
-  defaultValue?: string;
-  /**
-   * If `true`, the input will be disabled
-   */
-  disabled?: boolean;
-  /**
-   * The error text content that will be shown below the input
-   */
-  errorText?: string;
-  /**
-   * The helper text content that will be shown below the input
-   */
-  helperText?: string;
-  /**
-   * Hides the label above the input
-   */
-  hideLabel?: boolean;
-  /**
-   * The id of the input element
-   */
-  id: string;
-  /**
-   * If `true`, the input will be displayed in an invalid state.
-   */
-  invalid?: boolean;
-  /**
-   * The label for the input
-   */
-  label?: string | React.ReactNode;
-  /**
-   * Callback fired when the state is changed
-   */
-  onChange?: React.ChangeEventHandler<HTMLInputElement>;
-  /**
-   * Button click callback
-   */
-  onButtonClick?: React.MouseEventHandler<HTMLButtonElement>;
-  /**
-   * Short hint displayed in the input before the user enters a value
-   */
-  placeholder?: string;
-  /**
-   * If `true`, prevents the user from changing the value of the field (not from interacting with the field)
-   */
-  readOnly?: boolean;
-  /**
-   * If `true`, the label is displayed as required and the `input` element will be required
-   */
-  required?: boolean;
-  /**
-   * Override or extend the styles applied to the component
-   */
-  style?: React.CSSProperties;
-  /**
-   * The success text content that will be shown below the input
-   */
-  successText?: string;
-  /**
-   * The info text content that will be shown below the input
-   */
-  infoText?: string;
-  /**
-   * Aria-label text for the tooltip
-   */
-  tooltipLabel?: string;
-  /**
-   * Aria-label text for the tooltip trigger button
-   */
-  tooltipButtonLabel?: string;
-  /**
-   * The text content of the tooltip
-   */
-  tooltipText?: string;
-  /**
-   * Type of the input element
-   */
-  type?: string;
-  /**
-   * The value of the input element, required for a controlled component
-   */
-  value?: string;
-  /**
-   * The `ref` is forwarded to the native input element.
-   */
-  ref?: React.Ref<HTMLInputElement>;
-  /**
-   * Button icon
-   */
-  buttonIcon?: React.ReactNode;
-  /**
-   * Button aria-label
-   */
-  buttonAriaLabel?: string;
-} & React.ComponentPropsWithoutRef<'input'>;
+export type TextInputProps = MergeAndOverrideProps<
+  AllElementPropsWithoutRef<'input'>,
+  {
+    /**
+     * Additional class names to apply to the text input
+     */
+    className?: string;
+    /**
+     * The aria-label for the clear button.
+     * @default 'Clear'
+     */
+    clearButtonAriaLabel?: string;
+    /**
+     * Use clear button
+     */
+    clearButton?: boolean;
+    /**
+     * Additional children to render after the input.
+     */
+    children?: React.ReactNode;
+    /**
+     * The default input element value. Use when the component is not controlled
+     */
+    defaultValue?: string;
+    /**
+     * If `true`, the input will be disabled
+     */
+    disabled?: boolean;
+    /**
+     * The error text content that will be shown below the input
+     */
+    errorText?: string;
+    /**
+     * The helper text content that will be shown below the input
+     */
+    helperText?: string;
+    /**
+     * Hides the label above the input
+     */
+    hideLabel?: boolean;
+    /**
+     * The id of the input element
+     */
+    id: string;
+    /**
+     * If `true`, the input will be displayed in an invalid state.
+     */
+    invalid?: boolean;
+    /**
+     * The label for the input
+     */
+    label?: string | React.ReactNode;
+    /**
+     * Callback fired when the state is changed
+     */
+    onChange?: React.ChangeEventHandler<HTMLInputElement>;
+    /**
+     * Button click callback
+     */
+    onButtonClick?: React.MouseEventHandler<HTMLButtonElement>;
+    /**
+     * Short hint displayed in the input before the user enters a value
+     */
+    placeholder?: string;
+    /**
+     * If `true`, prevents the user from changing the value of the field (not from interacting with the field)
+     */
+    readOnly?: boolean;
+    /**
+     * If `true`, the label is displayed as required and the `input` element will be required
+     */
+    required?: boolean;
+    /**
+     * Override or extend the styles applied to the component
+     */
+    style?: React.CSSProperties;
+    /**
+     * The success text content that will be shown below the input
+     */
+    successText?: string;
+    /**
+     * The info text content that will be shown below the input
+     */
+    infoText?: string;
+    /**
+     * Aria-label text for the tooltip
+     */
+    tooltipLabel?: string;
+    /**
+     * Aria-label text for the tooltip trigger button
+     */
+    tooltipButtonLabel?: string;
+    /**
+     * The text content of the tooltip
+     */
+    tooltipText?: string;
+    /**
+     * Type of the input element
+     */
+    type?: string;
+    /**
+     * The value of the input element, required for a controlled component
+     */
+    value?: string;
+    /**
+     * The `ref` is forwarded to the native input element.
+     */
+    ref?: React.Ref<HTMLInputElement>;
+    /**
+     * Button icon
+     */
+    buttonIcon?: React.ReactNode;
+    /**
+     * Button aria-label
+     */
+    buttonAriaLabel?: string;
+  }
+>;
 
 export const TextInput = React.forwardRef<HTMLInputElement, TextInputProps>(
   (
