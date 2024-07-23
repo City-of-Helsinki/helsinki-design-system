6279453b2bd3e8661c17240e6bad14152ed46480
NikoHelle <niko.helle@hyperactive.fi>
2024-07-23 10:51:06 +0300

    (hds-2269) Tooltip to accept div element props

--- packages/react/src/components/tooltip/Tooltip.test.tsx
+++ packages/react/src/components/tooltip/Tooltip.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, fireEvent, waitFor } from '@testing-library/react';
 
 import { Tooltip } from './Tooltip';
+import { getCommonElementTestProps, getElementAttributesMisMatches } from '../../utils/testHelpers';
 
 describe('<TooltipNew /> spec', () => {
   it('renders the component', () => {
@@ -37,5 +38,23 @@ describe('<TooltipNew /> spec', () => {
     fireEvent.click(getByRole('button'));
     waitFor(() => getByRole('section'));
     expect(asFragment()).toMatchSnapshot();
+  });
+  it('native html props are passed to the element', async () => {
+    const divProps = getCommonElementTestProps('div');
+    const { getByTestId } = render(
+      <Tooltip
+        {...divProps}
+        small
+        placement="auto"
+        buttonLabel="Tooltip button"
+        tooltipLabel="Tooltip label"
+        buttonClassName="buttonClassName"
+        tooltipClassName="tooltipClassName"
+      >
+        Tooltip content
+      </Tooltip>,
+    );
+    const element = getByTestId(divProps['data-testid']);
+    expect(getElementAttributesMisMatches(element, divProps)).toHaveLength(0);
   });
 });
