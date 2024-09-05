9386af1c213fc793003dd4935236bf3b13f9e115
timwessman <tim.wessman@gofore.com>
2024-09-05 15:14:23 +0300

    clean up the code

--- site/modify-mdx-loader.js
+++
@@ -1,8 +0,0 @@
-// modify-mdx-loader.js
-module.exports = function (source) {
-  if (this.resourcePath.endsWith('tabs.mdx')) {
-    // Add the export statement to the content of tabs.mdx
-    return `${source}\n\nexport default (props) => <><PageTabs pageContext={props.pageContext}>{props.children}</PageTabs></>;`;
-  }
-  return source;
-};
