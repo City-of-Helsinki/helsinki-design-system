e321739ac30dfe5b4fa4161cb28bd8d95ef6a538
Tuomo Kivinen <mrtuomok@gmail.com>
2024-09-09 11:30:33 +0300

    (HDS-2389) remove old analytics

--- analytics/scanner.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  crawlFrom: './tmp',
-  importedFrom: 'hds-react',
-  exclude: (dirname) => dirname === 'node_modules',
-  processors: ['count-components', 'count-components-and-props'],
-  includeSubComponents: true,
-};
