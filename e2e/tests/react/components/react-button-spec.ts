ccd2d7e7899f76dc04ca6b43158e7345c17c5db2
NikoHelle <niko.helle@hyperactive.fi>
2024-09-11 14:35:15 +0300

    (hds-2405) All React tests to use same helper

--- e2e/tests/react/components/react-button-spec.ts
+++ e2e/tests/react/components/react-button-spec.ts
@@ -1,29 +1,12 @@
-import { test, expect } from '@playwright/test';
-import { getComponentStorybookUrls, takeScreenshotWithSpacing, takeStateScreenshots } from '../../../helpers';
+import { test } from '@playwright/test';
+import { takeAllStorySnapshots } from '../../../helpers';
 
-test.describe('Button', () => {
+const componentName = 'button';
+const storybook = 'react';
+const takeStateSnapshots = true;
 
-  const componentName = 'button';
-  let componentUrls: string[] = [];
-
-  test('button getStorybookUrls', async ({ page }) => {
-    componentUrls = await getComponentStorybookUrls(page, componentName, 'react');
-    expect(componentUrls.length).toBeGreaterThan(0);
-  });
-
-  test('button components', async ({ page, isMobile }) => {
-    for (const componentUrl of componentUrls) {
-      await page.goto(`file://${componentUrl}`);
-      const container = page.locator('body');
-      const screenshotName = `react-${componentUrl.split('/').pop()}-${isMobile ? 'mobile' : 'desktop'}`;
-      await takeScreenshotWithSpacing(page, container, screenshotName, 0);
-
-      const buttons = await container.locator('[data-playwright="true"]').all();
-
-      for (const [index, button] of buttons.entries()) {
-        const screenshotPrefix = `react-${screenshotName}-components-${index}`;
-        await takeStateScreenshots(page, button, screenshotPrefix);
-      }
-    }
+test.describe(`Testing ${storybook} component "${componentName}"`, () => {
+  test('Take snapshots of all stories', async ({ page, isMobile }) => {
+    await takeAllStorySnapshots({ page, isMobile, takeStateSnapshots, storybook, componentName });
   });
 });
