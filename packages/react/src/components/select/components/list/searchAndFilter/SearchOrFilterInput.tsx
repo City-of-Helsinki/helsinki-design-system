91a2cf1d8ea5794b9f6ecadc400eb53d9494d8b0
NikoHelle <niko.helle@hyperactive.fi>
2024-09-24 22:07:54 +0300

    (hds-2069) Added search to components

--- packages/react/src/components/select/components/list/searchAndFilter/SearchOrFilterInput.tsx
+++ packages/react/src/components/select/components/list/searchAndFilter/SearchOrFilterInput.tsx
@@ -2,25 +2,33 @@ import React from 'react';
 
 import styles from '../../../Select.module.scss';
 import { TextInput, TextInputProps } from '../../../../textInput/TextInput';
-import { SelectDataHandlers, SelectMetaData } from '../../../types';
+import { SelectDataHandlers, SelectMetaData, TextKey } from '../../../types';
 import classNames from '../../../../../utils/classNames';
 import { createInputOnChangeListener, getVisibleGroupLabels } from '../../../utils';
 import { eventIds } from '../../../events';
 import { useSelectDataHandlers } from '../../../hooks/useSelectDataHandlers';
 import { getTextKey } from '../../../texts';
 
+const typeIndicator = '{{type}}';
+
+const getTextKeyWithType = (key: string, isSearchInput): TextKey => {
+  const inputType = isSearchInput ? 'search' : 'filter';
+  return key.replace(typeIndicator, inputType) as TextKey;
+};
+
 const createFilterInputProps = (
   { getMetaData, trigger, getData }: SelectDataHandlers,
   inputType: Exclude<SelectMetaData['listInputType'], undefined>,
 ): TextInputProps => {
   const metaData = getMetaData();
-  const { filter, elementIds, refs } = metaData;
+  const { filter, elementIds, refs, search } = metaData;
   const { multiSelect, groups } = getData();
   const hasVisibleGroupLabels = getVisibleGroupLabels(groups).length > 0;
-  const value = filter;
-  const label = getTextKey('filterClearButtonAriaLabel', metaData);
-  const clearButtonAriaLabel = getTextKey('filterLabel', metaData);
-  const placeholder = getTextKey('filterPlaceholder', metaData);
+  const isSearchInput = inputType === 'search';
+  const value = isSearchInput ? search : filter;
+  const label = getTextKey(getTextKeyWithType(`${typeIndicator}ClearButtonAriaLabel`, isSearchInput), metaData);
+  const clearButtonAriaLabel = getTextKey(getTextKeyWithType(`${typeIndicator}Label`, isSearchInput), metaData);
+  const placeholder = getTextKey(getTextKeyWithType(`${typeIndicator}Placeholder`, isSearchInput), metaData);
 
   return {
     className: classNames(styles.searchOrFilterInput),
