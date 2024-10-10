ff8b1f0faff76d70f2aa1a46fe8770610eab7752
NikoHelle <niko.helle@hyperactive.fi>
2024-10-10 14:40:48 +0300

    (hds-2069) Use focus/keyboard/ hooks in Container

--- packages/react/src/components/select/components/Container.tsx
+++ packages/react/src/components/select/components/Container.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 import styles from '../Select.module.scss';
 import { DivElementProps, SelectDataHandlers } from '../types';
+import { useFocusHandling } from '../hooks/useFocusHandling';
 import { useSelectDataHandlers } from '../hooks/useSelectDataHandlers';
 import classNames from '../../../utils/classNames';
+import { useKeyboard } from '../hooks/useKeyboard';
 
 function createContainerProps(props: DivElementProps, { getMetaData }: SelectDataHandlers): DivElementProps {
   const { elementIds } = getMetaData();
@@ -16,6 +18,12 @@ function createContainerProps(props: DivElementProps, { getMetaData }: SelectDat
 
 export const Container = (props: Partial<DivElementProps>) => {
   const dataHandlers = useSelectDataHandlers();
+  const trackingProps = useFocusHandling();
+  const keyTrackingProps = useKeyboard();
   const { children, ...rest } = createContainerProps(props, dataHandlers);
-  return <div {...rest}>{children}</div>;
+  return (
+    <div {...rest} {...trackingProps} {...keyTrackingProps}>
+      {children}
+    </div>
+  );
 };
