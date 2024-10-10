962943cde3c2375078f0a13cf910c6c5903f166c
NikoHelle <niko.helle@hyperactive.fi>
2024-10-10 14:36:24 +0300

    (hds-2069) Rename e2e "helpers" to "playwright" util
    There will be many more utils, so this is better namings

--- e2e/tests/react/components/react-hero-spec.ts
+++ e2e/tests/react/components/react-hero-spec.ts
@@ -1,10 +1,10 @@
 import { test } from '@playwright/test';
-import { takeAllStorySnapshots } from '../../../helpers';
+import { takeAllStorySnapshots } from '../../../utils/playwright.util';
 
 const componentName = 'hero';
 const storybook = 'react';
 const takeStateSnapshots = false;
-const bodySpacing = 30 // extra spacing around the body screenshots
+const bodySpacing = 30; // extra spacing around the body screenshots
 
 test.describe(`Testing ${storybook} component "${componentName}"`, () => {
   test('Take snapshots of all stories', async ({ page, isMobile }) => {
