89166a10589830cfe7626add7826f92783a6fe5a
NikoHelle <niko.helle@hyperactive.fi>
2024-11-07 14:49:31 +0200

    (hds-2383) Simplify CookieConsentCore stories

--- packages/react/src/components/cookieConsentCore/types.ts
+++ packages/react/src/components/cookieConsentCore/types.ts
@@ -1,3 +1,12 @@
+import { CookieConsentCore } from './cookieConsentCore';
+
+export type CookieCore = CookieConsentCore;
+export type CreateParams = Parameters<typeof CookieConsentCore.create>;
+export type CreateProps = {
+  siteSettings: CreateParams[0];
+  options?: Options;
+};
+
 export type Options = {
   language?: string | undefined;
   theme?: string | undefined;
@@ -8,3 +17,11 @@ export type Options = {
   settingsPageSelector?: string | undefined;
   disableAutoRender?: boolean | undefined;
 };
+
+declare global {
+  interface Window {
+    hds: {
+      cookieConsent: CookieCore;
+    };
+  }
+}
