b3e7a6a7725d1c89cd797f8b5b52ac2952ea5126
NikoHelle <niko.helle@hyperactive.fi>
2024-11-25 09:58:03 +0200

    (release-3.12.0) (hds-2535) (hds-2534) Fix issue with StrictMode
    If items are added directly to the module array, the array size increases and same modules are added twice on second render.

--- packages/react/src/components/login/components/LoginProvider.tsx
+++ packages/react/src/components/login/components/LoginProvider.tsx
@@ -35,7 +35,7 @@ export const LoginProvider = ({
     debug,
   };
 
-  const mods = modules || [];
+  const mods = modules ? [...modules] : [];
   if (sessionPollerSettings) {
     mods.push(createSessionPoller(sessionPollerSettings));
   }
