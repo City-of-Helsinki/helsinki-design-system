b91e6d9bd6e848825722780f330de28e9553737c
NikoHelle <niko.helle@hyperactive.fi>
2024-12-03 22:36:15 +0200

    (release-3.12.0) (hds-2539) Single use AbortSignal

--- packages/react/src/components/login/utils/abortFetch.ts
+++ packages/react/src/components/login/utils/abortFetch.ts
@@ -2,6 +2,15 @@ export function isAbortError(error: Error): boolean {
   return !!error && error.name === 'AbortError';
 }
 
+export function appendAbortSignal(target: Parameters<typeof fetch>[1]): AbortController['abort'] {
+  const abortController = new AbortController();
+  // eslint-disable-next-line no-param-reassign
+  target.signal = abortController.signal;
+  return (reason?: unknown) => {
+    abortController.abort(reason);
+  };
+}
+
 export function createFetchAborter() {
   let isAborted = false;
   let abortController: AbortController | undefined;
