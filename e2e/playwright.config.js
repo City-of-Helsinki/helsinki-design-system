79813f184619263de174eee100e3434934178ce4
Tuomo Kivinen <mrtuomok@gmail.com>
2025-02-10 11:02:50 +0200

    fix: (HDS-2635) fix 2e2 visual tests by simply increasing the screen height to prevent scrollbar and overflow, add slowMo to add a small wait after all interactions and a timeout to all expects

--- e2e/playwright.config.js
+++ e2e/playwright.config.js
@@ -34,8 +34,6 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     actionTimeout: 30 * 1000,
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.E2E_TESTS_ENV_URL ?? 'https://hds.hel.fi',
     ignoreHTTPSErrors: true,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -43,6 +41,14 @@ export default defineConfig({
     // contextOptions: { recordVideo: { dir: './report/videos/' } },
     screenshot: 'only-on-failure',
     video: 'off',
+    channel: 'chromium',
+    launchOptions: {
+      slowMo: 100,
+    },
+    retries: 3,
+    expect: {
+      timeout: 2000,
+    },
   },
   projects: [
     {
@@ -50,13 +56,8 @@ export default defineConfig({
       testMatch: [/tests/],
       use: {
         ...devices['Desktop Chrome'],
-        channel: 'chromium',
-        args: ['--no-sandbox'],
         viewport: { width: 1280, height: 720 },
         hasTouch: false,
-        launchOptions: {
-          slowMo: 100,
-        },
       },
     },
     {
@@ -64,13 +65,8 @@ export default defineConfig({
       testMatch: [/tests/],
       use: {
         ...devices['Desktop Chrome'],
-        channel: 'chromium',
-        args: ['--no-sandbox'],
-        viewport: { width: 320, height: 576 },
+        viewport: { width: 320, height: 720 },
         hasTouch: true,
-        launchOptions: {
-          slowMo: 100,
-        },
       },
     },
   ],
