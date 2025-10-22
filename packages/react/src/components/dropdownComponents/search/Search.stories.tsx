0502f2b5504e6addcb111ee330d0741358ea33d6
Tuomo Kivinen <mrtuomok@gmail.com>
2025-10-22 11:44:11 +0300

    chore: add story with external button

--- packages/react/src/components/dropdownComponents/search/Search.stories.tsx
+++ packages/react/src/components/dropdownComponents/search/Search.stories.tsx
@@ -1,6 +1,8 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 
 import { Search, SearchProps } from './Search';
+import { Notification } from '../../notification';
+import { Button } from '../../button';
 
 export default {
   component: Search,
@@ -13,6 +15,8 @@ export default {
 
 export const Example = () => {
   const [value, setValue] = useState('');
+  const [showNotification, setShowNotification] = useState(false);
+  const [notificationMessage, setNotificationMessage] = useState('');
 
   const handleSearch: SearchProps['onSearch'] = useCallback((selectedOptions, lastClickedOption, data) => {
     console.log('Search event:', { selectedOptions, lastClickedOption, data });
@@ -21,6 +25,8 @@ export const Example = () => {
 
   const onSend = (val: string) => {
     console.log('Search submitted test:', val);
+    setNotificationMessage(`Search submitted: "${val}"`);
+    setShowNotification(true);
   };
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -42,10 +48,22 @@ export const Example = () => {
 
   return (
     <>
-      {/* <Search {...props} historyId="test" onSearch={handleSearch} onSend={onSend} /> */}
+      {showNotification && (
+        <Notification
+          label="Search submitted"
+          type="success"
+          position="bottom-right"
+          autoClose
+          closeButtonLabelText="Close notification"
+          dismissible
+          onClose={() => setShowNotification(false)}
+        >
+          {notificationMessage}
+        </Notification>
+      )}
       <Search
         {...props}
-        historyId="test2"
+        historyId="test"
         onSend={onSend}
         onBlur={onBlur}
         onFocus={onFocus}
@@ -54,6 +72,53 @@ export const Example = () => {
         onSearch={handleSearch}
       />
       <span>Value: {value}</span>
+    </>
+  );
+};
+
+export const WithExternalButton = () => {
+  const searchInputRef = useRef<HTMLInputElement>(null);
+  const [showNotification, setShowNotification] = useState(false);
+  const [notificationMessage, setNotificationMessage] = useState('');
+
+  const handleSearch: SearchProps['onSearch'] = useCallback((selectedOptions, lastClickedOption, data) => {
+    console.log('Search event:', { selectedOptions, lastClickedOption, data });
+    return Promise.resolve({ groups: [{ label: 'Search suggestions', options: ['tuomo', 'testaa', 'tuomo testaa'] }] });
+  }, []);
+
+  const onSend = (val: string) => {
+    setNotificationMessage(`Search submitted: "${val}"`);
+    setShowNotification(true);
+  };
+
+  const handleExternalButtonClick = () => {
+    const currentValue = searchInputRef.current?.value;
+    if (currentValue) {
+      onSend(currentValue);
+    }
+  };
+
+  const [props] = useState({
+    id: 'hds-search-component',
+  });
+
+  return (
+    <>
+      {showNotification && (
+        <Notification
+          label="Search submitted"
+          type="success"
+          position="bottom-right"
+          autoClose
+          closeButtonLabelText="Close notification"
+          dismissible
+          onClose={() => setShowNotification(false)}
+        >
+          {notificationMessage}
+        </Notification>
+      )}
+      <Search {...props} historyId="test" onSend={onSend} onSearch={handleSearch} ref={searchInputRef} />
+      <Button onClick={handleExternalButtonClick}>Send search</Button>
     </>
   );
 };
