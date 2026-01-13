7fbc908e0cd7f4acf8b45e998657a780d0c3936a
Tuomo Kivinen <mrtuomok@gmail.com>
2026-01-13 09:24:15 +0200

    chore: deprecate old Select and Combobox related stuff
    Refs: HDS-2874

--- packages/react/src/components/dropdown/index.ts
+++ packages/react/src/components/dropdown/index.ts
@@ -1,2 +1,10 @@
+/**
+ * @deprecated The dropdown/select module is deprecated and will be removed in the next major release.
+ * Please use the Select component from @hds/react instead.
+ */
 export * from './select';
+/**
+ * @deprecated The dropdown/combobox module is deprecated and will be removed in the next major release.
+ * Please use the Select component from @hds/react instead.
+ */
 export * from './combobox';
