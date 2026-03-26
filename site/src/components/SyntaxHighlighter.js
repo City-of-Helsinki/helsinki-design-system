74be27c01fc4223e1b6776147cecc28e5229f8d7
Mikko Riippi <mikko.riippi@gofore.com>
2026-03-26 16:14:43 +0200

    fix: docsite code example text contrast

--- site/src/components/SyntaxHighlighter.js
+++ site/src/components/SyntaxHighlighter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Highlight, { defaultProps } from 'prism-react-renderer';
-import theme from 'prism-react-renderer/themes/github';
+import theme from './codeTheme';
 
 import './SyntaxHighlighter.scss';
 
